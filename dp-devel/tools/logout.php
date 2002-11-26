e7899ea0e86bd16f7bf8cbc1489f16419c35bccb
cdubya <>
2002-11-26 04:05:36 +0000

    Changed texts01.archive.org to $_SERVER['SERVER_NAME'] to work with dev site

--- dp-devel/tools/logout.php
+++ dp-devel/tools/logout.php
@@ -2,6 +2,6 @@
 //clear cookie if one is already set
 
 
-setcookie("pguser","",time() - 86400,"/","texts01.archive.org",0);
+setcookie("pguser","",time() - 86400,"/",$_SERVER['SERVER_NAME'],0);
      echo "<p><META HTTP-EQUIV=\"refresh\" CONTENT=\"0 ;URL=../default.php\">"; 
 ?>
