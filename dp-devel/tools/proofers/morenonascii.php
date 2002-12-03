5d47b57986509457d46e434c562c6267e42649c7
carel <>
2002-12-03 02:27:53 +0000

    latin-1 extended character display

---
+++ dp-devel/tools/proofers/morenonascii.php
@@ -0,0 +1,108 @@
+<?PHP
+$relPath="./../../pinc/";
+include($relPath.'doctype.inc');
+echo $docType. "\r\n";
+?>
+<HTML><HEAD><TITLE>Latin-1 (ISO 8859-1) Character Set</TITLE>
+<BASEFONT SIZE="+4">
+</HEAD><BODY 
+BGCOLOR="#CCCCCC" TEXT="#000000" LINK="#000066" VLINK="#660000" ALINK="#006600"><CENTER><DIV ALIGN="CENTER"><TABLE 
+WIDTH="90%" BORDER="1" BORDERCOLOR="#666666" CELLPADDING="2"><TR><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#036;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#161;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#162;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#163;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#164;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#165;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#166;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#167;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#168;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#169;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#170;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#171;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#172;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#173;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#174;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#175;</B></FONT></TD></TR><TR><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#176;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#177;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#178;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#179;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#180;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#181;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#182;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#183;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#184;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#185;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#186;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#187;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#188;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#189;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#190;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#191;</B></FONT></TD></TR><TR><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#192;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#193;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#194;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#195;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#196;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#197;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#198;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#199;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#200;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#201;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#202;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#203;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#204;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#205;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#206;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#207;</B></FONT></TD></TR><TR><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#208;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#209;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#210;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#211;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#212;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#213;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#214;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#215;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#216;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#217;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#218;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#219;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#220;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#221;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#222;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#223;</B></FONT></TD></TR><TR><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#224;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#225;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#226;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#227;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#228;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#229;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#230;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#231;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#232;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#233;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#234;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#235;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#236;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#237;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#238;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#239;</B></FONT></TD></TR><TR><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#240;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#241;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#242;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#243;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#244;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#245;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#246;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#247;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#248;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#249;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#250;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#251;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#252;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#253;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#254;</B></FONT></TD><TD 
+ALIGN="center" VALIGN="center"><FONT SIZE="+1"><B>&#255;</B></FONT></TD></TR><TR><TD 
+ALIGN="center" VALIGN="center" COLSPAN="16"><A 
+HREF="#" onclick="window.close()"><B>Close</B></A></TD></TR></TABLE></DIV></CENTER></BODY></HTML>
