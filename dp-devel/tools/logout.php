098e5707ffecb0a3f04b2a41a50d08107f22a5b6
carel <>
2002-12-04 19:31:21 +0000

    updated to work with cookie class and provide valid html

--- dp-devel/tools/logout.php
+++ dp-devel/tools/logout.php
@@ -1,7 +1,13 @@
 <?
 //clear cookie if one is already set
-
-
-setcookie("pguser","",time() - 86400,"/",$_SERVER['SERVER_NAME'],0);
-     echo "<p><META HTTP-EQUIV=\"refresh\" CONTENT=\"0 ;URL=../default.php\">"; 
+$relPath='./../pinc/';
+include($relPath.'cookie.inc');
+$cookieC=new userCookie();
+$cookieSet=$cookieC->deleteCookie();
+include($relPath.'doctype.inc');
+echo $docType."\r\n";
+echo "<HTML><HEAD><TITLE>Logout Complete</TITLE>\r\n";
+echo "<META HTTP-EQUIV=\"refresh\" CONTENT=\"0 ;URL=../default.php\">";
+echo "</HEAD><BODY><A HREF=\"../default.php\">Return to Home Page.</A>".
+     "</BODY></HTML>";
 ?>
