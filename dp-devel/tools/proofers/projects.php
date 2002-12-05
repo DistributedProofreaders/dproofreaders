1eb53559227db317699bb1d8ecc8d0c3105e8f6a
carel <>
2002-12-05 22:41:43 +0000

    made it function for original intention + as view comments from proofers page.  Removed PM email link.

--- dp-devel/tools/proofers/projects.php
+++ dp-devel/tools/proofers/projects.php
@@ -8,11 +8,14 @@ include($relPath.'dp_main.inc');
     $authorsname = mysql_result($result, 0, "authorsname");
     $comments = mysql_result($result, 0, "comments");
     $username = mysql_result($result, 0, "username");
-
-    $users = mysql_query("SELECT email FROM users WHERE username = '$username'");
-    $email = mysql_result($users, 0, "email");
+if (isset($proofing))
+{   $phpuser = mysql_query("SELECT user_id FROM phpbb_users WHERE username = '$pguser'");
+   $user_id = mysql_result($phpuser, 0, "user_id");
+}
 include($relPath.'doctype.inc');
-echo "<HTML><HEAD><TITLE> Project Comments</TITLE>$docType";
+echo "$docType\r\n<HTML><HEAD><TITLE> Project Comments</TITLE>";
+if (!isset($proofing))
+{
 ?>
 <SCRIPT LANGUAGE="JavaScript" TYPE="text/javascript">
 <!-- 
@@ -27,13 +30,20 @@ else {newWidth=600;newHeight=450;}
 newFeatures="'toolbars=0,location=0,directories=0;status=0;menubar=0,scrollbars=1,resizable=1,width="+newWidth+",height="+newHeight+",top=0,left=5'";
 nwWin=window.open(winURL,"prooferWin",newFeatures);}
 // -->
-</SCRIPT></HEAD><BODY>
+</SCRIPT>
 <?PHP
-include('./projects_menu.inc');
+}
 ?>
+</HEAD><BODY>
+<?PHP
+if (!isset($proofing))
+{include('./projects_menu.inc');
+?>
+
 <br>
 Check out <a href="http://texts01.archive.org/dp/faq/document.html">Document Guidelines 1.20</a> for detailed project formatting comments. <BR>Instructions in Project Comments below take precedence over the guidelines.
-<P><table border=1 width=630>
+
+<P><table border=1 width=630><tr>
 <tr><td bgcolor="CCCCCC" align=center><h3><b>
 
 <?
@@ -42,16 +52,28 @@ Check out <a href="http://texts01.archive.org/dp/faq/document.html">Document Gui
     } else {
         echo "Second Round Project</b></h3></td><td bgcolor = \"CCCCCC\"><b>These are files that have already been proofed once, but now need to be examined <B>closely</B> for small errors that may have been missed. See <A HREF=\"http://www.promo.net/pg/vol/proof.html#What_kinds\" target = \" \">this page</A> for examples.</b></td>";
     }
+}
+else {
+?>
+<table border=1 width=630>
+<?PHP
+}
 
     echo "<tr><td bgcolor=\"CCCCCC\" align=center><b>Name of Work</b></td>";
     echo "<td>$nameofwork</td></tr>";
     echo "<tr><td bgcolor=\"CCCCCC\" align=center><b>Author</b></td>";
     echo "<td>$authorsname</td></tr>";
     echo "<tr><td bgcolor=\"CCCCCC\" align=center><b>Project Manager</b></td>";
-    echo "<td><A HREF=\"mailto: $email\">$username</A></td></tr>";
-
-    echo "<tr><td bgcolor = \"CCCCCC\"><h3>Project Comments</h3></td><td>$comments</td></tr></table>"; 
+    echo "<td>$username</td></tr>";
+if (!isset($proofing))
+  {echo "<tr><td bgcolor = \"CCCCCC\"><h3>Project Comments</h3></td><td>$comments</td></tr></table>";}
+else {echo "<tr><td bgcolor=\"CCCCCC\" align=center><b>Forum</b></td>";
+      echo "<td><a href=\"project_topic.php?projectid=$project&amp;user_id=$user_id&amp;action=c\">Discuss this Project in the Forum</td></tr>";
+      echo "<tr><td bgcolor = \"CCCCCC\" colspan=\"2\" align=\"center\"><h3>Project Comments</h3></td></tr><tr><td colspan=\"2\">$comments</td></tr></table>";
+      echo"<p><p><b> This information has been opened in a separate browser window, feel free to leave it open for reference or close it.</b>";
+     }
 echo "<BR>";
-include('./projects_menu.inc');
+if (!isset($proofing))
+{include('./projects_menu.inc');}
 ?>
 </BODY></HTML>
