f636f6793d461191eac66b57972757cef7d2ecfc
usfjoseph <>
2002-12-10 22:08:17 +0000

    small bug

--- dp-devel/tools/proofers/project_topic.php
+++ dp-devel/tools/proofers/project_topic.php
@@ -14,7 +14,7 @@ $post_ip = $_SERVER['REMOTE_ADDR'];
 $result = mysql_query("SELECT nameofwork, authorsname, topic_id FROM projects WHERE projectid='$project_id'");
 while($row = mysql_fetch_array($result)) {
 $title = $row['nameofwork'];
-$message =  "Discussion of ".$row['nameofwork']." by ".$row['authorsname'].<br><br>Click <a href='".$linkPath."projects.php?project=$project_id&prooflevel=0"";
+$message =  "Discussion of ".$row['nameofwork']." by ".$row['authorsname']."<br><br>Click <a href='".$linkPath."projects.php?project=$project_id&prooflevel=0'> here to view the project comments.";
 $topic_id = $row['topic_id'];
 }
 
