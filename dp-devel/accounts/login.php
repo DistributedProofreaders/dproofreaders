f85f3becb266ee3895e73386832b2fa44491da5d
aldarondo <>
2002-12-11 13:23:20 +0000

    Fixed Tiffany's grammar items.

--- dp-devel/accounts/login.php
+++ dp-devel/accounts/login.php
@@ -3,7 +3,7 @@ $relPath="./../pinc/";
 include($relPath.'connect.inc');
 include($relPath.'user.inc');
 $userC=new db_udb();
-$noLogin="Username or password is incorrect.<BR>If you feel you have recieved this message in error, please try to <A HREF=\"http://texts01.archive.org/dp/phpBB2/profile.php?mode=sendpassword\">reset</A> your password. If this fails, contact the <A HREF=\"mailto:charlz@lvcablemodem.com\">webmaster</A><BR><A HREF=\"signin.php\">Back</A> to sign in page.";
+$noLogin="Username or password is incorrect.<BR>If you feel you have received this message in error, please try to <A HREF=\"http://texts01.archive.org/dp/phpBB2/profile.php?mode=sendpassword\">reset</A> your password. If this fails, contact the <A HREF=\"mailto:charlz@lvcablemodem.com\">webmaster</A>.<BR><A HREF=\"signin.php\">Back</A> to the sign in page.";
 $htmlStart="<HTML><HEAD><TITLE>Login</TITLE>";
 $htmlMid="</HEAD><BODY>";
 $htmlEnd="</BODY></HTML>";
