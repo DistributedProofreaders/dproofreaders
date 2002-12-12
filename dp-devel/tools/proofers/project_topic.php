2fb476a7736b406cb5776296cd50ffe571d3cdd0
usfjoseph <>
2002-12-12 23:37:57 +0000

    Updated to reflect raz's account as the owner of the topics.  Can change the owner by changing $owner = ***

--- dp-devel/tools/proofers/project_topic.php
+++ dp-devel/tools/proofers/project_topic.php
@@ -5,9 +5,9 @@ include($relPath.'dp_main.inc');
 
 //Declare variables
 $timeposted = time();
-$user_id = $_GET['user_id'];
 $project_id = $_GET['project'];
 $post_ip = $_SERVER['REMOTE_ADDR'];
+$owner = 527;
 
 //Get info about project
 $result = mysql_query("SELECT nameofwork, authorsname, topic_id FROM projects WHERE projectid='$project_id'");
@@ -20,11 +20,11 @@ $topic_id = $row['topic_id'];
 //Determine if there is an existing topic or not
 if(($topic_id == "") || ($topic_id == 0)) {
 //Add Topic into phpbb_topics
-$insert_topic = mysql_query("INSERT INTO phpbb_topics (topic_id, forum_id, topic_title, topic_poster, topic_time, topic_views, topic_replies, topic_status, topic_vote, topic_type, topic_first_post_id, topic_last_post_id, topic_moved_id) VALUES (NULL, 2, '$title', $user_id, $timeposted, 0, 0, 0, 0, 0, 1, 1, 0)");
+$insert_topic = mysql_query("INSERT INTO phpbb_topics (topic_id, forum_id, topic_title, topic_poster, topic_time, topic_views, topic_replies, topic_status, topic_vote, topic_type, topic_first_post_id, topic_last_post_id, topic_moved_id) VALUES (NULL, 2, '$title', $owner, $timeposted, 0, 0, 0, 0, 0, 1, 1, 0)");
 $topic_id = mysql_insert_id();
 
 //Add Post into phpbb_posts
-$insert_post = mysql_query("INSERT INTO phpbb_posts (post_id, topic_id, forum_id, poster_id, post_time, poster_ip, post_username, enable_bbcode, enable_html, enable_smilies, enable_sig, post_edit_time, post_edit_count) VALUES (NULL,$topic_id, 2, $user_id, $timeposted, '$post_ip', NULL, 1, 0, 1, 1, NULL, 0)");
+$insert_post = mysql_query("INSERT INTO phpbb_posts (post_id, topic_id, forum_id, poster_id, post_time, poster_ip, post_username, enable_bbcode, enable_html, enable_smilies, enable_sig, post_edit_time, post_edit_count) VALUES (NULL,$topic_id, 2, $owner, $timeposted, '$post_ip', NULL, 1, 0, 1, 1, NULL, 0)");
 $post_id = mysql_insert_id();
 
 //Add Post Text into phpbb_posts_text
