874a24bcb606142e1679d6578039ef377d8ffeab
aldarondo <>
2002-12-15 15:51:27 +0000

    Feature to add/delete text yourself easily, along with missing code from original site

---
+++ dp-devel/tools/project_manager/add_files.php
@@ -0,0 +1,24 @@
+<?
+$relPath="./../../pinc/";
+include($relPath.'dp_main.inc');
+
+    $project = $_GET['project'];
+
+    $sql = mysql_query("SELECT username FROM projects WHERE projectid = '$project' LIMIT 1");
+    $username = mysql_result($sql, 0, "username");
+
+    $sql = mysql_query("SELECT sitemanager FROM users WHERE username = '$pguser' LIMIT 1");
+    $sitemanager = mysql_result($sql, 0, "sitemanager");
+
+    if (($sitemanager != 'yes') && ($pguser != $username)) {
+        echo "<P>You are not allowed to change this project. If this message is an error, contact the <a href=\"charlz@lvcablemodem.com\">site manager</a>.";
+        echo "<P>Back to <a href=\"projectmgr.php\">project manager</a> page.";
+    } else {
+        if ($sitemanager == 'yes') {
+            $string = "perl add_files.pl $project ../../projects/";
+        } else $string = "perl add_files.pl $project /home/dpscans/";
+        exec($string);
+        echo "<html><head><META HTTP-EQUIV=\"refresh\" CONTENT=\"0 ;URL=projectmgr.php?project=$project\"></head><body></body></html>";
+    }
+?>
+
