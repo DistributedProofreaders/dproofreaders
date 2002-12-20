5842f418a29b978096ac023452ee99d6c331a700
aldarondo <>
2002-12-20 07:42:11 +0000

    Fixed a bug where a newbie would get set to view no projects.

--- dp-devel/userprefs.php
+++ dp-devel/userprefs.php
@@ -70,7 +70,7 @@ echo "<tr><td width='21%'>Show projects from:</td><td width='79%'>";
 $array = implode('|', $p_l);
 dropdown_select('u_plist', $userP['u_plist'], $array);
 echo "</td></tr>";
-}
+} else echo "<input type='hidden' name='u_plist' value='1'>";
 
 echo "<tr><td width='21%'>Language:</td><td width='79%'>";
 $array = implode('|', $u_l);
