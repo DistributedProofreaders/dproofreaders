a9038104e43715a3e7d23deb90bec75245ced580
usfjoseph <>
2003-01-08 04:37:18 +0000

    Code used to view past news articles.  Pretty "naked" right now but will incorporate the redesign look into it.  Will also add a link to it on the default.php page soon.

---
+++ dp-devel/pastnews.php
@@ -0,0 +1,12 @@
+<?
+$relPath="./pinc/";
+include($relPath.'connect.inc');
+$db_Connection=new dbConnect();
+
+$result = mysql_query("SELECT * FROM news ORDER BY uid DESC");
+while($row = mysql_fetch_array($result)) {
+$date_posted = date("l, F jS, Y",$row['date_posted']);
+echo "<b>$date_posted</b><br>".$row['message']."<br><hr align='center' width='75%'><br>";
+}
+
+?>
