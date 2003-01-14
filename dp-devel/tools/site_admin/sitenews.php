16b9567ef386736282d939fdc19903498657d98e
usfjoseph <>
2003-01-14 22:13:42 +0000

    Fixed a small bug where the sql query appeared after an action on that specific query

--- dp-devel/tools/site_admin/sitenews.php
+++ dp-devel/tools/site_admin/sitenews.php
@@ -3,6 +3,7 @@ $relPath="./../../pinc/";
 include($relPath.'dp_main.inc');
 
 $uid = $userP['user_id'];
+$result = mysql_query("SELECT * FROM users WHERE id = $uid");
 if (mysql_result($result,0,"sitemanager") == "yes") {
 
 if ($action == "add") {
