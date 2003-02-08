c59aec76e5bc65e0027c75c4f1d66b35cd4892d2
carel <>
2003-02-08 00:58:03 +0000

    updated to work with changes in html class

--- dp-devel/accounts/addproofer.php
+++ dp-devel/accounts/addproofer.php
@@ -46,10 +46,8 @@ if ($password=="proofer") {
         $currtime = time();
 
         $total += 1;
-        $sql = "INSERT INTO phpbb_users (user_id, username, user_regdate, user_timezone, user_email, user_password, 
-user_viewemail) 
-                            VALUES ('$total', '$username', " . $currtime . ", '-8.00', '$email', '$passwd', '0')";
-
+        $sql = "INSERT INTO phpbb_users (user_id, username, user_regdate, user_timezone, user_email, user_password, user_viewemail) 
+            VALUES ('$total', '$username', " . $currtime . ", '-8.00', '$email', '$passwd', '0')";
         $result = mysql_query($sql);
 
         mail($email, "Welcome to the Distributed Proofreader's Site!",
@@ -98,39 +96,40 @@ $td2=$htmlC->startTD(1,0,0,0,"center",0,0,1);
 $td3=$htmlC->startTD(0,0,0,0,"center",0,0,1);
 $td4=$htmlC->startTD(1,0,2,0,"center",0,0,1);
 $td5=$htmlC->startTD(0,0,2,0,"left",0,0,1);
+$tde=$htmlC->closeTD(1);
+$tre=$htmlC->closeTD(1).$htmlC->closeTR(1);
+
 ?>
 <form method="post" action="addproofer.php">
 <input type=hidden name="password" value="proofer">
 <?php
     echo $tb.$tr.$td1;
-    echo "<b>Create A Distributed Proofreader Account<b>";
-    echo $tr.$td2."<b>Real Name:</b>";
+    echo "<b>Create A Distributed Proofreader Account</b>";
+    echo $tre.$tr.$td2."<b>Real Name:</b>";
     echo $td3.'<input type="text" maxlength=70 name="real_name" size=20>';
-    echo $tr.$td2."<b>User Name:</b>";
+    echo $tre.$tr.$td2."<b>User Name:</b>";
     echo $td3.'<input type="text" maxlength=70 name="userNM" size=20>';
-    echo $tr.$td2."<b>Password:</b>";
+    echo $tre.$tr.$td2."<b>Password:</b>";
     echo $td3.'<input type="password" maxlength=70 name="userPW" size=20>';
-    echo $tr.$td2."<b>E-mail Address:</b>";
+    echo $tre.$tr.$td2."<b>E-mail Address:</b>";
     echo $td3.'<input type="text" maxlength=70 name="email" size=20>';
-    echo $tr.$td2."<b>E-mail Updates:</b>";
-    echo $td3.'<input type="radio" name="email_updates" value="1" checked>Yes&nbsp;&nbsp;<input type="radio" name="email_updates" value="0">No';
-    echo $tr.$td1.'<input type="submit" value="Create Account">&nbsp;&nbsp;<input type="reset" value="Reset Form">';
-    echo $tr.$td5;
+    echo $tre.$tr.$td2."<b>E-mail Updates:</b>";
+    echo $tde.$td3.'<input type="radio" name="email_updates" value="1" checked>Yes&nbsp;&nbsp;<input type="radio" name="email_updates" value="0">No';
+    echo $tre.$tr.$td1.'<input type="submit" value="Create Account">&nbsp;&nbsp;<input type="reset" value="Reset Form">';
+    echo $tre.$tr.$td5;
 ?>
-<center><font size=+2>Privacy Statement:</font></center>
-<p><font size=+1>Usage of information:</font></p>
+<center><font size="+2">Privacy Statement:</font></center>
+<p><font size="+1">Usage of information:</font></p>
 <p>This information will allow the project managers to provide feedback to you directly or have users send to you a message via the phpBB forum. An introductory e-mail will also be sent to you.</p>
-<p><font size=+1>Removal of information:</font>
+<p><font size="+1">Removal of information:</font>
 <p>Requests to remove the information beforehand can be sent to the web site manager or will be removed based on it's age.</p>
-<p><font size=+1>Access of information:</font></p>
+<p><font size="+1">Access of information:</font></p>
 <p>Only the web site manager will have full access to the information in this form. Project managers will have access to e-mail you directly if needed. Any public information you fill out in the phpBB forum or profile will be accessible to other users.</p>
-<p><font size=+1>Tracking information:</font></p>
+<p><font size="+1">Tracking information:</font></p>
 <p>The only tracking information we collect at this time is the number of pages you have completed, the date your account was created, and your last login date. Only the pages completed out of the three will be available to end users, the rest is for statistical purposes and removal of old accounts by the web site managers.</p>
 <?php
-    echo $tr.$td4;
-?>
-<a href="../default.php"><b>Home</b></a></td>
-</form></tr></table></body></html>
-<?
+    echo $tre.$tr.$td4."<a href=\"../default.php\"><b>Home</b></a>";
+    echo $tre.$htmlC->closeTable(1)."</form>".$htmlC->closeBody(1);
+
 }
 ?>
