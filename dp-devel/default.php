a3b7561e63a1e6afb357626c784e7002c8e71980
aldarondo <>
2003-02-08 17:10:48 +0000

    Made minor touch-ups to the front page.

--- dp-devel/default.php
+++ dp-devel/default.php
@@ -1,22 +1,16 @@
-<?
+<?php
 $relPath="./pinc/";
 include($relPath.'connect.inc');
 $db_Connection=new dbConnect();
 include($relPath.'showstartexts.inc');
 $etext_limit = 10;
 ?>
-
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<LINK REL="SHORTCUT ICON" HREF="/dp/favicon.ico">
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="SHORTCUT ICON" href="/dp/favicon.ico">
 <title>Distributed Proofreaders</title>
 </head>
-
 <body>
-
-<table border=0><tr>
-<td>
+<table border=0><tr><td>
 <form action="accounts/login.php" method="post"><table border=1>
 <tr><td bgcolor=#CCCCCC><strong>Username</strong></td><td><input type="text" name="userNM" size=10 maxsize=50></td></tr>
 <tr><td bgcolor=#CCCCCC><strong>Password</strong></td><td><input type="password" name="userPW" size=10 maxsize=50></td></tr>
@@ -29,25 +23,20 @@ $etext_limit = 10;
 
 <table border=0 cellspacing=0>
 <tr><td width=1 bgcolor=#cccccc>&nbsp;</td><td bgcolor=#cccccc align="left"><font size=+1>About this site:</font></td><tr>
-<td bgcolor=#cccccc></td><td><br>This project is currently a private effort to support <a href="http://www.gutenberg.net">Project Gutenberg</a>
-and is not an 'official' Project Gutenberg site.
-<br>As such, if you have any questions or comments regarding this site
-please <a href="mailto:charlz@lvcablemodem.com">E-mail Charles Franks</a> not the folks
-at PG.
+<td bgcolor=#cccccc></td><td><br>This project is currently a sub-group of <a href="http://www.gutenberg.net">Project Gutenberg</a>
+and is not the official Project Gutenberg site.
+<br>As such, if you have any questions or comments regarding this site please <a href="mailto:dphelp@texts01.archive.org">E-mail DP-help</a> and not the folks at Project Gutenberg.
 <br><br></td><tr>
 
 <td bgcolor=#CCCCCC></td><td bgcolor=CCCCCC align=left><font size="+1">Site concept:</font></td><tr>
 <td bgcolor=#CCCCCC></td><td>
-<br>This site provides a web-based method of easing the proofreading work associated with the creation of Project Gutenberg E-Texts.
-By breaking the work into individual pages many proofreaders can be working on the same book at the same time.
-This significantly speeds up the proofreading/E-Text creation process.
-<P> When a proofer elects to proofread a page for a particular project, the text and image
-file are displayed on a single webpage. This allows the text file to be easily
+<p>This site provides a web-based method of easing the proofreading work associated with the creation of Project Gutenberg e-books. By breaking the work into individual pages many proofreaders can be working on the same book at the same time. This significantly speeds up the proofreading/e-book creation process.</p>
+<p>When a proofer elects to proofread a page for a particular project, the text and image
+file are displayed on a single web page. This allows the text file to be easily
 reviewed and compared to the image file, thus assisting the proofreading of the text file.
-The edited text file is then submitted back to the site via the same webpage that it was edited on.
-<p>Once all pages for a particular book have been processed the Project Manager joins the pieces, properly formats them into a PG E-Text and
-submits it to the PG archive.
-<p></td><tr>
+The edited text file is then submitted back to the site via the same web page that it was edited on.</p>
+<p>Once all pages for a particular book have been processed, a post-processor joins the pieces, properly formats them into a Project Gutenberg e-book and submits it to the Project Gutenberg archive.</p>
+</td><tr>
 
 <?
 $result = mysql_query("SELECT * FROM news ORDER BY uid DESC LIMIT 1");
@@ -65,8 +54,8 @@ echo $message;
 <input type="image" src="http://images.paypal.com/images/x-click-butcc-donate.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
 </form><p></td><tr>
 
-<td bgcolor=CCCCCC><td align=left><b>Hourly Page Count:&nbsp&nbsp</b> <? include("stats/hourly.txt"); ?>
-<br>Our goal this month is <b>34,000</b> pages which means <b>1,133</b> pages per day.<p></td><tr>
+<td bgcolor=CCCCCC><td align=left><b>Hourly Page Count:</b><br><? include("stats/hourly.txt"); ?>
+<br>Our goal this month is <b>134,008</b> pages which means <b>4,786</b> pages per day.<p></td><tr>
 
 <?
     $numdays = 1;
@@ -79,10 +68,7 @@ echo $message;
     $datefrom = $datethru - $daterange;
     $datefromformat = date("l jS",$datefrom)." of ".date("F",$datefrom);
     $datethruformat = date("l jS",$datethru)." of ".date("F",$datethru);
-// old table dates
-//    $sqldatefrom = date("Ymd",$datefrom);
-//    $sqldatethru = date("Ymd",$datethru);
-// new table dates
+// table dates
     $sqldatefrom = $datefrom;
     $sqldatethru = $datethru;
 
@@ -149,19 +135,16 @@ echo $message;
 
 <td width = 1 bgcolor=CCCCCC>&nbsp</td><td bgcolor=CCCCCC align=left><font size = "+1">Get Started!</font><td><tr>
 <td width = 1 bgcolor=CCCCCC>&nbsp</td><td>
-<br><font size = "+1">Proofreaders:</font></td><tr>
-<td bgcolor=CCCCCC></td><td>
-<font size="+1"><a href="accounts/addproofer.php">Create</a></font> an account to get started! You can also read the <a href="faq/ProoferFAQ.html">Frequently Asked Questions (FAQ)</a> to get an
+<br><u><font size = "+1">Proofreaders:</font></u>
+<br><font size="+1"><a href="accounts/addproofer.php">Create</a></font> an account to get started! You can also read the <a href="faq/ProoferFAQ.html">Frequently Asked Questions (FAQ)</a> to get an
  idea of what we do here at this site.
 <p><u><font size=+1>Already have an account?</font></u>
 <br><font size="+1"><a href="accounts/signin.php">Sign In</a></font> and start proofing!!
 <br><br></td><tr>
 
 <td bgcolor=CCCCCC></td><td bgcolor=CCCCCC align=left><font size="+1">Daily Statistics</font></td><tr>
-<td bgcolor=CCCCCC><td align=center><img SRC="stats/stats.png" width=600 height=500 ALT="stats.png"><p></td><tr>
-<td bgcolor=CCCCCC><td align=center><img SRC="stats/target.png" width=600 height=500 ALT="target.png"><p></td><tr>
-<td bgcolor=CCCCCC><td align=center>Note: if you can't see the stats image please first click the Refresh/Reload button on your browser and if the image still does not appear please <a href="mailto
-:charlz@lvcablemodem.com">E-mail me</a> and tell me the make and version of your browser as well as what operating system you are using.</td><tr>
+<td bgcolor=CCCCCC><td align=center><a href="stats/stats.png"><img SRC="stats/stats.png" width=300 height=250 ALT="stats.png"></a>&nbsp;<a href="stats/target.png"><img SRC="stats/target.png" width=300 height=250 ALT="target.png"></a><p></td><tr>
+<td bgcolor=CCCCCC><td align=center>Note: if you can't see the stats image please first click the Refresh/Reload button on your browser and if the image still does not appear please <a href="mailto:dphelp@texts01.archive.org">E-mail DP-help</a> and tell us the make and version of your browser as well as what operating system you are using.</td><tr>
 <td bgcolor=CCCCCC><td align=center><a href="previous_stats.html">Previous Months Statistics</a></td><tr>
 
 <td bgcolor="#cccccc">&nbsp;</td>
