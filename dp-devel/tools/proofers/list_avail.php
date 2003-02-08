8db5b86cf3ca01a2eb9cfc72578d42865fdb9ce7
carel <>
2003-02-08 04:55:28 +0000

    uses proof_list.inc

--- dp-devel/tools/proofers/list_avail.php
+++ dp-devel/tools/proofers/list_avail.php
@@ -22,37 +22,8 @@ include($relPath.'page_states.inc');
     $winPref=$userP['i_newwin'];
     $userP['i_newwin']=0;
 
-if ($userP['u_plist'] == 1 || $userP['u_plist'] == 3)
-  {
-    echo "<table border=1 width=630>";
-    echo "<tr><td bgcolor='#CCCCCC' colspan=2><h3>Current First - Round Projects</h3></td>";
-    echo "<td bgcolor='#CCCCCC' colspan=5> These files are output from the OCR software and have not been looked at.</td></tr>";
-    //Select all projects in the list for round 1
-    $result = mysql_query("SELECT * FROM projects WHERE state = '".AVAIL_PI_FIRST."' or state = '".VERIFY_PI_FIRST."' ORDER BY modifieddate asc, nameofwork asc");
-    showavailablebooks($result,AVAIL_FIRST,$userP,1);
-    echo "</table>";
-  }
-
-if ($userP['u_plist'] == 2 || $userP['u_plist'] == 3)
-  {
-    echo "\n<table border=1 width=630>";
-    echo "<br><tr><td bgcolor='CCCCCC' colspan=2><h3>Current Second - Round Projects </h3></td>";
-    if ($totalpages < 50)
-      {
-        echo "<td bgcolor='#cccccc' colspan=5>Second round projects are unavailable until you have proofed more than 50 first round pages.  After 50 pages of first round proofing the second round projects will be unlocked for you.";
-        echo "</td></tr></table>\n<p>";
-      }
-    else
-      {
-        echo "<td bgcolor='CCCCCC' colspan=5>These are files that have already been proofed once, but now need to be examined <B>closely</B> for small errors that may have been missed.";
-        echo "See <A HREF='http://www.promo.net/pg/vol/proof.html#What_kinds' target='_new'>this page</A> for examples.";
-        echo "</td></tr>";
-        //Select all projects in the list for round 2 
-          $result = mysql_query("SELECT * FROM projects WHERE state = '".AVAIL_PI_SECOND."' or state = '".VERIFY_PI_SECOND."' ORDER BY nameofwork ASC");
-          showavailablebooks($result,AVAIL_SECOND,$userP,1);
-        echo "</table>";
-      }
-  } 
+$tList=1;
+include_once('proof_list.inc');
 
 // restore old win pref
 $userP['i_newwin']=$winPref;
