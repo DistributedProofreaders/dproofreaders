46df2797064161e7c69f98d7ccca4146e19285fd
jmdyck <>
2003-02-25 07:47:17 +0000

    Various things. Mostly add the "Page Information" section.

--- dp-devel/tools/catalog/project_home.php
+++ dp-devel/tools/catalog/project_home.php
@@ -50,26 +50,6 @@ else
         echo "<tr><td>$key</td><td>$value</td></tr>\n";
     }
     echo "<tr><td>Project Directory</td><td><a href='../../projects/$projectid'>here</a></td></tr>\n";
-
-    // fileid, image, master_text, state
-    // round1_text, round1_user, round1_time
-    // round2_text, round2_user, round2_time
-    $res = mysql_query( "SELECT count(*) AS num_pages FROM $projectid") or die(mysql_error());
-    $num_pages = mysql_result($res,0,'num_pages');
-    echo "<tr><td># of pages</td><td>$num_pages</td></tr>\n";
-    echo "</table>\n";
-
-    echo "<p>Number of pages in various states:</p>\n";
-    echo "<table border=1>\n";
-    foreach ($page_states as $page_state)
-    {
-        $res = mysql_query( "SELECT count(*) AS num_pages FROM $projectid WHERE state='$page_state'") or die(mysql_error());
-        $num_pages = mysql_result($res,0,'num_pages');
-        if ( $num_pages != 0 )
-        {
-            echo "<tr><td>$page_state</td><td>$num_pages</td></tr>\n";
-        }
-    }
     echo "</table>\n";
 
     // ----------------------------------------------------------------
@@ -80,7 +60,11 @@ else
     echo "<h3>Things you can currently do with this project:</h3> (these would all be links)\n";
 
     echo "<ul>\n";
-    echo "<li> Discuss this project in its dedicated <a href='../proofers/project_topic.php?project=$projectid'>Forum</a>.</li>\n";
+
+    if ( 1 )
+    {
+        echo "<li> Discuss this project in its dedicated <a href='../proofers/project_topic.php?project=$projectid'>Forum</a>.</li>\n";
+    }
 
     if ( $proj_state != PROJ_COMPLETE && $proj_state != PROJ_COMPLETE )
     {
@@ -93,12 +77,23 @@ else
         echo "<li> (You are the project manager.) Upload images or OCR results.</li>\n";
     }
 
-    echo "<li> View page-images <a href='../../projects/$projectid/index.html'>online</a>.</li>\n";
-    echo "<li> Download a zip of page-images.</li>\n";
+    if ( 1 )
+    {
+        echo "<li> View page-images <a href='../../projects/$projectid/index.html'>online</a>.</li>\n";
+        echo "<li> Download a zip of page-images.</li>\n";
+    }
 
     if ( $proj_round == 'FIRST' || $proj_round == 'SECOND' )
     {
-        echo "<li><a href='../proofers/projects.php?project=$projectid&proofstate=X'>Start Proofing!</a></li>\n";
+        if ( $proj_round == 'FIRST' )
+        {
+            $available_state = PROJ_PROOF_FIRST_AVAILABLE;
+        }
+        else
+        {
+            $available_state = PROJ_PROOF_SECOND_AVAILABLE;
+        }
+        echo "<li><a href='../proofers/projects.php?project=$projectid&proofstate=$available_state'>Start Proofing!</a></li>\n";
         echo "<li> Re-proof pages (if you've proofed any).</li>\n";
     }
 
@@ -133,7 +128,64 @@ else
 
     echo "<hr>\n";
 
-    // Did you participate in this project?
+    echo "<h3>Page Information</h3>\n";
+
+    $res = mysql_query( "SELECT count(*) AS num_pages FROM $projectid") or die(mysql_error());
+    $num_pages = mysql_result($res,0,'num_pages');
+    echo "<p>Total number of pages: $num_pages</p>\n";
+
+    echo "<p>Number of pages in various states:</p>\n";
+    echo "<table border=1>\n";
+    foreach ($page_states as $page_state)
+    {
+        $res = mysql_query( "SELECT count(*) AS num_pages FROM $projectid WHERE state='$page_state'") or die(mysql_error());
+        $num_pages = mysql_result($res,0,'num_pages');
+        if ( $num_pages != 0 )
+        {
+            echo "<tr><td>$page_state</td><td>$num_pages</td></tr>\n";
+        }
+    }
+    echo "</table>\n";
+
+    echo "<p>Info for individual pages:</p>\n";
+
+    // fileid, image, master_text, state
+    // round1_text, round1_user, round1_time
+    // round2_text, round2_user, round2_time
+
+    echo "<table border=1>\n";
+    echo "<tr>\n";
+    echo "  <th>image</th>\n";
+    echo "  <th>fileid</th>\n";
+    echo "  <th>state</th>\n";
+    echo "  <th>master_text</th>\n";
+    echo "  <th>round1_user</th>\n";
+    echo "  <th>round1_time</th>\n";
+    echo "  <th>round1_text</th>\n";
+    echo "  <th>round2_user</th>\n";
+    echo "  <th>round2_time</th>\n";
+    echo "  <th>round2_text</th>\n";
+    echo "</tr>\n";
+    $query = "SELECT image, fileid, state, round1_user, round1_time, round2_user, round2_time FROM $projectid ORDER BY image ASC";
+    $pages_res = mysql_query($query) or die(mysql_error());
+    while ( $page_res = mysql_fetch_array( $pages_res, MYSQL_ASSOC ) )
+    {
+        echo "<tr>\n";
+        echo "  <td>{$page_res['image']}</td>\n";
+        echo "  <td>{$page_res['fileid']}</td>\n";
+        echo "  <td>{$page_res['state']}</td>\n";
+        echo "  <td><a href='get_page_text.php?project=$projectid&page_image={$page_res['image']}&which=master'>text</a>\n";
+        echo "  <td>{$page_res['round1_user']}</td>\n";
+        echo "  <td>{$page_res['round1_time']}</td>\n";
+        echo "  <td><a href='get_page_text.php?project=$projectid&page_image={$page_res['image']}&which=round1'>text</a>\n";
+        echo "  <td>{$page_res['round2_user']}</td>\n";
+        echo "  <td>{$page_res['round2_time']}</td>\n";
+        echo "  <td><a href='get_page_text.php?project=$projectid&page_image={$page_res['image']}&which=round2'>text</a>\n";
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
+
+    $nameofwork  = $project_res['nameofwork'];
 
     echo "</body>\n";
     echo "</html>\n";
