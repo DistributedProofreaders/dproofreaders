4ff0219a86909e6186739424ebe0d9a3de9fb748
usfjoseph <>
2003-02-25 21:22:39 +0000

    Fixed maybe_mail location with relPath

--- dp-devel/tools/project_manager/editproject.php
+++ dp-devel/tools/project_manager/editproject.php
@@ -3,7 +3,7 @@
 $relPath="./../../pinc/";
 include($relPath.'dp_main.inc');
 include_once($relPath.'project_states.inc');
-include_once 'maybe_mail.inc';
+include_once($relPath.'maybe_mail.inc');
 
 // Encodes a form parameter to allow it to contain double quotes.
 function encodeFormValue($value) {
