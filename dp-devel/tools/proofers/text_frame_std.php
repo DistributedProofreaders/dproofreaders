6b6b92da9b9555dba30f7691f2bc0122fd748dd2
usfjoseph <>
2003-03-07 22:43:58 +0000

    Added a way to find out how many pages a proofer has proofed. Held in $totalpages

--- dp-devel/tools/proofers/text_frame_std.php
+++ dp-devel/tools/proofers/text_frame_std.php
@@ -1,6 +1,12 @@
 <?
 $relPath="./../../pinc/";
 include_once($relPath.'dp_main.inc');
+
+//Find out how many pages have been proofed already
+    $pagessql = "SELECT pagescompleted FROM users WHERE username = '$pguser' LIMIT 1";
+    $pages = mysql_query($pagessql);
+    $totalpages = mysql_result($pages, 0, "pagescompleted");
+
 if ($userP['i_type'] != 1)
   {include('textframe_njs.inc');}
 else
