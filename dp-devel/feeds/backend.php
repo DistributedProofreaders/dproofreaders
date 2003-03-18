612175c453bc865c0d7bf4d779a82b0bae0c133a
usfjoseph <>
2003-03-18 23:22:28 +0000

    Used to either: a.) display the feed the user requests if the feed has not been modified recently or b.) updates the feed if it has not been updated recently & then displays the feed.
    Both types of feeds are available.  Our own format utilized with the projects.dtd DTD as well as 0.91 compliant RSS feeds.  2.0 is not utilized as of yet due to some programs still relying on 0.91

---
+++ dp-devel/feeds/backend.php
@@ -0,0 +1,114 @@
+<?
+//Try our best to make sure no browser caches the page
+header("Content-Type: text/xml");
+header("Expires: Sat, 1 Jan 2000 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . "GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+
+$content = $_GET['content']; //Which feed the user wants
+$refreshdelay = 30; //Time in minutes for how often the feeds get refreshed
+$refreshdelay = time()-($refreshdelay*60); //Find out how long ago $refreshdelay was in UNIX time
+
+//If the user did not specify a xml feed set posted as the default
+if (($content != "posted") & ($content != "proofing") & ($content != "news")) { $content = "posted"; }
+
+//Determine if we should display a 0.91 compliant RSS feed or our own feed
+if (isset($_GET['type'])) { $xmlfile = $content."_rss.xml"; } else { $xmlfile = $content.".xml"; }
+
+//Determine if the feed needs to be updated.  If not display feed to user
+if (filemtime($xmlfile) > $refreshdelay) {
+readfile($xmlfile);
+} else {
+$relPath="./../pinc/";
+include($relPath.'connect.inc');
+$db_Connection=new dbConnect();
+	
+	if ($content == "posted") {
+	$result = mysql_query("SELECT * FROM projects WHERE state='proj_submit_pgposted' ORDER BY modifieddate DESC LIMIT 10");
+		while ($row = mysql_fetch_array($result)) {
+		$posteddate = date("r",($row['modifieddate']));
+			if (isset($_GET['type'])) {
+				$data .= "<item>
+				<title>".$row['nameofwork']." - ".$row['authorsname']."</title>
+				<link>$siteurl/list_etexts.php?x=g#".$row['projectid']."</link>
+				<description>Language: ".$row['language']." - Genre: ".$row['genre']."</description>
+				</item>
+				";
+			} else {
+				$data .= "<project id=\"".$row['projectid']."\">
+				<nameofwork>".$row['nameofwork']."</nameofwork>
+				<authorsname>".$row['authorsname']."</authorsname>
+				<language>".$row['language']."</language>
+				<posteddate>".$posteddate."</posteddate>
+				<genre>".$row['genre']."</genre>
+				<links>
+				<text>".$row['txtlink']."</text>
+				<zip>".$row['ziplink']."</zip>
+				<html>".$row['htmllink']."</html>
+				<library>$siteurl/list_etexts.php?x=g#".$row['projectid']."</library>
+				</links>
+				</project>
+				";
+			}
+		}
+		$lastupdated = date("r");
+			if (isset($_GET['type'])) {
+				$xmlpage = "<"."?"."xml version=\"1.0\" encoding=\"ISO-8859-1\" ?".">
+				<!DOCTYPE rss SYSTEM \"http://my.netscape.com/publish/formats/rss-0.91.dtd\">
+				<rss version=\"0.91\">
+				<channel>
+				<title>Distributed Proofreaders - Latest Releases</title>
+				<link>$siteurl</link>
+				<description>The latest releases from Distributed Proofreaders posted to Project Gutenberg</description>
+				<language>en-us</language>
+				<webMaster>dphelp@texts01.archive.org</webMaster>
+				<pubDate>$lastupdated</pubDate>
+				<lastBuildDate>$lastupdated</lastBuildDate>
+				$data
+				</channel>
+				</rss>";
+			} else {
+				$xmlpage = "<"."?"."xml version=\"1.0\" encoding=\"ISO-8859-1\" ?".">
+				<!DOCTYPE projects SYSTEM \"$siteurl/feeds/projects.dtd\">
+				<!-- Last Updated: $lastupdated -->
+				<projects>
+				$data
+				</projects>";
+			}
+	}
+	
+	if ($content == "news") {
+	$result = mysql_query("SELECT * FROM news ORDER BY date_posted DESC LIMIT 10");
+		while ($row = mysql_fetch_array($result)) {
+		$posteddate = date("l, F jS, Y",($row['date_posted']));
+				$data .= "<item>
+				<title>Distributed Proofreaders News Update for $posteddate</title>
+				<link>$siteurl/pastnews.php?#".$row['uid']."</link>
+				<description>".strip_tags($row['message'])."</description>
+				</item>
+				";
+		}
+		$lastupdated = date("r");
+				$xmlpage = "<"."?"."xml version=\"1.0\" encoding=\"ISO-8859-1\" ?".">
+				<!DOCTYPE rss SYSTEM \"http://my.netscape.com/publish/formats/rss-0.91.dtd\">
+				<rss version=\"0.91\">
+				<channel>
+				<title>Distributed Proofreaders - Latest Releases</title>
+				<link>$siteurl</link>
+				<description>The latest news related to Distributed Proofreaders</description>
+				<language>en-us</language>
+				<webMaster>dphelp@texts01.archive.org</webMaster>
+				<pubDate>$lastupdated</pubDate>
+				<lastBuildDate>$lastupdated</lastBuildDate>
+				$data
+				</channel>
+				</rss>";
+	}
+
+$file = fopen($xmlfile,"w");
+fwrite($file,$xmlpage);
+$file = fclose($file);
+readfile($xmlfile);
+}
+?>
