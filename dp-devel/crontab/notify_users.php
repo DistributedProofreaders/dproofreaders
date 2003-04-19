d9522a76349849c96a57e996243c5e24db16e529
charlz <>
2003-04-19 19:40:05 +0000

    changed to reflect that old accounts will be marked as inactive vice deleted

--- dp-devel/crontab/notify_users.php
+++ dp-devel/crontab/notify_users.php
@@ -4,7 +4,7 @@ include($relPath.'dp_main.inc');
 
     $old_date = time() - 13176000; // 30 days less than 1/2 a year.
 
-    $result = mysql_query ("SELECT * FROM `users` WHERE last_login < $old_date");
+    $result = mysql_query ("SELECT * FROM `users` WHERE last_login < $old_date AND active ='yes'");
     $numrows = mysql_num_rows($result);
     $rownum = 0;
 
@@ -21,15 +21,15 @@ include($relPath.'dp_main.inc');
 "This is an automated message and your only e-mail reminder that your account on the
  Distributed Proofreaders site (http://texts01.archive.org/dp/) has been inactive
 for over 5 months now. In order to show a valid number of active members for our
-site, we will be removing this account from our site a month from today if it
-remains inactive.\n\n
+site, we will be marking this account as inactive a month from today if you do not
+log into the site.\n\n
 If you wish to receive no more mailings from us, you need to do nothing else and 
-this account will be removed from our system. If you have forgotten your password,
+this account will be marked as inactive. If you have forgotten your password,
 visit (http://texts01.archive.org/dp/phpBB2/profile.php?mode=sendpassword) to have
 it reset. We hope you care to join us, much has changed since you last saw us.\n\n
 Thanks!\n
 The Distributed Proofreaders Team", 
-"From: charlz@lvcablemodem.com\r\nReply-To: charlz@lvcablemodem.com\r\n");
+"From: dphelp@texts01.archive.org\r\nReply-To: dphelp@texts01.archive.org\r\n");
         }
 
         $rownum++;
