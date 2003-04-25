ec9406f9503bf119f3c15ac1c1d4c59e20d04ea9
usfjoseph <>
2003-04-25 15:34:34 +0000

    Added the feature where a thread is automagically created for a team when they register

--- dp-devel/userteams.php
+++ dp-devel/userteams.php
@@ -339,6 +339,7 @@ if (isset($ctid) || isset($mkPreview) || isset($mkMake))
         VALUES('".addslashes(stripAllString($teamname))."','".addslashes(stripAllString($text_data))."','$pguser','{$userP['u_id']}','".time()."')";
       mysql_query($teamQuery);
       $jtid=mysql_insert_id($db_link);
+      createThread($teamname,$text_data,$pguser, $jtid);
     //figure out which team to overwrite
       $otid=0;
       if (!isset($teamall))
@@ -423,7 +424,40 @@ if ($tRows==20)
 echo $tre.$tbe."<br>".$tb2.$tr.$td3."<strong><a href=\"userteams.php?ctid=1&amp;tstart=$tstart\">Create New Team</a></strong>";
 echo $tre.$tbe.$menuBar.$closePage;
 
+function createThread(tname, $tinfo, $owner, $tid) {
+	//Declare variables
+	$timeposted = time();
+	$post_ip = $_SERVER['REMOTE_ADDR'];
+	$owner = 527;
+	$title = $tname;
+	$message = "Team Name: $tname<br>Created By: $towner<br>Info: $tinfo<br>Team Page: <a href='http://texts01.archive.org/dp/userteams.php?tid=$tid'>http://texts01.archive.org/dp/userteams.php?tid=$tid<br><br>Use this area to have a discussion with your fellow teammates! :-D";
+	$message = addslashes($message);
 
+	//Add Topic into phpbb_topics
+	$insert_topic = mysql_query("INSERT INTO phpbb_topics (topic_id, forum_id, topic_title, topic_poster, topic_time, topic_views, topic_replies, topic_status, topic_vote, topic_type, topic_first_post_id, topic_last_post_id, topic_moved_id) VALUES (NULL, 11, '$title', $owner, $timeposted, 0, 0, 0, 0, 0, 1, 1, 0)");
+	$topic_id = mysql_insert_id();
 
+	//Add Post into phpbb_posts
+	$insert_post = mysql_query("INSERT INTO phpbb_posts (post_id, topic_id, forum_id, poster_id, post_time, poster_ip, post_username, enable_bbcode, enable_html, enable_smilies, enable_sig, post_edit_time, post_edit_count) VALUES (NULL,$topic_id, 11, $owner, $timeposted, '$post_ip', NULL, 1, 0, 1, 1, NULL, 0)");
+	$post_id = mysql_insert_id();
 
+	//Add Post Text into phpbb_posts_text
+	$insert_post_text = mysql_query("INSERT INTO phpbb_posts_text (post_id, bbcode_uid, post_subject, post_text) VALUES ($post_id, '', '$title', '$message')");
+
+	//Update phpbb_topics with post_id
+	$update_topic = mysql_query("UPDATE phpbb_topics SET topic_first_post_id=$post_id, topic_last_post_id=$post_id WHERE topic_id=$topic_id");
+
+	//Update forum post count
+	$get_count = mysql_query("SELECT forum_posts, forum_topics FROM phpbb_forums WHERE forum_id=11");
+	while($row = mysql_fetch_array($get_count)) {
+	$forum_posts = $row['forum_posts'];
+	$forum_topics = $row['forum_topics'];
+	$forum_posts++;
+	$forum_topics++;
+	$update_count = mysql_query("UPDATE phpbb_forums SET forum_posts=$forum_posts, forum_topics=$forum_topics, forum_last_post_id=$post_id WHERE forum_id=11");
+	}
+
+	//Update teams db with topic_id so it can be deleted
+	$update_project = mysql_query("UPDATE user_teams SET topic_id=$topic_id WHERE id='$tid'");
+}
 ?>
