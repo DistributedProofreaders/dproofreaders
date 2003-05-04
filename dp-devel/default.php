c64205b7ae2e2a0c330e0aa3002a5859bd26fac4
usfjoseph <>
2003-05-04 01:10:22 +0000

    Updated to use the new theme

--- dp-devel/default.php
+++ dp-devel/default.php
@@ -1,165 +1,42 @@
 <?php
 $relPath="./pinc/";
 include($relPath.'connect.inc');
+include_once($relPath.'theme.inc');
 $db_Connection=new dbConnect();
 include($relPath.'showstartexts.inc');
+theme("Welcome", "header");
 $etext_limit = 10;
+
+//get total number of users
+$users = mysql_query("SELECT count(*) AS numusers FROM users WHERE pagescompleted >=1");
+$totalusers = mysql_result($users,0,"numusers");
+
+//get total users active in the last 24 hours
+$begin_time = time() - (60 * 60 * 24);
+$users = mysql_query("SELECT count(*) AS numusers FROM users WHERE last_login > $begin_time");
+$activeusers = mysql_result($users,0,"numusers");
 ?>
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<link rel="SHORTCUT ICON" href="/dp/favicon.ico">
-<title>Distributed Proofreaders</title>
-</head>
-<body>
-<table border=0><tr><td>
-<form action="accounts/login.php" method="post"><table border=1>
-<tr><td bgcolor=#CCCCCC><strong>Username</strong></td><td><input type="text" name="userNM" size=10 maxsize=50></td></tr>
-<tr><td bgcolor=#CCCCCC><strong>Password</strong></td><td><input type="password" name="userPW" size=10 maxsize=50></td></tr>
-<tr><td colspan=2 align=center><a href="accounts/addproofer.php">New User?</a> <input type="submit" value="Auth me"></td></tr>
-</table></form>
-</td>
-<td width=640 align="center" bgcolor=#FFFFFF><img SRC="web_graphics/title2.gif" BORDER=0 height=120 width=640 ALT="title2.gif"></td>
-</tr></table>
-<br><br>
 
-<table border=0 cellspacing=0>
-<tr><td width=1 bgcolor=#cccccc>&nbsp;</td><td bgcolor=#cccccc align="left"><font size=+1>About this site:</font></td><tr>
-<td bgcolor=#cccccc></td><td><br>This project is currently a sub-group of <a href="http://www.gutenberg.net">Project Gutenberg</a>
-and is not the official Project Gutenberg site.
-<br>As such, if you have any questions or comments regarding this site please <a href="mailto:dphelp@texts01.archive.org">E-mail DP-help</a> and not the folks at Project Gutenberg.
-<br><br></td><tr>
+<center><i><b><? echo number_format($activeusers); ?> active users out of <? echo number_format($totalusers); ?> total users in the past twenty-four hours!</b></i></center><br>
+<font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b>About This Site</b></font><br>
+Distributed Proofreaders was founded in 2000 by Charles Franks to support the digitization of Public Domain books. Originally conceived to assist <a href='http://www.gutenberg.net' target='_new'>Project Gutenberg</a> (PG), Distributed Proofreaders (DP) is now the main source of PG e-books. In 2002 DP became an official Project Gutenberg site, as such Distributed Proofreaders is supported by Project Gutenberg.  If you have any questions or comments regarding this site please e-mail: <a href='mailto:dphelp@texts01.archive.org'>dphelp@texts01.archive.org</a>.<br><br>
 
-<td bgcolor=#CCCCCC></td><td bgcolor=CCCCCC align=left><font size="+1">Site concept:</font></td><tr>
-<td bgcolor=#CCCCCC></td><td>
-<p>This site provides a web-based method of easing the proofreading work associated with the creation of Project Gutenberg e-books. By breaking the work into individual pages many proofreaders can be working on the same book at the same time. This significantly speeds up the proofreading/e-book creation process.</p>
-<p>When a proofer elects to proofread a page for a particular project, the text and image
-file are displayed on a single web page. This allows the text file to be easily
-reviewed and compared to the image file, thus assisting the proofreading of the text file.
-The edited text file is then submitted back to the site via the same web page that it was edited on.</p>
-<p>Once all pages for a particular book have been processed, a post-processor joins the pieces, properly formats them into a Project Gutenberg e-book and submits it to the Project Gutenberg archive.</p>
-</td><tr>
+<font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b>Site Concept</b></font><br>
+This site provides a web-based method of easing the proofreading work associated with the digitization of Public Domain books into Project Gutenberg e-books. By breaking the work into individual pages many proofreaders can be working on the same book at the same time. This significantly speeds up the proofreading/e-book creation process.
+<p>When a proofer elects to proofread a page of a particular book, the text and image file are displayed on a single web page. This allows the page text to be easily reviewed and compared to the image file, thus assisting the proofreading of the page text. The edited text is then submitted back to the site via the same web page that it was edited on. A second proofreader is then presented with the work of the first proofreader and the page image. Once they have verified the work of the first proofreader and corrected any additional errors the page text is again submitted back to the site.
+<p>Once all pages for a particular book have been processed, a post-processor joins the pieces, properly formats them into a Project Gutenberg e-book and submits it to the Project Gutenberg archive.<br><br>
 
+<font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b>How You Can Help</b></font><br>
+The first step to take to help us out would be to register to be a new proofreader.  You can do so by clicking <a href="/accounts/addproofer.php">here</a> or by clicking "Register" at the top of the screen.  After you register be sure to read over both the email you receive as well as FAQ Central which provides helpful resources on how to proofread.  FAQ Central can be reached <a href="<? echo $siteurl; ?>/faq/faq_central.html">here</a> or by clicking "Help" at the top of any screen.  After you have registered & read through some of the intro documents choose an interesting-looking book from our Current Projects and try proofreading a page or two.
+<p>Remember that there is no committment expected on this site. Proofread as often or as seldom as you like, and as many or as few pages as you like.  We encourage people to do "a page a day", but it's entirely up to you! We hope you will join us in our mission of "preserving the literary history of the world in a freely available form for everyone to use".<br><br>
+
+<font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b>Most Recent Projects</b></font><br>
 <?
-$result = mysql_query("SELECT * FROM news ORDER BY uid DESC LIMIT 1");
-$date_posted = date("l F jS, Y", mysql_result($result,0,"date_posted"));
-$message = mysql_result($result,0,"message");
-echo "<td bgcolor='#cccccc'></td><td bgcolor='#cccccc' align='left'><font size='+1'>Update: $date_posted</font></td></tr>";
-echo "<td bgcolor='#cccccc'></td><td>";
-echo $message;
-?>
-
-<br>You can view the financial statement for this site <a href="finance.html">here.</a>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_xclick">
-<input type="hidden" name="business" value="charlz@lvcablemodem.com">
-<input type="image" src="http://images.paypal.com/images/x-click-butcc-donate.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-</form><p></td><tr>
-
-<td bgcolor=CCCCCC><td align=left><b>Hourly Page Count:</b><br><? include("stats/hourly.txt"); ?>
-<br>Our goal this month is <b>134,008</b> pages which means <b>4,786</b> pages per day.<p></td><tr>
-
-<?
-    $numdays = 1;
-
-    //Changing this will change how many days will show at the top of the main page
-    $daterange=$numdays * 86400;
-
-    //Get dates
-    $datethru = mktime();
-    $datefrom = $datethru - $daterange;
-    $datefromformat = date("l jS",$datefrom)." of ".date("F",$datefrom);
-    $datethruformat = date("l jS",$datethru)." of ".date("F",$datethru);
-// table dates
-    $sqldatefrom = $datefrom;
-    $sqldatethru = $datethru;
-
-    //Put a few items into variables for handy usage later
-    $blankline = "\n<td width = 1 bgcolor=CCCCCC>&nbsp</td><td>";
-    $tailmessage[4]="have posted to the PG archive.\n\n<p>\n";
-    $tailmessage[3]="has finished going through the site and moves up to Silver.\n\n<p>\n";
-    $tailmessage[2]="have been released for proofing and get Bronze Stars.\n\n<p>\n";
-
-    print "<td width = 1 bgcolor=CCCCCC>&nbsp</td>
-    <td bgcolor= CCCCCC align = left><font size=\"+1\">
-    Site Activity from ".$datefromformat." to ".$datethruformat."</font></td><tr>";
-    print $blankline."<p>\n";
-
-    $result = mysql_query("SELECT nameofwork, authorsname FROM projects WHERE ".SQL_CONDITION_GOLD." and modifieddate >= '$sqldatefrom' and modifieddate <= '$sqldatethru' ORDER BY modifieddate DESC");
-
-    $rownum = 0;
-    $numgold = mysql_num_rows($result);
-    while ($rownum < $numgold) {
-        $title = mysql_result($result, $rownum, "nameofwork");
-        $author = mysql_result($result, $rownum, "authorsname");
-
-        print "<i>".$title."</i>, ".$author."<br>\n";
-        $rownum++;
-    } //End while
-
-    if ($rownum >0) {
-        print $tailmessage[4];
-    }
-
-    $result = mysql_query("SELECT nameofwork, authorsname FROM projects WHERE ".SQL_CONDITION_SILVER." AND modifieddate >= '$sqldatefrom' and modifieddate <= '$sqldatethru' ORDER BY modifieddate desc");
-
-    $rownum = 0;
-    $numsilver = mysql_num_rows($result);
-    while ($rownum < $numsilver) {
-        $title = mysql_result($result, $rownum, "nameofwork");
-        $author = mysql_result($result, $rownum, "authorsname");
-
-        print "<i>".$title."</i>, ".$author."<br>\n";
-        $rownum++;
-    } //End while
-
-    if ($rownum >0) {
-        print $tailmessage[3];
-    }
-
-    $result = mysql_query("SELECT nameofwork, authorsname FROM projects WHERE ".SQL_CONDITION_BRONZE." AND (modifieddate >= '$sqldatefrom' AND modifieddate <= '$sqldatethru') ORDER BY modifieddate desc");
-
-    $rownum = 0;
-    $numbronze = mysql_num_rows($result);
-    while ($rownum < $numbronze) {
-        $title = mysql_result($result, $rownum, "nameofwork");
-        $author = mysql_result($result, $rownum, "authorsname");
-
-        print "<i>".$title."</i>, ".$author."<br>\n";
-        $rownum++;
-    } //End while
-
-    if ($rownum >0) {
-        print $tailmessage[2];
-    }
-?>
-</tr><tr>
-
-<td width = 1 bgcolor=CCCCCC>&nbsp</td><td bgcolor=CCCCCC align=left><font size = "+1">Get Started!</font><td><tr>
-<td width = 1 bgcolor=CCCCCC>&nbsp</td><td>
-<br><u><font size = "+1">Proofreaders:</font></u>
-<br><font size="+1"><a href="accounts/addproofer.php">Create</a></font> an account to get started! 
-You can also read the <a href="faq/faq_central.html">Frequently Asked Questions (FAQs)</a> to get an
- idea of what we do here at this site.
-<p><u><font size=+1>Already have an account?</font></u>
-<br><font size="+1"><a href="accounts/signin.php">Sign In</a></font> and start proofing!!
-<br><br></td><tr>
-
-<td bgcolor=CCCCCC></td><td bgcolor=CCCCCC align=left><font size="+1">Daily Statistics</font></td><tr>
-<td bgcolor=CCCCCC><td align=center><a href="stats/stats.png"><img SRC="stats/stats.png" width=300 height=250 ALT="stats.png"></a>&nbsp;<a href="stats/target.png"><img SRC="stats/target.png" width=300 height=250 ALT="target.png"></a><p></td><tr>
-<td bgcolor=CCCCCC><td align=center>Note: if you can't see the stats image please first click the Refresh/Reload button on your browser and if the image still does not appear please <a href="mailto:dphelp@texts01.archive.org">E-mail DP-help</a> and tell us the make and version of your browser as well as what operating system you are using.</td><tr>
-<td bgcolor=CCCCCC><td align=center><a href="previous_stats.html">Previous Months Statistics</a></td><tr>
-
-<td bgcolor="#cccccc">&nbsp;</td>
-<td bgcolor="#cccccc"><font size="+1">How are we doing so far?</font></td>
-</table>
-
-<? 
 //Gold E-texts
 showstartexts($etext_limit,'gold');
 //Silver E-texts
 showstartexts($etext_limit,'silver');
 //Bronze E-texts
 showstartexts($etext_limit,'bronze');
+theme("", "footer");
 ?>
-
-</body>
-</html>
