eda92cb548bd1f0aa69bb95866e14cab3d2ab5ab
jmdyck <>
2003-05-05 07:29:16 +0000

    Add comment for $siteurl.

--- dp-devel/pinc/site_vars.php
+++ dp-devel/pinc/site_vars.php
@@ -6,5 +6,8 @@ $admin_email='';
 $admin_name='';
 
 $siteurl='';
+// This is the HTTP URL that resolves to the directory where the code was
+// installed (i.e., the directory that corresponds to 'dp-devel' in the CVS
+// repository -- it should contain directories such as 'pinc' and 'tools').
 
 ?>
