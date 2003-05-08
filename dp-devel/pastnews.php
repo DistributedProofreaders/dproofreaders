ba7a9021898d5f6ccbd1587c583bb6afd56a5f7a
usfjoseph <>
2003-05-08 23:34:25 +0000

    Updated to include the use of the new themes

--- dp-devel/pastnews.php
+++ dp-devel/pastnews.php
@@ -2,11 +2,17 @@
 $relPath="./pinc/";
 include($relPath.'connect.inc');
 $db_Connection=new dbConnect();
+include_once($relPath.'theme.inc');
+theme("Archived Site Updates", "header");
+
+echo "<br>";
 
 $result = mysql_query("SELECT * FROM news ORDER BY uid DESC");
 while($row = mysql_fetch_array($result)) {
 $date_posted = date("l, F jS, Y",$row['date_posted']);
 echo "<a name='".$row['uid']."'><b>$date_posted</b><br>".$row['message']."<br><hr align='center' width='75%'><br>";
 }
+
+theme("", "footer");
 
 ?>
