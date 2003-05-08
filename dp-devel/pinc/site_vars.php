1717a9c346cc6a54318614d4923baf0b73961be0
jmdyck <>
2003-05-08 23:51:47 +0000

    Define $site_dir.

--- dp-devel/pinc/site_vars.php
+++ dp-devel/pinc/site_vars.php
@@ -2,10 +2,13 @@
 // Variables (constants?) whose values are specific
 // to the local installation of the DP code.
 
-$siteurl='';
-// This is the HTTP URL that resolves to the directory where the code was
+$site_dir = '<<SITE_DIR>>';
+// This is the location in the local file system where the code was
 // installed (i.e., the directory that corresponds to 'dp-devel' in the CVS
 // repository -- it should contain directories such as 'pinc' and 'tools').
+
+$siteurl='';
+// This is the HTTP URL that resolves to the directory described above.
 
 $admin_email='';
 $admin_name='';
