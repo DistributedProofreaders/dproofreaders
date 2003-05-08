f22f3a0f81b16d0f111d75c40cca42ad9cb35ad8
jmdyck <>
2003-05-08 23:48:25 +0000

    minor code movement

--- dp-devel/pinc/site_vars.php
+++ dp-devel/pinc/site_vars.php
@@ -2,12 +2,12 @@
 // Variables (constants?) whose values are specific
 // to the local installation of the DP code.
 
-$admin_email='';
-$admin_name='';
-
 $siteurl='';
 // This is the HTTP URL that resolves to the directory where the code was
 // installed (i.e., the directory that corresponds to 'dp-devel' in the CVS
 // repository -- it should contain directories such as 'pinc' and 'tools').
+
+$admin_email='';
+$admin_name='';
 
 ?>
