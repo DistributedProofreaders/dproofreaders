db02cd43b4966e3bb8ee1ab7071b6c547624e8c8
jmdyck <>
2003-05-13 05:29:03 +0000

    Eliminate most of the tracing: it produces too much output.

--- dp-devel/stats/update_daily_page_counts.php
+++ dp-devel/stats/update_daily_page_counts.php
@@ -14,24 +14,28 @@ include($relPath.'connect.inc');
 new dbConnect();
 
 $EOL = "\n";
-$trace = TRUE;
+$testing_this_script=TRUE;
 
-if ($trace)
+if ($testing_this_script)
 {
     echo "<pre>", $EOL;
 }
 
-// Figure out the last day (X) for which a count was taken.
-$res = mysql_query( 'SELECT MAX(date) FROM pagestats WHERE pages != 0' )
-    or die(mysql_error());
-list($X_date) = mysql_fetch_array($res);
-if ($trace)
+if ($testing_this_script)
 {
-    echo 'Last counted date was ', $X_date, $EOL;
+    $X_date = '2003-01-01';
 }
-
-// testing:
-$X_date = '2003-01-01'; $testing_this_script=TRUE;
+else
+{
+    // Figure out the last day (X) for which a count was taken.
+    $res = mysql_query( 'SELECT MAX(date) FROM pagestats WHERE pages != 0' )
+        or die(mysql_error());
+    list($X_date) = mysql_fetch_array($res);
+    if ($testing_this_script)
+    {
+        echo 'Last counted date was ', $X_date, $EOL;
+    }
+}
 
 list($X_year,$X_month,$X_day) = explode('-',$X_date);
 
@@ -43,19 +47,10 @@ for ( $d = 1; ; $d++ )
     // Consider the day (Y) that is $d days after $X_date.
     $Y_start_ts = mktime(0,0,0,$X_month, $X_day+$d, $X_year);
     $Y_date = date('Y-m-d', $Y_start_ts );
-    if ($trace)
-    {
-        echo $EOL;
-        echo 'Considering ', $Y_date, ':', $EOL;
-    }
 
     // If Y is today (or later), we're done.
     if ($Y_start_ts >= $today_start_ts)
     {
-        if ( $trace )
-        {
-            echo "That's today (or later); we're done.", $EOL;
-        }
         break;
     }
 
@@ -63,13 +58,13 @@ for ( $d = 1; ; $d++ )
 
     $total_n_pages_proofed = get_n_pages_proofed( $Y_start_ts, $Y_end_ts );
 
-    if ($trace)
+    if ($testing_this_script)
     {
         echo "update pagestats: $Y_date $total_n_pages_proofed", $EOL;
     }
 }
 
-if ($trace)
+if ($testing_this_script)
 {
     echo "</pre>", $EOL;
 }
@@ -77,16 +72,6 @@ if ($trace)
 function get_n_pages_proofed( $start_ts, $end_ts )
 // Return the total number of pages proofed between the two timestamps.
 {
-    global $trace, $EOL;
-
-    if ($trace)
-    {
-        echo '    Looking for all pages proofed between ', $EOL;
-        echo '    ', date('r',$start_ts), ' and', $EOL;
-        echo '    ', date('r',$end_ts), $EOL;
-        echo '    ....', $EOL;
-    }
-
     $total_n_pages_proofed = 0;
 
     // Only consider projects that have been modified after Jan 1, 2003
@@ -100,11 +85,6 @@ function get_n_pages_proofed( $start_ts, $end_ts )
     while( $project_row = mysql_fetch_array($res) )
     {
         list($projectid) = $project_row;
-
-        if ($trace)
-        {
-            echo "    project $projectid: ";
-        }
 
         $res2 = mysql_query("
             SELECT COUNT(*) FROM $projectid
@@ -120,23 +100,14 @@ function get_n_pages_proofed( $start_ts, $end_ts )
 
         if (!$res2)
         {
-            echo mysql_error(), $EOL;
+            // Probably the project's page-table does not exist.
+            // Not sure why.
             continue;
         }
 
         list($n_pages_proofed) = mysql_fetch_array($res2);
 
-        if ($trace)
-        {
-            echo $n_pages_proofed, $EOL;
-        }
-
         $total_n_pages_proofed += $n_pages_proofed;
-    }
-
-    if ($trace)
-    {
-        echo "    total: $total_n_pages_proofed", $EOL;
     }
 
     return $total_n_pages_proofed;
