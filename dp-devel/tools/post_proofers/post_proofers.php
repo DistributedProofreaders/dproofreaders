3e465db952910fde53e9d0049a0b138e40b63812
usfjoseph <>
2003-05-13 14:18:08 +0000

    removed some tables to stay consistent with new design -- also broke into 4 smaller tables due to user requests

--- dp-devel/tools/post_proofers/post_proofers.php
+++ dp-devel/tools/post_proofers/post_proofers.php
@@ -7,93 +7,38 @@ include($relPath.'bookpages.inc');
 include($relPath.'showavailablepost.inc');
 theme("Post Processing", "header");
 
-echo "<br>";
-?>
+echo "<br><p>This is the post processing section. The books listed below have already gone through two rounds of proofreading on this site and they now need to be massaged into a final e-text. Once you have checked out and downloaded a book it will remain checked out to you until you check it back in. <b>The Completed Post-Processing feature is currently disabled until it gets fully developed. E-mail the project manager with the completed project for now.</b>";
+echo "<p>Here is a <a href='$siteurl/faq/post_proof.html'><b>Post Proof FAQ</b></a> that details all of the steps that we normally take to post proof an etext. There is a <a href='$forums_url/viewforum.php?f=3'>forum</a> to post all your questions. If no books are listed that means no work is currently available, please check back later!";
+echo "<p><b>First Time here?</b>  Juliet Sutherland is our Post Processing Coordinator. Please read the FAQ, select an easy work to get started on (usually fiction with a low page count is a good starter book) and write <a href='mailto:juliet.sutherland@verizon.net'>Juliet</a> with any questions/comments.";
+echo "<p><hr width=75% align='center'><br>";
 
+//List the "My Checked Out Post-Processing Books"
+echo "<center><b>My Checked Out Post-Processing Books:</b></center>";
+echo "<table align='center' border=1 width=630 cellpadding=0 cellspacing=0 style='border-collapse: collapse;' bordercolor='#111111'>";
+$rows = mysql_query("SELECT projectid, nameofwork, authorsname, username, scannercredit, language FROM projects WHERE checkedoutby = '$pguser' AND state='".PROJ_POST_CHECKED_OUT."'");
+showavailablepost($rows,PROJ_POST_CHECKED_OUT);
+echo "</table><br>";
 
-<table border="1" width="630" cellpadding="0" cellspacing="0" style="border-collapse: collapse;" bordercolor="#111111">
-<tr><td width="126" align="center" bgcolor="#cccccc"><a href="<? echo $forums_url ?>/index.php">Forums</a></td>
-<td width="126" align="center" bgcolor="#cccccc"><?
-    $result = mysql_query("SELECT manager FROM users WHERE username = '$pguser'");
-    $manager = mysql_result($result, 0, "manager");
+//List the "My Checked Out Verifying Post-Processing Books"
+echo "<center><b>My Checked Out Verifying Post-Processing Books:</b></center>";
+echo "<table align='center' border=1 width=630 cellpadding=0 cellspacing=0 style='border-collapse: collapse;' bordercolor='#111111'>";
+$rows = mysql_query("SELECT projectid, nameofwork, authorsname, username, scannercredit, language, postproofer FROM projects WHERE checkedoutby = '$pguser' AND state='".PROJ_POST_VERIFYING."'");
+showavailableverify($rows,PROJ_POST_VERIFYING);
+echo "</table><br>";
 
-    if ($manager == "yes") {
-        echo "<a href = \"../project_manager/projectmgr.php\">Manage Books</a>";
-    } else echo "&nbsp;";
+//List the "Available for Post Processing Books"
+echo "<center><b>Available for Post-Processing Books:</b></center>";
+echo "<table align='center' border=1 width=630 cellpadding=0 cellspacing=0 style='border-collapse: collapse;' bordercolor='#111111'>";
+$rows = mysql_query("SELECT username, projectid, nameofwork, authorsname, language, genre FROM projects WHERE state='".PROJ_POST_AVAILABLE."'");
+showavailablepost($rows,PROJ_POST_AVAILABLE);
+echo "</table><br>";
 
-?></td>
-<td width="126" align="center" bgcolor="#cccccc">&nbsp;</td>
-<td width=126 bgcolor =CCCCCC align=center><a href ="../proofers/proof_per.php">Proofread Books</a></td>
-<td width=126 bgcolor ="CCCCCC" align=center><a href ="../logout.php">Logout</a></td></tr></table>
-<P>
-<table border="1" width="630" cellpadding="0" cellspacing="0" style="border-collapse: collapse;" bordercolor="#111111">
-<tr><td colspan="7" bgcolor="CCCCCC">
+//List the "Available for Post-Processing Verification & Posting Books"
+echo "<center><b>Available for Post-Processing Verification & Posting Books:</b></center>";
+echo "<table align='center' border=1 width=630 cellpadding=0 cellspacing=0 style='border-collapse: collapse;' bordercolor='#111111'>";
+$rows = mysql_query("SELECT username, projectid, nameofwork, authorsname, language, genre, postproofer FROM projects WHERE state='".PROJ_POST_VERIFY."'");
+showavailableverify($rows,PROJ_POST_VERIFY);
+echo "</table><br>";
 
-<P>This is the post processing section. The books listed below have already gone through two rounds of proofreading on this site and they now need to be massaged into a final e-text. Once you have checked out and downloaded a book it will remain checked out to you until you check it back in. <b>The Completed Post-Processing feature is currently disabled until it gets fully developed. E-mail the project manager with the completed project for now.</b>
-
-<p>Here is a <a href="<? echo $siteurl ?>/faq/post_proof.html"><b>Post Proof FAQ</b></a> that details all of the 
-steps that we normally take to post proof an etext. There is a <a href="<? echo $forums_url ?>/viewforum.php?f=3">forum</a> to post all your questions. If no books are listed that means no work is currently available, please check back later!
-<p>
-<b>First Time here?</b>  Juliet Sutherland is our Post Processing Coordinator. Please read the FAQ, select an easy work to get 
-started on (usually fiction with a low page count is a good starter book) and write <a href = 
-"mailto:juliet.sutherland@verizon.net"> Juliet</a> with any questions/comments.<p></td></tr>
-
-
-<tr><td colspan="7" align="center" bgcolor="999999"><B>My Checked Out Post-Processing Books</B></td></tr>
-<?
-   //list all first post processing books the user has checked out
-$rows = mysql_query("SELECT projectid, nameofwork, authorsname, username, scannercredit, language 
-                     FROM projects 
-                     WHERE checkedoutby = '$pguser' AND state='".PROJ_POST_CHECKED_OUT."'");
-    showavailablepost($rows,PROJ_POST_CHECKED_OUT);
-?>
-
-
-<tr><td colspan="7" align="center" bgcolor="999999"><B>My Checked Out Verifying Post-Processing Books</B></td></tr>
-<?
-   //list all verifying post processing books the user has checked out
-$rows = mysql_query("SELECT projectid, nameofwork, authorsname, username, scannercredit, language, postproofer
-                     FROM projects
-                     WHERE checkedoutby = '$pguser' AND state='".PROJ_POST_VERIFYING."'");
-    showavailableverify($rows,PROJ_POST_VERIFYING);
-?>
-
-
-<tr><td colspan="7" align="center" bgcolor="999999"><B>Available Post-Processing Books</B></td></tr>
-<?
-    // list projects which are available for first post proofing
-    $rows = mysql_query("SELECT username, projectid, nameofwork, authorsname, language, genre 
-                         FROM projects 
-                         WHERE state='".PROJ_POST_AVAILABLE."'");
-    showavailablepost($rows,PROJ_POST_AVAILABLE);
-?>
-
-
-<tr><td colspan="7" align="center" bgcolor="999999"><B>Books Available for Post-Processing Verification and Posting</B></td></tr>
-<?
-    // list projects which are available for post proofing verification
-    $rows = mysql_query("SELECT username, projectid, nameofwork, authorsname, language, genre, postproofer
-                         FROM projects 
-                         WHERE state='".PROJ_POST_VERIFY."'");
-    showavailableverify($rows,PROJ_POST_VERIFY);
-?>
-
-
-<tr><td bgcolor="#999999" colspan="7">&nbsp;</td></tr></table>
-<P><table border=1 cellpadding=0 cellspacing=0 style="border-collapse: collapse" bordercolor=#111111 width=630><tr>
-<td width=126 bgcolor ="CCCCCC" align=center><a href ="<? echo $forums_url ?>/index.php">Forums</a></td>
-<td width=126 bgcolor =CCCCCC align=center><?
-
-    if ($manager == "yes") {
-        echo "<a href = \"../project_manager/projectmgr.php\">Manage Books</a>";
-    } else echo "&nbsp;";
-
-?></td>
-<td width=126 bgcolor =CCCCCC align=center>&nbsp;</td>
-<td width=126 bgcolor =CCCCCC align=center><a href ="../proofers/proof_per.php">Proofread Books</a></td>
-<td width=126 bgcolor ="CCCCCC" align=center><a href ="../logout.php">Logout</a></td></tr>
-</table>
-
-<?
 theme("", "footer");
 ?>
