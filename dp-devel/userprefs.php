b0a11ded3c1468b023988f6b04c5091cd0c97a99
usfjoseph <>
2003-05-17 17:13:40 +0000

    Added spacing between the ? marks for better clarity Updated to use new them & removed grey background Add info on what the ? mark was to be used for

--- dp-devel/userprefs.php
+++ dp-devel/userprefs.php
@@ -4,6 +4,7 @@ include($relPath.'v_site.inc');
 include($relPath.'dp_main.inc');
 include($relPath.'html_main.inc');
 include($relPath.'doctype.inc');
+include_once($relPath.'theme.inc');
 
 $uid = $userP['user_id'];
 
@@ -67,11 +68,11 @@ else {echo "$docType<html><head><META HTTP-EQUIV=\"refresh\" CONTENT=\"0 ;URL=to
 exit;}
 
 if (@$_POST["insertdb"] == "") {
-
+theme("Personal Preferences", "header");
+echo "<br><center>";
 $htmlC->startHeader("User Preferences");
 $popHelpDir='faq/pophelp/prefs/set_';
 include($relPath.'js_newpophelp.inc');
-$htmlC->startBody(0,0,0,0);
 $tb=$htmlC->startTable(0,0,0,1);
 $tr=$htmlC->startTR(0,0,1);
 $td1=$htmlC->startTD(2,0,6,0,"center",0,0,1);
@@ -92,13 +93,13 @@ echo "<form action='userprefs.php' method='post'>";
 echo $tb;
 echo $tr.$td1;
 
-echo "<font size=\"+2\"><b>Preferences Page for $pguser</b></font>";
+echo "<font size=\"+2\"><b>Preferences Page for $pguser</font></b><br><i>(click the ? for help on that specific preference)</i>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Current Profile:</strong>";
 echo $tde.$td3;
 echo "<input type='text' name='profilename' value='{$userP['profilename']}'>";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('profilename');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('profilename');\">?</a>&nbsp;</b>";
 echo $tde.$td1a;
 // show all profiles
 echo "<select name='c_profile' ID='c_profile'>";
@@ -112,94 +113,94 @@ echo "<select name='c_profile' ID='c_profile'>";
   }
 echo "</select>";
 echo " <input type=\"submit\" value=\"Switch Profiles\" name=\"swProfile\">&nbsp;";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('switch');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('switch');\">?</a>&nbsp;</b>";
 echo $tre.$tr.$td2;
 echo "<strong>Name:</strong>";
 echo $tde.$td3;
 echo "<input type='text' name='real_name' value='$real_name'>";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('name');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('name');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Screen Resolution:</strong>";
 echo $tde.$td3;
 $array = implode('|', $i_r);
 dropdown_select('i_res', $userP['i_res'], $array);
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('screenres');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('screenres');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Email:</strong>";
 echo $tde.$td3;
 echo "<input type='text' name='email' value='$email'>";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('email');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('email');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Interface Type:</strong>";
 echo $tde.$td3;
 radio_select('i_type', $userP['i_type'], 0, 'Standard');
 radio_select('i_type', $userP['i_type'], 1, 'Enhanced');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('facetype');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('facetype');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Email Updates:</strong>";
 echo $tde.$td3;
 radio_select('email_updates', $userP['email_updates'], '1', 'Yes');
 radio_select('email_updates', $userP['email_updates'], '0', 'No');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('updates');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('updates');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Interface Layout:</strong>";
 echo $tde.$td3;
 radio_select('i_layout', $userP['i_layout'], 1, '<img src="tools/proofers/gfx/bt4.png" width="26" alt="Vertical">');
 radio_select('i_layout', $userP['i_layout'], 0, '<img src="tools/proofers/gfx/bt5.png" width="26" alt="Horizontal">');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('layout');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('layout');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Show Projects From:</strong>";
 echo $tde.$td3;
 $array = implode('|', $p_l);
 dropdown_select('u_plist', $userP['u_plist'], $array);
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('showrounds');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('showrounds');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Launch in New Window:</strong>";
 echo $tde.$td3;
 radio_select('i_newwin', $userP['i_newwin'], 1, 'Yes');
 radio_select('i_newwin', $userP['i_newwin'], 0, 'No');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('newwindow');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('newwindow');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Language:</strong>";
 echo $tde.$td3;
 $array = implode('|', $u_l);
 dropdown_select('u_lang', $userP['u_lang'], $array);
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('lang');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('lang');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Show Toolbar:</strong>";
 echo $tde.$td3;
 radio_select('i_toolbar', $userP['i_toolbar'], 1, 'Yes');
 radio_select('i_toolbar', $userP['i_toolbar'], 0, 'No');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('toolbar');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('toolbar');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Password:</strong>";
 echo $tde.$td3;
 echo "<a href='$reset_password_url'>Reset Password</a>";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('password');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('password');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Show Statusbar:</strong>";
 echo $tde.$td3;
 radio_select('i_statusbar', $userP['i_statusbar'], 1, 'Yes');
 radio_select('i_statusbar', $userP['i_statusbar'], 0, 'No');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('statusbar');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('statusbar');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Show Rank Neighbors:</strong>";
 echo $tde.$td3;
 $array = implode('|', $u_n);
 dropdown_select('u_neigh', $userP['u_neigh'], $array);
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('neighbors');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('neighbors');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Show Top 10:</strong>";
 echo $tde.$td3;
 radio_select('u_top10', $userP['u_top10'], 1, 'Yes');
 radio_select('u_top10', $userP['u_top10'], 0, 'No');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('topten');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('topten');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Theme:</strong>";
@@ -212,116 +213,116 @@ while ($row = mysql_fetch_array($result)) {
 	echo ">".$row['name']."</option>";
 }
 echo "</select>";
-echo $tde.$td3a."<b><a href=\"Javascript:newHelpWin('theme');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('theme');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Stats Bar Alignment:</strong>";
 echo $tde.$td3;
 radio_select('u_align', $userP['u_align'], 1, 'Left');
 radio_select('u_align', $userP['u_align'], 0, 'Right');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('align');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('align');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td4;
 echo "<img src='tools/proofers/gfx/bt4.png'><b>Vertical Interface Preferences</b>";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('vertprefs');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('vertprefs');\">?</a>&nbsp;</b>";
 echo $tde.$td4;
 echo "<img src='tools/proofers/gfx/bt5.png'><b>Horizontal Interface Preferences</b>";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('horzprefs');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('horzprefs');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Font Face:</strong>";
 echo $tde.$td3;
 $array = implode('|', $f_f);
 dropdown_select('v_fntf', $userP['v_fntf'], $array);
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('v_fontface');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('v_fontface');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Font Face:</strong>";
 echo $tde.$td3;
 $array = implode('|', $f_f);
 dropdown_select('h_fntf', $userP['h_fntf'], $array);
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('h_fontface');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('h_fontface');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Font Size:</strong>";
 echo $tde.$td3;
 $array = implode('|', $f_s);
 dropdown_select('v_fnts', $userP['v_fnts'], $array);
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('v_fontsize');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('v_fontsize');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Font Size:</strong>";
 echo $tde.$td3;
 $array = implode('|', $f_s);
 dropdown_select('h_fnts', $userP['h_fnts'], $array);
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('h_fontsize');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('h_fontsize');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Image Zoom:</strong>";
 echo $tde.$td3;
 echo "<input type=\"text\" name=\"v_zoom\" value=\"{$userP['v_zoom']}\" size=\"3\">% of 1000 pixels";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('v_zoom');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('v_zoom');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Image Zoom:</strong>";
 echo $tde.$td3;
 echo "<input type=\"text\" name=\"h_zoom\" value=\"{$userP['h_zoom']}\" size=\"3\">% of 1000 pixels";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('h_zoom');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('h_zoom');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Text Frame Size:</strong>";
 echo $tde.$td3;
 echo "<input type=\"text\" name=\"v_tframe\" value=\"{$userP['v_tframe']}\" size=\"3\">% of browser width";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('v_textsize');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('v_textsize');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Text Frame Size:</strong>";
 echo $tde.$td3;
 echo "<input type=\"text\" name=\"h_tframe\" value=\"{$userP['h_tframe']}\" size=\"3\">% of browser height";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('h_textsize');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('h_textsize');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Scroll Text Frame:</strong>";
 echo $tde.$td3;
 radio_select('v_tscroll', $userP['v_tscroll'], 1, 'Yes');
 radio_select('v_tscroll', $userP['v_tscroll'], 0, 'No');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('v_scroll');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('v_scroll');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Scroll Text Frame:</strong>";
 echo $tde.$td3;
 radio_select('h_tscroll', $userP['h_tscroll'], 1, 'Yes');
 radio_select('h_tscroll', $userP['h_tscroll'], 0, 'No');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('h_scroll');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('h_scroll');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Number of Text Lines:</strong>";
 echo $tde.$td3;
 echo "<input type=\"text\" name=\"v_tlines\" value=\"{$userP['v_tlines']}\" size=\"3\">";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('v_textlines');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('v_textlines');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Number of Text Lines:</strong>";
 echo $tde.$td3;
 echo "<input type=\"text\" name=\"h_tlines\" value=\"{$userP['h_tlines']}\" size=\"3\">";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('h_textlines');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('h_textlines');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Length of Text Lines:</strong>";
 echo $tde.$td3;
 echo "<input type=\"text\" name=\"v_tchars\" value=\"{$userP['v_tchars']}\" size=\"3\"> characters";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('v_textlength');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('v_textlength');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Length of Text Lines:</strong>";
 echo $tde.$td3;
 echo "<input type=\"text\" name=\"h_tchars\" value=\"{$userP['h_tchars']}\" size=\"3\"> characters";
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('h_textlength');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('h_textlength');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td2;
 echo "<strong>Wrap Text:</strong>";
 echo $tde.$td3;
 radio_select('v_twrap', $userP['v_twrap'], 1, 'Yes');
 radio_select('v_twrap', $userP['v_twrap'], 0, 'No');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('v_wrap');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('v_wrap');\">?</a>&nbsp;</b>";
 echo $tde.$td2;
 echo "<strong>Wrap Text:</strong>";
 echo $tde.$td3;
 radio_select('h_twrap', $userP['h_twrap'], 1, 'Yes');
 radio_select('h_twrap', $userP['h_twrap'], 0, 'No');
-echo $tde.$td3a."<b><a href=\"JavaScript:newHelpWin('h_wrap');\">?</a></b>";
+echo $tde.$td3a."<b>&nbsp;<a href=\"JavaScript:newHelpWin('h_wrap');\">?</a>&nbsp;</b>";
 
 echo $tre.$tr.$td5;
 if (isset($project) && isset($proofstate))
@@ -335,7 +336,9 @@ echo "<center><input type='submit' value='Save Preferences' name='change'> &nbsp
 if ($pf_num < 10)
   {echo "<input type='submit' value='Save as New Profile' name='mkProfile'> &nbsp;";}
 echo "<input type='submit' value='Quit' name='quitnc'></center>";
-echo $tre.$htmlC->closeTable(1)."</form>".$htmlC->closeBody(1);
+echo $tre.$htmlC->closeTable(1)."</form>";
+echo "<br></center>";
+theme("", "footer");
 } else {
 $user_id = $_POST['user_id'];
 $real_name = $_POST['real_name'];
