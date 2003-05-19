5b157de3471ed3a755348109a9c3f4f17b18d778
jmdyck <>
2003-05-19 01:49:37 +0000

    Use metarefresh.

--- dp-devel/accounts/login.php
+++ dp-devel/accounts/login.php
@@ -3,6 +3,7 @@ $relPath="./../pinc/";
 include($relPath.'v_site.inc');
 include($relPath.'connect.inc');
 include($relPath.'user.inc');
+include($relPath.'metarefresh.inc');
 $userC=new db_udb();
 $noLogin="Username or password is incorrect.<BR>If you feel you have received this message in error, please try to <A HREF=\"$reset_password_url\">reset</A> your password. If this fails, contact the <A HREF=\"mailto:$site_manager_email_addr\">site manager</A>.<BR><A HREF=\"signin.php\">Back</A> to the sign in page.";
 $htmlStart="<HTML><HEAD><TITLE>Login</TITLE>";
@@ -28,14 +29,18 @@ $uC=$userC->checkLogin($userNM,$userPW);
           if ($uP)
           {
           // send them to the correct page
-          $htmlStart.="<META HTTP-EQUIV=\"refresh\" CONTENT=\"1 ;URL=../tools/";
-          $htmlMid=".php\">".$htmlMid;
 // isn't this the same as the manager field in users?
 //        $result = mysql_query("SELECT value FROM usersettings WHERE username = '$username' AND setting = 'manager'");
 // needs to be included in user.inc, if not....
           if ($userC->manager=='yes')
-          {echo $htmlStart."project_manager/projectmgr".$htmlMid.$htmlEnd;}
-          else {echo $htmlStart."proofers/proof_per".$htmlMid.$htmlEnd;}
+          {
+              $url = "../tools/project_manager/projectmgr.php";
+          }
+          else
+          {
+              $url = "../tools/proofers/proof_per.php";
+          }
+          metarefresh(1,$url,"Login","");
           }
           else die ($noLogin);
      }
