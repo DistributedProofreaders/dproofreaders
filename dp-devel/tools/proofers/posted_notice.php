9991f47370b5b5e9cee874e06c7ddc1b00beaedf
jmdyck <>
2003-05-19 12:09:06 +0000

    Use metarefresh.

--- dp-devel/tools/proofers/posted_notice.php
+++ dp-devel/tools/proofers/posted_notice.php
@@ -1,13 +1,17 @@
 <?
 $relPath="./../../pinc/";
+include($relPath.'metarefresh.inc');
 include($relPath.'dp_main.inc');
+
 $project = $_GET['project'];
 $proofstate = $_GET['proofstate'];
+
 $result = mysql_query("SELECT * FROM usersettings WHERE username = '".$pguser."' AND setting = 'posted_notice' AND value = '".$project."'");
 if (mysql_num_rows($result) == 0) {
     $insert = mysql_query("INSERT INTO usersettings (username, setting, value) VALUES ('".$pguser."', 'posted_notice', '".$project."')");
 } else {
     $del = mysql_query("DELETE FROM usersettings WHERE username = '".$pguser."' AND setting = 'posted_notice' AND value = '".$project."'");
 }
-echo "<html><head><META HTTP-EQUIV=\"refresh\" CONTENT=\"0; URL=projects.php?project=$project&proofstate=$proofstate\"></head><body></body></html>";
+
+metarefresh(0, "projects.php?project=$project&proofstate=$proofstate", "Posted Notice", "");
 ?>
