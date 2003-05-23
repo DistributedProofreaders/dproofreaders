f2ed194b7489d0239eca25f3ab3e9bd90a085862
jmdyck <>
2003-05-23 06:53:07 +0000

    Use *_forum_url variable(s) from v_site.inc

--- dp-devel/tools/post_proofers/post_proofers.php
+++ dp-devel/tools/post_proofers/post_proofers.php
@@ -8,7 +8,7 @@ include($relPath.'showavailablepost.inc');
 theme("Post Processing", "header");
 
 echo "<br><p>This is the post processing section. The books listed below have already gone through two rounds of proofreading on this site and they now need to be massaged into a final e-text. Once you have checked out and downloaded a book it will remain checked out to you until you check it back in. <b>The Completed Post-Processing feature is currently disabled until it gets fully developed. E-mail the project manager with the completed project for now.</b>";
-echo "<p>Here is a <a href='$siteurl/faq/post_proof.php'><b>Post Proof FAQ</b></a> that details all of the steps that we normally take to post proof an etext. There is a <a href='$forums_url/viewforum.php?f=3'>forum</a> to post all your questions. If no books are listed that means no work is currently available, please check back later!";
+echo "<p>Here is a <a href='$siteurl/faq/post_proof.php'><b>Post Proof FAQ</b></a> that details all of the steps that we normally take to post proof an etext. There is a <a href='$post_processing_forum_url'>forum</a> to post all your questions. If no books are listed that means no work is currently available, please check back later!";
 echo "<p><b>First Time here?</b>  Juliet Sutherland is our Post Processing Coordinator. Please read the FAQ, select an easy work to get started on (usually fiction with a low page count is a good starter book) and write <a href='mailto:juliet.sutherland@verizon.net'>Juliet</a> with any questions/comments.";
 echo "<p><hr width=75% align='center'><br>";
 
