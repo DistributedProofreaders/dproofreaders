c9fe53471df5252e36668738c94da9b6ada6bb07
jmdyck <>
2003-06-01 03:37:08 +0000

    Use check_username and abort_login.

--- dp-devel/accounts/login.php
+++ dp-devel/accounts/login.php
@@ -1,59 +1,75 @@
 <?PHP
 $relPath="./../pinc/";
 include($relPath.'v_site.inc');
+include($relPath.'username.inc');
 include($relPath.'connect.inc');
 include($relPath.'user.inc');
 include($relPath.'metarefresh.inc');
 include($relPath.'theme.inc');
-$userC=new db_udb();
-$noLogin="Username or password is incorrect.<BR>If you feel you have received this message in error, please try to <A HREF=\"$reset_password_url\">reset</A> your password. If this fails, contact the <A HREF=\"mailto:$site_manager_email_addr\">site manager</A>.<BR><A HREF=\"signin.php\">Back</A> to the sign in page.";
-$htmlStart="<HTML><HEAD><TITLE>Login</TITLE>";
-$htmlMid="</HEAD><BODY>";
-$htmlEnd="</BODY></HTML>";
-$noLogin=$htmlStart.$htmlMid.$noLogin.$htmlEnd;
-extract($_POST);
 
-if (ereg("[^-a-zA-Z0-9@._ ]", $userNM) || strlen($userNM) > 25) {
-	theme("Invalid Username", "header");
-	echo "<b><center>Your username has invalid characters in it or is too long.  Please hit back & try again.<br>";
-	echo "If you believe this is in error please contact <a href='mailto:$general_help_email_addr'>$general_help_email_addr</a>.</center></b>";
-	theme("", "footer");
-	exit();
-} elseif (ereg("[^-a-zA-Z0-9@._ ]", $userPW) || strlen($userPW) > 32) {
-	theme("Invalid Password", "header");
-	echo "<b><center>Your password has invalid characters in it or is too long.  Please hit back & try again.<br>";
-	echo "If you believe this is in error please contact <a href='mailto:$general_help_email_addr'>$general_help_email_addr</a>.</center></b>";
-	theme("", "footer");
-	exit();
+function abort_login( $error )
+{
+    global $reset_password_url;
+    global $site_manager_email_addr;
+
+    theme("Login Failed", "header");
+
+    echo "<br>\n";
+    echo "<b>$error</b><br>\n";
+    echo "<br>\n";
+    echo "Please hit Back & try again.<br>\n";
+    echo "If the problem persists, you can try to <A HREF=\"$reset_password_url\">reset</A> your password.<br>\n";
+    echo "If this fails, contact the <a href=\"mailto:$site_manager_email_addr\">site manager</a>.<br>\n";
+    echo "<br>\n";
+    echo "<a href=\"signin.php\">Back</A> to the sign in page.";
+
+    theme("", "footer");
+    exit();
 }
 
-if (!empty($userNM) && !empty($userPW))
+extract($_POST);
+
+$err = check_username($userNM);
+if ($err != '')
 {
+    abort_login($err);
+}
+
+if ($userPW == '')
+{
+    abort_login( "You did not supply a password." );
+}
+
 // $userNM = str_replace("\'", "''", $userNM);
 
+$userC=new db_udb();
+
 $uC=$userC->checkLogin($userNM,$userPW);
-     if ($uC)
-     {
-     $uP=$userC->getUserPrefs($userNM);
-          if ($uP)
-          {
-          // send them to the correct page
+if (!$uC)
+{
+    abort_login('Username or password is incorrect.');
+}
+
+$uP=$userC->getUserPrefs($userNM);
+if (!$uP)
+{
+    abort_login('Username or password is incorrect.');
+}
+
+// The login is successful!
+
+// send them to the correct page
 // isn't this the same as the manager field in users?
 //        $result = mysql_query("SELECT value FROM usersettings WHERE username = '$username' AND setting = 'manager'");
 // needs to be included in user.inc, if not....
-          if ($userC->manager=='yes')
-          {
-              $url = "../tools/project_manager/projectmgr.php";
-          }
-          else
-          {
-              $url = "../tools/proofers/proof_per.php";
-          }
-          metarefresh(1,$url,"Login","");
-          }
-          else die ($noLogin);
-     }
-     else die ($noLogin);
+if ($userC->manager=='yes')
+{
+  $url = "../tools/project_manager/projectmgr.php";
 }
-else die ($noLogin);
+else
+{
+  $url = "../tools/proofers/proof_per.php";
+}
+metarefresh(1,$url,"Login","");
+
 ?>
