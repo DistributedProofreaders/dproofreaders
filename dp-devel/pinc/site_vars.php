1bd2952f3e8a89e641f2defbdaced6f93c60024c
jmdyck <>
2003-06-07 00:38:05 +0000

    Change SITE_{DIR,URL} to CODE_{DIR,URL}. A couple other minor changes.

--- dp-devel/pinc/site_vars.php
+++ dp-devel/pinc/site_vars.php
@@ -2,19 +2,19 @@
 // Variables (constants?) whose values are specific
 // to the local installation of the DP code.
 
-$site_dir = '<<SITE_DIR>>';
+$site_dir = '<<CODE_DIR>>';
 // This is the location in the local file system where the code was
 // installed (i.e., the directory that corresponds to 'dp-devel' in the CVS
 // repository -- it should contain directories such as 'pinc' and 'tools').
 
-$siteurl=''; // <<SITE_URL>>
+$siteurl='<<CODE_URL>>';
 // This is the HTTP URL that resolves to the directory described above.
 
 $projects_dir = '<<PROJECTS_DIR>>';
 $projects_url = '<<PROJECTS_URL>>';
 
-$dynstats_dir = '/0/htdocs/stats'; // <<DYNSTATS_DIR>>
-$dynstats_url = 'http://www.pgdp.net/stats'; // <<DYNSTATS_URL>>
+$dynstats_dir = '<<DYNSTATS_DIR>>';
+$dynstats_url = '<<DYNSTATS_URL>>';
 
 $forums_url = '<<FORUMS_URL>>';
 $reset_password_url        = "$forums_url/profile.php?mode=sendpassword";
@@ -35,8 +35,9 @@ $auto_email_addr = $general_help_email_addr;
 // -----------------------------------------------------------------------------
 
 $testing = TRUE;
-// So far, the only effect of setting $testing to TRUE is to prevent email
-// messages from being sent. Instead, the site shows a copy of the message
-// that would have been sent. See pinc/maybe_mail.inc.
+// So far, the effects of setting $testing to TRUE are:
+// (1) It prevents email messages from being sent. Instead, the site shows a
+//     copy of the message that would have been sent. See pinc/maybe_mail.inc.
+// (2) metarefresh delays by 15 seconds.
 
 ?>
