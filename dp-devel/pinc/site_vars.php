292d2ef2d2ce59844bdd90c46384c7fd7faf733b
jmdyck <>
2003-06-07 02:13:59 +0000

    Rename $site_dir as $code_dir.

--- dp-devel/pinc/site_vars.php
+++ dp-devel/pinc/site_vars.php
@@ -2,7 +2,7 @@
 // Variables (constants?) whose values are specific
 // to the local installation of the DP code.
 
-$site_dir = '<<CODE_DIR>>';
+$code_dir = '<<CODE_DIR>>';
 // This is the location in the local file system where the code was
 // installed (i.e., the directory that corresponds to 'dp-devel' in the CVS
 // repository -- it should contain directories such as 'pinc' and 'tools').
