9a22163a8c252283f7c8d9e0e6d4fe5c4baf0ec9
jmdyck <>
2003-06-12 06:10:17 +0000

    Translate the functionality of add_files.pl into PHP.

--- dp-devel/tools/project_manager/add_files.php
+++ dp-devel/tools/project_manager/add_files.php
@@ -4,25 +4,75 @@ include($relPath.'v_site.inc');
 include($relPath.'metarefresh.inc');
 include($relPath.'dp_main.inc');
 include($relPath.'project_edit.inc');
+include($relPath.'page_states.inc');
 
-    $project = $_GET['project'];
+$projectid = $_GET['project'];
 
-    abort_if_cant_edit_project( $project );
+abort_if_cant_edit_project( $project );
 
-    {
-        if ($userP['sitemanager'] == 'yes') {
-            $string = "perl add_files.pl $project $projects_dir/";
-        } else $string = "perl add_files.pl $project $uploads_dir/";
-        echo "here is the output of \"$string\"...<br>\n";
-        echo "<hr>\n";
-        echo "<pre>\n";
-        system($string);
-        echo "</pre>\n";
-        echo "<hr>\n";
-        echo "Return to <a href='projectmgr.php?project=$project'>Project Page</a>.\n";
+if ($userP['sitemanager'] == 'yes')
+{
+	// It's assumed that anyone with site-manager privileges
+	// would upload their files directly into the projects area.
+	// Maybe this should be an option, as sometimes a site-manager
+	// may want to upload to dpscans.
+	$source_area = $projects_dir;
+}
+else
+{
+	$source_area = $uploads_dir;
+}
 
-        // metarefresh(0, "projectmgr.php?project=$project", "Files Added", "");
+echo "<pre>\n";
 
-    }
+$source_project_dir = "$source_area/$projectid";
+$dest_project_dir   = "$projects_dir/$projectid";
+
+if ($source_project_dir != $dest_project_dir)
+{
+	echo "copying page-images from\n";
+	echo "    $source_project_dir\n";
+	echo "to\n";
+	echo "    $dest_project_dir\n";
+	system("cp $source_project_dir/*.png $dest_project_dir");
+}
+
+echo "\n";
+echo "adding text from each text-file to the $projectid table...\n";
+foreach ( glob("$source_project_dir/*.txt") as $txt_file_path )
+{
+	// basename() corrupts its first argument!
+	// So don't pass $txt_file_path to it directly.
+	$file_base = basename(strval($txt_file_path),'.txt');
+	echo "    $file_base.txt\n";
+
+	// We assume that there is a correspondingly-named .png file.
+	// If there isn't, it will discovered at the verification stage?
+	$image_file_name = "$file_base.png";
+
+	// I'm pretty sure that fileid serves no architectural purpose.
+	// That is, it's redundant, given the 'image' field.
+	// However, until the code is changed to use 'image' instead of 'fileid',
+	// we have to define it, and presumably make it unique over a project.
+	// The former code used a random-number generator, which guaranteed
+	// unpredictability, but not uniqueness.
+
+	$sql_command = "
+		INSERT INTO $projectid
+		SET
+			fileid      = '$file_base',
+			image       = '$image_file_name',
+			master_text = LOAD_FILE('$txt_file_path'),
+			round1_time = NOW(),
+			state       = '".AVAIL_FIRST."'
+	";
+	// echo $sql_command, "\n";
+	$res = mysql_query($sql_command);
+	if (!$res) echo "        ", mysql_error(), "\n";
+}
+
+echo "</pre>\n";
+echo "<hr>\n";
+echo "Return to <a href='projectmgr.php?project=$project'>Project Page</a>.\n";
+
 ?>
-
