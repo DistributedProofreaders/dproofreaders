b5af629219a8e443912a0a2afc6823c093dc2aae
jmdyck <>
2003-06-30 19:32:36 +0000

    Re the problem of special characters in directory names: handle it differently. More robustly?

--- dp-devel/tools/project_manager/add_files.php
+++ dp-devel/tools/project_manager/add_files.php
@@ -29,15 +29,7 @@ if ( $_GET['source_dir'] == '' )
 }
 else
 {
-	$source_dir = $_GET['source_dir'];
-	// Disallow apostrophes, as that's how we quote the dir name.
-	if ( ereg( "'", $source_dir ) )
-	{
-		echo "Source directory name \"$source_dir\" contains an apostrophe, which is not allowed.";
-		echo "<hr>\n";
-		echo "Return to <a href='projectmgr.php?project=$projectid'>Project Page</a>.\n";
-		return;
-	}
+	$source_dir = stripslashes($_GET['source_dir']);
 	// Prevent sneaky parent-link tricks.
 	if ( ereg( '\.\.', $source_dir ) )
 	{
@@ -53,13 +45,31 @@ echo "<pre>\n";
 $source_project_dir = "$source_area/$source_dir";
 $dest_project_dir   = "$projects_dir/$projectid";
 
+// Rather than performing commands from an arbitrary location,
+// using absolute paths, e.g.
+//     system("cp $source_project_dir/*.png $dest_project_dir");
+//     glob("$source_project_dir/*.txt"),
+// we chdir into $source_project_dir and do *local* commands from there.
+// That way, we don't have to worry about any shell-special or
+// glob-special characters in $source_project_dir.
+// (There don't appear to be any chdir-special characters.)
+$r = chdir($source_project_dir);
+if ( !$r )
+{
+	echo "Directory '$source_project_dir' does not exist, or is inaccessible.\n";
+	echo "<hr>\n";
+	echo "Return to <a href='projectmgr.php?project=$projectid'>Project Page</a>.\n";
+	return;
+}
+
+
 if ($source_project_dir != $dest_project_dir)
 {
 	echo "copying page-images from\n";
 	echo "    $source_project_dir\n";
 	echo "to\n";
 	echo "    $dest_project_dir\n";
-	system("cp '$source_project_dir'/*.png $dest_project_dir");
+	system("cp *.png $dest_project_dir");
 }
 
 $n_txt_files_found = 0;
@@ -71,18 +81,18 @@ echo "\n";
 echo "For each text file in\n";
 echo "    $source_project_dir\n";
 echo "adding a row to the $projectid table...\n";
-foreach ( glob("'$source_project_dir'/*.txt") as $txt_file_path )
+foreach ( glob("*.txt") as $txt_file_name )
 {
 	$n_txt_files_found++;
 
 	// basename() corrupts its first argument!
-	// So don't pass $txt_file_path to it directly.
-	$file_base = basename(strval($txt_file_path),'.txt');
+	// So don't pass $txt_file_name to it directly.
+	$file_base = basename(strval($txt_file_name),'.txt');
 	echo "    $file_base.txt\n";
 
 	// We assume that there is a correspondingly-named .png file.
 	// If there isn't, it will discovered at the verification stage?
-	$image_file_name = "$file_base.png";
+	$image_file_name = addslashes("$file_base.png");
 
 	// I'm pretty sure that fileid serves no architectural purpose.
 	// That is, it's redundant, given the 'image' field.
@@ -91,6 +101,8 @@ foreach ( glob("'$source_project_dir'/*.txt") as $txt_file_path )
 	// The former code used a random-number generator, which guaranteed
 	// unpredictability, but not uniqueness.
 
+	// We need to pass an absolute path to LOAD_FILE.
+	$txt_file_path = addslashes("$source_project_dir/$txt_file_name");
 	$sql_command = "
 		INSERT INTO $projectid
 		SET
