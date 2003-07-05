3dd49b8affb7d1add0b782035a2170081e47c59a
jmdyck <>
2003-07-05 19:56:21 +0000

    Change "it's" to "its".

--- dp-devel/accounts/addproofer.php
+++ dp-devel/accounts/addproofer.php
@@ -273,7 +273,7 @@ $tre=$htmlC->closeTD(1).$htmlC->closeTR(1);
 <p><font size="+1">Usage of information:</font></p>
 <p>This information will allow the project managers to provide feedback to you directly or have users send to you a message via the phpBB forum. An introductory e-mail will also be sent to you.</p>
 <p><font size="+1">Removal of information:</font>
-<p>Requests to remove the information beforehand can be sent to the web site manager or will be removed based on it's age.</p>
+<p>Requests to remove the information beforehand can be sent to the web site manager or will be removed based on its age.</p>
 <p><font size="+1">Access of information:</font></p>
 <p>Only the web site manager will have full access to the information in this form. Project managers will have access to e-mail you directly if needed. Any public information you fill out in the phpBB forum or profile will be accessible to other users.</p>
 <p><font size="+1">Tracking information:</font></p>
