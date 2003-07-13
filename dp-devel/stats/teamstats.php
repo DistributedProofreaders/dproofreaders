0fcb01aecd4dd9e6637c72c1dac4a6b5c42db35e
usfjoseph <>
2003-07-13 13:56:38 +0000

    Script to be run daily that updates the user_teams_stats table with how many pages each team has done for a specific day.  Also updates the average_pages field in the user_teams table.  Only to be run once and after the new tables/fields have been added.

---
+++ dp-devel/stats/teamstats.php
@@ -0,0 +1,32 @@
+<?
+$relPath='./../pinc/';
+include($relPath.'v_site.inc');
+include($relPath.'connect.inc');
+$db_Connection=new dbConnect();
+
+$today = getdate();
+$midnight = mktime(0,0,0,$today['mon'],$today['mday'],$today['year']);
+$prev_midnight = $midnight - 86400;
+
+//Find out if the script has been run once already for today
+$result = mysql_query("SELECT MAX(date_updated) FROM user_teams_stats");
+	if (mysql_result($result,0,0) == $midnight && empty($_GET['testing'])) {
+		echo "<center>This script has already been run today!</center>";
+	} else {
+	//Update user_teams_stats with previous days page count
+	$result = mysql_query("SELECT id, teamname, page_count FROM user_teams");
+		while($row = mysql_fetch_assoc($result)) {
+			$prevDayCount = mysql_query("SELECT page_count FROM user_teams_stats WHERE date_updated = $prev_midnight && team_id = ".$row['id']."");
+			$todaysCount = $row['page_count'] - mysql_result($prevDayCount,0,"page_count");
+			$updateCount = mysql_query("INSERT INTO user_teams_stats (team_id, date_updated, page_count) VALUES (".$row['id'].", $midnight, $todaysCount)");
+		}
+	//Total all of a teams page counts and average it
+	$result = mysql_query("SELECT id, teamname, page_count FROM user_teams");
+		while($row = mysql_fetch_assoc($result)) {
+			$pageCountArray = mysql_query("SELECT page_count FROM user_teams_stats WHERE team_id = ".$row['id']."");
+			$numRecords = mysql_num_rows($pageCountArray);
+			$avgCount = $row['page_count']/$numRecords;
+			$updateAvgCount = mysql_query("UPDATE user_teams SET daily_average = $avgCount WHERE id = ".$row['id']."");
+		}
+	}
+?>
