d1fd5e70dbb65df19cdffdea263365c6d5c17aa1
charlz <>
2003-07-15 02:06:31 +0000

    module to mark users inactive vice archiving them

---
+++ dp-devel/crontab/inactivate_users.php
@@ -0,0 +1,13 @@
+<?
+$relPath="./../pinc/";
+include($relPath.'v_site.inc');
+include($relPath.'dp_main.inc');
+
+
+//this module sets users inactive who have not logged into the site in 6 months
+
+    $old_date = time() - 15768000; // 6 months ago.
+
+    $result = mysql_query ("UPDATE `users` SET active = 'no' WHERE last_login < $old_date AND active ='yes'");
+  
+?>
