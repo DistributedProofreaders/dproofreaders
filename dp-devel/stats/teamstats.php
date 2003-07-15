62cdf33b615abd5cbe43f5922ed6ce24ef55e1ab
usfjoseph <>
2003-07-15 19:14:16 +0000

    Changed so that it doesn't calculate for the default DP team

--- dp-devel/stats/teamstats.php
+++ dp-devel/stats/teamstats.php
@@ -16,16 +16,20 @@ $result = mysql_query("SELECT MAX(date_updated) FROM user_teams_stats");
 	//Update user_teams_stats with previous days page count
 	$result = mysql_query("SELECT id, teamname, page_count FROM user_teams");
 		while($row = mysql_fetch_assoc($result)) {
-			$prevDayCount = mysql_query("SELECT page_count FROM user_teams_stats WHERE date_updated = $prev_midnight && team_id = ".$row['id']."");
-			$todaysCount = $row['page_count'] - mysql_result($prevDayCount,0,"page_count");
-			$updateCount = mysql_query("INSERT INTO user_teams_stats (team_id, date_updated, page_count) VALUES (".$row['id'].", $midnight, $todaysCount)");
+			if ($row['id'] != 1) {
+				$prevDayCount = mysql_query("SELECT page_count FROM user_teams_stats WHERE date_updated = $prev_midnight && team_id = ".$row['id']."");
+				$todaysCount = $row['page_count'] - mysql_result($prevDayCount,0,"page_count");
+				$updateCount = mysql_query("INSERT INTO user_teams_stats (team_id, date_updated, page_count) VALUES (".$row['id'].", $midnight, $todaysCount)");
+			}
 		}
 	//Total all of a teams page counts and average it
 	$result = mysql_query("SELECT id FROM user_teams");
 		while($row = mysql_fetch_assoc($result)) {
-			$avgPageCount = mysql_query("SELECT AVG(page_count) AS avgCount FROM user_teams_stats WHERE team_id = ".$row['id']."");
-			$avgCount = mysql_result($avgPageCount,0,"avgCount");
-			$updateAvgCount = mysql_query("UPDATE user_teams SET daily_average = $avgCount WHERE id = ".$row['id']."");
+			if ($row['id'] != 1) {
+				$avgPageCount = mysql_query("SELECT AVG(page_count) AS avgCount FROM user_teams_stats WHERE team_id = ".$row['id']."");
+				$avgCount = mysql_result($avgPageCount,0,"avgCount");
+				$updateAvgCount = mysql_query("UPDATE user_teams SET daily_average = $avgCount WHERE id = ".$row['id']."");
+			}
 		}
 	}
 ?>
