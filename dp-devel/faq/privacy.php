173697f96975070b7d425e3009374289b3dfd1d5
jmdyck <>
2003-07-21 19:47:38 +0000

    From Tim Bonham.

---
+++ dp-devel/faq/privacy.php
@@ -0,0 +1,26 @@
+<p align="center"><font size="+2">Privacy Statement:</font></p>
+
+<p><font size="+1">Usage of information:</font></p>
+
+<p>This information will allow the project managers to provide feedback to you directly
+or have users send to you a message via the phpBB forum. An introductory e-mail will
+also be sent to you.</p>
+
+<p><font size="+1">Removal of information:</font>
+
+<p>Requests to remove the information beforehand can be sent to the web site manager or
+will be removed based on its age.</p>
+
+<p><font size="+1">Access of information:</font></p>
+
+<p>Only the web site manager will have full access to the information in this form.
+Project managers will have access to e-mail you directly if needed. Any public
+information you fill out in the phpBB forum or profile will be accessible to other
+users.</p>
+
+<p><font size="+1">Tracking information:</font></p>
+
+<p>The only tracking information we collect at this time is the number of pages you have
+completed, the date your account was created, and your last login date. Only the pages
+completed out of the three will be available to end users, the rest is for statistical
+purposes and removal of old accounts by the web site managers.</p>
