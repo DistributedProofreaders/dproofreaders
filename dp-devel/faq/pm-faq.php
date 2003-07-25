72a4e55e06a4bdcc90aacde8f626cd09e0739c84
jmdyck <>
2003-07-25 23:24:59 +0000

    Eliminate overlap with the Prep Guidelines and the Post-Proofing FAQ. Reorganized the remainder. It still needs updating, but that should be easier now.

--- dp-devel/faq/pm-faq.php
+++ dp-devel/faq/pm-faq.php
@@ -5,165 +5,73 @@ include($relPath.'connect.inc');
 include($relPath.'theme.inc');
 new dbConnect();
 $no_stats=1;
-theme('Project Managers\' FAQ','header');
+theme('Project Manager\'s Guide','header');
 ?>
 
-<h1>Project Managers Frequently Asked Questions (FAQ)</h1>
-<br><br>
-Authors: Charles Franks <a href="mailto:charlz@lvcablemodem.com">charlz@lvcablemodem.com</a>, Robert Rowe <a href="mailto:robert_rowe@yahoo.com">robert_rowe@yahoo.com</a><br>
-Date: 12/17/2001<br>
-Updated: 06/02/2002<br>
-<br><br>
-
-The purpose of this FAQ is to provide answers to common questions and explain the process of becoming a Project Manager or otherwise providing
-material to the Distributed Proofreaders web site.
+<h1>Project Manager's Guide</h1>
 
 <p>
+The
+<a href="scan/submitting.php">Scanning, Preprocessing & Submitting Guidelines</a>
+describe the life of a Distributed Proofreaders project
+up to the point at which the image files and text files
+have been uploaded to the DP server.
+This document describes everything that happens after that,
+from the point of view of a Project Manager (PM).
+</p>
+
+<p>
+PMs are welcome to do the prep work on their project,
+or they can choose to manage a project that someone else has prepared.
+<font style='color:red'>
+[How do they see what's available to be claimed?]
+</font>
+</p>
+
+<p>
+To become a PM, 
+<a href="mailto:charlz@lvcablemodem.com">email Charlz</a>.
+</p>
+
 <hr>
-<p>
-<b>Contents</b><br><br>
 
-1. <a href="#1">What are the steps in obtaining/processing books through Distributed Proofreaders?</a><br>
-2. <a href="#2">How do I get my book processed through Distributed Proofreaders?</a><br>
-3. <a href="#3">How do I become a Project Manager?</a><br>
-4. <a href="#4">What do I need to do while the project is going through the site? (Shepherding a Project) </a><br>
-5. <a href="#5">How do I get the 'finished product' off the DPR website? </a><br>
-6. <a href="#6">How do I process the output from the site to make a PG e-text?</a><br>
-7. <a href="#7">How do I submit a finished e-text for posting?</a><br>
-8. <a href="#8">What Scanning/OCR program(s) should I use?</a><br>
+<h3>Project Manager's Workflow</h3>
 
-
-<br><br>
-<hr>
-<br><br>
-<a name="1"><b>1. What are the steps in obtaining/processing books through Distributed Proofreaders?</b></a>
-<p>
-<blockquote>
-
-Several of these steps mention using Robert's Proofreaders Toolkit (PRTK). PRTK is a tool written specifically for PM's and we use it to streamline many common tasks. It is availible for Windows only and you can get the latest copy at <a href="http://robertrowe.dns2go.com">Robert Rowe's Website.</a> For those without access to a computer running Windows, feel free to email <a href="mailto:robert_rowe@yahoo.com">Robert Rowe</a> for assistance. He has volunteered to run your project through PRTK for you.<br><br>
-
-<b>A. Find an elgible book.</b><br>
-    <blockquote>
-    Basic eligibility: copyrighted 1922 or earlier. <br><br>
-    Possible sources include: Flea Market, Yard Sale, Estate Auctions, Online Auctions, Local Library, etc. Libraries usually don't make good sources because most won't let you check out their old books. Also, the scanning process is a little rough on the book and it may get damaged.
-    </blockquote>
-
-<b>B. Ensure the book is not in the PG Archive or on David Price's <a href="http://www.dprice48.freeserve.co.uk/GutIP.html">In-Progress List</a>.</b><br>
-    <blockquote>
-    It is better to check the In-progress list than searching PG. David's list includes projects already started by other volunteers. If you find your book listed but the clearance date is over a year old then it is probably OK to go ahead and do.
-    </blockquote>
-
-<b>C. Buy/borrow the book.</b><p>
-
-<b>D. Send copies of the Title Page and Verso (TP&V) to Michael Hart for clearance.</b><br>
-    <blockquote>
-    As of January, 2002, there are two ways to submit a text for clearance.
-    <p>
-    <strong>To submit by paper mail,</strong> photocopy the front and back
-    of the title page, even if the back is blank, write your e-mail address
-    on it, and send the photocopies to:<br><br>
-
-    <ul>
-
-    MICHAEL STERN HART<br>
-    405 WEST ELM STREET<br>
-    URBANA, IL 61801-3231 USA<br>
-
-    </ul>
-
-    <p>
-    This is called Title Page &amp; Verso, or TP&amp;V for short, and is needed
-    for copyright research. A colored envelope is best, to make sure your letter
-    is easily recognized as TP&amp;V.</p>
-    <p>
-    E-mail Michael <a href="mailto:hart@pobox.com">hart@pobox.com</a> when you
-    send them, so he knows they're on the way. It's a good idea to check back
-    with him by e-mail after a week or so if you haven't heard from him.</p>
-    <p>
-    <strong>To submit by e-mail,</strong> scan the front and back of
-    the title page, even if the back is blank, make sure that the
-    print is legible, and e-mail the images to
-    <a href="mailto:gbnewby@ils.unc.edu">Greg Newby</a>
-    as TIFF, JPEG or GIF in medium resolution.
-
-    <p>
-    Whichever method you use, you should expect to get an e-mail back after
-    about a week, with one line containing the Author, Title, your name
-    and date with the word "OK" at the end. This means that your text has been cleared.
-    If you don't get any response, e-mail to check that your TP&amp;V was
-    received OK. If the word at the end of the line is not "OK", then your
-    text is not eligible.
-    <p>
-
-    </blockquote>
-<b>E. Start scanning (once the clearance is obtained).</b><br>
-    <blockquote>
-    You should set your scanner to "Line Art" or "Black and White" mode. Your resolution should be 300 X 300. If the book is very small or if it has very small print you might want to try a resolution of 400 X 400. A higher resolution will just increase the file size without much (if any) improvement in image quality. (Except on very small print). To speed up the scanning process scan 'two-up' images (two book pages per image). To help with the next step, processing into 'one-up' images, try to keep the book in the same place on the scanner for each scan, say tight into a corner.
-    </blockquote>
-<b>F. Process the book pages into 'one-up' (one book page per image) files.</b><br>
-    <blockquote>
-    Generally there are two 'easy' ways to get 'one-up' images from 'two-up' images:
-<p> 1.) If your scanning program has an option to automatically split images as they are scanned, set this option (See <a href="#8">Scanning/OCR programs</a>). I don't really recommend this as the output can be inconsistent.
-
-<p>2.) Use PRTK (See PRTK Help) to split images containing 2 pages. If you keep the book in the same place on the scanner for each scan PRTK can easily split the images.
+<ol>
+<li><a href='#create'>Create the project.</a>
+<li><a href='#release'>Release it.</a>
+<li><a href='#shepherd'>Shepherd it through proofing.</a>
+<li><a href='#postprocess'>Post-process it.</a>
+<li><a href='#submit_to_pg'>Submit it to Project Gutenberg.</a>
+</ol>
 
 <p>
-If your scanner has an ADF (Automatic Document Feeder) you may wish to cut the pages from the spine and feed them through your ADF. In addition, you will probably want to clean and crop your images. PRTK has several tools to assist with this process.
-    </blockquote>
-<b>G. OCR image files into text files (one text file per image).</b><br>
-    <blockquote>
-    If your OCR program has an option to insert a blank line between paragraphs then use this option. It will cut way down on this kind of mistake and the proofers will appreciate it. If your program does not have this option, PRTK has a tool to assist you with inserting this line. If you are using Finreader for your scanning/OCR, Robert has a set of saved settings that you may find useful.
-    </blockquote>
-<b>H. Automatically Pre-Proof pages using PRTK.</b><br>
-    <blockquote>
-    PRTK has a pre-proofing tool designed to detect and correct many common mistakes left by OCR programs. (See PRTK Help)
-    </blockquote>
-<b>I. Create the project on the DPR website.</b><br>
-    <blockquote>
-    On the main PM page (at the bottom) is a link to the "Create Project" page. After you create your project, it will be listed on your PM page. Each project has an "Edit Comments" link. You can email Robert or myself for a file containing the "standard" comments or make up anything you like. HTML is allowed in the comments including links to example pictures.
-    </blockquote>
-<b>J. Convert your images to PNG.</b><br>
-    <blockquote>
-    Browsers are limited to certain types of image files. We have found that the PNG file type best suits our needs.
-    </blockquote>
-<b>K. Load images and text files onto the website.</b><br>
-    <blockquote>
-    You have 3 ways to upload your text and images.<br><br>
+See also the <a href="DPflow.php">DP Workflow Diagram</a>,
+which gives an overview of how material moves through the site
+and what the PMs do.
+</p>
 
-    1. Use the "Upload a master file to this project" link on the project page.<br>
-    2. Use Robert's Project Uploader. (contact Robert for the latest version)<br>
-    3. FTP the files to Robert or myself and we'll bulk upload them for you.<br>
-    </blockquote>
-<b>L. Release the project for proofing.</b><br>
+<ol>
+<li><a name='create'><b>Create the project on the DP website.</b></a>
     <blockquote>
-    This is done by toggling the "Availibility" of the project on the PM's page. Please coordinate your release with the other PM's. We all take turns and try to keep an even mix of books without having too many/few availible.
+    On the main PM page (at the top) is a link to the "Create Project" page.
+    After you create your project, it will be listed on your PM page.
+    Each project has an "Edit Comments" link.
+    You can email Robert or Charlz for a file containing the "standard" comments or make up anything you like.
+    HTML is allowed in the comments including links to example pictures.
     </blockquote>
-<b>M. Shepherd the project through the site. (See <a href="#4">Shepherding a Project</a>)</b><p>
+</li>
 
-<b>N. Download the output from the site and massage it into a postable e-text. (PRTK helps immensely here)</b><p>
+<li><a name='release'><b>Release the project for proofing.</b></a>
     <blockquote>
-    The Project Status page has a link for this step. The site will stich your files together (with a file separator) and provide a download link. PRTK has a Post-Processing tool designed to read this file and assist in making the final formatting adjustments.
+    This is done by toggling the "Availability" of the project on the PM's page.
+    Please coordinate your release with the other PM's.
+    We all take turns and try to keep an even mix of books
+    without having too many/few available.
     </blockquote>
-<b>O. Send the e-text in for posting to the PG archive.</b><br>
-    <blockquote>
-    Most PM's email me their completed etext for a final double check and submission. If you send me you etext, please include your "Raw" file (straight from the site) also as this helps tremendously.
-    </blockquote>
-</blockquote>
+</li>
 
-<p>
-<a name="2"><b>2. How do I get my book processed through Distributed Proofreaders?</b></a>
-    <blockquote>
-    You can complete as many of the steps in the first question as you would like, everything from buying a
-    book and sending it to myself or one of the PM's, to doing all the steps including submitting it to PG for posting.
-    </blockquote>
-<p>
-<a name="3"><b>3. How do I become a Project Manager?</b></a><br><br>
-    <blockquote>
-    <a href="mailto:charlz@lvcablemodem.com">Email Me</a>  and we'll talk :) You can also review the <a href="DPflow.php">Workflow Diagram</a> which gives an overview of how material moves through the site and what the PM's do.
-    </blockquote>
-
-<p>
-<a name="4"><b>4. What do I need to do while the project is going through the site? (Shepherding a Project) </b></a>
+<li><a name='shepherd'><b>Shepherd the project through the site.</b></a>
     <blockquote>
     There are 4 main tasks you will need to perform to assure your project runs smoothly through the site. All these tasks are done via the Project Page using the links provided.<br><br>
 
@@ -174,70 +82,68 @@ If your scanner has an ADF (Automatic Document Feeder) you may wish to cut the p
 
     We are working on a script that will automate task #2 and have talked about automating task #3. Tasks 1 & 4 will probably always be done by the PM. The client has features designed to help prevent duplicates and MIA's. As more people start using the client you should see a reduction in their occurence.
     </blockquote>
-<p>
-<a name="5"><b>5. How do I get the 'finished product' off the DPR website? </b></a>
-    <blockquote>
-    After all the pages have gone through both rounds of proofing there is a 'concatenate' link, once you click on this you are
-    taken to a page that asks you what you want to name the resultant file and whether or not you want page separators between
-    each page. The page separators contain the filename of the text file and are very handy in the final proofing/assembly stage.
-    </blockquote>
-<p>
-<a name="6"><b>6. How do I process the output from the site to make a PG e-text?</b></a>
-    <blockquote>
-    I generally make a 'working' copy of the file and keep the original for cross-referencing text to the original file. If you have questions
-    you can search the 'original' copy for the suspect text, find out what file it was in and review the corresponding image.
-    <p>
-    Removing the page separators and correctly joining the pages in the 'working' copy is where PRTK becomes invaluable as this process is semi-automatic.
-    <p>
-    Use GutCheck (PRTK has a front end for GutCheck) and correct what it finds. Pay special attention to items marked with an asterisk.
-    <p>
-    Next, I generally follow this process in MS WORD:
+</li>
 
-    <pre>
-    - replace ^p^p with @@
-    - replace ^p with a space
-    - replace @@ with ^p^p
-    - replace double spaces with single spaces (cleans up extra whitespace)
-    - Fix em-dashes
-         -- replace blah --blah with blah--blah
-            likewise blah - blah and blah-- blah
-    - search for spaced punctuation
-         -- blah " blah  blah ? blah blah ! blah etc.
-    - replace -- with @@
-    - replace common single space words with @ (depends on length of text as this
-      saves time on really large texts)
-         -- to-day should be to@day, etc.
-         -- go through each remaining - and see if it should be -- (hyphens that
-            should be em-dashes)
-    - spell check
-    - search for * and fix
-    - seach for _ and make into capitals
-         -- except for _I_
-    - select all text
-         --set font to 'courier new' and size 10
-         --set right margin to around 65-70 characters.
-           A right margin of 2 inches is about right.
-    - do 'save as text with line breaks'
-    - go through and properly space/indent poetry, songs etc.
-    - Add blank lines (5 before and 1 after) chapter/section headings
-    </pre>
-    <p>
-    Robert has a Word macro that automates part of the above. PRTK has a "Dask Check" tool to assist with the correction of em-dashes and formatter tool to assist with capitalization, indention, etc.
-    <p>
+<li><a name='postprocess'><b>Download the output from the site and massage it into a postable e-text.</b>
+    <blockquote>
+    After all the pages have gone through both rounds of proofing,
+    the site will stitch together the resulting page-texts
+    (with page separators)
+    and add the resulting file to the project's directory.
+    You can download this file by selecting <b>D/L</b> on the PM page,
+    or <b>Download Zipped Text</b> on the post-processing page.
 
+    <p>
+    If you want to post-process the book,
+    <font style='color:red'>
+    [you would have said so earlier].
+    </font>
+    Follow the instructions in the
+    <a href='post_proof.php'>Post-Proofing FAQ</a>.
+
+    Or you can
+    <font style='color:red'>
+    [somehow]
+    </font>
+    make the project available
+    for someone else to post-process.
+    (They will find it on the post-processing page,
+    and check it out by selecting <b>Check Out Book</b>.)
     </blockquote>
-<p>
-<a name="7"><b>7. How do I submit a finished e-text for posting? </b></a>
+
+<li><a name='submit_to_pg'><b>Submit the finished e-text to Project Gutenberg</b></a>
+
     <blockquote>
-    On you first few e-texts you probably want to send it to me for posting.. I will review it, give you feedback and submit it to PG. After you are confident
-    you can submit them on your own  :)
+    On your first few projects,
+    you probably want to send it to Charlz for posting.
+    (Please include your "raw" file that you downloaded from the site,
+    as this helps tremendously.)
+    He will review it, give you feedback, and submit it to PG.
+    After you are confident,
+    you can submit them on your own.
     </blockquote>
-<p>
-<a name="8"><b>8. What Scanning/OCR program(s) should I use?</b></a>
-    <blockquote>
-    No one will tell you that you should or shouldn't use a specific program. You can use whatever works for you. That said, Robert and I use Finereader from a company called ABBYY (<a href="http://www.abbyy.com">http://www.abbyy.com</a>). In our opinion, this is the best program for scanning and OCRing our books. They have a trial version on their website if you want to try it out. If you do decide to use Finreader, both Robert and I will be glad to answer any questions you have about using it. We have plans to create a "How To" that illustrates in greater detail the process of creating an etext using Finereader and PRTK.
-    </blockquote>
-</p>
+</li>
+</ol>
+
+<hr>
+<h4>Document History</h4>
+
+<ul>
+<li>2001-12-17: first written by
+    Charles Franks
+    <a href="mailto:charlz@lvcablemodem.com">charlz@lvcablemodem.com</a>
+    and
+    Robert Rowe
+    <a href="mailto:robert_rowe@yahoo.com">robert_rowe@yahoo.com</a>
+<li>2002-06-02: Updated
+<li>2003-07-25: jmdyck: eliminated overlap with the
+<a href='scan/submitting.php'>Prep Guidelines</a> and the
+<a href='post_proof.php'>Post-Proofing FAQ</a>.
+Reorganized the remainder.
+(It still needs updating, but that should be easier now.)
+</ul>
+
+<br>
 
 <?
 theme('','footer');
