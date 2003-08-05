8749d4062ab3e6d8609d45f6a345a1caf3e013ce
usfjoseph <>
2003-08-05 18:54:20 +0000

    The privacy statement is now pulled from the /faq/privacy.php file

--- dp-devel/accounts/addproofer.php
+++ dp-devel/accounts/addproofer.php
@@ -278,20 +278,11 @@ $tre=$htmlC->closeTD(1).$htmlC->closeTR(1);
     echo $tde.$td3.'<input type="radio" name="email_updates" value="1" checked>Yes&nbsp;&nbsp;<input type="radio" name="email_updates" value="0">No';
     echo $tre.$tr.$td1.'<input type="submit" value="Create Account">&nbsp;&nbsp;<input type="reset" value="Reset Form">';
     echo $tre.$tr.$td5;
-?>
-<center><font size="+2">Privacy Statement:</font></center>
-<p><font size="+1">Usage of information:</font></p>
-<p>This information will allow the project managers to provide feedback to you directly or have users send to you a message via the phpBB forum. An introductory e-mail will also be sent to you.</p>
-<p><font size="+1">Removal of information:</font>
-<p>Requests to remove the information beforehand can be sent to the web site manager or will be removed based on its age.</p>
-<p><font size="+1">Access of information:</font></p>
-<p>Only the web site manager will have full access to the information in this form. Project managers will have access to e-mail you directly if needed. Any public information you fill out in the phpBB forum or profile will be accessible to other users.</p>
-<p><font size="+1">Tracking information:</font></p>
-<p>The only tracking information we collect at this time is the number of pages you have completed, the date your account was created, and your last login date. Only the pages completed out of the three will be available to end users, the rest is for statistical purposes and removal of old accounts by the web site managers.</p>
-<?php
-     echo $tre.$htmlC->closeTable(1)."</form>";
 
-     echo "</center>";
+    include($code_dir.'/faq/privacy.php');
+
+    echo $tre.$htmlC->closeTable(1)."</form>";
+    echo "</center>";
 }
 theme("", "footer");
 ?>
