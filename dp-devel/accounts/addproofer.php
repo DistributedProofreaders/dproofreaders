426f8fd901bd070f4949bc7a3a600ea65fff19d1
usfjoseph <>
2003-08-15 18:28:49 +0000

    Default user will now have the top ten displayed as well as 20 neighbors

--- dp-devel/accounts/addproofer.php
+++ dp-devel/accounts/addproofer.php
@@ -50,8 +50,8 @@ if ($password=="proofer") {
 
     $todaysdate = time();
 
-    $result = mysql_query ("INSERT INTO users (id, real_name, username, email, manager, date_created, emailupdates, u_plist)
-                VALUES ('$ID', '$real_name', '$username', '$email', 'no', '$todaysdate', '$email_updates', '3')");
+    $result = mysql_query ("INSERT INTO users (id, real_name, username, email, manager, date_created, emailupdates, u_plist, u_top10, u_neigh)
+                VALUES ('$ID', '$real_name', '$username', '$email', 'no', '$todaysdate', '$email_updates', '3', '1', '10')");
 
     if (!$result) {
         abort_registration(
