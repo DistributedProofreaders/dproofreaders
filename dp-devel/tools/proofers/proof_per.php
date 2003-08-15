bc7cce6b99aee6a1833312e9bf1bf72c44e54d56
usfjoseph <>
2003-08-15 12:08:05 +0000

    Fixed spacing from translations.  Also updated SQL queries and modernized the random generator for the random rules

--- dp-devel/tools/proofers/proof_per.php
+++ dp-devel/tools/proofers/proof_per.php
@@ -14,14 +14,13 @@ theme("Personal Page for $pguser", "header");
 
 
 	//Display News Updates
-	$result = mysql_query("SELECT * FROM news ORDER BY uid DESC LIMIT 1");
-	$date_posted = date("l, F jS, Y", mysql_result($result,0,"date_posted"));
-	$message = mysql_result($result,0,"message");
-	echo "<font size=2 face=" . $theme['font_mainbody'] . "><center><b>"; 
-	echo _("News Update for") . " $date_posted (<a href='$code_url/pastnews.php'>"; 
+	$result = mysql_query("SELECT date_posted, message FROM news ORDER BY uid DESC LIMIT 1");
+	$news = mysql_fetch_assoc($result);
+	echo "<font size=2 face=" . $theme['font_mainbody'] . "><center><b>";
+	echo _("News Update for")." ".date("l, F jS, Y", $news['date_posted'])." (<a href='$code_url/pastnews.php'>";
 	echo _("archives") . "</a>)</b></font><br><br><font size=2 face=";
-	echo $theme['font_mainbody'] . ">$message<hr width='75%'></center></font><br>";
-	
+	echo $theme['font_mainbody'] . ">".$news['message']."<hr width='75%'></center></font><br>";
+
 	// If Post Processor give link to post processing page.
     	$result = mysql_query("SELECT pagescompleted FROM users WHERE username = '$pguser'");
     	$postprocessorpages = mysql_result($result,0);
@@ -34,13 +33,13 @@ theme("Personal Page for $pguser", "header");
 
 <font face="<? echo $theme['font_mainbody']; ?>"><b><? echo _("Providing Content"); ?></b></font><br>
 <? echo _("Want to help out the site by providing material for us to proof?"); ?>
-<a href="<? echo $code_url ?>/faq/scan/submitting.php"><? echo _("Find out how!"); ?></a>
+ <a href="<? echo $code_url ?>/faq/scan/submitting.php"><? echo _("Find out how!"); ?></a>
 <br><br>
 
 <font face="<? echo $theme['font_mainbody']; ?>"><b><? echo _("Post Processing"); ?></b></font><br>
 <? echo _("After going through two rounds of proofreading, the books need to be massaged into a final e-text. You can help in the"); ?> <a href ="<? echo $code_url ?>/tools/post_proofers/post_proofers.php"><? echo _("post processing"); ?></a> <? echo _("phase of Distributed Proofreaders!  Currently there are"); ?> <b><? echo $numprojects; ?></b> <? echo _("projects waiting."); ?>
 
-<? 
+<?
 if ($yourprojects > 0) { echo "  ". _("You currently have <b>$yourprojects</b> projects checked out."); }
 echo "<br><br>";
 }
@@ -50,13 +49,10 @@ echo "<br><br>";
 <font face="<? echo $theme['font_mainbody']; ?>"><b>Random Rule</b></font><br>
 
 <?
-	$result = mysql_query("SELECT count(id) FROM rules");
- 	$num_rules = mysql_result($result,0);
-	$randid  = rand(0,$num_rules); 
-        $result = mysql_query("SELECT subject,rule,doc FROM rules WHERE id=$randid");
+        $result = mysql_query("SELECT subject,rule,doc FROM rules ORDER BY RAND(NOW()) LIMIT 1");
     	$rule = mysql_fetch_assoc($result);
     	echo "<i>".$rule['subject']."</i><br>";
-    	echo "".$rule['rule']."<br><br>";
+    	echo "".$rule['rule']."<br>";
     	echo "See the <a href='$code_url/faq/document.php#".$rule['doc']."'>".$rule['subject']."</a> section of the <a href='$code_url/faq/document.php'>Proofing Guidelines</a><br><br>";
 
 	echo "<center><hr width='75%'></center><br>";
