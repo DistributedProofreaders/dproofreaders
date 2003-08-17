87ee764e2042410c644a1042ba4352b1e17fc742
usfjoseph <>
2003-08-17 02:12:46 +0000

    Centered to make it look better as well as added searches for both members & teams.  Links below search for mbr lists & team lists

--- dp-devel/stats/stats_central.php
+++ dp-devel/stats/stats_central.php
@@ -6,11 +6,17 @@ include($relPath.'theme.inc');
 new dbConnect();
 
 theme('Stats Central','header');
-echo "<h1>Stats Central</h1>";
+echo "<center><h1><i>Stats Central</i></h1></center>";
 
-echo "<img src=\"jpgraph_files/curr_month_pages_graph.php\">";   
-echo "<p>";
-echo "<img src=\"jpgraph_files/total_pages_graph.php\">";
+echo "<table border='0' align='center' width='95%' cellspacing='2' cellpadding='2'>";
+echo "<tr><form action='$code_url/stats/members/mbr_list.php' method='post'>";
+echo "<td align='left'><font color='".$theme['color_headerbar_font']."'><input type='text' name='uname' size='20'>&nbsp;<input type='submit' value='Member Search'></td></form>";
+echo "<form action='$code_url/stats/teams/tlist.php' method='post'>";
+echo "<td align='right'><font color='".$theme['color_headerbar_font']."'><input type='text' name='tname' size='20'>&nbsp;<input type='submit' value='Team Search'></td></form>";
+echo "</tr><tr><td align='center'><a href='$code_url/stats/members/mbr_list.php'>Member List</a></td><td align='center'><a href='$code_url/stats/teams/tlist.php'>Team List</a></td></tr></table><br>";
+
+echo "<center><img src=\"jpgraph_files/curr_month_pages_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/total_pages_graph.php\"></center>";
 
 theme('','footer');
 ?>
