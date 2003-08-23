433fd30ad52c7f74b4d5f568212c8fb5b69531c4
aldarondo <>
2003-08-23 18:39:27 +0000

    Initial version of book statistics

---
+++ dp-devel/stats/books/bdetail.php
@@ -0,0 +1,21 @@
+<?
+$relPath="./../../pinc/";
+include_once($relPath.'v_site.inc');
+include_once($relPath.'connect.inc');
+include_once($relPath.'theme.inc');
+include_once('../includes/book.php');
+include('
+$db_Connection=new dbConnect();
+
+$result = mysql_query("SELECT * FROM projects WHERE projectid = ".$_GET['project']."");
+$curProj = mysql_fetch_assoc($result);
+
+theme($curProj['nameofwork']."'s Details", "header");
+echo "<br><center>";
+if (!empty($curProj['projectid'])) {
+	showProjProfile($curProj);
+}
+
+echo "</center>";
+theme("", "footer");
+?>
