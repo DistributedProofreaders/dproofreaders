5e2a6c800589a96cb36de656cb1114efd70a01a0
usfjoseph <>
2003-08-24 14:19:34 +0000

    Changed include's to include_once's

--- dp-devel/stats/to_be_released.php
+++ dp-devel/stats/to_be_released.php
@@ -1,10 +1,10 @@
 <?
 $relPath="../pinc/";
-include($relPath.'dp_main.inc');
-include($relPath.'bookpages.inc');
-include($relPath.'showavailablebooks.inc');
-include($relPath.'project_states.inc');
-include($relPath.'page_states.inc');
+include_once($relPath.'dp_main.inc');
+include_once($relPath.'bookpages.inc');
+include_once($relPath.'showavailablebooks.inc');
+include_once($relPath.'project_states.inc');
+include_once($relPath.'page_states.inc');
 include_once($relPath.'theme.inc');
 
 theme("Books To Be Released", "header");
@@ -25,16 +25,16 @@ if ($order == 'default'){
     echo "<html><body><table cols = \"3\" border =\"1\">";
     echo "<td><b>Index</b></td><td><b>Name of Work</b></td>
           <td><b><a href =\"to_be_released.php?order=username\">Project Manager</b></td>
-          <td><b><a href = \"to_be_released.php?order=modifieddate\">Date Last 
+          <td><b><a href = \"to_be_released.php?order=modifieddate\">Date Last
 Modified</a></b></td><td><b>Language</b></td><td><b>Genre</b></td><tr>";
-                     
+
     $index = 0;
    while ($rownum < $numrows) {
     $nameofwork = mysql_result($result, $rownum, "nameofwork");
     $username = mysql_result($result, $rownum, "username");
     $modifieddate = mysql_result($result, $rownum, "modifieddate");
     $language = mysql_result($result, $rownum, "language");
-    $genre = mysql_result($result, $rownum, "genre");    
+    $genre = mysql_result($result, $rownum, "genre");
 
     $today = getdate($modifieddate);
     $month = $today['month'];
@@ -42,11 +42,11 @@ Modified</a></b></td><td><b>Language</b></td><td><b>Genre</b></td><tr>";
     $year = $today['year'];
     $datestamp = "$month $mday,$year";
     $rownum++;
-    echo "<td>$rownum</td><td width 
+    echo "<td>$rownum</td><td width
 =\"200\">$nameofwork</td><td>$username</td><td>$datestamp</td><td>$language</td><td>$genre</td><tr>";
-    
+
    }
-    
+
 echo "</table></body></html>";
 theme("","footer");
 ?>
