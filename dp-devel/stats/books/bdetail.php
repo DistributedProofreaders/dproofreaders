c05db17ae1420dad0ca31a9851be59d61453080e
aldarondo <>
2003-08-25 00:51:09 +0000

    Fixed error on line 8

--- dp-devel/stats/books/bdetail.php
+++ dp-devel/stats/books/bdetail.php
@@ -4,7 +4,6 @@ include_once($relPath.'v_site.inc');
 include_once($relPath.'connect.inc');
 include_once($relPath.'theme.inc');
 include_once('../includes/book.php');
-include('
 $db_Connection=new dbConnect();
 
 $result = mysql_query("SELECT * FROM projects WHERE projectid = ".$_GET['project']."");
@@ -12,6 +11,7 @@ $curProj = mysql_fetch_assoc($result);
 
 theme($curProj['nameofwork']."'s Details", "header");
 echo "<br><center>";
+
 if (!empty($curProj['projectid'])) {
 	showProjProfile($curProj);
 }
