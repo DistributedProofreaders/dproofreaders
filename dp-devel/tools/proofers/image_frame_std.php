1d37181ec5f1556cbd2aa0d4487758c1be9bbef3
usfjoseph <>
2003-09-07 14:30:34 +0000

    Removed preload images

--- dp-devel/tools/proofers/image_frame_std.php
+++ dp-devel/tools/proofers/image_frame_std.php
@@ -22,15 +22,4 @@ width="<?PHP
   echo $iWidth;
 ?>"></div></center>
 
-<?
-//Preload the next 4 pages
-//Load the page that is next.  Then load the page that is 2 pages from the one we are doing
-//Then we load the 4th page & 6th page after what we are currently proofing.
-//This is to help with the possibility that not just one person is proofing the project
-echo "<img alt='Preload Image' width='1' height='1' src='$projects_url/".$npage['project']."/".str_pad(($npage['image']+1),3,0,STR_PAD_LEFT).".png'>\n";
-echo "<img alt='Preload Image' width='1' height='1' src='$projects_url/".$npage['project']."/".str_pad(($npage['image']+2),3,0,STR_PAD_LEFT).".png'>\n";
-echo "<img alt='Preload Image' width='1' height='1' src='$projects_url/".$npage['project']."/".str_pad(($npage['image']+4),3,0,STR_PAD_LEFT).".png'>\n";
-echo "<img alt='Preload Image' width='1' height='1' src='$projects_url/".$npage['project']."/".str_pad(($npage['image']+6),3,0,STR_PAD_LEFT).".png'>\n";
-?>
-
 </body></html>
