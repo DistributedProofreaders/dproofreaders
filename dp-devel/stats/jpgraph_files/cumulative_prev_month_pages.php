84dc9fa30e62c53e036f84546dee696d48318447
big_bill_boy <>
2003-11-01 07:15:44 +0000

    temporary hack to let us see the previous month's results a bit longer

---
+++ dp-devel/stats/jpgraph_files/cumulative_prev_month_pages.php
@@ -0,0 +1,83 @@
+<?
+$relPath="./../../pinc/";
+include_once($relPath.'v_site.inc');
+include_once($jpgraph_dir.'/src/jpgraph.php');
+include_once($jpgraph_dir.'/src/jpgraph_line.php');
+include_once($relPath.'connect.inc');
+new dbConnect();
+
+
+// temporary hack for October only
+
+//Create pages per day graph for current month
+$day = 31;
+$year  = 2003;
+$month = 10;
+
+$today = $year."-".$month."-".$day;
+
+//query db and put results into arrays
+$result = mysql_query("SELECT pages,date,dailygoal FROM pagestats WHERE month = '$month' AND year = '$year' ORDER BY date ASC");
+$i = 0;
+$p = 0;
+$g = 0;
+
+while ($row = mysql_fetch_assoc($result)) {
+	if (substr($row['date'], -2) < $day) { $datay1[$i] = $row['pages']+$p; }
+	$datay2[$i] = $row['dailygoal']+$g;
+	$datax[$i] = $row['date'];
+	$p = $p+$row['pages'];
+	$g = $g+$row['dailygoal'];
+	$i++;
+}
+
+if (empty($datay1)) {
+	$datay1[0] = 0;
+}
+
+// Create the graph. These two calls are always required
+//Last value controls how long the graph is cached for in minutes
+$graph = new Graph(640,400,"auto",1);
+$graph->SetScale("textint");
+$graph->SetMarginColor('white'); //Set background to white
+$graph->SetShadow(); //Add a drop shadow
+$graph->img->SetMargin(70,30,20,100); //Adjust the margin a bit to make more room for titles left, right , top, bottom
+
+//Create the bar plot
+$lplot1 = new LinePlot($datay1);
+$lplot1->SetColor("blue");
+$lplot1->SetWeight(1);
+$lplot1->SetLegend("Total Pages Completed");
+$lplot1->SetFillColor("blue");
+
+//Create the linear goal plot
+$lplot2=new LinePlot($datay2);
+$lplot2->SetColor("limegreen");
+$lplot2->SetWeight(2);
+$lplot2->SetLegend("Monthly Goal");
+
+$graph->Add($lplot1); //Add the linear goal plot to the graph
+$graph->Add($lplot2); //Add the bar pages completed plot to the graph
+
+//set X axis
+$graph->xaxis->SetTickLabels($datax);
+$graph->xaxis->SetLabelAngle(90);
+$graph->xaxis->title->Set("");
+
+//Set Y axis
+$graph->yaxis->title->Set('Pages');
+$graph->yaxis->SetTitleMargin(45);
+
+
+// temp hack
+$graph->title->Set("Cumulative Pages Completed for Oct 2003");
+$graph->title->SetFont(FF_FONT1,FS_BOLD);
+$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
+$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
+
+$graph->legend->Pos(0.05,0.5,"right" ,"top"); //Align the legend
+
+// Display the graph
+$graph->Stroke();
+?>
+
--- dp-devel/stats/stats_central.php
+++ dp-devel/stats/stats_central.php
@@ -86,6 +86,10 @@ echo "<center><img src=\"jpgraph_files/curr_month_pages_graph.php\"></center><br
 echo "<center><img src=\"jpgraph_files/cumulative_month_pages.php\"></center><br>";
 echo "<center><img src=\"jpgraph_files/total_pages_graph.php\"></center><br>";
 
+echo "<hr><br><br>Special temporary graphs showing last month's results:<br><br>"
+echo "<center><img src=\"jpgraph_files/prev_month_pages_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_prev_month_pages.php\"></center><br>";
+
 
 theme('','footer');
 ?>
