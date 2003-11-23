b79fbade708963b38e34db629b10b019758a1a32
big_bill_boy <>
2003-11-23 11:28:45 +0000

    special colour legend - slightly tweaked

--- dp-devel/tools/proofers/proof_per.php
+++ dp-devel/tools/proofers/proof_per.php
@@ -90,7 +90,7 @@ echo "<br><br>";
 <p><font face="<? echo $theme['font_mainbody']; ?>">Legend for Special Books:<br><br><b><span class="orange"> Halloween </span>&nbsp; 
 <span class="green"> Ramadan </span>&nbsp;<span class="paleblue"> recent authors' birthdays </span>&nbsp;
   <span class="richblue"> TODAY's author birthdays </span>&nbsp;<br><span class="yellow"> Children's Book Week </span> 
-   &nbsp;<span class="grey"> Native American Heritage Month </span></b></font></p>
+   &nbsp;<span class="grey"> Native American Heritage Month </span></b></font></p><br>
 
 
 <?
