e641295ac905823254d79124da4d3e2aee555a8f
big_bill_boy <>
2003-11-28 12:58:16 +0000

    Added info on book to delete warning (Task #72) (minor syntax fix)

--- dp-devel/tools/project_manager/changestate.php
+++ dp-devel/tools/project_manager/changestate.php
@@ -28,7 +28,7 @@ include($relPath.'project_edit.inc');
     if ($newstate == PROJ_DELETE && $always != 'yes')
     {
 	// Give them a warning before deleting a project, explaining why it should not be done.
-	echo "<P><B>NOTE:</B> You no longer delete a project from the site, you move it to the posted to Project Gutenberg status. Deleting is only for a project that is beyond repair.';
+	echo "<P><B>NOTE:</B> You no longer delete a project from the site, you move it to the posted to Project Gutenberg status. Deleting is only for a project that is beyond repair.";
 	print "<P>Are you sure you want to change this state and delete $nameofwork by $author ($projectid)? If so, click <A HREF=\"changestate.php?project=$projectid&state=$newstate&always=yes\">here</a>, otherwise back to <a href=\"projectmgr.php\">project listings</a>.";
     }
     else if ($newstate == PROJ_SUBMIT_PG_POSTED)
