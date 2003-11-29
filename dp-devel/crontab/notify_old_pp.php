55c7e5c68a8b113792783b253f1822168a4b0ead
big_bill_boy <>
2003-11-29 14:12:57 +0000

    one mail per PPer (instead of one mail per project) part of Task #2

--- dp-devel/crontab/notify_old_pp.php
+++ dp-devel/crontab/notify_old_pp.php
@@ -9,35 +9,65 @@ $db_Connection=new dbConnect();
     $old_date = time() - 7776000; // 90 days ago.
 
     //get projects that have been checked out longer than old_date
-    $result = mysql_query("SELECT nameofwork, checkedoutby, modifieddate
+    $result = mysql_query("SELECT nameofwork, checkedoutby, modifieddate, projectid, authorsname, 
+				DATE_FORMAT(FROM_UNIXTIME(modifieddate), '%e %M  %Y') as Nicedate
                      FROM projects
-                     WHERE state = '".PROJ_POST_FIRST_CHECKED_OUT."' AND modifieddate <= $old_date");
+                     WHERE state = '".PROJ_POST_FIRST_CHECKED_OUT."' AND modifieddate <= $old_date ORDER BY checkedoutby, modifieddate");
 
     $numrows = mysql_num_rows($result);
     $rownum = 0;
 
+    $PPinQuestion = "";
+    $lastwork = "";	
+    $projectslist = "";
+    $numprojs = 0;
+
    while ($rownum < $numrows) {
+
         $nameofwork = mysql_result($result, $rownum, "nameofwork");
+        $authorsname = mysql_result($result, $rownum, "authorsname");
         $checkedoutby = mysql_result($result, $rownum, "checkedoutby");
         $modifieddate = mysql_result($result, $rownum, "modifieddate");
+	$projectid = mysql_result($result, $rownum, "projectid");
+	$nicedate = mysql_result($result, $rownum, "nicedate");
 
-echo "$nameofwork, $checkedoutby<br>";
-$userresult = mysql_query ("SELECT email FROM users WHERE username = '$checkedoutby'");
-$email = mysql_result($userresult, 0, "email");
+        if ($PPinQuestion != $checkedoutby) {
+	    if ($rownum > 0) {
 
+		    $userresult = mysql_query ("SELECT email FROM users WHERE username = '$PPinQuestion'");
+		    $email = mysql_result($userresult, 0, "email");
+			
+		    echo $PPinQuestion . "\n" . $projectslist ."\n\n";
 
-            maybe_mail("$email", "Distributed Proofreaders: checked out book: $nameofwork ",
-                 "This is an automated message.\n\n".
-"We show that you have had\n\n $nameofwork\n\n checked out for more than 90 days. 
-If you wish to continue working on this project and/or need help please 
+		    $message = "This is an automated message.\n\n".
+"Our database indicates that you have had ". ($numprojs == 1) ? "a project" : "several projects" . " checked out for more than 90 days:\n\n" 
+. $projectslist . "\n\n 
+If you wish to continue working on ". ($numprojs == 1) ? "this project" : "some or all of these projects" . " and/or need help please 
 forward this email with a brief description of the status to dphelp@pgdp.net.\n\n
-If you no longer wish to have this text assigned to you please visit the 
+If you no longer wish to have ". ($numprojs == 1) ? "this text" : "some or all of these texts" . " assigned to you please visit the 
 Distributed Proofreaders website Post Processing section and select Return 
-to Available for this book or forward this email to dphelp@pgdp.net and 
-state that you would no longer like to have the book assigned to you so 
-that we may return it to available.\n\n 
-Thanks!\nThe Distributed Proofreaders Team\n(http://www.pgdp.net)", 
-"From: $auto_email_addr\r\nReply-To: $auto_email_addr\r\n");
+to Available for the ". ($numprojs == 1) ? "book" : "books" . " in question or forward this email to dphelp@pgdp.net and 
+state that you would no longer like to have the ". ($numprojs == 1) ? "book" : "books" . " in question assigned to you so 
+that we may return ".($numprojs == 1) ? "it" : "them" ." to available.\n\n 
+Thanks!\nThe Distributed Proofreaders Team\n(http://www.pgdp.net)";
+		
+//	            maybe_mail("$email", "Subject","$message", "From: $auto_email_addr\r\nReply-To: $auto_email_addr\r\n");
+
+		
+		    $PPinQuestion = $checkedoutby;
+		    $projectslist = "";
+		    $numprojs = 0;
+ 	    }
+	}
+
+	$numprojs++;
+	
+	$projectslist .= "$nameofwork by $authorsname ($projectid), out since $nicedate\n";
+        if (numprojs == 1) {
+		$subject = "Distributed Proofreaders: $nameofwork checked out over 90 days";
+	} else {
+		$subject = "Distributed Proofreaders: $numprojs projects checked out over 90 days";
+	}	
 
         $rownum++;
 }
