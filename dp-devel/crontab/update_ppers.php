331cfe519a8f342f97194d63943caa0f3b89282c
usfjoseph <>
2003-12-10 01:46:12 +0000

    Used to update the users table post processor field with post processors that have uploaded a PP project

---
+++ dp-devel/crontab/update_ppers.php
@@ -0,0 +1,19 @@
+<?
+$relPath='./../pinc/';
+include($relPath.'v_site.inc');
+include($relPath.'connect.inc');
+$db_Connection=new dbConnect();
+
+$result = mysql_query("SELECT DISTINCT(postproofer) FROM projects");
+while ($row = mysql_fetch_assoc($result)) {
+	$isPPQuery = mysql_query("SELECT postprocessor FROM users WHERE username = '".$row['postproofer']."'");
+	$isPP = mysql_result($isPPQuery, 0, "postprocessor");
+	if ($isPP != "yes") {
+		$updatePPQuery = mysql_query("UPDATE users SET postprocessor = 'yes' WHERE username = '".$row['postproofer']."'");
+	}
+}
+
+echo "<center>Update of Post Processors Complete!</center>";
+
+?>
+
