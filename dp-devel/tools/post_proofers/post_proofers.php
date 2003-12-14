9139565f3e425e65499f2ed7e8a5991b2f042bb9
aldarondo <>
2003-12-14 14:52:52 +0000

    Updated comments to be different for experienced volunteer & new user, standardized terminology to be post processing and e-text.

--- dp-devel/tools/post_proofers/post_proofers.php
+++ dp-devel/tools/post_proofers/post_proofers.php
@@ -6,80 +6,69 @@ include_once($relPath.'theme.inc');
 include($relPath.'bookpages.inc');
 include($relPath.'show_projects_in_state.inc');
 include_once($relPath.'user_is.inc');
+
 theme("Post Processing", "header");
 
+$isPPV = user_is_proj_facilitator();
+
+if (!$isPPV) {
 echo "
 <br>
 <p>
-    This is the post processing section.
     The books listed below have already gone through
     two rounds of proofreading on this site
     and they now need to be massaged into a final e-text.
     Once you have checked out and downloaded a book
     it will remain checked out to you until you check it back in. 
-    When you have finished your work on the book, choose Upload 
-    for Verification from the drop-down list for that project. If
+    When you have finished your work on the book, select <i>Upload 
+    for Verification</i> from the drop-down list for that project. If
     you have several files to submit for a single project (say 
     a text and HTML version), zip them up together first.
 </p>
 <p>
-    Here is a <a href='$code_url/faq/post_proof.php'><b>Post Proof FAQ</b></a>
-    that details all of the steps that we normally take to post proof an etext.
-    There is a <a href='$post_processing_forum_url'>forum</a>
-    to post all your questions.
-    If no books are listed that means no work is currently available --
-    please check back later!
-</p>
-<p>
     <b>First Time here?</b>
-    Juliet Sutherland (JulietS) is our Post Processing Coordinator. Julie Barkley
-    (juliebarkley) maintains our PP FAQ. If this is your first time here...
-    Please read the FAQ, read it again ;), select an easy work to get started on
-    (usually fiction with a low page count is a good starter book; projects
-    whose manager was BEGIN make excellent first projects for a new PPer)
-    and write <a href='mailto:juliet.sutherland@verizon.net'>Juliet</a>
-    with any questions/comments.
-</p>
+    Juliet Sutherland (<a href='http://www.pgdp.net/phpBB2/privmsg.php?mode=post&u=1396'>JulietS</a>) 
+    is our Post Processing Coordinator. Julie Barkley
+    (<a href='http://www.pgdp.net/phpBB2/privmsg.php?mode=post&u=1674'>juliebarkley</a>)
+    maintains our <a href='$code_url/faq/post_proof.php'>Post Processing FAQ</a>.
+    Please read the FAQ as it covers all the steps needed to post process an e-text.
+    Select an easy work to get started on (usually fiction with a low page count is a 
+    good starter book; projects whose manager is BEGIN make excellent first projects
+    for a new post processor) and check out the <a href='$post_processing_forum_url'>
+    Post Processing Forum</a> to post all your questions. If nothing interests you
+    rigt now, check back later and there will be more!
+</p>";
+} else {
+echo "
+<br>
 <p>
-    If you are new to this page, make sure you only select projects
-    from the Books Available for Post-Processing section (and NOT the
-    Books Available for Post-Processing Verification & Posting section, 
-    which are reserved for an invited list of experienced volunteers only).
-</p>
-<p>
-    PPV is like a second round for PPed books; it allows another set of 
-    eyes to scan the books and make sure no errors are missed.  PPVers 
-    also give feedback to PPers, so that new PPers can learn the ropes 
-    a little faster.  Because PPV involves both feedback to new PPers 
-    and looking for easily-missed errors, only experienced PPers who 
-    receive permission can check out PPV books. Please do not check out 
-    these books if you have not yet received PPV clearance.
-</p>
-<p>
-    <hr width=75% align='center'>
+    As an experienced volunteer, you have access to do verification of texts
+    that have been Post Processed already. The <a href='$code_url/faq/post_proof.php'>
+    Post Processing FAQ</a> is available for more details on the role and the
+    <a href='$post_processing_forum_url'>Post Processing Forum</a> is available
+    for any of your questions.
 </p>
 ";
+}
 
-$isPPV = user_is_proj_facilitator();
-
-echo "<center><b>Books I Have Checked Out for Post-Processing:</b></center>";
+echo "<hr width=75% align='center'><center><b>Books I Have Checked Out for Post Processing:</b></center>";
 show_projects_in_state(PROJ_POST_FIRST_CHECKED_OUT);
 echo "<br>";
 
 if ($isPPV) {
 
-echo "<center><b>Books I Have Checked Out for Verifying Post-Processing:</b></center>";
+echo "<center><b>Books I Have Checked Out for Verifying Post Processing:</b></center>";
 show_projects_in_state(PROJ_POST_SECOND_CHECKED_OUT);
 echo "<br>";
 }
 
-echo "<center><b>Books Available for Post-Processing:</b></center>";
+echo "<center><b>Books Available for Post Processing:</b></center>";
 show_projects_in_state(PROJ_POST_FIRST_AVAILABLE);
 echo "<br>";
 
 if ($isPPV) {
 
-echo "<center><b>Books Available for Post-Processing Verification & Posting:</b></center>";
+echo "<center><b>Books Available for Post Processing Verification & Posting:</b></center>";
 show_projects_in_state(PROJ_POST_SECOND_AVAILABLE);
 echo "<br>";
 }
