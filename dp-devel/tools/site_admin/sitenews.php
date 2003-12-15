5f39060739de126faf26c30d7187ee3ee1c3e111
aldarondo <>
2003-12-15 22:45:58 +0000

    Added documentation to the code and made it more readable

--- dp-devel/tools/site_admin/sitenews.php
+++ dp-devel/tools/site_admin/sitenews.php
@@ -11,65 +11,64 @@ $result = mysql_query("SELECT * FROM users WHERE username = '$pguser'");
 
 if (mysql_result($result,0,"sitemanager") == "yes") {
 
-if (isset($_GET['action']) && $_GET['action'] == "add") {
-$message = strip_tags($_POST['message'], '<a><b><i><u><font>');
-$message = nl2br($message);
-$date_posted = date(U);
-$insert_news = mysql_query("INSERT INTO news (uid, date_posted, message) VALUES (NULL, '$date_posted', '$message')");
-header("Location: sitenews.php");
-}
+    // Save a new site news
+    if (isset($_GET['action']) && $_GET['action'] == "add") {
+        $message = strip_tags($_POST['message'], '<a><b><i><u><font>');
+        $message = nl2br($message);
+        $date_posted = date(U);
+        $insert_news = mysql_query("INSERT INTO news (uid, date_posted, message) VALUES (NULL, '$date_posted', '$message')");
+        header("Location: sitenews.php");
+    }
+    // View a specific site news
+    elseif (isset($_GET['action']) && $_GET['$action'] == "view") {
+        $uid = $_GET['uid'];
+        $result = mysql_query("SELECT * FROM news WHERE uid = $uid");
+        $date_posted = date("l, F jS, Y",mysql_result($result,0,'date_posted'));
+        echo "<b>$date_posted</b><br>";
+        echo mysql_result($result,0,"message");
+        echo "<br><br><a href='javascript:history.back()'>Go Back...</a>";
+    }
+    // Delete a specific site news
+    elseif (isset($_GET['action']) && $_GET['action'] == "delete") {
+        $uid = $_GET['uid'];
+        $result = mysql_query("DELETE FROM news WHERE uid=$uid");
+        header("Location: sitenews.php");
+    }
+    // Save an update to a specific site news
+    elseif (isset($_GET['action']) && $_GET['action'] == "edit_update") {
+        $message = $_POST['message'];
+        $message = strip_tags($_POST['message'], '<a><b><i><u><font>');
+        $message = nl2br($message);
+        $uid = $_POST['uid'];
+        $result = mysql_query("UPDATE news SET message='$message' WHERE uid=$uid");
+        header("Location: sitenews.php");
+    }
+    // Add/Edit form for a specific site news
+    else {
+        $action = "add";
+        $submit_query = "Add Site Update";
+        if (isset($_GET['action']) && $_GET['action'] == "edit") {
+            $uid = $_GET['uid'];
+            $result = mysql_query("SELECT * FROM news WHERE uid=$uid");
+            $message = mysql_result($result,0,"message");
+            $action = "edit_update";
+            $submit_query = "Edit Site Update";
+        }
+        if (empty($message)) { $message = ""; }
 
-elseif (isset($_GET['action']) && $_GET['$action'] == "view") {
-$uid = $_GET['uid'];
-$result = mysql_query("SELECT * FROM news WHERE uid = $uid");
-$date_posted = date("l, F jS, Y",mysql_result($result,0,'date_posted'));
-echo "<b>$date_posted</b><br>";
-echo mysql_result($result,0,"message");
-echo "<br><br><a href='javascript:history.back()'>Go Back...</a>";
+        echo "<form action='sitenews.php?action=$action' method='post'>";
+        echo "<center><textarea name='message' cols=50 rows=5>$message</textarea><br><input type='submit' value='$submit_query' name='submit'></center><br><br>";
+        echo "<input type='hidden' name='uid' value='$uid'></form>";
+        $result = mysql_query("SELECT * FROM news ORDER BY uid DESC");
+        while($row = mysql_fetch_array($result)) {
+            $date_posted = date("l, F jS, Y",$row['date_posted']);
+            echo "[<a href='sitenews.php?action=view&uid=".$row['uid']."')'>View</a>]&nbsp;[<a href='sitenews.php?action=edit&uid=".$row['uid']."'>Edit</a>]&nbsp;[<a href='sitenews.php?action=delete&uid=".$row['uid']."'>Delete</a>] -- ";
+            echo $row['message']." ($date_posted)<br><br>";
+        }
+    }
 }
-
-elseif (isset($_GET['action']) && $_GET['action'] == "delete") {
-$uid = $_GET['uid'];
-$result = mysql_query("DELETE FROM news WHERE uid=$uid");
-header("Location: sitenews.php");
-}
-
-elseif (isset($_GET['action']) && $_GET['action'] == "edit_update") {
-$message = $_POST['message'];
-$message = strip_tags($_POST['message'], '<a><b><i><u><font>');
-$message = nl2br($message);
-$uid = $_POST['uid'];
-$result = mysql_query("UPDATE news SET message='$message' WHERE uid=$uid");
-header("Location: sitenews.php");
-}
-
 else {
-$action = "add";
-$submit_query = "Add Site Update";
-if (isset($_GET['action']) && $_GET['action'] == "edit") {
-$uid = $_GET['uid'];
-$result = mysql_query("SELECT * FROM news WHERE uid=$uid");
-$message = mysql_result($result,0,"message");
-$action = "edit_update";
-$submit_query = "Edit Site Update";
-}
-
-if (empty($message)) { $message = ""; }
-
-echo "<form action='sitenews.php?action=$action' method='post'>";
-echo "<center><textarea name='message' cols=50 rows=5>$message</textarea><br><input type='submit' value='$submit_query' name='submit'></center><br><br>";
-echo "<input type='hidden' name='uid' value='$uid'></form>";
-$result = mysql_query("SELECT * FROM news ORDER BY uid DESC");
-while($row = mysql_fetch_array($result)) {
-$date_posted = date("l, F jS, Y",$row['date_posted']);
-echo "[<a href='sitenews.php?action=view&uid=".$row['uid']."')'>View</a>]&nbsp;[<a href='sitenews.php?action=edit&uid=".$row['uid']."'>Edit</a>]&nbsp;[<a href='sitenews.php?action=delete&uid=".$row['uid']."'>Delete</a>] -- ";
-echo $row['message']." ($date_posted)<br><br>";
-}
-}
-
-} else {
-
-echo "You are not authorized to use this form.";
+    echo "You are not authorized to use this form.";
 }
 
 theme("", "footer");
