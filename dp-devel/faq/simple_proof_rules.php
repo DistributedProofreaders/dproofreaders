038fe81527bbcf376f68d5a6b584fc9a8d6d2b11
big_bill_boy <>
2003-12-19 12:10:43 +0000

    syntax tweak

--- dp-devel/faq/simple_proof_rules.php
+++ dp-devel/faq/simple_proof_rules.php
@@ -4,7 +4,7 @@ include($relPath.'theme.inc');
 $no_stats=1;
 theme('Beginners' Simple Proofing Rules','header');
 echo <br><br>;
-include('simple_proof_text.inc');
+include($relPath.'simple_proof_text.inc');
 echo <br><br>;
 theme('','footer');
 ?>
