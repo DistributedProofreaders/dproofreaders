13eac160a9a23a758528a33bb1d60d3acfcf5ced
big_bill_boy <>
2003-12-20 10:58:59 +0000

    syntax correction

--- dp-devel/faq/simple_proof_rules.php
+++ dp-devel/faq/simple_proof_rules.php
@@ -3,7 +3,7 @@ $relPath='../pinc/';
 include($relPath.'v_site.inc');
 include($relPath.'theme.inc');
 $no_stats=1;
-theme('Beginners' Simple Proofing Rules','header');
+theme("Beginners' Simple Proofing Rules",'header');
 echo <br><br>;
 include($relPath.'simple_proof_text.inc');
 echo <br><br>;
