f7495b315d16fce804b6544530b4ef2ede549890
big_bill_boy <>
2003-12-20 11:19:24 +0000

    syntax correction

--- dp-devel/faq/simple_proof_rules.php
+++ dp-devel/faq/simple_proof_rules.php
@@ -4,8 +4,8 @@ include($relPath.'v_site.inc');
 include($relPath.'theme.inc');
 $no_stats=1;
 theme("Beginners' Simple Proofing Rules",'header');
-echo <br><br>;
+echo "<br><br>";
 include($relPath.'simple_proof_text.inc');
-echo <br><br>;
+echo "<br><br>";
 theme('','footer');
 ?>
