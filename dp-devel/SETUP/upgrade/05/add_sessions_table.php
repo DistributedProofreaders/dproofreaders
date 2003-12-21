a229d805f8b989d5f5306202534891ed3a1ff79f
usfjoseph <>
2003-12-21 21:37:11 +0000

    ONE TIME USE SCRIPT -- Used to add sessions table to the database to allow for PHP Sessions

---
+++ dp-devel/SETUP/upgrade/05/add_sessions_table.php
@@ -0,0 +1,16 @@
+<?
+$relPath='./pinc/';
+include($relPath.'connect.inc');
+$db_Connection=new dbConnect();
+
+//----------------------------------------------------------------------------------------------------------------------
+$result = mysql_query("CREATE TABLE `session` (
+`sid` varchar(32) NOT NULL default '',
+`expiration` int(11) NOT NULL default '0',
+`value` text NOT NULL,
+PRIMARY KEY  (`sid`)
+)");
+
+echo "<center>";
+echo "<p>Addition of new table 'sessions' complete!";
+?>
