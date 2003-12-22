77869c117ed09dc0ab1d5278b7527606fb9698fb
aldarondo <>
2003-12-22 04:21:17 +0000

    Added control to limiting number of news and RSS/XML links

--- dp-devel/pastnews.php
+++ dp-devel/pastnews.php
@@ -1,20 +1,27 @@
 <?
 $relPath="./pinc/";
-include($relPath.'dp_main.inc');
+include_once($relPath.'dp_main.inc');
 include_once($relPath.'theme.inc');
 
 // Very basic display of the past news stories
 //
 // Sorts the news by their id's and then prints one by one.
-//
-// Possible future improvements:
-//   Show only X of the latest news
-//   RSS Feed of the site news
 
 theme("Archived Site Updates", "header");
 
-$result = mysql_query("SELECT * FROM news ORDER BY uid DESC");
+echo "<center>Feeds: <a href='$code_url/feeds/backend.php?content=news'><img src='$code_url/graphics/xml.gif'></a>";
+echo "<a href='$code_url/feeds/backend.php?content=news&type=rss'><img src='$code_url/graphics/rss.gif'></a>";
 
+if (isset($_GET['num'])) {
+    $num = " LIMIT ".$_GET['num'];
+    echo " <a href='pastnews.php'>Show All News</a>";
+} else $num = "";
+
+echo "</center>";
+
+$result = mysql_query("SELECT * FROM news ORDER BY uid DESC".$num);
+
+$total = 1;
 while($row = mysql_fetch_array($result)) {
     $date_posted = date("l, F jS, Y",$row['date_posted']);
     echo "<br><a name='".$row['uid']."'><b>$date_posted</b><br>".$row['message']."<br><hr align='center' width='75%'><br>";
