3b4633b86fa83180010a97ecca6d598a30e1ca21
usfjoseph <>
2003-12-23 11:40:14 +0000

    Misspelling of the table name

--- dp-devel/SETUP/upgrade/05/add_sessions_table.php
+++ dp-devel/SETUP/upgrade/05/add_sessions_table.php
@@ -4,7 +4,7 @@ include($relPath.'connect.inc');
 $db_Connection=new dbConnect();
 
 //----------------------------------------------------------------------------------------------------------------------
-$result = mysql_query("CREATE TABLE `session` (
+$result = mysql_query("CREATE TABLE `sessions` (
 `sid` varchar(32) NOT NULL default '',
 `expiration` int(11) NOT NULL default '0',
 `value` text NOT NULL,
