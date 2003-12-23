6c1155e60e08e76de6fc7c07a8e3fff5599c9bfe
big_bill_boy <>
2003-12-23 11:52:27 +0000

    echos number of projects created, proofed and PPd since start of month

---
+++ dp-devel/stats/statestats.php
@@ -0,0 +1,62 @@
+<?php
+$relPath='./../pinc/';
+include($relPath.'v_site.inc');
+include($relPath.'connect.inc');
+include('statestats.inc');
+$db_Connection=new dbConnect();
+
+
+
+// display project count progress - here for the moment, can be moved to stats bar later
+$cday = date('d'); $cmonth = date('m'); $cyear = date('Y');
+$today = date('Y-m-d');
+
+if ($cday != 1) {
+    $start_date = $cyear."-".$cmonth."-01";
+    $descrip = _("so far this month");
+} else {
+    $descrip = _("since the start of last month");
+    if ($cmonth != 1) {
+	$temp = $cmonth -1;
+	$start_date = $cyear."-".$temp."-01";
+    } else {
+	$temp = $cyear - 1;
+	$start_date = $temp."-12-01";
+    }
+}
+
+echo "Today is $today, start is $start_date, descrip is $descrip  ";
+
+	
+
+$created = state_change_since ( "
+				state != 'project_new'
+				",$start_date);
+
+
+echo "br"
+echo "<b>$created</b>._("projects have been created")." $descrip<br>";
+
+$FinProof = state_change_since ( "
+				(state LIKE 'proj_submit%' 
+				OR state LIKE 'proj_correct%' 
+				OR state LIKE 'proj_post%')
+			",$start_date);
+
+
+echo "br"
+echo "<b>$FinProof</b>._("projects have finished proofing")." $descrip<br>";
+
+
+$FinPP = state_change_since ( "
+				(state LIKE 'proj_submit%' 
+				OR state LIKE 'proj_correct%' 
+				OR state LIKE 'proj_post_second%')
+	",$start_date);
+
+
+echo "br"
+echo "<b>$FinPP</b>._("projects have finished PPing")." $descrip<br>";
+
+
+?>
