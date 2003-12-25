62fb5c865ecd92acee36966bb5105b89136f6524
aldarondo <>
2003-12-25 14:19:12 +0000

    Added simple confirmation for deleting a file from a project

--- dp-devel/tools/project_manager/deletefile.php
+++ dp-devel/tools/project_manager/deletefile.php
@@ -7,15 +7,18 @@ include($relPath.'project_edit.inc');
 
     $project = $_GET['project'];
     $fileid = $_GET['fileid'];
+    $confirm = $_GET['confirm'];
     
     abort_if_cant_edit_project( $project );
 
-    {
+    if (!empty($confirm)) {
         if ($fileid == '') {
            $sql = "DELETE FROM $project WHERE 1";
         } else $sql = "DELETE FROM $project WHERE fileid = '$fileid'";
         mysql_query($sql);
 
         metarefresh(0, "project_detail.php?project=$project", "Page(s) Deleted", ""); 
+    } else {
+        echo "Confirm delete of $fileid <a href="deletefile.php?project=$project&fileid=$fileid&confirm=yes">here</a>";
     }
 ?>
