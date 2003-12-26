0f8ad84c67268937b6b8f5e8ba6471a3d9d92b52
aldarondo <>
2003-12-26 03:44:17 +0000

    Added form to adjust image size using same percentage as the end-users would do

--- dp-devel/tools/project_manager/displayimage.php
+++ dp-devel/tools/project_manager/displayimage.php
@@ -4,6 +4,16 @@ $relPath='../../pinc/';
 include($relPath.'v_site.inc');
 $project = $_GET['project'];
 $imagefile = $_GET['imagefile'];
-printf ("<img src=\"$projects_url/%s/%s\" width=750 border=0>", $project, $imagefile);
+
+if (!empty($_GET['percent'])) {
+    $width = 10 * $_GET['percent'];
+} else $width = 1000;
+
+printf ("<img src=\"$projects_url/%s/%s\" width=$width border=0>", $project, $imagefile);
 ?>
+<form method=get action="displayimage.php">
+<input type=hidden name="project" value="<?echo $project;?>">
+<input type=hidden name="imagefile" value="<?echo $imagefile;?>">
+<input type=text maxlength=3 name="percent"> % <input type="submit" value="Resize">
+</form>
 </body></html>
