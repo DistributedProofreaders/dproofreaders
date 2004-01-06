5e03ab7056aae4bbe60259419ecd21b290df48c3
big_bill_boy <>
2004-01-06 09:28:47 +0000

    Summary info useful for mentors: For currently available MENTORS books, which beginner did which page and how many pages each beginner did

---
+++ dp-devel/tools/proofers/for_mentors.php
@@ -0,0 +1,64 @@
+<?
+$relPath='./../../pinc/';
+include_once($relPath.'dp_main.inc');
+include_once($relPath.'f_dpsql.inc');
+include_once($relPath.'theme.inc');
+include_once($relPath.'project_states.inc');
+
+theme("For Mentors", "header");
+
+echo "<br><br><h2>For Mentors</h2><br>\n";
+
+echo "<br>\n";
+
+
+// MENTORS ONLY books available in Round 2
+
+
+echo "<h3>Currently Available MENTORS ONLY projects</h3><br><br>\n";
+
+$result = mysql_query("SELECT projectid, nameofwork, authorsname FROM projects WHERE difficulty = 'beginner' AND
+			state='".PROJ_PROOF_SECOND_AVAILABLE."' || state='".PROJ_PROOF_SECOND_VERIFY."'
+			ORDER BY modifieddate ASC");
+
+while ($row =  mysql_fetch_array($result)) 
+{
+
+echo "$row['nameofwork'] $row['authorsname']<br><br>"; 
+
+dpsql_dump_query("
+	SELECT
+		SUBSTRING_INDEX(image,'.',1) as 'Page', round1_user as 'Proofer'
+	FROM $row['projectid']
+	ORDER BY 1
+");
+
+echo "<br><br>";
+
+dpsql_dump_query("
+	SELECT
+		round1_user as 'Proofer', count(image) as 'Pages done in this project'
+	FROM $row['projectid']
+	GROUP BY 1
+	ORDER BY 1
+");
+
+
+echo "<br><br><br>\n";
+
+}
+
+
+
+dpsql_dump_query("
+	SELECT
+		SUM(pages) as 'Total Pages Proofed So Far'
+	FROM pagestats
+");
+
+
+
+
+theme("","footer");
+?>
+
