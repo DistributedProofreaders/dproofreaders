3d254b3e06cad5f0d61c085b673055fa482dba5f
big_bill_boy <>
2004-01-06 14:20:24 +0000

    creates table to store project state stats TABLE ALREADY ON LIVE SITE!!!

---
+++ dp-devel/SETUP/upgrade/05/add_project_state_stats_table.php
@@ -0,0 +1,21 @@
+<?php
+$relPath='./pinc/';
+include($relPath.'connect.inc');
+$db_Connection=new dbConnect();
+
+//----------------------------------------------------------------------------------------------------------------------
+$result = mysql_query("CREATE TABLE `project_state_stats` (
+  `year` smallint(4) NOT NULL default '0',
+  `month` tinyint(2) NOT NULL default '0',
+  `day` tinyint(2) NOT NULL default '0',
+  `date` date  NOT NULL default '0000-00-00',
+  `state` varchar(50) NOT NULL default '0',
+  `num_projects` int(12) NOT NULL default '0',
+  `comments` varchar(255)
+  KEY `task_id` (`task_id`)
+) TYPE=MyISAM");
+
+
+echo "<center>";
+echo "<p>Addition of 'project_state_stats' table complete!";
+?>
