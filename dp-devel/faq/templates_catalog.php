60d1e7dd9e94d6735c1b7c3014ff3cb9656db3ca
big_bill_boy <>
2004-01-09 06:49:02 +0000

    added sample fill-in-the-blank layouts

--- dp-devel/faq/templates_catalog.php
+++ dp-devel/faq/templates_catalog.php
@@ -25,7 +25,7 @@ margin-right: 3.18cm; margin-top: 2.54cm; margin-bottom: 2.54cm }
 <!-- BG1a.txt info -->
 <a name="bg1a"></a>
 <?
-include($relpath."templates/comment_files/BG1a.txt");
+include(../pinc/templates/comment_files/BG1a.txt');
 ?>
 		
 
@@ -58,8 +58,83 @@ include($relpath."templates/comment_files/BG1a.txt");
 
 <!-- This code was made by cedron -->
 <a name="cedrlayout"></a> <strong>LAYOUT</strong> 
+<p>
+<HR><blockquote>
+<font COLOR="#000099" SIZE="+1">Project vitals : </font> 
+&nbsp;&nbsp;&nbsp;&nbsp;Publication Date: 1000 &nbsp;&nbsp; Author Lifespan: 
+1000-1000
+</blockquote>
 
+<HR><blockquote>
+<font COLOR="#000099" SIZE="+1">Deviations from the standard Proofing 
+Guidelines :</font>&nbsp;&nbsp; There are no exceptions to the primary 
+guidelines required for&nbsp;this&nbsp;project.
+</blockquote>
+
+<HR><blockquote>
+<font COLOR="#000099" SIZE="+1">Difficulty details :</font> AVERAGE<br>
+&nbsp;&nbsp;This page is 90% Easy, with a few difficult pages and some 
+poor OCR scanning
+</blockquote>
+
+<HR><blockquote>
+<font COLOR="#000099" SIZE="+1">What to watch for :</font> &nbsp;&nbsp; 
+Extra markings such as * . , scattered throughout the pages.
+</blockquote>
+
+<HR><br>
+
+<blockquote>
+
+<font COLOR="#9C9C63" SIZE="+2"><b> ~TITLE~ </b><br>
+By ~AUTHOR~ </font><br><br>
+<b> ~PUBLISHER, CITY, DATE~ </b><br><br><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+
+</blockquote>
+<br>
+</p>
+<hr>
+<br>
+<br>
+<br>
 <strong>CODE</strong> <a name="cedrcode"></a>
+<p>
+&lt;p&gt;<br>
+&lt;HR&gt;&lt;blockquote&gt;<br>
+&lt;font COLOR=&quot;#000099&quot; SIZE=&quot;+1&quot;&gt;Project 
+vitals : &lt;/font&gt; 
+&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;<br>
+&lt;/blockquote&gt;
+<p>&lt;HR&gt;&lt;blockquote&gt;<br>
+  &lt;font COLOR=&quot;#000099&quot; SIZE=&quot;+1&quot;&gt;Deviations 
+from the 
+  standard Proofing Guidelines :&lt;/font&gt;&amp;nbsp;&amp;nbsp; <br>
+  &lt;/blockquote&gt;</p>
+<p>&lt;HR&gt;&lt;blockquote&gt;<br>
+  &lt;font COLOR=&quot;#000099&quot; SIZE=&quot;+1&quot;&gt;Difficulty 
+details 
+  :&lt;/font&gt; &amp;nbsp;&amp;nbsp; <br>
+  &lt;/blockquote&gt;</p>
+<p>&lt;HR&gt;&lt;blockquote&gt;<br>
+  &lt;font COLOR=&quot;#000099&quot; SIZE=&quot;+1&quot;&gt;What to 
+watch for 
+  :&lt;/font&gt; &amp;nbsp;&amp;nbsp; <br>
+  &lt;/blockquote&gt;</p>
+<p>&lt;HR&gt;&lt;br&gt;</p>
+<p>&lt;blockquote&gt;</p>
+<p>&lt;font COLOR=&quot;#9C9C63&quot; SIZE=&quot;+2&quot;&gt;&lt;b&gt; 
+~TITLE~ 
+  &lt;/b&gt;&lt;br&gt;<br>
+  By ~AUTHOR~ &lt;/font&gt;&lt;br&gt;&lt;br&gt;<br>
+  &lt;b&gt; ~PUBLISHER, CITY, DATE~ 
+&lt;/b&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</p>
+<p>&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;</p>
+<p>&lt;/blockquote&gt;&lt;br&gt;</p>
+<p>&lt;/p&gt; </p>
+
+
 
 
 
@@ -68,8 +143,257 @@ include($relpath."templates/comment_files/BG1a.txt");
 
 <!-- This code was made by cgehring -->
 <a name="cgehlayout"></a> <strong>LAYOUT</strong> 
+<center>
+  <table width="100%" table bgcolor="#FFFFFF" border="1" 
+bordercolor="#111111" cellspacing="0" cellpadding="2">
+    <tr>
+      <td bgcolor="#336633" align=center colspan=2>
+        <font size="+2" color="#FFFFFF">
+          <b>
+            Special Instructions and Proofing Notes:
+          </b>
+        </font>
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="#e0e8dd" width="160" align=left valign=top>
+        Special Instructions:
+      </td>
+      <td bgcolor="#FFFFFF" width="*" align=left valign=top>
+        *****PUT YOUR SPECIAL INSTRUCTIONS HERE*****
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="#e0e8dd" width="160" align=left valign=top>
+        Notes:
+      </td>
+      <td bgcolor="#FFFFFF" width="*" align=left valign=top>
+        *****PUT ANY NOTES TO THE PROOFERS HERE*****
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="#e0e8dd" width="160" align=left valign=top>
+        Copyright Information:
+      </td>
+      <td bgcolor="#FFFFFF" width="*" align=left valign=top>
+        *****PUT COPYRIGHT AND PRINTING INFORMATION HERE*****
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="#336633" align=center colspan=2>
+        <font size="+2" color="#FFFFFF">
+          <b>
+            Author Information:
+          </b>
+        </font>
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="#e0e8dd" width="160" align=left valign=top>
+        Date of Birth:
+      </td>
+      <td bgcolor="#FFFFFF" width="*" align=left valign=top>
+        *****PUT BIRTHDATE AND PLACE HERE*****
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="#e0e8dd" width="160" align=left valign=top>
+        Date of Death:
+      </td>
+      <td bgcolor="#FFFFFF" width="*" align=left valign=top>
+        *****PUT DEATHDATE AND PLACE HERE*****
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="#e0e8dd" width="160" align=left valign=top>
+        Biography:
+        <br><br>
+        <img src="" alt="*****PUT THE URL OF THE ARTISTS PICTURE HERE. 
+I RECOMEND UPLOADING IT WITH YOUR OTHER PNG FILES AND CALLING IT 
+SOMETHING DESCRIPTIVE, LIKE BIO.PNG *BE SURE THAT WHATEVER YOU CALL IT, YOU 
+DO NOT HAVE A TEXT FILE THAT IS TITLED THE SAME!* THAT MEANS THAT IF YOU 
+CALL THE PNG BIO.PNG, DO NOT UPLOAD A TEXT FILE NAMED BIO.TXT*****" 
+width="150">
+      </td>
+      <td bgcolor="#FFFFFF" width="*" align=left valign=top>
+        *****PUT BIOGRAPHICAL INFORMATION HERE. I SUGGEST USING GOOGLE 
+TO FIND INFORMATION. SOMETIMES THE JACKET COVER OF THE BOOK WILL HAVE A 
+SHORT BIO AS WELL. REMEMBER, THE BOOK MAY HAVE BEEN PRINTED BEFORE THE 
+AUTHOR DIED, SO PAY SPECIAL ATTENTION TO THAT.*****
+      </td>
+    </tr>
+  </table>
+  <table width="100%" table bgcolor="#FFFFFF" border="1" 
+bordercolor="#111111" cellspacing="0" cellpadding="2"> 
+    <tr>
+      <td bgcolor="#336633" align=center colspan=2>
+        <font size="+2" color="#FFFFFF">
+          <b>
+            Books by This Author in PG:
+          </b>
+        </font>
+      </td>
+    </tr>
+    <tr>
+      <td bgcolor="FFFFFF" align=left valign=top width="50%">
+*****PUT OTHER BOOKS BY THE AUTHOR IN PG HERE. AT THE END OF EACH 
+TITLE, PUT THE &lt;br&gt; TAG.
+EXAMPLE:
+BOOK BY THE AUTHOR<br>
+ANOTHER BOOK<br>
+PUT HALF OF THE TITLES HERE,
+      </td>
+      <td bgcolor="FFFFFF" align=left valign=top width="50%">
+AND THE OTHER HALF OF THE TITLES HERE. I SUGGEST PUTTING THEM IN 
+ALPHABETICAL ORDER WITH THE FIRST HALF IN THE FIRST COLUMN, AND THE SECOND 
+HALF IN THE SECOND COLUMN.*****
+      </td>
+    </tr>
+  </table>
+<hr>
+<br>
+<br>
+<br>
 <strong>CODE</strong> <a name="cgehcode"></a>
-
+  <p align="left">&lt;!-- This code was made by cgehring --&gt;<br>
+    &lt;center&gt;<br>
+    &lt;table width=&quot;100%&quot; table bgcolor=&quot;#FFFFFF&quot; 
+border=&quot;1&quot; 
+    bordercolor=&quot;#111111&quot; cellspacing=&quot;0&quot; 
+cellpadding=&quot;2&quot;&gt;<br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;#336633&quot; align=center colspan=2&gt;<br>
+    &lt;font size=&quot;+2&quot; color=&quot;#FFFFFF&quot;&gt;<br>
+    &lt;b&gt;<br>
+    Special Instructions and Proofing Notes:<br>
+    &lt;/b&gt;<br>
+    &lt;/font&gt;<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;#e0e8dd&quot; width=&quot;160&quot; align=left 
+valign=top&gt;<br>
+    Special Instructions:<br>
+    &lt;/td&gt;<br>
+    &lt;td bgcolor=&quot;#FFFFFF&quot; width=&quot;*&quot; align=left 
+valign=top&gt;<br>
+    *****PUT YOUR SPECIAL INSTRUCTIONS HERE*****<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;#e0e8dd&quot; width=&quot;160&quot; align=left 
+valign=top&gt;<br>
+    Notes:<br>
+    &lt;/td&gt;<br>
+    &lt;td bgcolor=&quot;#FFFFFF&quot; width=&quot;*&quot; align=left 
+valign=top&gt;<br>
+    *****PUT ANY NOTES TO THE PROOFERS HERE*****<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;#e0e8dd&quot; width=&quot;160&quot; align=left 
+valign=top&gt;<br>
+    Copyright Information:<br>
+    &lt;/td&gt;<br>
+    &lt;td bgcolor=&quot;#FFFFFF&quot; width=&quot;*&quot; align=left 
+valign=top&gt;<br>
+    *****PUT COPYRIGHT AND PRINTING INFORMATION HERE*****<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;#336633&quot; align=center colspan=2&gt;<br>
+    &lt;font size=&quot;+2&quot; color=&quot;#FFFFFF&quot;&gt;<br>
+    &lt;b&gt;<br>
+    Author Information:<br>
+    &lt;/b&gt;<br>
+    &lt;/font&gt;<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;#e0e8dd&quot; width=&quot;160&quot; align=left 
+valign=top&gt;<br>
+    Date of Birth:<br>
+    &lt;/td&gt;<br>
+    &lt;td bgcolor=&quot;#FFFFFF&quot; width=&quot;*&quot; align=left 
+valign=top&gt;<br>
+    *****PUT BIRTHDATE AND PLACE HERE*****<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;#e0e8dd&quot; width=&quot;160&quot; align=left 
+valign=top&gt;<br>
+    Date of Death:<br>
+    &lt;/td&gt;<br>
+    &lt;td bgcolor=&quot;#FFFFFF&quot; width=&quot;*&quot; align=left 
+valign=top&gt;<br>
+    *****PUT DEATHDATE AND PLACE HERE*****<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;#e0e8dd&quot; width=&quot;160&quot; align=left 
+valign=top&gt;<br>
+    Biography:<br>
+    &lt;br&gt;&lt;br&gt;<br>
+    &lt;img src=&quot;http://*****PUT THE URL OF THE ARTISTS PICTURE 
+HERE. I RECOMEND 
+    UPLOADING IT WITH YOUR OTHER PNG FILES AND CALLING IT SOMETHING 
+DESCRIPTIVE, 
+    LIKE BIO.PNG *BE SURE THAT WHATEVER YOU CALL IT, YOU DO NOT HAVE A 
+TEXT FILE 
+    THAT IS TITLED THE SAME!* THAT MEANS THAT IF YOU CALL THE PNG 
+BIO.PNG, DO 
+    NOT UPLOAD A TEXT FILE NAMED BIO.TXT*****&quot; 
+width=&quot;150&quot;&gt;<br>
+    &lt;/td&gt;<br>
+    &lt;td bgcolor=&quot;#FFFFFF&quot; width=&quot;*&quot; align=left 
+valign=top&gt;<br>
+    *****PUT BIOGRAPHICAL INFORMATION HERE. I SUGGEST USING GOOGLE TO 
+FIND INFORMATION. 
+    SOMETIMES THE JACKET COVER OF THE BOOK WILL HAVE A SHORT BIO AS 
+WELL. REMEMBER, 
+    THE BOOK MAY HAVE BEEN PRINTED BEFORE THE AUTHOR DIED, SO PAY 
+SPECIAL ATTENTION 
+    TO THAT.*****<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;/table&gt;<br>
+    &lt;table width=&quot;100%&quot; table bgcolor=&quot;#FFFFFF&quot; 
+border=&quot;1&quot; 
+    bordercolor=&quot;#111111&quot; cellspacing=&quot;0&quot; 
+cellpadding=&quot;2&quot;&gt; 
+    <br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;#336633&quot; align=center colspan=2&gt;<br>
+    &lt;font size=&quot;+2&quot; color=&quot;#FFFFFF&quot;&gt;<br>
+    &lt;b&gt;<br>
+    Books by This Author in PG:<br>
+    &lt;/b&gt;<br>
+    &lt;/font&gt;<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;tr&gt;<br>
+    &lt;td bgcolor=&quot;FFFFFF&quot; align=left valign=top 
+width=&quot;50%&quot;&gt;<br>
+    *****PUT OTHER BOOKS BY THE AUTHOR IN PG HERE. AT THE END OF EACH 
+TITLE, PUT 
+    THE &amp;lt;br&amp;gt; TAG.<br>
+    EXAMPLE:<br>
+    BOOK BY THE AUTHOR&lt;br&gt;<br>
+    ANOTHER BOOK&lt;br&gt;<br>
+    PUT HALF OF THE TITLES HERE,<br>
+    &lt;/td&gt;<br>
+    &lt;td bgcolor=&quot;FFFFFF&quot; align=left valign=top 
+width=&quot;50%&quot;&gt;<br>
+    AND THE OTHER HALF OF THE TITLES HERE. I SUGGEST PUTTING THEM IN 
+ALPHABETICAL 
+    ORDER WITH THE FIRST HALF IN THE FIRST COLUMN, AND THE SECOND HALF 
+IN THE 
+    SECOND COLUMN.*****<br>
+    &lt;/td&gt;<br>
+    &lt;/tr&gt;<br>
+    &lt;/table&gt;<br>
+    &lt;/center&gt; </p>
+</center> 
 <?
 theme('','footer');
 ?>
