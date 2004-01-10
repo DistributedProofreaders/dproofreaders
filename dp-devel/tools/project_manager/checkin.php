4d46e137d1140881ac60183629ee8558a2004006
big_bill_boy <>
2004-01-10 23:26:28 +0000

    decrement user and team page counts when a page is cleared

--- dp-devel/tools/project_manager/checkin.php
+++ dp-devel/tools/project_manager/checkin.php
@@ -37,6 +37,44 @@ include($relPath.'project_edit.inc');
 	exit;
     }
 
+
+
+    // if page is cleared, decrement user's page count
+
+    $result = mysql_query("
+	SELECT $user_field_name
+	FROM $project
+	WHERE fileid = '$fileid'
+    ");
+    
+    $data = mysql_fetch_array($result);
+    $proofer = $data[0];
+
+    $sql = "UPDATE users SET pagescompleted = pagescompleted-1 WHERE username = '$proofer'";
+    $result = mysql_query($sql);
+
+
+    // also decrement the counts of their teams    
+
+    $result = mysql_query("
+	SELECT team_1, team_2, team_3
+	FROM users
+	WHERE username = '$proofer'
+    ");
+    
+    $data = mysql_fetch_array($result);
+    $team1 = $data['team_1'];
+    $team2 = $data['team_2'];
+    $team3 = $data['team_3'];
+    
+
+    $sql = "UPDATE user_teams SET page_count = page_count-1 WHERE id=1 OR id=$team1 OR id=$team2 OR id=$team3";
+    $result = mysql_query($sql);
+
+
+
+    // now clear the page
+
     $result = mysql_query("
 	UPDATE $project
 	SET $text_field_name='',
@@ -45,6 +83,9 @@ include($relPath.'project_edit.inc');
 	    state='$new_state'
 	WHERE fileid = '$fileid'
     ");
+
+
+
     metarefresh(0, "project_detail.php?project=$project", "Page Checked In ($round_number)", "");
 ?>
 
