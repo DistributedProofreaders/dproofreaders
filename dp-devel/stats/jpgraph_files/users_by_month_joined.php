3f511e6e00681368a1f7ee5e836d87d3f22bc03f
big_bill_boy <>
2004-01-26 06:09:59 +0000

    new users per month, and new users who went on to proof

--- dp-devel/stats/jpgraph_files/users_by_month_joined.php
+++ dp-devel/stats/jpgraph_files/users_by_month_joined.php
@@ -10,21 +10,35 @@ new dbConnect();
 ///////////////////////////////////////////////////
 //Numbers of users logging on in last hour, day, week and 28 days
 //query db and put results into arrays
-$result = mysql_query("
-	SELECT FROM_UNIXTIME(date_created, '%Y-%m') as Date, count(*) as Num FROM users 
+$result1 = mysql_query("
+	SELECT FROM_UNIXTIME(date_created, '%Y-%m') as Month, count(*) as Joined FROM users 
 	GROUP BY FROM_UNIXTIME(date_created, '%Y-%m')
 	ORDER BY FROM_UNIXTIME(date_created, '%Y-%m')
-";
+");
+
+$result2 = mysql_query("
+	SELECT FROM_UNIXTIME(date_created, '%Y-%m') as Month, count(*) as EverProofed FROM users 
+	WHERE pagescompleted > 0 
+	GROUP BY FROM_UNIXTIME(date_created, '%Y-%m')
+	ORDER BY FROM_UNIXTIME(date_created, '%Y-%m')
+");
 
 
-
-$mynumrows = mysql_numrows($result);
+$mynumrows = mysql_numrows($result1);
         $count = 0;
         while ($count < $mynumrows) {
-        $datay[$count] = mysql_result($result, $count,"Num");
-        $datax[$count] = mysql_result($result, $count,"Date");
+        $data1y[$count] = mysql_result($result, $count,"Joined");
+        $datax[$count] = mysql_result($result, $count,"Month");
             $count++;
         }
+
+        $count = 0;
+        while ($count < $mynumrows) {
+        $data2y[$count] = mysql_result($result, $count,"EverProofed");
+            $count++;
+        }
+
+
 
 // Create the graph. These two calls are always required
 //Last value controls how long the graph is cached for in minutes
@@ -33,13 +47,11 @@ $graph->SetScale("textint");
 
 //set X axis
 $graph->xaxis->SetTickLabels($datax);
-// Only draw labels on every 2nd tick mark
-//$graph->xaxis->SetTextLabelInterval(91.25);
 $graph->xaxis->SetLabelAngle(90);
 $graph->xaxis->title->Set("");
-$graph->xaxis->SetTextTickInterval(91.25);
+
 //Set Y axis
-$graph->yaxis->title->Set('New Users Per Month');
+$graph->yaxis->title->Set('New Users');
 $graph->yaxis->SetTitleMargin(45);
 
 //Set background to white
@@ -53,17 +65,32 @@ $graph->SetShadow();
 
 $graph->img->SetMargin(70,30,20,100);
 
-// Create a bar pot
-$bplot = new BarPlot($datay);
-$graph->Add($bplot);
+
+ // Create the bar plots
+$b1plot = new BarPlot ($data1y);
+$b1plot ->SetFillColor ("aquamarine");
+$b1plot->SetLegend(_("Newly Joined Users"));
+
+$b2plot = new BarPlot ($data2y);
+$b2plot ->SetFillColor ("chartreuse");
+$b2plot->SetLegend(_("Newly Joined Users who Proofed"));
+
+// Create the grouped bar plot
+$gbplot = new GroupBarPlot (array($b1plot ,$b2plot));
+
+// ...and add it to the graPH
+$graph->Add( $gbplot);
+
 
 // Setup the title
-$graph->title->Set("Number of new users who joined each month");
+$graph->title->Set("Number of New Users By Month");
 
 
 $graph->title->SetFont(FF_FONT1,FS_BOLD);
 $graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
 $graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
+
+$graph->legend->Pos(0.15,0.1,"left" ,"top"); //Align the legend
 
 // Display the graph
 $graph->Stroke();
