af6baa2eff19153231f1a8e80a0227f0ef5a57e3
big_bill_boy <>
2004-01-31 11:40:21 +0000

    HTML for legend for special colours Factored out so can be changed as often as needed without running into problems with other files being worked on - One day may contain data-driven output

---
+++ dp-devel/tools/proofers/special_legend.php
@@ -0,0 +1,16 @@
+
+Legend for Special Books:
+<br><br><b>
+<span class="green">India's Republic Day</span>&nbsp;
+<span class="otherblue">Burns' Night</span>&nbsp;
+<br>
+<span class="paleblue"> Authors with recent birthdays </span>&nbsp;
+<span class="richblue"> Authors with birthdays today </span>&nbsp;
+<br>
+<span class="flyblue"> Wright Flight Anniversary </span>&nbsp;
+<span class="orange"> Halloween </span>&nbsp;
+<span class="purple"> Other Special </span>
+</b></font>
+</p>
+<br>
+
