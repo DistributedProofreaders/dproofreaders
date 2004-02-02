7daf3c4c16f7fead2f72f4179f173eba7281d2e2
big_bill_boy <>
2004-02-02 11:40:23 +0000

    host page for various "projects posted" graphs

---
+++ dp-devel/stats/proj_posted_graphs.php
@@ -0,0 +1,20 @@
+<?
+$relPath='./../pinc/';
+include($relPath.'v_site.inc');
+include($relPath.'project_states.inc');
+include($relPath.'connect.inc');
+include($relPath.'theme.inc');
+new dbConnect();
+
+theme('Posted Projects Graphs','header');
+echo "<center><h1><i>Posted Projects Graphs</i></h1></center>";
+
+echo "<br><br>";
+
+echo "<center><img src=\"jpgraph_files/curr_month_proj_posted_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_month_proj_posted.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/total_proj_posted_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_total_proj_posted_graph.php\"></center><br>";
+
+theme('','footer');
+?>
