f59da56ef152be9c5ae8c66c19f3bb1c3fe1acb0
big_bill_boy <>
2004-02-02 11:34:19 +0000

    host page for various "projects proofed" graphs

---
+++ dp-devel/stats/proj_proofed_graphs.php
@@ -0,0 +1,20 @@
+<?
+$relPath='./../pinc/';
+include($relPath.'v_site.inc');
+include($relPath.'project_states.inc');
+include($relPath.'connect.inc');
+include($relPath.'theme.inc');
+new dbConnect();
+
+theme('Proofed Projects Graphs','header');
+echo "<center><h1><i>Proofed Projects Graphs</i></h1></center>";
+
+echo "<br><br>";
+
+echo "<center><img src=\"jpgraph_files/curr_month_proj_proofed_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_month_proj_proofed.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/total_proj_proofed_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_total_proj_proofed_graph.php\"></center><br>";
+
+theme('','footer');
+?>
