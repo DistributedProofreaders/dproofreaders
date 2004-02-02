9f40ec1c5325e4f40d7b49b96ddd497788bfd383
big_bill_boy <>
2004-02-02 11:43:11 +0000

    host page for various "projects PPd" graphs

---
+++ dp-devel/stats/proj_PPd_graphs.php
@@ -0,0 +1,20 @@
+<?
+$relPath='./../pinc/';
+include($relPath.'v_site.inc');
+include($relPath.'project_states.inc');
+include($relPath.'connect.inc');
+include($relPath.'theme.inc');
+new dbConnect();
+
+theme('PPd Projects Graphs','header');
+echo "<center><h1><i>PPd Projects Graphs</i></h1></center>";
+
+echo "<br><br>";
+
+echo "<center><img src=\"jpgraph_files/curr_month_proj_PPd_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_month_proj_PP.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/total_proj_PPd_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_total_proj_PPd_graph.php\"></center><br>";
+
+theme('','footer');
+?>
