af58bd424aad693ae2700d13b93b2a1db0502f76
big_bill_boy <>
2004-02-02 11:22:59 +0000

    host page for various "projects created" graphs

---
+++ dp-devel/stats/proj_created_graphs.php
@@ -0,0 +1,20 @@
+<?
+$relPath='./../pinc/';
+include($relPath.'v_site.inc');
+include($relPath.'project_states.inc');
+include($relPath.'connect.inc');
+include($relPath.'theme.inc');
+new dbConnect();
+
+theme('Created Projects Graphs','header');
+echo "<center><h1><i>Created Projects Graphs</i></h1></center>";
+
+echo "<br><br>";
+
+echo "<center><img src=\"jpgraph_files/curr_month_proj_created_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_month_proj_created_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/total_proj_created_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_total_proj_created_graph.php\"></center><br>";
+
+theme('','footer');
+?>
