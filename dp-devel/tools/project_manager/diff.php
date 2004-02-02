9f0961166df08487c603ce8941d1ac40c52d1bdf
smolenski <>
2004-02-02 08:09:31 +0000

    Frontend for DifferenceEngine.php; I didn't want to insert DE into it because this way it is easier to exchange new versions

---
+++ dp-devel/tools/project_manager/diff.php
@@ -0,0 +1,44 @@
+<?php
+$relPath="./../../pinc/";
+include_once($relPath.'v_site.inc');
+include_once($relPath.'theme.inc');
+include_once($relPath.'project_edit.inc');
+include_once('projectmgr.inc');
+
+$no_stats=1;
+theme("Difference", "header");
+
+abort_if_not_manager();
+
+$projectid = $_GET['project'];
+abort_if_cant_edit_project( $projectid );
+
+$fileid=$_GET['file'];
+$round=$_GET['round'];
+
+if($round==1) {
+	$fields="master_text,round1_text";
+} else {
+	$fields="round2_text,round2_text";
+}
+
+$res = mysql_query("SELECT $fields FROM $projectid WHERE fileid='$fileid'");
+
+$txt=mysql_fetch_row($res);
+
+class OutputPage {
+	function addHTML($text) {
+		echo $text;
+	}
+}
+
+function wfMsg($key) {
+	return ($key=="lineno")?"Line $1":$key;
+}
+
+$wgOut=new Outputpage();
+
+include("DifferenceEngine.php");
+DifferenceEngine::showDiff($txt[0],$txt[1],"Old text","New text");
+
+?>
