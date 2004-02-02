c1da1fc442d69569c7ac85f7f0ae0d92bccc3cfc
jmdyck <>
2004-02-02 06:27:58 +0000

    Drop user-specific stuff because sessions on pgdp.net are causing me problems.

--- dp-devel/tools/project_manager/generate_post_files.php
+++ dp-devel/tools/project_manager/generate_post_files.php
@@ -1,20 +1,15 @@
 <?
 $relPath="./../../pinc/";
-include_once($relPath.'user_is.inc');
 include_once($relPath.'connect.inc');
 include_once('sendtopost.php');
 
 $dbConnection = new dbConnect();
 
-if ( !user_is_a_sitemanager() and !user_is_proj_facilitator() )
-{
-	echo "<p>You are not allowed to run this script.</p>";
-	exit();
-}
-
 foreach ( array('projectID3ff8d4d07cefe','projectID3ff8d5bf11840') as $projectid )
 {
-	echo "<p>generating files for $projectid...</p>";
+	echo "<p>generating files for $projectid...</p>\n";
+	flush();
 	generate_post_files( $projectid );
+	flush();
 }
 ?>
