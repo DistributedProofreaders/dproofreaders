cd03d93de211e5027697e0d095f02e2e799645c7
big_bill_boy <>
2004-02-03 08:34:55 +0000

    host page for graphs of pages proofed

---
+++ dp-devel/stats/pages_proofed_graphs.php
@@ -0,0 +1,20 @@
+<?
+$relPath='./../pinc/';
+include($relPath.'v_site.inc');
+include($relPath.'project_states.inc');
+include($relPath.'connect.inc');
+include($relPath.'theme.inc');
+new dbConnect();
+
+theme('Pages Proofed Graphs','header');
+echo "<center><h1><i>Pages Proofed Graphs</i></h1></center>";
+
+echo "<br><br>";
+
+echo "<center><img src=\"jpgraph_files/curr_month_pages_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_month_pages.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/total_pages_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/cumulative_total_pages_graph.php\"></center><br>";
+
+theme('','footer');
+?>
