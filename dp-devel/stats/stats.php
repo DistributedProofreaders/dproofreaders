61f3a75b800d80edd3210e6d687a99d3bea82ac0
big_bill_boy <>
2004-02-05 11:25:30 +0000

    continue if no table, log middle and end

--- dp-devel/stats/stats.php
+++ dp-devel/stats/stats.php
@@ -22,21 +22,21 @@ $handle = fopen ($filename, "w");
 fclose($handle);
 
 if (!file_exists($stats_inc_path)) {
-	touch($stats_inc_path);
-	$statsfile = fopen($stats_inc_path, "w");
-	fputs($statsfile, "<?\n");
-	fputs($statsfile, "\$sitestats['monthly'] = \"0\";\n");
-	fputs($statsfile, "\$sitestats['daily'] = \"0\";\n");
-	fputs($statsfile, "\$sitestats['goal'] = \"0\";\n");
-	fputs($statsfile, "?>\n");
-	}
+        touch($stats_inc_path);
+        $statsfile = fopen($stats_inc_path, "w");
+        fputs($statsfile, "<?\n");
+        fputs($statsfile, "\$sitestats['monthly'] = \"0\";\n");
+        fputs($statsfile, "\$sitestats['daily'] = \"0\";\n");
+        fputs($statsfile, "\$sitestats['goal'] = \"0\";\n");
+        fputs($statsfile, "?>\n");
+        }
 
 $today = getdate();
 $midnight = mktime(0,0,0,$today['mon'],$today['mday'],$today['year']);
 
 $tracetime = time();
 mysql_query("INSERT INTO job_logs (filename, tracetime, event, comments)
-		VALUES ('stats.php', $tracetime, 'BEGIN', 'ok to run, no lock file')");
+               VALUES ('stats.php', $tracetime, 'BEGIN', 'ok to run, no lock file')");
 
 //limit to looking at projects which do not have
 //the archive flag set to 1 in order to limit run time
@@ -47,20 +47,21 @@ mysql_query("INSERT INTO job_logs (filename, tracetime, event, comments)
 // and posted to PG before midnight.
 // When we have the big table this whole level of the code
 // will become uneccessary, anyway
+
 $allProjects = mysql_query("
 	SELECT projectid FROM projects 
 	WHERE archived = '0' AND 
 		(state != '".PROJ_NEW."') AND 
 		(state != '".PROJ_SUBMIT_PG_POSTED."') AND 
-		(state !='".PROJ_POST_SECOND_AVAILABLE."') AND 
-		(state !='".PROJ_POST_SECOND_CHECKED_OUT."') AND 
-		(state != '".PROJ_POST_SECOND_CHECKED_OUT."' 
-			OR modifieddate > $midnight) AND 
-		(state != '".PROJ_PROOF_FIRST_WAITING_FOR_RELEASE."' 
-			OR modifieddate > $midnight) AND 
-		(state != '".PROJ_POST_FIRST_UNAVAILABLE."' 
-			OR modifieddate > $midnight)
-	");
+ 		(state !='".PROJ_POST_SECOND_AVAILABLE."') AND 
+ 		(state !='".PROJ_POST_SECOND_CHECKED_OUT."') AND 
+ 		(state != '".PROJ_POST_SECOND_CHECKED_OUT."' 
+ 			OR modifieddate > $midnight) AND 
+ 		(state != '".PROJ_PROOF_FIRST_WAITING_FOR_RELEASE."' 
+ 			OR modifieddate > $midnight) AND 
+ 		(state != '".PROJ_POST_FIRST_UNAVAILABLE."' 
+ 			OR modifieddate > $midnight)
+ 	");
 $numProjects = mysql_num_rows($allProjects);
 
 while ($i < $numProjects) {
@@ -74,12 +75,26 @@ while ($i < $numProjects) {
         $result2 = mysql_query("SELECT COUNT(image) FROM $projectID WHERE
            state='save_second' AND round2_time >= $midnight");
 
-        $row1 = mysql_fetch_row($result1);
-        $pages1 = $row1[0];
-
-        $row2 = mysql_fetch_row($result2);
-        $pages2 = $row2[0];
-
+        if (!$result1)
+        {
+            // Probably the project's page-table does not exist.
+            // Not sure why.
+            $pages1 =  0;
+            continue;
+        } else {
+               $row1 = mysql_fetch_row($result1);
+                $pages1 = $row1[0];
+        }
+        if (!$result2)
+        {
+            // Probably the project's page-table does not exist.
+            // Not sure why.
+            $pages2 = 0;
+            continue;
+        } else {
+               $row2 = mysql_fetch_row($result2);
+                $pages2 = $row2[0];
+        }
         $rows = $pages1+$pages2;
 
         $dailyPages = $dailyPages+$rows;
@@ -88,6 +103,16 @@ while ($i < $numProjects) {
 
 //echo result so we know cron job is working and to avoid timeout
 echo "Daily pages: ".number_format($dailyPages);
+
+
+$tracetimea = time();
+$tooktime = $tracetimea - $tracetime;
+$qrytext = "INSERT INTO job_logs (filename, tracetime, event, comments)
+                VALUES ('stats.php', $tracetimea, 'MIDDLE', 'started at $tracetimea, took $tooktime seconds so far, looked at $numProjects projects')";
+$resultA = mysql_query($qrytext);
+
+
+
 
 $result = mysql_query("SELECT SUM(pages) AS monthlypages FROM pagestats WHERE month=".$today['mon']." AND year=".$today['year']."");
 $monthlyPages = mysql_result($result, 0, "monthlypages");
@@ -106,23 +131,25 @@ $i=0;
 $lines = file($stats_inc_path);
 $statsfile = fopen($stats_inc_path, "w");
 while ($i < count($lines)) {
-	if (substr($lines[$i], 12, 7) == "monthly") { fputs($statsfile, "\$sitestats['monthly'] = \"$monthlyPages\";\n"); }
-	elseif (substr($lines[$i], 12, 5) == "daily") {	fputs($statsfile, "\$sitestats['daily'] = \"$dailyPages\";\n");	}
-	elseif (substr($lines[$i], 12, 4) == "goal" && $updateMonthlyGoal == 1) { fputs($statsfile, "\$sitestats['goal'] = \"$monthlyGoal\";\n");	}
-	else { fputs($statsfile, trim($lines[$i])."\n"); }
-	$i++;
-	}
-	fclose($statsfile);
+        if (substr($lines[$i], 12, 7) == "monthly") { fputs($statsfile, "\$sitestats['monthly'] = \"$monthlyPages\";\n"); }
+        elseif (substr($lines[$i], 12, 5) == "daily") { fputs($statsfile, "\$sitestats['daily'] = \"$dailyPages\";\n");  }
+        elseif (substr($lines[$i], 12, 4) == "goal" && $updateMonthlyGoal == 1) { fputs($statsfile, "\$sitestats['goal'] = \"$monthlyGoal\";\n"); }
+        else { fputs($statsfile, trim($lines[$i])."\n"); }
+        $i++;
+        }
+        fclose($statsfile);
 //delete lock file
 unlink ($filename);
 
-$tracetime2 = time();
-$timetorun = $tracetime2 - $tracetime1;
+$tracetimea = time();
+$tooktime = $tracetimea - $tracetime;
+$qrytext = "INSERT INTO job_logs (filename, tracetime, event, comments)
+                VALUES ('stats.php', $tracetimea, 'END', 'started at $tracetimea, took $tooktime seconds total')";
+$resultA = mysql_query($qrytext);
 
-mysql_query("INSERT INTO job_logs (filename, timestamp, event, comments)
-		VALUES ('stats.php', $tracetime2, 'END', 'started at $tracetime, took $timetorun seconds, looked at $numProjects projects')");
+
 
 
 }
-?>
 
+?>
