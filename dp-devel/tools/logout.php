4a39be37682e23e00ade3bc45c0186870b76f9eb
smolenski <>
2004-02-13 01:36:00 +0000

    Sessions vs cookies; hope I haven't messed up the includes.

--- dp-devel/tools/logout.php
+++ dp-devel/tools/logout.php
@@ -1,17 +1,18 @@
 <?
 //clear cookie if one is already set
 $relPath='./../pinc/';
-include($relPath.'cookie.inc');
+//include($relPath.'cookie.inc');
 include($relPath.'metarefresh.inc');
 include($relPath.'dp_main.inc');
+include_once($relPath.'v_site.inc');
 
-if (0) {
+if ($use_cookies) {
 	$cookieC=new userCookie();
 	$cookieSet=$cookieC->deleteCookie();
+} else {
+	session_unset();
+	session_destroy();
 }
-
-session_unset();
-session_destroy();
 
 metarefresh(0, "../default.php", "Logout Complete",
      "<A HREF=\"../default.php\">Return to DP Home Page.</A>");
