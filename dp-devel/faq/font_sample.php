19f36d0e8361a778a503da3485ef91e528ef13b5
big_bill_boy <>
2004-02-13 11:55:48 +0000

    added trial code to generate multiple versions of the page with different fonts

--- dp-devel/faq/font_sample.php
+++ dp-devel/faq/font_sample.php
@@ -3,111 +3,100 @@ $relPath='../pinc/';
 include_once($relPath.'v_site.inc');
 include_once($relPath.'connect.inc');
 include_once($relPath.'theme.inc');
+include_once($relPath.'prefs_options.inc');
 new dbConnect();
-theme('Custom Font Comparison','header');
+$no_stats=1;
+theme(_('Custom Font Comparison'),'header');
+
+
+// get font variable from url, if any
+
+if isset($_GET['compare']) 
+{
+	$tfont = $_GET['compare'];
+}
+else
+{
+// if none, then determine user's current proofing font
+
+	if ($userP['i_layout']==1)
+	{
+		$tfonti = $userP['v_fntf'];
+		$tfont = $f_f[$tfonti];
+	}
+	else
+	{
+	    $tfonti = $userP['h_fntf'];
+	    $tfont = $f_f[$tfonti];
+	}
+}
+
+
+// use this font  -- Mike?
+
+
+// echo text
+
+$exp_text = "
+	<H1>DPCustomMono2</H1>
+	<P>"
+	._("DPCustomMono2 is a font adapted by DP's own big_bill, 
+	based on the suggestions and ideas of many experienced proofreaders, 
+	that helps proofreaders find mistakes. 
+	You can change the font that you use for proofing in your") 
+	." <a href='$code_url/userprefs.php'>"._("preferences")."</a>." 
+	._("Here are some samples that compare DPCustomMono2 to other fonts. 
+	For information on installing and using the font, read the"). " <a href='$forums_url/viewtopic.php?p=31521#31521'>"._("Wiki about this")
+	."</a></P> 
+
+	<P>
+	<FONT FACE='DPCustomMono2, Times New Roman, Times, serif'>"
+	._("If you already have the font installed, 
+	you will see this sentence in the DPCustomMono2 typeface.
+	If this sentence looks normal, you can download DPCustomMono2 from")
+	." <a href='DPCustomMono2.ttf'>"._("here")."</a> "._("(right click the link, and choose &ldquo;Save 
+	Target As...&rdquo;).")
+	."</FONT></P>";
+
+echo $exp_text;
+
+
+$first = 1;
+
+
+
+for ($f_f as $otherfont) {
+
+	if ($otherfont != $tfont) {
+		if (! $first) {
+			echo " | ";
+		} else {
+			$first = 0;
+		}
+		
+		echo "<a href='font_sample.php?compare=$otherfont'>$otherfont</a>";
+
+	}
+}
+
+
+echo _("View original image")."</P>\n";
+
+echo "<hr style='width: 546; text-align: left;'>";
+echo "<P>";
+echo _("On this page, the top font is")." <b>$tfont</b>, "._("and the bottom example is").
+	" <b>DPCustomMono2</b>.</P>";
+
+echo "
+	<p><img border='0' src='$tfont_A.gif' width='415' height='285'></p>
+	<p><img border='0' src='DPCustomMono2_A.gif' width='546' height='242'></p>
+	<hr style='width: 546; text-align: left;'>
+	<p><img border='0' src='$tfont_B.gif' width='414' height='198'></p>
+	<p><img border='0' src='DPCustomMono2_B.gif' width='582' height='200'></p>
+	<hr style='width: 546; text-align: left;'>
+	<p><img border='0' src='$tfont_C.gif' width='402' height='174'></p>
+	<p><img border='0' src='DPCustomMono2_C.gif' width='582' height='180'></p>
+";
+
+theme("", "footer"); 
 ?>
-
-
-
-<H1>DPCustomMono2</H1>
-<P>DPCustomMono2 is a font adapted by DP's own big_bill, based on the suggestions and ideas of many experienced proofreaders, that helps proofreaders
-find mistakes. You can change the font that you use for proofreading in
-your <? echo "<a href='$code_url/userprefs.php'>preferences</a>"; ?>. Here are some samples that compare DPCustomMono2 to
-other fonts. For information on installing and using the font, read the <? echo "<a href='$forums_url/viewtopic.php?p=31521#31521'>Wiki about this</a>"; ?></P> 
-<P><FONT FACE="DPCustomMono2, Times New Roman, Times, serif">If you already have the font
-installed, you will see this sentence in the DPCustomMono2 typeface.
-If this sentence looks normal, you can download DPCustomMono2 from
-<a href="DPCustomMono2.ttf">here</a> (right click the link, and choose &ldquo;Save to Disk&rdquo;).</FONT></P>
-<P><BR><BR>
-</P>
-<TABLE WIDTH=1108 BORDER=0 CELLPADDING=4 CELLSPACING=0>
-	<COL WIDTH=506>
-	<COL WIDTH=586>
-	<THEAD>
-		<TR VALIGN=TOP>
-			<TH WIDTH=506>
-				<P><IMG SRC="Courier.gif" NAME="Graphic1" ALIGN=LEFT WIDTH=582 HEIGHT=614 BORDER=0><BR CLEAR=LEFT><BR>
-				</P>
-			</TH>
-			<TH WIDTH=586>
-				<P><IMG SRC="DPCustomMono2.gif" NAME="Graphic2" ALT="Sample of DPCustomMono2 font" ALIGN=LEFT WIDTH=582 HEIGHT=622 BORDER=0><BR CLEAR=LEFT><BR>
-				</P>
-			</TH>
-		</TR>
-	</THEAD>
-	<TBODY>
-		<TR VALIGN=TOP>
-			<TD WIDTH=506>
-				<P ALIGN=CENTER>Courier</P>
-			</TD>
-			<TD WIDTH=586>
-				<P ALIGN=CENTER>DPCustomMono2</P>
-			</TD>
-		</TR>
-		<TR VALIGN=TOP>
-			<TD WIDTH=506>
-				<P ALIGN=CENTER><IMG SRC="Arial.gif" NAME="Graphic4" ALIGN=LEFT WIDTH=414 HEIGHT=617 BORDER=0><BR CLEAR=LEFT><BR>
-				</P>
-			</TD>
-			<TD WIDTH=586>
-				<P ALIGN=CENTER><IMG SRC="DPCustomMono2.gif" NAME="Graphic3" ALT="Sample of DPCustomMono2 font" ALIGN=LEFT WIDTH=582 HEIGHT=622 BORDER=0><BR CLEAR=LEFT><BR>
-				</P>
-			</TD>
-		</TR>
-		<TR VALIGN=TOP>
-			<TD WIDTH=506>
-				<P ALIGN=CENTER>Arial</P>
-			</TD>
-			<TD WIDTH=586>
-				<P ALIGN=CENTER>DPCustomMono2</P>
-			</TD>
-		</TR>
-		<TR VALIGN=TOP>
-			<TD WIDTH=506>
-				<P ALIGN=CENTER><IMG SRC="Times.gif" NAME="Graphic6" ALIGN=LEFT WIDTH=407 HEIGHT=618 BORDER=0><BR CLEAR=LEFT><BR>
-				</P>
-			</TD>
-			<TD WIDTH=586>
-				<P ALIGN=CENTER><IMG SRC="DPCustomMono2.gif" NAME="Graphic5" ALT="Sample of DPCustomMono2 font" ALIGN=LEFT WIDTH=582 HEIGHT=622 BORDER=0><BR CLEAR=LEFT><BR>
-				</P>
-			</TD>
-		</TR>
-		<TR VALIGN=TOP>
-			<TD WIDTH=506>
-				<P ALIGN=CENTER>Times</P>
-			</TD>
-			<TD WIDTH=586>
-				<P ALIGN=CENTER>DPCustomMono2</P>
-			</TD>
-		</TR>
-	</TBODY>
-</TABLE>
-<TABLE WIDTH=100% BORDER=0 CELLPADDING=4 CELLSPACING=3>
-	<COL WIDTH=256*>
-	<THEAD>
-		<TR>
-			<TD WIDTH=100% VALIGN=TOP>
-				<P ALIGN=CENTER><IMG SRC="Original.gif" NAME="Graphic7" ALIGN=LEFT WIDTH=374 HEIGHT=539 BORDER=0><BR CLEAR=LEFT><BR>
-				</P>
-			</TD>
-		</TR>
-	</THEAD>
-	<TBODY>
-		<TR>
-			<TD WIDTH=100% VALIGN=TOP>
-				<P ALIGN=LEFT>Original Image</P>
-			</TD>
-		</TR>
-	</TBODY>
-</TABLE>
-<P ALIGN=CENTER><BR><BR>
-</P>
-
-<style>
-<!--
-h1 {font-family: Verdana, Arial, sans-serif;}
-p {font-family: "Times New Roman, Times, serif";
--->
-</style>
-
-<? theme("", "footer"); ?>
