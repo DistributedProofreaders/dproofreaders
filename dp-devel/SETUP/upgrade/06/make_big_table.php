d53d0df44f43984d43dfe36785fe31e595bfb9cf
dkretz <>
2004-02-14 20:26:27 +0000

    replaced iterative insertion of each row in a project table with a single SQL command to insert all pages for the project table.

--- dp-devel/SETUP/upgrade/06/make_big_table.php
+++ dp-devel/SETUP/upgrade/06/make_big_table.php
@@ -13,12 +13,41 @@ while ($row = mysql_fetch_assoc($result)) {
 	if ($result1 == FALSE || mysql_num_rows($result1) == 0) {
 		echo $row['projectid']." -- Not moved due to either missing table or 0 rows<br>";
 	} else {
-		$projectid = substr($row['projectid'], -13);
+        // DAK - Keep project id for now.
+		// $projectid = substr($row['projectid'], -13);
+		$projectid = $row['projectid'];
+
+        // DAK - Replaced page iteration with single SQL command for each
+        // project table.
+
+        /*
 		while ($row1 = mysql_fetch_assoc($result1)) {
-			$result2 = mysql_query("INSERT INTO project_pages (projectid, fileid, image, master_text, round1_text, round2_text, round1_user, round2_user, round1_time, round2_time, state, b_user, b_code) VALUES ('$projectid', '".$row1['fileid']."', '".$row1['image']."', '".mysql_escape_string($row1['master_text'])."', '".mysql_escape_string($row1['round1_text'])."', '".mysql_escape_string($row1['round2_text'])."', '".$row1['round1_user']."', '".$row1['round2_user']."', ".$row1['round1_time'].", ".$row1['round2_time'].", '".$row1['state']."', '".$row1['b_user']."', ".$row1['b_code'].")");
+			$result2 = mysql_query(
+                "INSERT INTO project_pages \
+               (projectid, fileid, image, master_text, round1_text, \
+               round2_text, round1_user, round2_user, round1_time,  \
+               round2_time, state, b_user, b_code)  \
+               VALUES  \
+               ('$projectid', '".$row1['fileid']."',  \
+               '".$row1['image']."',  \
+               '".mysql_escape_string($row1['master_text'])."',  \
+               '".mysql_escape_string($row1['round1_text'])."',  \
+               '".mysql_escape_string($row1['round2_text'])."',  \
+               '".$row1['round1_user']."', '".$row1['round2_user']."',  \
+               ".$row1['round1_time'].", ".$row1['round2_time'].",  \
+               '".$row1['state']."', '".$row1['b_user']."',  \
+               ".$row1['b_code'].")");
 		}
+        */
+        $result2 = mysql_query( \
+               "INSERT project_pages \
+                SELECT '$projectid', `fileid` , `image` , `master_text` ,\
+                `round1_text` , `round2_text` , `round1_user` , `round2_user` , \
+                `round1_time` , `round2_time` , `state` , `b_user` , `b_code` , \
+                NULL, NULL \
+                FROM $projectid" ) ;
+
 		echo $row['projectid']." -- Moved to project_pages table<br>";
-		//$result3 = mysql_query("DROP TABLE ".$row['projectid']."");
 	}
 }
 
