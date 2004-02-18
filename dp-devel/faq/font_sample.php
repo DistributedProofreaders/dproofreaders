9078b122c00f259fb48e818336850f15b3416034
big_bill_boy <>
2004-02-18 09:28:24 +0000

    used selected font etc

--- dp-devel/faq/font_sample.php
+++ dp-devel/faq/font_sample.php
@@ -32,10 +32,19 @@ else
 }
 
 
-// use this font  -- Mike?
+// use this font, unless it's DPCM2
 
+if ($tfont != 'DPCustomMono2') {
+	echo "<FONT FACE='$tfont'>";
+    	$DPCM = 0;
+} else {
+	echo "<FONT FACE='Times New Roman, Times, serif'>";
+	$DPCM = 1;
+}
 
 // echo text
+
+
 
 $exp_text = "
         <H1>DPCustomMono2</H1>
@@ -47,16 +56,39 @@ $exp_text = "
         ." <a href='$code_url/userprefs.php'>"._("preferences")."</a>. "
         ._("Here are some samples that compare DPCustomMono2 to other fonts.
         For information on installing and using the font, read the"). " <a href='$forums_url/viewtopic.php?p=31521#31521'>"._("Wiki about this")
-        ."</a></P>
+        ."</a></P>";
 
+echo $exp_text;
+
+$exp_text ="
         <P>
-        <FONT FACE='DPCustomMono2, Times New Roman, Times, serif'>"
-        ._("If you already have the font installed,
-        you will see this sentence in the DPCustomMono2 typeface.
-        If this sentence looks normal, you can download DPCustomMono2 from")
+        <FONT FACE='DPCustomMono2, $tfont'>"
+
+if ($DPCM) {
+
+$exp_text .= _("You currently have DPCustomMono2 selected as your default proofing font.")." ";
+
+} 
+
+$exp_text .= 
+	_("If you already have the font installed,
+        you will see this paragraph in the DPCustomMono2 typeface.
+        If this paragraph's font doesn't look radically different 
+	to that of the paragraph above, you can download DPCustomMono2 from")
         ." <a href='DPCustomMono2.ttf'>"._("here")."</a> "._("(right click the link, and choose &ldquo;Save
-        Target As...&rdquo;).")
-        ."</FONT></P>";
+        Target As...&rdquo;). After you have installed the font please refresh this page 
+	to make sure DPCustomMono2 is installed correctly. 
+	") ;
+
+if ($DPCM) {
+
+$exp_text .= _("If DpCustomMono is displayed correctly in this paragraph, 
+	then please browse through the gallery of font comparisons below 
+	to remind yourself why its so useful.")." ";
+
+} 
+
+$exp_text .= "</FONT></P>";
 
 echo $exp_text;
 
@@ -65,14 +97,17 @@ $first = 1;
 
 foreach ($f_f as $otherfont) {
 
-        if (($otherfont != $tfont) AND ($otherfont != _("Browser Default")) AND ($otherfont != 'DPCustomMono2')) {
+        if (($otherfont != $tfont) AND 
+		($otherfont != _("Browser Default")) AND 
+		($otherfont != 'DPCustomMono2') AND
+		($otherfont != _("Monospaced")) {
                 if (! $first) {
                         echo " | ";
                 } else {
                         $first = 0;
                 }
 
-                echo "<a href='font_sample.php?compare=$otherfont'>$otherfont</a>";
+                echo "<font face='$otherfont'><a href='font_sample.php?compare=$otherfont'>$otherfont</a></font>";
 
         }
 }
@@ -86,14 +121,14 @@ echo _("On this page, the top font is")." <b>$tfont</b>, "._("and the bottom exa
         " <b>DPCustomMono2</b>.</P>";
 
 echo "
-        <p><img border='0' src='".$tfont."_A.gif' width='415' height='285'></p>
-        <p><img border='0' src='DPCustomMono2_A.gif' width='546' height='242'></p>
+        <p><img border='0' src='images".$tfont."_A.gif' width='415' height='285'></p>
+        <p><img border='0' src='images/DPCustomMono2_A.gif' width='546' height='242'></p>
         <hr style='width: 546; text-align: left;'>
-        <p><img border='0' src='".$tfont."_B.gif' width='414' height='198'></p>
-        <p><img border='0' src='DPCustomMono2_B.gif' width='582' height='200'></p>
+        <p><img border='0' src='images/".$tfont."_B.gif' width='414' height='198'></p>
+        <p><img border='0' src='images/DPCustomMono2_B.gif' width='582' height='200'></p>
         <hr style='width: 546; text-align: left;'>
-        <p><img border='0' src='".$tfont."_C.gif' width='402' height='174'></p>
-        <p><img border='0' src='DPCustomMono2_C.gif' width='582' height='180'></p>
+        <p><img border='0' src='images/".$tfont."_C.gif' width='402' height='174'></p>
+        <p><img border='0' src='images/DPCustomMono2_C.gif' width='582' height='180'></p>
 ";
 
 theme("", "footer");
