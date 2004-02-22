aac1393ef738b48697d021fe701bb8bdd50797ab
big_bill_boy <>
2004-02-22 01:45:36 +0000

    stats for PPvers

---
+++ dp-devel/stats/ppv_stats.php
@@ -0,0 +1,62 @@
+<?
+$relPath='../pinc/';
+include_once($relPath.'dp_main.inc');
+include_once($relPath.'f_dpsql.inc');
+include_once($relPath.'theme.inc');
+
+theme("Post-Processing Verification Statistics", "header");
+
+echo "<br><br><h2>Post-Processing Verification Statistics</h2><br>\n";
+
+echo "<br>\n";
+
+
+$state_selector = "
+	(state LIKE 'proj_submit%')
+";
+
+
+dpsql_dump_query("
+	SELECT
+		SUM(num_projects) as 'Total Projects Post-Processed So Far'
+	FROM project_state_stats WHERE $state_selector
+	GROUP BY date ORDER BY date DESC LIMIT 1
+");
+
+echo "<br>\n";
+echo "<br>\n";
+
+echo "<h3>Number of Distinct Post-Processing Verifiers</h3>\n";
+
+dpsql_dump_query("
+	SELECT
+		count(distinct username) as 'Different PPVers'
+	FROM usersettings
+	WHERE setting = 'post_proof_verifier' and value = 'yes' 
+");
+
+echo "<br>\n";
+
+
+
+echo "<h3>Post-Processing Verifiers</h3>\n";
+echo "<h4>(Number of Projects Posted to PG</h4>\n";
+
+dpsql_dump_ranked_query("
+	SELECT checkedoutby as 'PPVer', count(  *  ) as 'Projects PPVd'
+	FROM  `projects` , user_settings
+	WHERE 1  AND checkedoutby != postproofer AND state LIKE  '%posted%'
+		and checkedoutby = user_settings.username 
+		and setting = 'post_proof_verifier' and value = 'yes' 
+	GROUP  BY 1 
+	ORDER  BY 2  DESC ");
+
+echo "<br>\n";
+
+echo _("Note that the above figures are as accurate as possible within the bounds of the current database structure");
+
+echo "<br>\n";
+
+theme("","footer");
+?>
+
