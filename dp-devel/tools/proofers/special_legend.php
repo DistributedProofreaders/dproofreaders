44c8f620e08be305b77fc7368ce37f7e14fb895f
big_bill_boy <>
2004-03-07 00:22:09 +0000

    updated for IWD

--- dp-devel/tools/proofers/special_legend.php
+++ dp-devel/tools/proofers/special_legend.php
@@ -1,14 +1,13 @@
 
 Legend for Special Books:
 <br><br><b>
-<span class="pink">Saint Valentine's Day</span>&nbsp;
+<span class="nicegreean">International Women's Day</span>&nbsp;
 <span class="midgrey">Black History Month</span>&nbsp;
-<span class="green">India's Republic Day</span>&nbsp;
 <br>
 <span class="paleblue"> Authors with recent birthdays </span>&nbsp;
 <span class="richblue"> Authors with birthdays today </span>&nbsp;
 <br>
-<span class="orange"> Halloween </span>&nbsp;
+<span class="green">India's Republic Day</span>&nbsp;
 <span class="fadedyellow"> Other Special </span>
 </b>
 
