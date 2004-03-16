94fb1e71ced1f79611c6df1c769f1d5e968a379f
big_bill_boy <>
2004-03-16 10:25:17 +0000

    tweaked greens

--- dp-devel/tools/proofers/special_legend.php
+++ dp-devel/tools/proofers/special_legend.php
@@ -1,13 +1,16 @@
 
 Legend for Special Books:
 <br><br><b>
+<span class="red">Ides of March</span>&nbsp;
+<span class="green">Saint Patrick's Day</span>&nbsp;
+<br>
 <span class="nicegreen">International Women's Day</span>&nbsp;
 <span class="midgrey">Black History Month</span>&nbsp;
 <br>
 <span class="paleblue"> Authors with recent birthdays </span>&nbsp;
 <span class="richblue"> Authors with birthdays today </span>&nbsp;
 <br>
-<span class="green">India's Republic Day</span>&nbsp;
+<span class="midgreen">India's Republic Day</span>&nbsp;
 <span class="fadedyellow"> Other Special </span>
 </b>
 
