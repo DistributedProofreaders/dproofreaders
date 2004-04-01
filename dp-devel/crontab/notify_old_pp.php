dda38ff741e50e70553aaa1be2e144658ea2fa82
pourlean <>
2004-04-01 08:07:26 +0000

    Added pointer to Missing Pages Wiki

--- dp-devel/crontab/notify_old_pp.php
+++ dp-devel/crontab/notify_old_pp.php
@@ -46,6 +46,7 @@ Our database indicates that you have had a PP project checked out for more than 
 $projectslist\n\n 
 If you have completed your work on the book, please log in to www.pgdp.net and visit the Post Processing page at $code_url/tools/post_proofers/post_proofers.php and, under 'Books I have checked out for Post-Processing' select the 'Upload for verification' option and follow the prompts. You will be able to leave a message for the verifier during this process, if you have any special information or comments to pass on.\n\n
 If you haven't yet finished and wish to continue working on this book and/or need help please forward a copy of this email (quoting the information on the book, above) with a brief description of the status to dphelp@pgdp.net.\n\n
+If you are waiting on missing images or page scans, please add the details to the Missing Page Wiki at: $forums_url/viewtopic.php?t=7584\n\n
 If you no longer wish to have this text assigned to you please visit the Distributed Proofreaders website Post Processing section and select Return to Available for this book, or forward this email to dphelp@pgdp.net and state that you would no longer like to have the book in question assigned to you so that we may return it to the available pool for someone else to work on.\n\n 
 Thanks!\nThe Distributed Proofreaders Team\n(http://www.pgdp.net)";
 		    } else {
@@ -54,6 +55,7 @@ Our database indicates that you have had $numprojs PP projects checked out for m
 $projectslist\n\n 
 If you have completed your work on any of these books, please log in to www.pgdp.net and visit the Post Processing page at $code_url/tools/post_proofers/post_proofers.php and, under 'Books I have checked out for Post-Processing' select the 'Upload for verification' option and follow the prompts. You will be able to leave a message for the verifier during this process, if you have any special information or comments to pass on.\n\n
 If you wish to continue working on some or all of these books and/or need help please forward this email, quoting the list of books, with a brief description of the status for each of the various books listed above to dphelp@pgdp.net.\n\n
+If you are waiting on missing images or page scans, please add the details to the Missing Page Wiki at: $forums_url/viewtopic.php?t=7584\n\n
 If you no longer wish to have some or all of these books assigned to you please visit the Distributed Proofreaders website Post Processing section and select Return to Available for the books in question or forward this email to dphelp@pgdp.net and state that you would no longer like to have the books in question assigned to you so that we may return them to the available pool for someone else to work on.\n\n 
 Thanks!\nThe Distributed Proofreaders Team\n(http://www.pgdp.net)";
 		    }
