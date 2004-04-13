5f921ea5d2202a905492b921177460a0d134e97e
aldarondo <>
2004-04-13 11:59:31 +0000

    Fixed minor bug

--- dp-devel/tools/project_manager/displayimage.php
+++ dp-devel/tools/project_manager/displayimage.php
@@ -1,9 +1,4 @@
 <html><head></head><body>
-<form method=get action="displayimage.php">
-<input type=hidden name="project" value="<?echo $project;?>">
-<input type=hidden name="imagefile" value="<?echo $imagefile;?>">
-<input type=text maxlength=3 name="percent" size=3> % <input type="submit" value="Resize" size=3>
-</form>
 <?
 $relPath='../../pinc/';
 include($relPath.'v_site.inc');
@@ -13,7 +8,13 @@ $imagefile = $_GET['imagefile'];
 if (!empty($_GET['percent'])) {
     $width = 10 * $_GET['percent'];
 } else $width = 1000;
-
-printf ("<img src=\"$projects_url/%s/%s\" width=$width border=0>", $project, $imagefile);
 ?>
+
+<form method=get action="displayimage.php">
+<input type=hidden name="project" value="<?echo $project;?>">
+<input type=hidden name="imagefile" value="<?echo $imagefile;?>">
+<input type=text maxlength=3 name="percent" size=3> % <input type="submit" value="Resize" size=3>
+</form>
+
+<? printf ("<img src=\"$projects_url/%s/%s\" width=$width border=0>", $project, $imagefile); ?>
 </body></html>
