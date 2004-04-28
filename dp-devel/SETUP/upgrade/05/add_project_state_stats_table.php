232a72904f4a7c3c3d5e9ca0f54d7721b40ae744
jmdyck <>
2004-04-28 05:19:57 +0000

    Add:   a comma,   a sensible key. and   an outlet for error messages.

--- dp-devel/SETUP/upgrade/05/add_project_state_stats_table.php
+++ dp-devel/SETUP/upgrade/05/add_project_state_stats_table.php
@@ -11,9 +11,9 @@ $result = mysql_query("CREATE TABLE `project_state_stats` (
   `date` date  NOT NULL default '0000-00-00',
   `state` varchar(50) NOT NULL default '0',
   `num_projects` int(12) NOT NULL default '0',
-  `comments` varchar(255)
-  KEY `task_id` (`task_id`)
-) TYPE=MyISAM");
+  `comments` varchar(255),
+  UNIQUE date (`date`)
+) TYPE=MyISAM") or die(mysql_error());
 
 
 echo "<center>";
