f884d887fbee6b24c7fd1b6ce5cca69d6759f918
aldarondo <>
2004-05-02 17:23:12 +0000

    Start Proofreading Link Shows At Bottom If Comments are newer than the last page proofread by the user

--- dp-devel/tools/proofers/projects.php
+++ dp-devel/tools/proofers/projects.php
@@ -24,7 +24,26 @@ slim_header(_("Project Comments"));
 if (!isset($proofing) && $userP['i_newwin']==1)
 {include($relPath.'js_newwin.inc');}
 if (!isset($proofing)) {
+    $top_menu = 1;
+
+    if ($proofstate==PROJ_PROOF_FIRST_AVAILABLE) {
+	$wTime="round1_time"; $wState=SAVE_FIRST; $wName="round1_user";
+    } else {
+	$wTime="round2_time"; $wState=SAVE_SECOND; $wName="round2_user";
+    }
+
+    // Get Last Page Date Proofed By Current User
+
+    $proofdate=mysql_query("SELECT $wTime FROM $project WHERE state='$wState' AND $wname='$pguser' ORDER BY $wTime DESC LIMIT 1");
+    if (mysql_num_rows($proofdate)!=0) {
+        $my_last_page_date = mysql_result($proofdate,0,$wTime);
+    } else $my_last_page_date = 0;
+
+    $project = mysql_fetch_assoc(mysql_query("SELECT modifieddate FROM projects WHERE projectid = '$project'"));
+    $comments_last_modified = $project['modifieddate'];
+
     include('./projects_menu.inc');
+    $top_menu = 0;
 ?>
 <br><i><? echo _("Please scroll down and read the Project Comments for any special instructions <b>before</b> proofreading!"); ?></i><br>
 <br>
