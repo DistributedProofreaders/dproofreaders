d3c6bc7aecec0fd44fd1afc2ad46f2356ac318d4
jmdyck <>
2004-05-19 01:49:09 +0000

    Use page_clear().

--- dp-devel/tools/project_manager/checkin.php
+++ dp-devel/tools/project_manager/checkin.php
@@ -1,11 +1,8 @@
 <?
 $relPath="./../../pinc/";
-include($relPath.'v_site.inc');
-include($relPath.'metarefresh.inc');
-include($relPath.'dp_main.inc');
-include_once($relPath.'page_states.inc');
-include_once($relPath.'f_project_states.inc');
-include($relPath.'project_edit.inc');
+include_once($relPath.'project_edit.inc');
+include_once($relPath.'metarefresh.inc');
+include_once('page_operations.inc');
 
     $project = $_GET['project'];
     $fileid = $_GET['fileid'];
@@ -16,87 +13,13 @@ include($relPath.'project_edit.inc');
 
     if (!empty($confirm)) {
 
-        $sql = mysql_query("SELECT state FROM projects WHERE projectid = '$project'");
-        $projstate = mysql_result($sql, 0, "state");
-
-        $inRound=projectStateRound($projstate);
-
-        if (($inRound=='NEW' || $inRound=='PR' || $inRound=='FIRST') && ($pagestate == SAVE_FIRST)) {
-	    $round_number = 1;
- 	    $text_field_name = 'round1_text';
-	    $user_field_name = 'round1_user';
-	    $time_field_name = 'round1_time';
-	    $new_state = AVAIL_FIRST;
-
-        } else if (($inRound=='SECOND') && ($pagestate == SAVE_SECOND)) {
-	    $round_number = 2;
-	    $text_field_name = 'round2_text';
-	    $user_field_name = 'round2_user';
-	    $time_field_name = 'round2_time';
-	    $new_state = AVAIL_SECOND;
-
-        } else {
-            print "File can not be checked back in due to the project not currently being available or available in a different state. Go <a href=\"project_detail.php?project=$project\">back</a>.";
-	    exit;
-        }
-
-        // if page is cleared, decrement user's page count
-
-        $result = mysql_query("
-	SELECT $user_field_name
-	FROM $project
-	WHERE fileid = '$fileid'");
-    
-        $data = mysql_fetch_array($result);
-        $proofer = $data[0];
-
-        $sql = "UPDATE users SET pagescompleted = pagescompleted-1 WHERE username = '$proofer'";
-        $result = mysql_query($sql);
-
-
-        // also decrement the counts of their teams    
-
-        $result = mysql_query("
-	SELECT team_1, team_2, team_3
-	FROM users
-	WHERE username = '$proofer'");
-    
-        $data = mysql_fetch_array($result);
-        $team1 = $data['team_1'];
-        $team2 = $data['team_2'];
-        $team3 = $data['team_3'];
-    
-
-        $sql = "UPDATE user_teams SET page_count = page_count-1 WHERE id=1 OR id=$team1 OR id=$team2 OR id=$team3";
-        $result = mysql_query($sql);
-
-
-
-        // now clear the page
-
-
-        if ($writeBIGtable) {
-	    $result = mysql_query("
-		UPDATE project_pages
-		SET $text_field_name='',
-		    $user_field_name='',
-		    $time_field_name='',
-		    state='$new_state'
-		WHERE projectid = '$project' AND fileid = '$fileid'
-            ");
-
-
-        }
-
-        $result = mysql_query("
-	    UPDATE $project
-    	SET $text_field_name='',
-	    $user_field_name='',
-	    $time_field_name='',
-	    state='$new_state'
-	WHERE fileid = '$fileid'");
-
-
+	$err = page_clear( $project, $fileid );
+	if ( $err )
+	{
+		echo "$err\n";
+		echo "Go <a href='project_detail.php?project=$project'>back</a>.";
+		exit;
+	}
 
         metarefresh(0, "project_detail.php?project=$project", "Page Checked In ($round_number)", "");
     } else {
