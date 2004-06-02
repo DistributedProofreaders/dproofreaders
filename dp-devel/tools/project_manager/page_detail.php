2db3c419ad8a6c6fc4f7f3c5de326d471f3809b0
pourlean <>
2004-06-02 06:42:03 +0000

    Removed browser trap by providing a link back to the comments page

--- dp-devel/tools/project_manager/page_detail.php
+++ dp-devel/tools/project_manager/page_detail.php
@@ -2,14 +2,25 @@
 $relPath="./../../pinc/";
 include_once($relPath.'v_site.inc');
 include_once($relPath.'dp_main.inc');
-include_once($relPath.'theme.inc');
 include_once('page_table.inc');
 
-theme("Project Details", "header");
+$myresult = mysql_query("
+                SELECT nameofwork, state FROM projects WHERE projectid = '$project'
+        ");
+
+$row = mysql_fetch_assoc($myresult);
+$state = $row['state'];
+$title = $row['nameofwork'];
+
+$label = _("Return to Project Comments page for");
+
+echo "<br>\n";
+echo "<a href='$code_url/tools/proofers/projects.php?project=$project&amp;proofstate=$state'>$label $title</a>";
+echo "<br>\n";
 
 include_once('detail_legend.inc');
 
 echo_page_table( $project, $show_image);
 
-theme("","footer");
 ?>
+
