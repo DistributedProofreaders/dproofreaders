fdcdb006d0f1c9cf87a9a4204c8f783a682b2b19
mike_cie <>
2004-06-04 14:57:22 +0000

    Added missing double quote

--- dp-devel/quiz/4/main.php
+++ dp-devel/quiz/4/main.php
@@ -3,14 +3,13 @@
 <head>
 <meta name="generator" content="HTML Tidy, see www.w3.org">
 <title>DP -- Proofing Quiz</title>
-<META http-equiv="Content-Type" content="text/html">
+<META http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 </head>
 <frameset cols="60%,*">
 <frameset name="left" rows="40%,*">
 <frame src="orig.php">
 <frame name="pf" src="proof.php">
 </frameset>
-<frame name="right" src="right.php>
+<frame name="right" src="right.php">
 </frameset>
 </html>
-
