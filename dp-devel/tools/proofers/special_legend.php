12f947fed857b3361aa8f3b99a303b552912b4a2
big_bill_boy <>
2004-06-04 15:19:57 +0000

    updated the colour code (resynch from live)

--- dp-devel/tools/proofers/special_legend.php
+++ dp-devel/tools/proofers/special_legend.php
@@ -2,9 +2,7 @@
 Legend for Special Books:
 <br><br><b>
 <span class="earthblue">World Earth Day</span>&nbsp;
-<br>
 <span class="pink">April Fool's Day</span>&nbsp;
-<span class="midgrey">Black History Month</span>&nbsp;
 <br>
 <span class="paleblue"> Authors with recent birthdays </span>&nbsp;
 <span class="richblue"> Authors with birthdays today </span>&nbsp;
