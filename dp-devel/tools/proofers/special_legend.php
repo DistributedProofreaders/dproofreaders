a94dad0a46138fd8a2073ccb67fc380816722d8a
big_bill_boy <>
2004-06-04 14:51:00 +0000

    updated the colour code (resynch from live)

--- dp-devel/tools/proofers/special_legend.php
+++ dp-devel/tools/proofers/special_legend.php
@@ -1,16 +1,13 @@
 
 Legend for Special Books:
 <br><br><b>
-<span class="red">Ides of March</span>&nbsp;
-<span class="green">Saint Patrick's Day</span>&nbsp;
+<span class="earthblue">World Earth Day</span>&nbsp;
 <br>
-<span class="nicegreen">International Women's Day</span>&nbsp;
+<span class="pink">April Fool's Day</span>&nbsp;
 <span class="midgrey">Black History Month</span>&nbsp;
 <br>
 <span class="paleblue"> Authors with recent birthdays </span>&nbsp;
 <span class="richblue"> Authors with birthdays today </span>&nbsp;
 <br>
-<span class="midgreen">India's Republic Day</span>&nbsp;
 <span class="fadedyellow"> Other Special </span>
 </b>
-
