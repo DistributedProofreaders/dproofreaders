b3b527c529453f3abcf4ccc72c4b54576919a6a3
jmdyck <>
2004-06-21 08:15:17 +0000

    Regularize the indenting.

--- dp-devel/tools/site_admin/stats_repair.php
+++ dp-devel/tools/site_admin/stats_repair.php
@@ -10,20 +10,20 @@ new dbConnect();
 $user_condition =  "u_id >= 20000 ";
 
 $users = mysql_query("
-           SELECT u_id, username FROM users WHERE $user_condition ORDER BY u_id
-        ");
+	SELECT u_id, username FROM users WHERE $user_condition ORDER BY u_id
+");
 
 while ($user_row = mysql_fetch_assoc($users)) {
 
-    $user = $user_row['u_id'];
-    $username = $user_row['username'];
+	$user = $user_row['u_id'];
+	$username = $user_row['username'];
 
 	echo "attempting to repair stats for $username\n";
 
-    $userdates = mysql_query("
-           SELECT * FROM member_stats WHERE date_updated >=  1080979200
+	$userdates = mysql_query("
+		SELECT * FROM member_stats WHERE date_updated >=  1080979200
 			AND u_id = '$user' ORDER by date_updated
-        ");
+	");
 
 	$userdate_row = mysql_fetch_assoc($userdates);
 
@@ -37,9 +37,9 @@ while ($user_row = mysql_fetch_assoc($users)) {
 		$diff = $new_total - $yester_total;
 
 		$updq = mysql_query("
-				UPDATE member_stats SET daily_pagescompleted = $diff
-				WHERE u_id = '$user' AND date_updated = $currdate
-				");
+			UPDATE member_stats SET daily_pagescompleted = $diff
+			WHERE u_id = '$user' AND date_updated = $currdate
+		");
 
 		$yester_total = $new_total;
 
