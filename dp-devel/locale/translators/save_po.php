7feedf2ab363ea80577f377a83d1595a08436cdd
smolenski <>
2004-07-16 14:54:57 +0000

    Error reporting; UTF-8->

--- dp-devel/locale/translators/save_po.php
+++ dp-devel/locale/translators/save_po.php
@@ -26,7 +26,7 @@ if (isset($_POST['lang']) && isset($_POST['save_po'])) {
 	fputs($po_file, "\"Last-Translator: $real_name &lt;$email_addr&gt;\\n\"\n");
 	fputs($po_file, "\"Language-Team: ".$iso_639[$lang]." &lt;$email_addr&gt;\\n\"\n");
 	fputs($po_file, "\"MIME-Version: 1.0\\n\"\n");
-	fputs($po_file, "\"Content-Type: text/plain; charset=UTF-8\\n\"\n");
+	fputs($po_file, "\"Content-Type: text/plain; charset=$charset\\n\"\n");
 	fputs($po_file, "\"Content-Transfer-Encoding: 8bit\\n\"\n\n");
 
 	$i = 0;
@@ -43,9 +43,17 @@ if (isset($_POST['lang']) && isset($_POST['save_po'])) {
 	}
 	fclose($po_file);
 
-	exec("msgfmt messages.po -o messages.mo");
+	exec("msgfmt messages.po -o messages.mo 2>&1",$exec_out,$ret_var);
 
-	echo "<center><b><i><font size='+2'>"._("Translation Completed!")."</font></i></b><br><br>";
+	if($ret_var) {
+		echo "<center>"._("Translation <b>not</b> completed!")."</center><br>"._("Following error is given:")."<br><br>";
+		echo "<pre>";
+		foreach($exec_out as $v)
+			echo $v."\n";
+		echo "</pre><br>";
+	} else {
+		echo "<center><b><i><font size='+2'>"._("Translation Completed!")."</font></i></b><br><br>";
+	}
 	echo "<a href='$code_url/locale/translators/'>"._("Please click here to return to the Translation Center.")."</a></center>";
 }
 
@@ -60,19 +68,27 @@ if (isset($_POST['lang']) && isset($_POST['rebuild_strings'])) {
 	$translation = parse_po(file("$dyn_locales_dir/$lang/LC_MESSAGES/temp.po"));
 
 	chdir($code_dir);
-	exec("xgettext -j `find -name \"*.php\" -o -name \"*.inc\"` -p $dyn_locales_dir/$lang/LC_MESSAGES/ --keyword=_ -C -L PHP");
+	exec("xgettext -j `find -name \"*.php\" -o -name \"*.inc\"` -p $dyn_locales_dir/$lang/LC_MESSAGES/ --keyword=_ -C -L PHP 2>&1",$exec_out,$ret_var);
 
-	$i=4;
-	$lines = file("$dyn_locales_dir/$lang/LC_MESSAGES/temp.po");
-	$po_file = fopen("$dyn_locales_dir/$lang/LC_MESSAGES/temp.po", "w");
-	fputs($po_file, "# ".str_replace("\n", "\n# ", $_POST['comments'])."\n");
-	while ($i < count($lines)) {
-		fputs($po_file, $lines[$i]);
-		$i++;
+	if($ret_var) {
+                echo "<center>"._("Strings <b>not</b> rebuilt!")."</center><br>"._("Following error is given:")."<br><br>";
+                echo "<pre>";
+                foreach($exec_out as $v)
+                        echo $v."\n";
+                echo "</pre><br>";
+        } else {
+		$i=4;
+		$lines = file("$dyn_locales_dir/$lang/LC_MESSAGES/temp.po");
+		$po_file = fopen("$dyn_locales_dir/$lang/LC_MESSAGES/temp.po", "w");
+		fputs($po_file, "# ".str_replace("\n", "\n# ", $_POST['comments'])."\n");
+		while ($i < count($lines)) {
+			fputs($po_file, $lines[$i]);
+			$i++;
+		}
+		fclose($po_file);
+
+		echo "<center><b><i><font size='+2'>"._("Strings Rebuilt!")."</font></i></b><br><br>";
 	}
-	fclose($po_file);
-
-	echo "<center><b><i><font size='+2'>"._("Strings Rebuilt!")."</font></i></b><br><br>";
 	echo "<a href='index.php?func=translate&lang=$lang'>"._("Please click here to return to translate the ").$iso_639[$lang]." "._("language file").".</a>";
 }
 
