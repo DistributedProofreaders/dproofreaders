71354dd4bb22ea2e9f54167ee889146cd0257374
smolenski <>
2004-08-02 01:23:23 +0000

    Major update: ability to translate files partially; some HTML tidying

--- dp-devel/locale/translators/index.php
+++ dp-devel/locale/translators/index.php
@@ -7,6 +7,7 @@ include_once($relPath.'iso_639_list.inc');
 include_once($relPath.'metarefresh.inc');
 include_once('parse_po.inc');
 
+$no_stats=1;
 theme(_("Translation Center"), "header");
 
 if (isset($_GET['func'])) { $func = $_GET['func']; } else { $func = ""; }
@@ -17,22 +18,61 @@ if (empty($_REQUEST['lang']) && empty($func)) {
 	echo "<br><br><center>"._("Please click the <i>Translate</i> link next to the language you would like to translate to begin:")."</center><br><br>";
 
 	$dir = opendir($dyn_locales_dir);
-	echo "<table border='0' cellspacing='3' cellpadding='' width='100%'><ul>";
+	echo "<table border='0' cellspacing='3' cellpadding='' width='100%'><ul>\n";
  	while (false !== ($file = readdir($dir))) {
  		if ($file != "." && $file != ".." && $file != "CVS" && $file != "translators") {
- 			echo "<tr><td width='50%'><li>".$iso_639[$file]."</li></td><td width='50%'>[ <a href='index.php?func=translate&lang=$file'>"._("Translate")."</a> ]</td></tr>";
+ 			echo "<tr><td width='50%'><li>".$iso_639[$file]."</li></td><td width='50%'>[ <a href='index.php?func=translate&lang=$file'>"._("Translate")."</a> ]</td></tr>\n";
  		}
  	}
- 	echo "</ul></table>";
+ 	echo "</ul></table>\n";
 }
 
 if (!empty($_GET['lang']) && $func == "translate") {
 	$lang = $_GET['lang'];
+	$location = $_GET['location'];
 	$translation = parse_po(file("$dyn_locales_dir/$lang/LC_MESSAGES/messages.po"));
-	$i = 0;
+
+/* Structure of $translation array:
+$translation=array(
+	'comments'=>"Comments",
+	'location'=>array(
+		0=>"#: default.php:22",
+		1=>"#: default.php:22",
+		...
+	),
+	'msgid'=>array(
+		0=>" active users out of ",
+		1=>" total users in the past twenty-four hours.",
+		...
+	),
+	'msgstr'=>array(
+		0=>" utenti attivi su ",
+		1=>" utenti totali nelle ultime 24 ore.",
+		...
+	);
+*/
+
 	if (!isset($translation['location']))
 	{
 		echo "\n<p>"._("Something is wrong: I cannot find any translatable strings in the translation file. Perhaps the file is corrupt?");
+	}
+	else if(!$location)
+	{
+		$locations=array_flip(
+			preg_replace(
+		                array("/^[^\s]+\s+/","/:[0-9]+.*/s"),
+                		"",
+		                $translation['location']
+			)
+		);
+
+		echo "<table border='0' cellspacing='3' cellpadding='' width='100%'><ul>\n";
+		echo "<tr><td width='50%'><li>"._("All")."</li></td><td width='50%'>[ <a href='index.php?func=translate&lang=$lang&location=all'>"._("Translate")."</a> ]</td></tr>\n";
+		foreach($locations as $k=>$v)
+			echo "<tr><td width='50%'><li>$k</li></td><td width='50%'>[ <a href='index.php?func=translate&lang=$lang&location=$k'>"._("Translate")."</a> ] (<a href='$code_url/$k' target='_new'>"._("Location")."</a>)</td></tr>\n";
+                echo "</ul><tr><td width='50%'><form action='save_po.php' method='post'>";
+                echo "<input type='hidden' name='lang' value='$lang'>";
+		echo "<center><input type='submit' name='rebuild_strings' value='"._("Rebuild String List")."'></center><br></form></td></tr></table>\n";
 	}
 	else
 	{
@@ -42,19 +82,23 @@ if (!empty($_GET['lang']) && $func == "translate") {
 		echo "<form action='save_po.php' method='post'>";
 		echo "<br><center><b>"._("Comments")."</b>:<br><textarea name='comments' rows=5 cols=85>".trim($translation['comments'])."</textarea></center><br>\n";
 
+	        $i = 0;
 		while ($i < $numOfTranslations) {
-			$location = trim(substr($translation['location'][$i], 2, strpos(substr($translation['location'][$i], 2), ":")));
-			echo "<b><i>".visible_invisibles(htmlspecialchars($translation['msgid'][$i]))."</b></i>\n(<a href='$code_url/$location' target='_new'>Location</a>)<br>\n";
-			echo "<input type='hidden' name='location_".$i."' value='".base64_encode(serialize($translation['location'][$i]))."'>\n<input type='hidden' name='msgid_".$i."' value='".base64_encode(serialize(trim($translation['msgid'][$i])))."'>\n";
-			echo "<textarea name='msgstr_".$i."'rows=3 cols=85>".trim(htmlspecialchars($translation['msgstr'][$i]))."</textarea><br><br>\n\n";
-			$i++;
+			$loc = trim(substr($translation['location'][$i], 2, strpos(substr($translation['location'][$i], 2), ":")));
+			if($location=="all" || $location==$loc) {
+				echo "<b><i>".visible_invisibles(htmlspecialchars($translation['msgid'][$i]))."</b></i>\n(<a href='$code_url/$loc' target='_new'>"._("Location")."</a>)<br>\n";
+				echo "<input type='hidden' name='location_".$i."' value='".base64_encode(serialize($translation['location'][$i]))."'>\n<input type='hidden' name='msgid_".$i."' value='".base64_encode(serialize($translation['msgid'][$i]))."'>\n";
+				echo "<textarea name='msgstr_".$i."'rows=3 cols=85>".htmlspecialchars($translation['msgstr'][$i])."</textarea><br><br>\n\n";
 			}
+			$i++;
+		}
 
 		echo "<input type='hidden' name='numofTranslations' value='".($numOfTranslations-1)."'>";
 		echo "<input type='hidden' name='lang' value='$lang'>";
+		echo "<input type='hidden' name='location' value='$location'>";
 		echo "<center><input type='submit' name='save_po' value='"._("Save and Compile")."'>&nbsp;";
-		echo "<input type='submit' name='rebuild_strings' value='"._("Rebuild String List")."'></center><br>";
-		echo "</td></tr></table>";
+		echo "<input type='submit' name='rebuild_strings' value='"._("Rebuild String List")."'></center><br></form>";
+		echo "</td></tr></table>\n";
 		}
 	}
 
