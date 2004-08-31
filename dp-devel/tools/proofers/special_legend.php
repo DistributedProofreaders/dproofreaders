e66ab13a5a25b62b75d0d879db0c5298800e5730
pourlean <>
2004-08-31 22:24:32 +0000

    Updated colour codes for special books

--- dp-devel/tools/proofers/special_legend.php
+++ dp-devel/tools/proofers/special_legend.php
@@ -1,8 +1,8 @@
 
 Legend for Special Books:
 <br><br><b>
+<span class="red">WWI Anniversary</span>&nbsp;
 <span class="midgreen">George Sand Anniversary</span>&nbsp;
-<span class="pink">April Fool's Day</span>&nbsp;
 <br>
 <span class="paleblue"> Authors with recent birthdays </span>&nbsp;
 <span class="richblue"> Authors with birthdays today </span>&nbsp;
