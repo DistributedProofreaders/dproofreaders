3c718220ec8bc9ce00e80e43ac1f8ff653b07ab4
jmdyck <>
2004-09-13 00:54:07 +0000

    ProofingRoundDescriptor's page-state fields: rename them with a "page_" prefix.

--- dp-devel/tools/proofers/processtext.php
+++ dp-devel/tools/proofers/processtext.php
@@ -156,7 +156,7 @@ else if ($tbutton==B_QUIT || $tbutton==B_SAVE_AND_QUIT || $tbutton==B_RETURN_PAG
 
 if ($tbutton==B_REPORT_BAD_PAGE)
 {
-$badState=$tpage->prd->bad_state;
+$badState=$tpage->prd->page_bad_state;
 include('badpage.php');
 } // end B_REPORT_BAD_PAGE
 
