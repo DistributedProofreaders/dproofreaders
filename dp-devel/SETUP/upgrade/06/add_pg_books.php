59c13faf6e6cca70d2831a97017d1b460fcfe5cd
jmdyck <>
2004-09-17 06:39:09 +0000

    *** empty log message ***

---
+++ dp-devel/SETUP/upgrade/06/add_pg_books.php
@@ -0,0 +1,15 @@
+<?php
+$relPath='./pinc/';
+include($relPath.'connect.inc');
+new dbConnect();
+
+$sql = "
+    CREATE TABLE pg_books (
+        etext_number SMALLINT UNSIGNED NOT NULL PRIMARY KEY,
+        formats      TINYTEXT          NOT NULL,
+    )
+    COMMENT='Each row represents a different PG etext'
+";
+mysql_query($sql) or die( mysql_error() );
+echo "<center>Table 'pg_books' created successfully.</center>";
+?>
