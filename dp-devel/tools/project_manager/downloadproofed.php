60661d580f44c02da03ad9d7e7d804b207107a71
jmdyck <>
2004-10-02 22:33:33 +0000

    Replace GET-parameter 'state' with 'round_num'.

--- dp-devel/tools/project_manager/downloadproofed.php
+++ dp-devel/tools/project_manager/downloadproofed.php
@@ -4,18 +4,18 @@ include($relPath.'dp_main.inc');
 include_once($relPath.'page_states.inc');
     $project = $_GET['project'];
     $fileid = $_GET['fileid'];
-    $state = $_GET['state'];
+    $round_num = $_GET['round_num'];
 
-    if ($state == UNAVAIL_FIRST) {
+    if ($round_num == 0) {
         $result = mysql_query("SELECT master_text FROM $project WHERE fileid = '$fileid'"); 
         $data = mysql_result($result, 0, "master_text");
-    } else if ($state == SAVE_FIRST) {
+    } else if ($round_num == 1) {
         $result = mysql_query("SELECT round1_text FROM $project WHERE fileid = '$fileid'"); 
         $data = mysql_result($result, 0, "round1_text");
-    } else if ($state == SAVE_SECOND) {
+    } else if ($round_num == 2) {
         $result = mysql_query("SELECT round2_text FROM $project WHERE fileid = '$fileid'"); 
         $data = mysql_result($result, 0, "round2_text");
-    } else $data = "ERROR: Incorrect state parameter = ".$state." passed to script downloadproofed.php";
+    } else $data = "ERROR: Incorrect round_num parameter = ".$round_num." passed to script downloadproofed.php";
 
     header("Content-type: text/plain; charset=$charset");
     // SENDING PAGE-TEXT TO USER
