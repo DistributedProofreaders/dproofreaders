e41faa3cd7e2f847345c70f7addc4c529671bb4f
jmdyck <>
2004-10-02 21:46:23 +0000

    Make it independent of the "two rounds" constraint.

--- dp-devel/tools/project_manager/diff.php
+++ dp-devel/tools/project_manager/diff.php
@@ -1,6 +1,7 @@
 <?php
 $relPath="./../../pinc/";
 include_once($relPath.'v_site.inc');
+include_once($relPath.'RoundDescriptor.inc');
 include_once($relPath.'theme.inc');
 include_once($relPath.'project_edit.inc');
 include_once('projectmgr.inc');
@@ -19,13 +20,9 @@ $projectid = $_GET['project'];
 $fileid=$_GET['file'];
 $round=$_GET['round'];
 
-if($round==1) {
-	$fields="master_text,round1_text";
-} else {
-	$fields="round1_text,round2_text";
-}
+$prd = get_PRD_for_round($round);
 
-$res = mysql_query("SELECT $fields FROM $projectid WHERE fileid='$fileid'");
+$res = mysql_query("SELECT $prd->prevtext_column_name, $prd->text_column_name FROM $projectid WHERE fileid='$fileid'");
 
 $txt=mysql_fetch_row($res);
 
