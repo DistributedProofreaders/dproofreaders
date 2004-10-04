777efa5b9e14437d192eeda6e5284260bcca1375
jmdyck <>
2004-10-04 02:19:38 +0000

    Formerly, projects_menu.inc would display the "Start Proofreading" link (or the "Round Complete" or "Start Link appears below" alternatives). But it was an awkward fit, because that code required lots of project-specific info, and projects_menu.inc is otherwise fairly generic.
    So I pulled that code out of projects_menu.inc, brought it here,
    rearranged the decision logic, mixed in the "Please scroll down" line,
    added an explicit statement of the project comments' last mod time,
    and put it into its own box.

--- dp-devel/tools/proofers/projects.php
+++ dp-devel/tools/proofers/projects.php
@@ -21,42 +21,117 @@ if (!isset($proofing))
         include($relPath.'js_newwin.inc');
     }
 
+    include('./projects_menu.inc');
+
+    function echo_blurb_box( $blurb )
+    {
+        echo "<br>";
+        echo "<table width='630' bgcolor='DDDDDD'>";
+        echo "<tr><td align='center'>";
+        echo $blurb;
+        echo "</td></tr>";
+        echo "</table>";
+        echo "<br>";
+        echo "\n";
+    }
+
     project_update_page_counts( $project );
 
     $num_pages_available = Project_getNumAvailablePagesInRound( $project, $proofstate );
-    $pages_are_available = ( $num_pages_available > 0 );
+    if ( $num_pages_available == 0 )
+    {
+        $top_blurb = $bottom_blurb =
+            _("Round Complete")
+            . "<br>"
+            . _("There are no pages available for proofreading.");
+    }
+    else
+    {
+        // If there's any proofreading to be done, this is the link to use.
+        $url = "proof_frame.php?project=$project&amp;proofstate=$proofstate"; // encoded for use in attribute
+        $label = _("Start Proofreading");
+        $proofreading_link = "<b><a href='$url' target='proofframe'>$label</a></b>";
 
-    $top_menu = 1;
+        // When were the project comments last modified?
+        $res = mysql_fetch_assoc(mysql_query("SELECT modifieddate FROM projects WHERE projectid = '$project'"));
+        $comments_timestamp = $res['modifieddate'];
+        $comments_time_str = strftime(_("%A, %B %e, %Y at %X"), $comments_timestamp);
+        $comments_last_modified_blurb = _("Project Comments last modified:") . " " . $comments_time_str;
 
-    // Get Last Page Date Proofed By Current User
+        // Other possible components of blurbs:
+        $please_scroll_down = _("Please scroll down and read the Project Comments for any special instructions <b>before</b> proofreading!");
+        $the_link_appears_below = _("The 'Start Proofreading' link appears below the Project Comments");
+        $comments_have_changed =
+            "<font color='red'>"
+            . "<b>"
+            . _("Project Comments have changed!")
+            . "</b>"
+            . "</font>";
 
-    $prd = get_PRD_for_project_state($proofstate);
-    $proofdate = mysql_query("
-        SELECT {$prd->time_column_name}
-        FROM $project
-        WHERE state='{$prd->page_save_state}' AND {$prd->user_column_name}='$pguser'
-        ORDER BY {$prd->time_column_name} DESC
-        LIMIT 1
-    ");
-    if (mysql_num_rows($proofdate)!=0) {
-        $my_last_page_date = mysql_result($proofdate,0,$prd->time_column_name);
-    } else $my_last_page_date = 0;
+        // ---
 
-    $project_comments = mysql_fetch_assoc(mysql_query("SELECT modifieddate FROM projects WHERE projectid = '$project'"));
-    $comments_last_modified = $project_comments['modifieddate'];
+        $bottom_blurb =
+            $comments_last_modified_blurb
+            . "<br>"
+            . $proofreading_link;
 
-    include('./projects_menu.inc');
-    $top_menu = 0;
+        // Has the user saved a page of this project since the comments were
+        // last changed?  If not, it's unlikely they've seen the revised comments.
+        $prd = get_PRD_for_project_state($proofstate);
+        $res = mysql_query("
+            SELECT {$prd->time_column_name}
+            FROM $project
+            WHERE state='{$prd->page_save_state}' AND {$prd->user_column_name}='$pguser'
+            ORDER BY {$prd->time_column_name} DESC
+            LIMIT 1
+        ");
+        if (mysql_num_rows($res) == 0)
+        {
+            // The user has not saved any pages for this project.
+            $top_blurb =
+                $please_scroll_down
+                . "<br>"
+                . $comments_last_modified_blurb
+                . "<br>"
+                . $the_link_appears_below;
+        }
+        else
+        {
+            // The user has saved a page for this project.
+            $my_latest_save_timestamp = mysql_result($res,0,$prd->time_column_name);
 
-    echo "<br>";
-    echo "<i>";
-    echo _("Please scroll down and read the Project Comments for any special instructions <b>before</b> proofreading!");
-    echo "</i>";
-    echo "<br>";
-    echo "<br>";
+            if ($my_latest_save_timestamp < $comments_timestamp)
+            {
+                // The latest page-save was before the comments were revised.
+                // The user probably hasn't seen the revised project comments.
+                $top_blurb =
+                    $comments_have_changed
+                    . "<br>"
+                    . $please_scroll_down
+                    . "<br>"
+                    . $comments_last_modified_blurb
+                    . "<br>"
+                    . $the_link_appears_below;
+            }
+            else
+            {
+                // The latest page-save was after the comments were revised.
+                // We'll assume that the user has read the comments.
+                $top_blurb =
+                    $please_scroll_down
+                    . "<br>"
+                    . $comments_last_modified_blurb
+                    . "<br>"
+                    . $proofreading_link;
+            }
+        }
+    }
+
+    echo_blurb_box( $top_blurb );
 
     echo_project_info( $project, $proofstate, TRUE );
-    echo "<BR>";
+
+    echo_blurb_box( $bottom_blurb );
 
     include('./projects_menu.inc');
 }
