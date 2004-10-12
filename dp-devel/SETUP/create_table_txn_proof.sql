2c52e38dc06822507faf366b57ba124641b094f6
dkretz <>
2004-10-12 18:05:39 +0000

    Create log table for storing proofing transactions

---
+++ dp-devel/SETUP/create_table_txn_proof.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE `dp_db`.`txn_proof` (
+`txn_id` int( 20 ) NOT NULL AUTO_INCREMENT ,
+`txn_time` int( 20 ) NOT NULL ,
+`txn_code` char( 1 ) NOT NULL ,
+`username` varchar( 25 ) NOT NULL ,
+`projectid` varchar( 22 ) NOT NULL ,
+`fileid` varchar( 20 ) default NULL ,
+PRIMARY KEY ( `txn_id` ) ,
+KEY `txn_time` ( `txn_time` ) ,
+KEY `username` ( `username` ) ,
+KEY `projectid` ( `projectid` , `fileid` ) 
+) TYPE = MYISAM ;
+
