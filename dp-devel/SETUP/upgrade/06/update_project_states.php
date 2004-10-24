61ef067b7d39c6d98695eee1b9b852b3013efeb3
jmdyck <>
2004-10-24 01:50:59 +0000

    Rephrase.

--- dp-devel/SETUP/upgrade/06/update_project_states.php
+++ dp-devel/SETUP/upgrade/06/update_project_states.php
@@ -11,32 +11,39 @@ new dbConnect();
 // have been discontinued.
 // Find any projects in those states and move them to PROJ_POST_COMPLETE.
 
-$new_state = PROJ_POST_COMPLETE;
-
 echo "<pre>\n";
 
-$res = mysql_query("
-	SELECT nameofwork, state, projectid
-	FROM projects
-	WHERE state='proj_submit_pgunavailable'
-	   OR state='proj_submit_pgavailable'
-	   OR state='proj_submit_pgposting'
-") or die(mysql_error());
+$changes = array(
+	array('proj_submit_pgunavailable',PROJ_POST_COMPLETE),
+	array('proj_submit_pgavailable',  PROJ_POST_COMPLETE),
+	array('proj_submit_pgposting',    PROJ_POST_COMPLETE),
+);
 
-$n = mysql_num_rows($res);
-echo "$n projects to update...\n";
-echo "\n";
-
-while ( list($nameofwork,$state,$projectid) = mysql_fetch_row($res) )
+foreach ( $changes as $change )
 {
-	echo "Project $projectid: \"$nameofwork\"\n";
-	echo "    Changing its state to '$new_state' from '$state'\n";
-	mysql_query("
-		UPDATE projects
-		SET state='$new_state'
-		WHERE projectid='$projectid'
+	list($old_state,$new_state) = $change;
+
+	echo "Moving projects from '$old_state' to '$new_state'\n";
+	$res = mysql_query("
+		SELECT nameofwork, state, projectid
+		FROM projects
+		WHERE state='$old_state'
 	") or die(mysql_error());
+
+	$n = mysql_num_rows($res);
+	echo "$n projects to update...\n";
 	echo "\n";
+
+	while ( list($nameofwork,$state,$projectid) = mysql_fetch_row($res) )
+	{
+		echo "    Project $projectid: \"$nameofwork\"\n";
+		mysql_query("
+			UPDATE projects
+			SET state='$new_state'
+			WHERE projectid='$projectid'
+		") or die(mysql_error());
+		echo "\n";
+	}
 }
 echo "done.\n";
 echo "</pre>\n";
