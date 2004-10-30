757645331b8fbb54c19904e77ba7db779537ef64
jmdyck <>
2004-10-30 21:44:51 +0000

    Make the code somewhat easier to read.

--- dp-devel/stats/requested_books.php
+++ dp-devel/stats/requested_books.php
@@ -13,28 +13,29 @@ echo "<br><h2 style='color: $theme[color_headerbar_bg];'>$title</h2><br>\n
 
 echo "<br><br><h3 style='color: $theme[color_headerbar_bg];'>" . _("Most Requested Books Being Proofread") . "</h2><br>\n";
 
-        $comments_url1 = mysql_escape_string("<a href='".$code_url."/tools/proofers/projects.php?project=");
-        $comments_url2 = mysql_escape_string("'>");
-        $comments_url3 = mysql_escape_string("</a>");
+$comments_url1 = mysql_escape_string("<a href='".$code_url."/tools/proofers/projects.php?project=");
+$comments_url2 = mysql_escape_string("'>");
+$comments_url3 = mysql_escape_string("</a>");
 
 dpsql_dump_themed_ranked_query("
-SELECT
-CONCAT('$comments_url1',projectID,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
-`authorsname` AS 'Author', 
-`genre` AS 'Genre',
-`language` AS 'Language',
-COUNT(*) AS 'Notification Requests' 
-FROM `usersettings`,`projects` 
-WHERE `value` = `projectid` 
-AND `setting` = 'posted_notice' 
-AND (`state` = '".PROJ_PROOF_FIRST_WAITING_FOR_RELEASE."'
-  OR `state` = '".PROJ_PROOF_SECOND_WAITING_FOR_RELEASE."'
-  OR `state` = '".PROJ_PROOF_FIRST_AVAILABLE."'
-  OR `state` = '".PROJ_PROOF_SECOND_AVAILABLE."'
-)
-GROUP BY `value` 
-ORDER BY 'Notification Requests' DESC 
-LIMIT 50");
+    SELECT
+        CONCAT('$comments_url1',projectID,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
+        authorsname AS 'Author', 
+        genre AS 'Genre',
+        language AS 'Language',
+        COUNT(*) AS 'Notification Requests' 
+    FROM usersettings, projects 
+    WHERE value = projectid 
+        AND setting = 'posted_notice' 
+        AND (state = '".PROJ_PROOF_FIRST_WAITING_FOR_RELEASE."'
+          OR state = '".PROJ_PROOF_SECOND_WAITING_FOR_RELEASE."'
+          OR state = '".PROJ_PROOF_FIRST_AVAILABLE."'
+          OR state = '".PROJ_PROOF_SECOND_AVAILABLE."'
+        )
+    GROUP BY value 
+    ORDER BY 'Notification Requests' DESC 
+    LIMIT 50
+");
 
 echo "<br>\n";
 echo "<br><br><h3 style='color: $theme[color_headerbar_bg];'>" . _("Most Requested Books In Post-Processing") . "</h2><br>\n";
@@ -42,37 +43,38 @@ echo "<br><br><h3 style='color: $theme[color_headerbar_bg];'>" . _("Most Request
 //        $post_url1 = mysql_escape_string("<a href='".$code_url."/tools/post_proofers/post_comments.php?project=");
 
 dpsql_dump_themed_ranked_query("
-SELECT
-concat('$comments_url1',projectID,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
-`authorsname` AS 'Author', 
-`genre` AS 'Genre',
-`language` AS 'Language',
-COUNT(*) as 'Notification Requests' 
-FROM `usersettings`,`projects` 
-WHERE `value` = `projectid` 
-AND `setting` = 'posted_notice' 
-AND (`state` = 'proj_post_first_unavailable' OR `state` = 'proj_post_first_available' OR `state` = 'proj_post_first_checked_out' OR `state` = 'proj_post_second_available' OR `state` = 'proj_post_second_checked_out' OR `state` = 'proj_post_complete')
-GROUP BY `value` 
-ORDER BY 'Notification Requests' DESC 
-LIMIT 50");
+    SELECT
+        CONCAT('$comments_url1',projectID,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
+        authorsname AS 'Author', 
+        genre AS 'Genre',
+        language AS 'Language',
+        COUNT(*) AS 'Notification Requests' 
+    FROM usersettings, projects 
+    WHERE value = projectid 
+        AND setting = 'posted_notice' 
+        AND (state = 'proj_post_first_unavailable' OR state = 'proj_post_first_available' OR state = 'proj_post_first_checked_out' OR state = 'proj_post_second_available' OR state = 'proj_post_second_checked_out' OR state = 'proj_post_complete')
+    GROUP BY value 
+    ORDER BY 'Notification Requests' DESC 
+    LIMIT 50
+");
 
 echo "<br>\n";
 echo "<br><br><h3 style='color: $theme[color_headerbar_bg];'>" . _("Most Requested Books Posted to Project Gutenberg") . "</h2><br>\n";
 
-        $pg_url1 = mysql_escape_string("<a href='http://www.gutenberg.org/etext/");
- dpsql_dump_themed_ranked_query("
-SELECT
-CONCAT('$pg_url1',postednum,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
-`authorsname` AS 'Author', 
-`genre` AS 'Genre',
-`language` AS 'Language',
-`int_level` AS 'Notification Requests' 
-FROM `projects` 
-WHERE (`state` = 'proj_submit_pgunavailable' OR `state` = 'proj_submit_pgavailable' OR `state` = 'proj_submit_pgposting' OR `state` = 'proj_submit_pgposted' OR `state` = 'proj_correct_checked_out' OR `state` = 'proj_correct_available')
-AND int_level !=0
-ORDER BY 'Notification Requests' DESC 
-LIMIT 50");
-
+$pg_url1 = mysql_escape_string("<a href='http://www.gutenberg.org/etext/");
+dpsql_dump_themed_ranked_query("
+    SELECT
+        CONCAT('$pg_url1',postednum,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
+        authorsname AS 'Author', 
+        genre AS 'Genre',
+        language AS 'Language',
+        int_level AS 'Notification Requests' 
+    FROM projects 
+    WHERE (state = 'proj_submit_pgunavailable' OR state = 'proj_submit_pgavailable' OR state = 'proj_submit_pgposting' OR state = 'proj_submit_pgposted' OR state = 'proj_correct_checked_out' OR state = 'proj_correct_available')
+        AND int_level !=0
+    ORDER BY 'Notification Requests' DESC 
+    LIMIT 50
+");
 
 theme("","footer");
 
