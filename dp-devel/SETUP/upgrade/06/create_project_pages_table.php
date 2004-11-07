73ebb904dff81f7c8bfc5b12857b41521f609bc2
jmdyck <>
2004-11-07 02:31:51 +0000

    Define the table with columns+keys for rounds up to MAX_NUM_PAGE_EDITING_ROUNDS. The file is now independent of the "two rounds" constraint.

--- dp-devel/SETUP/upgrade/06/create_project_pages_table.php
+++ dp-devel/SETUP/upgrade/06/create_project_pages_table.php
@@ -1,31 +1,36 @@
 <?PHP
 $relPath = './pinc/';
 include_once($relPath.'connect.inc');
+include_once($relPath.'RoundDescriptor.inc');
 new dbConnect();
 
+$items_for_rounds = "";
+for ($rn = 1; $rn <= MAX_NUM_PAGE_EDITING_ROUNDS; $rn++ )
+{
+    $prd = get_PRD_for_round($rn);
+    $items_for_rounds .= "
+        {$prd->time_column_name}   int( 20 )     NOT NULL default '0' KEY,
+        {$prd->user_column_name}   varchar( 25 ) NOT NULL default ''  KEY,
+        {$prd->text_column_name}   longtext      NOT NULL,
+    ";
+        // KEY {$prd->time_column_name} ( {$prd->time_column_name} ),
+        // KEY {$prd->user_column_name} ( {$prd->user_column_name} ),
+}
+
 $sql = "
-    CREATE TABLE project_pages (
+    CREATE TABLE project_pages_test (
         projectid     varchar( 25 ) NOT NULL default '',
         fileid        varchar( 20 ) NOT NULL default '',
         image         varchar( 8 )  NOT NULL default '',
         master_text   longtext      NOT NULL,
-        round1_text   longtext      NOT NULL,
-        round2_text   longtext      NOT NULL,
-        round1_user   varchar( 25 ) NOT NULL default '',
-        round2_user   varchar( 25 ) NOT NULL default '',
-        round1_time   int( 20 )     NOT NULL default '0',
-        round2_time   int( 20 )     NOT NULL default '0',
+        $items_for_rounds
         state         varchar( 50 ) NOT NULL default '',
         b_user        varchar( 25 ) NOT NULL default '',
         b_code        int( 1 )      NOT NULL default '0',
         metadata      set( 'frontmatter', 'backmatter', 'division', 'verse', 'poetry', 'letter', 'toc', 'footnote', 'sidenote', 'epigraph', 'table', 'list', 'math', 'drawing', 'badscan', 'blank', 'illustration', 'missing', 'drawing', 'math' ) NOT NULL default '',
         orig_page_num varchar( 6 ) NOT NULL default '',
 
-        PRIMARY KEY ( projectid, fileid ),
-        KEY round1_user ( round1_user ),
-        KEY round2_user ( round2_user ),
-        KEY round1_time ( round1_time ),
-        KEY round2_time ( round2_time ) 
+        PRIMARY KEY ( projectid, fileid )
     )
     TYPE = MYISAM
 ";
