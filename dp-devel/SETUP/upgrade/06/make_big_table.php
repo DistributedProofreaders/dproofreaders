53af08ce7d9ea78172f9a7f2eabb9b25b8385590
jmdyck <>
2004-11-07 02:48:14 +0000

    Indentation was mix of tabs and spaces. Make it all spaces.

--- dp-devel/SETUP/upgrade/06/make_big_table.php
+++ dp-devel/SETUP/upgrade/06/make_big_table.php
@@ -14,32 +14,33 @@ $numBad = 0;
 $numPartial = 0;
 
 while ($row = mysql_fetch_assoc($result)) {
-	$result1 = mysql_query("SELECT * FROM ".$row['projectid']."");
-        $numPages = mysql_num_rows($result1);
-	if ($result1 == FALSE || $numPages == 0) {
-		echo $row['projectid']." -- Not moved due to either missing table or 0 rows<br>";
-		$numBad++;
-	} else {
-        // DAK - Keep project id for now.
-		// $projectid = substr($row['projectid'], -13);
-		$projectid = $row['projectid'];
+    $result1 = mysql_query("SELECT * FROM ".$row['projectid']."");
+    $numPages = mysql_num_rows($result1);
+    if ($result1 == FALSE || $numPages == 0) {
+        echo $row['projectid']." -- Not moved due to either missing table or 0 rows<br>";
+        $numBad++;
+    } else {
+    // DAK - Keep project id for now.
+        // $projectid = substr($row['projectid'], -13);
+        $projectid = $row['projectid'];
 
-	        $result2 = mysql_query( 
-        	       "INSERT project_pages 
-	                SELECT '".$projectid."', fileid , image , master_text ,
-        	        round1_text , round2_text , round1_user , round2_user , 
-	                round1_time , round2_time , state , b_user , b_code , 
-        	        NULL, NULL 
-                	FROM ".$projectid ) ;
+        $result2 = mysql_query( 
+            "INSERT project_pages 
+            SELECT '".$projectid."', fileid , image , master_text ,
+            round1_text , round2_text , round1_user , round2_user , 
+            round1_time , round2_time , state , b_user , b_code , 
+            NULL, NULL 
+            FROM ".$projectid ) ;
 
-		echo $row['projectid']." -- Moved to project_pages table (".$numPages." pages copied)<br>";
-		$numOK++;
+        echo $row['projectid']." -- Moved to project_pages table (".$numPages." pages copied)<br>";
+        $numOK++;
 
-	}
+    }
 }
 
 echo "<br><h1><center><b>Finished!</b></center></h1><br>";
 echo "<br><br><br><h1><center><b>Out of ".$numProjs." projects:<br>";
 echo $numOK." OK, and ".$numBad." bad.</b></center></h1><br>";
 
+// vim: sw=4 ts=4 expandtab
 ?>
