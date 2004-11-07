835dd2cdd32d339df1bb3804dd496829ccd02a85
jmdyck <>
2004-11-07 02:33:15 +0000

    Whoops, committed the wrong version last time.

--- dp-devel/SETUP/upgrade/06/create_project_pages_table.php
+++ dp-devel/SETUP/upgrade/06/create_project_pages_table.php
@@ -12,9 +12,9 @@ for ($rn = 1; $rn <= MAX_NUM_PAGE_EDITING_ROUNDS; $rn++ )
         {$prd->time_column_name}   int( 20 )     NOT NULL default '0' KEY,
         {$prd->user_column_name}   varchar( 25 ) NOT NULL default ''  KEY,
         {$prd->text_column_name}   longtext      NOT NULL,
+        KEY {$prd->time_column_name} ( {$prd->time_column_name} ),
+        KEY {$prd->user_column_name} ( {$prd->user_column_name} ),
     ";
-        // KEY {$prd->time_column_name} ( {$prd->time_column_name} ),
-        // KEY {$prd->user_column_name} ( {$prd->user_column_name} ),
 }
 
 $sql = "
