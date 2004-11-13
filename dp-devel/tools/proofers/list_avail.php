a1c97ec5110d7064dd7e3a124926fdb3c8b3feca
jmdyck <>
2004-11-13 09:01:44 +0000

    We haven't needed the "pagescompleted" query here in ages.

--- dp-devel/tools/proofers/list_avail.php
+++ dp-devel/tools/proofers/list_avail.php
@@ -9,11 +9,6 @@ echo "<br>";
 // $userP['prefschanged'] will be set ==1 if they have changed prefs while in proofing mode
 // should offer link to save changes and|or restore defaults
 
-//get total pages completed
-    $pagessql = "SELECT pagescompleted FROM users WHERE username = '$pguser' LIMIT 1";
-    $pages = mysql_query($pagessql);
-    $totalpages = mysql_result($pages, 0, "pagescompleted");
-
 // store old win pref
     $winPref=$userP['i_newwin'];
     $userP['i_newwin']=0;
