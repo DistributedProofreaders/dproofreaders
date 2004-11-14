d58c10eafa98a2b0d4687972056b7176f1a35f6a
jmdyck <>
2004-11-14 07:15:24 +0000

    Convert expressions in queue_defns.release_criterion column

---
+++ dp-devel/SETUP/upgrade/06/update_queue_defns.php
@@ -0,0 +1,46 @@
+<?PHP
+$relPath = './pinc/';
+include_once($relPath.'connect.inc');
+new dbConnect();
+
+// Convert expressions in queue_defns.release_criterion column:
+// get rid of 'projects2' and 'pages2',
+// replace 'projects1' and 'pages1' with just 'projects' and 'pages'.
+
+echo "<pre>\n";
+
+$res = mysql_query("
+	SELECT name, release_criterion
+	FROM queue_defns
+") or die(mysql_error());
+
+while( list($name,$release_criterion) = mysql_fetch_row($res) )
+{
+	if ( !preg_match( '/(pages|projects)[12]/', $release_criterion ) )
+	{
+		continue;
+	}
+
+	// echo "$name\n";
+	$rc = $release_criterion;
+	$rc = preg_replace( '/\s+/', ' ', $rc );
+	$rc = preg_replace( '/(pages|projects)2/', 'ZERO', $rc );
+	$rc = preg_replace( '/\s*\+\s*ZERO/', '', $rc );
+	$rc = preg_replace( '/ZERO\s*\+\s*/', '', $rc );
+	$rc = preg_replace( '/ZERO\s*<\s*\d+/', 'TRUE', $rc );
+	$rc = preg_replace( '/\(TRUE or TRUE\)/', 'TRUE', $rc );
+	$rc = preg_replace( '/\s*(and|&&)\s*TRUE/', '', $rc );
+	$rc = preg_replace( '/(pages|projects)1/', '\1', $rc );
+
+	$name = addslashes($name);
+	// echo "    $rc               $release_criterion\n";
+	mysql_query("
+		UPDATE queue_defns
+		SET release_criterion='$rc'
+		WHERE name='$name'
+	") or die(mysql_error());
+}
+
+echo "done.\n";
+echo "</pre>\n";
+?>
