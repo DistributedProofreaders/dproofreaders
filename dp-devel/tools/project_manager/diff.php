b3270fc722a690739526d77ab58aa217b1ba17e7
jmdyck <>
2004-11-15 02:19:36 +0000

    Rename diff.php's 'round' parameter as 'round_num'.

--- dp-devel/tools/project_manager/diff.php
+++ dp-devel/tools/project_manager/diff.php
@@ -18,9 +18,9 @@ echo "</style>";
 $projectid = $_GET['project'];
 
 $fileid=$_GET['file'];
-$round=$_GET['round'];
+$round_num=$_GET['round_num'];
 
-$prd = get_PRD_for_round($round);
+$prd = get_PRD_for_round($round_num);
 
 $res = mysql_query("SELECT $prd->prevtext_column_name, $prd->text_column_name, image FROM $projectid WHERE fileid='$fileid'");
 
