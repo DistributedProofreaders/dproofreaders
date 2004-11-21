8dbaa8454d22f9bc4fc3a24da9cf997dc882146f
charlz <>
2004-11-21 15:56:57 +0000

    script to move archived projectID tables to dp_archive db

---
+++ dp-devel/crontab/move2archive.php
@@ -0,0 +1,40 @@
+<?
+//Moves archived projects out of the active db into dp_archive
+
+$relPath="./../pinc/";
+include($relPath.'dp_main.inc');
+include($relPath.'bookpages.inc');
+include($relPath.'showavailablebooks.inc');
+include($relPath.'project_states.inc');
+include($relPath.'page_states.inc');
+
+
+    //get projects that have archived flag set
+
+    $result = mysql_query("SELECT projectid
+                     FROM projects
+                     WHERE archived = '1'");
+
+    $numrows = mysql_numrows($result);
+    $rownum = 0;
+echo "number of rows is $numrows<br>";
+  
+while ($rownum < $numrows) {
+
+$projectid = mysql_result($result, $rownum, "projectid");
+
+echo "moving project number $rownum which is $projectid<br>";
+
+$sql = "CREATE TABLE dp_archive.$projectid SELECT * FROM $projectid";
+echo mysql_errno().": ".mysql_error()."<BR>";
+            $moveresult = mysql_query($sql);
+
+
+$sql = "DROP TABLE IF EXISTS $projectid";
+$dropresult = mysql_query($sql);
+
+$rownum++;
+}
+
+
+?>
