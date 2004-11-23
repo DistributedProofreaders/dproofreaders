86bf943afb575c0a1558a2b67a4549c1c1952844
jmdyck <>
2004-11-23 03:59:37 +0000

    Fix indentation.

--- dp-devel/crontab/move2archive.php
+++ dp-devel/crontab/move2archive.php
@@ -9,31 +9,31 @@ include($relPath.'project_states.inc');
 include($relPath.'page_states.inc');
 
 
-    //get projects that have archived flag set
+//get projects that have archived flag set
 
-    $result = mysql_query("SELECT projectid
+$result = mysql_query("SELECT projectid
                      FROM projects
                      WHERE archived = '1'");
 
-    $numrows = mysql_numrows($result);
-    $rownum = 0;
+$numrows = mysql_numrows($result);
+$rownum = 0;
 echo "number of rows is $numrows<br>";
   
 while ($rownum < $numrows) {
 
-$projectid = mysql_result($result, $rownum, "projectid");
+    $projectid = mysql_result($result, $rownum, "projectid");
 
-echo "moving project number $rownum which is $projectid<br>";
+    echo "moving project number $rownum which is $projectid<br>";
 
-$sql = "CREATE TABLE dp_archive.$projectid SELECT * FROM $projectid";
-echo mysql_errno().": ".mysql_error()."<BR>";
-            $moveresult = mysql_query($sql);
+    $sql = "CREATE TABLE dp_archive.$projectid SELECT * FROM $projectid";
+    echo mysql_errno().": ".mysql_error()."<BR>";
+    $moveresult = mysql_query($sql);
 
 
-$sql = "DROP TABLE IF EXISTS $projectid";
-$dropresult = mysql_query($sql);
+    $sql = "DROP TABLE IF EXISTS $projectid";
+    $dropresult = mysql_query($sql);
 
-$rownum++;
+    $rownum++;
 }
 
 
