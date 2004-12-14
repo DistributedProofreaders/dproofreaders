55e159522807892f829e81ba6659c631dba1fdb5
jmdyck <>
2004-12-14 08:36:17 +0000

    As part of processing, move project's directory out of $projects_dir.

--- dp-devel/crontab/archive_projects.php
+++ dp-devel/crontab/archive_projects.php
@@ -12,6 +12,8 @@ header('Content-type: text/plain');
 // Find projects that were posted to PG a while ago
 // (that haven't been archived yet), and:
 // -- move the project's page-table to the archive database,
+// -- move the project's directory out of $projects_dir
+//    (for later off-site migration),
 // -- mark the project as having been archived.
 
 $dry_run = array_get( $_GET, 'dry_run', '' );
@@ -50,6 +52,26 @@ while ( list($projectid, $mod_time, $nameofwork) = mysql_fetch_row($result) )
             ALTER TABLE $projectid
             RENAME AS dp_archive.$projectid
         ") or die(mysql_error());
+    }
+
+    $project_dir = "$projects_dir/$projectid";
+    if (file_exists($project_dir))
+    {
+        $new_dir = "/data/htdocs/out/$projectid";
+        if ($dry_run)
+        {
+            echo "    Move $project_dir to $new_dir.\n";
+        }
+        else
+        {
+            // Remove uncompressed versions of whole-project texts, leaving zips.
+            exec( "rm $project_dir/projectID*.txt" );
+            rename( $project_dir, $new_dir ) or die( "Unable to move $project_dir to $new_dir" );
+        }
+    }
+    else
+    {
+        echo "    Warning: $project_dir does not exist.\n";
     }
 
     if ($dry_run)
