e242332d8164231f4fa5ba70513ab32c4da9ae29
big_bill_boy <>
2005-02-11 03:55:13 +0000

    simple calculation of appropriate tickinterval (spacing between labels), so we never have more than about 30 (task #604)

--- dp-devel/stats/jpgraph_files/day_users_logging_on.php
+++ dp-devel/stats/jpgraph_files/day_users_logging_on.php
@@ -33,8 +33,19 @@ $graph->SetScale("textint");
 $graph->xaxis->SetTickLabels($datax);
 $graph->xaxis->SetLabelAngle(90);
 $graph->xaxis->title->Set("");
-// Only draw one label a week ( = 168 hours)
-$graph->xaxis->SetTextTickInterval(168);
+// calculate tick interval based on number of datapoints
+// the data is hourly, there are 168 hours in a week
+// once we have more than about 30 labels, the axis is getting too crowded
+if ($mynumrows < (30 * 168)) {
+        $tick = 168;            // one label per week
+} else if ($mynumrows < (30 * 168 * 4)) {
+        $tick = 168 * 4;        // one label per 4 weeks (pseudo-month)
+} else if ($mynumrows < (30 * 168 * 13)) {
+        $tick = 168 * 13;       // one label per quarter
+} else {
+        $tick = 168 * 52;       // one label per year
+}
+$graph->xaxis->SetTextTickInterval($tick);
 
 //Set Y axis
 $graph->yaxis->title->Set('Fresh Logons');
