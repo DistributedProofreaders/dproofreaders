2123d77b8f121088dd32426b8e94d6dece5535f4
big_bill_boy <>
2005-02-11 04:28:47 +0000

    simple calculation of appropriate texttickinterval (spacing between labels), so we never have more than about 30 (task #604) labels once a day/week/4weeks/quarter/year, depending on number of datapoints

--- dp-devel/stats/jpgraph_files/total_proj_posted_graph.php
+++ dp-devel/stats/jpgraph_files/total_proj_posted_graph.php
@@ -62,7 +62,21 @@ $graph->Add($bplot); //Add the bar plot to the graph
 $graph->xaxis->SetTickLabels($datax);
 $graph->xaxis->SetLabelAngle(90);
 $graph->xaxis->title->Set("");
-$graph->xaxis->SetTextTickInterval(28);
+// calculate tick interval based on number of datapoints
+// the data is daily, there are 7 days in a week
+// once we have more than about 30 labels, the axis is getting too crowded
+if ($mynumrows < 30 ) {
+        $tick = 1;            // one label per day
+} else if ($mynumrows < (30 * 7)) {
+        $tick = 7;            // one label per week
+} else if ($mynumrows < (30 * 7 * 4)) {
+        $tick = 7 * 4;        // one label per 4 weeks (pseudo-month)
+} else if ($mynumrows < (30 * 7 * 13)) {
+        $tick = 7 * 13;       // one label per quarter
+} else {
+        $tick = 7  * 52;       // one label per year
+}
+$graph->xaxis->SetTextTickInterval($tick);
 
 //Set Y axis
 $graph->yaxis->title->Set(_("Projects"));
