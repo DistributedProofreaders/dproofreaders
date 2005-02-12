ee1cfaa0cf17e43eabcc315551e5bdcc17e0b14a
jmdyck <>
2005-02-12 07:03:49 +0000

    One-time script to create & populate 'current_tallies' table

---
+++ dp-devel/SETUP/upgrade/06/create_current_tallies_table.php
@@ -0,0 +1,70 @@
+<?PHP
+
+// One-time script to create & populate 'current_tallies' table
+
+$relPath='../../../pinc/';
+include_once($relPath.'connect.inc');
+include_once($relPath.'f_dpsql.inc');
+new dbConnect();
+
+// -----------------------------------------------
+// Create 'current_tallies' table.
+
+dpsql_query("
+    CREATE TABLE current_tallies (
+        holder_type  CHAR(1)          NOT NULL,
+        holder_id    INT(6)  UNSIGNED NOT NULL,
+        tally_name   CHAR(2)          NOT NULL,
+        tally_value  INT(8)           NOT NULL,
+
+        PRIMARY KEY (holder_type, holder_id, tally_name),
+        INDEX rank_arena (holder_type, tally_name),
+    )
+") or die("Aborting.");
+
+// -----------------------------------------------
+// Move user page-tallies from 'users' table.
+
+dpsql_query("
+    INSERT INTO current_tallies
+    SELECT 'U', u_id, 'P', pagescompleted
+    FROM users
+    ORDER BY u_id
+") or die("Aborting.");
+
+// For ease of backing out during testing,
+// merely rename 'pagescompleted' column, rather than dropping it.
+dpsql_query("
+    ALTER TABLE users
+    CHANGE pagescompleted pagescompleted_hidden MEDIUMINT(8) DEFAULT '0'
+") or die("Aborting.");
+
+// ALTER TABLE users
+// DROP COLUMN pagescompleted
+
+// -----------------------------------------------
+// Move team page-tallies from 'user_teams' table.
+
+dpsql_query("
+    INSERT INTO current_tallies
+    SELECT 'T', id, 'P', page_count
+    FROM user_teams
+    ORDER BY id
+") or die("Aborting.");
+
+// For ease of backing out during testing,
+// merely rename 'page_count' column, rather than dropping it.
+dpsql_query("
+    ALTER TABLE user_teams
+    CHANGE page_count page_count_hidden INT(20) NOT NULL DEFAULT '0';
+") or die("Aborting.");
+
+// ALTER TABLE user_teams
+// DROP COLUMN page_count
+
+// -----------------------------------------------
+
+echo "Done!";
+
+// vim: sw=4 ts=4 expandtab
+?>
