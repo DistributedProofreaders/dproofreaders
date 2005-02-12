72b1519f8a179859adee8e25275346f042742e23
big_bill_boy <>
2005-02-12 05:19:40 +0000

    Displays useful information for PMs on the available special days (in support of task #591)

---
+++ dp-devel/tools/project_manager/show_specials.php
@@ -0,0 +1,30 @@
+<?
+$relPath='../../pinc/';
+include_once($relPath.'dp_main.inc');
+include_once($relPath.'f_dpsql.inc');
+include_once($relPath.'theme.inc');
+$no_stats = 1;
+
+$title = _("Details of Special Days/Weeks/Months");
+theme($title, "header");
+
+echo "<br><h2>$title</h2>\n";
+
+
+dpsql_dump_query("
+        SELECT
+                concat('<span style=\"background-color: #',color,'\">',display_name,'</span>') as 'Name',
+                comment as 'Comment',
+                DATE_FORMAT(concat('2000-',open_month,'-',open_day),'%e %b') as 'Start Date',
+                concat('<a href=\"',url,'\">',url,'</a>') as 'More Info'
+        FROM special_days
+        WHERE enable = 1
+        ORDER BY open_month, open_day
+
+");
+
+
+echo "<br>\n";
+
+theme("","footer");
+?>
