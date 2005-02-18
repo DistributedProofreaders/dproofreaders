2715f013fd1550b6a089209aa1c73fc74c6e8165
big_bill_boy <>
2005-02-18 06:28:38 +0000

    fix for nit mentioned in reopened task #380 (case sensitive check of PPer name)

--- dp-devel/tools/project_manager/editproject.php
+++ dp-devel/tools/project_manager/editproject.php
@@ -32,7 +32,7 @@ function saveProject() {
    if (empty($_POST['genre'])) { $errormsg .= "Genre is required.<br>"; }
    if (!empty($_POST['checkedoutby'])) {
         $checkedoutby = $_POST['checkedoutby'];
-        $result = mysql_query("SELECT u_id FROM users WHERE username = '$checkedoutby'");
+        $result = mysql_query("SELECT u_id FROM users WHERE STRCMP(username, $checkedoutby) = 0");
         if (mysql_num_rows($result) == 0) {
              $errormsg .= "PPer/PPVer must be an existing user.<br>"; 
         }
