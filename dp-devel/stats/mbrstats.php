49a2d5ee6acf4563b0f344bd425f696b5b5e73fe
jmdyck <>
2005-02-25 00:19:07 +0000

    Extract code from     stats/mbrstats.php     stats/teamstats.php to define     TallyBoard::take_snapshot()

--- dp-devel/stats/mbrstats.php
+++ dp-devel/stats/mbrstats.php
@@ -30,57 +30,19 @@ function maybe_query( $query )
 $today = getdate();
 $midnight = mktime(0,0,0,$today['mon'],$today['mday'],$today['year']);
 
-//Find out if the script has been run once already for today
-$result = mysql_query("
-	SELECT MAX(timestamp)
-	FROM past_tallies
-	WHERE holder_type='U' AND tally_name='P'
-");
-$max_update = mysql_result($result,0,0);
-	if ($max_update == $midnight && !$testing) {
-		echo "<center>This script has already been run today!</center>\n";
-		maybe_query("INSERT INTO job_logs (filename, tracetime, event, comments) VALUES ('mbrstats.php', ".time().", 'FAIL', 'Already been run today!')");
-	} else {
-		$tracetime=time();
-		maybe_query("INSERT INTO job_logs (filename, tracetime, event, comments) VALUES ('mbrstats.php', $tracetime, 'BEGIN', 'Started generating member statistics for $midnight')");
-		//Update the page count rank for the previous day
-		$rankArray = users_get_page_tally_ranks();
+$tracetime=time();
+maybe_query("INSERT INTO job_logs (filename, tracetime, event, comments) VALUES ('mbrstats.php', $tracetime, 'BEGIN', 'Started generating member statistics for $midnight')");
 
-		//Update past_tallies with previous days page count
-		$result = mysql_query("
-			SELECT holder_id, tally_value
-			FROM past_tallies
-			WHERE
-				timestamp = $max_update
-				AND holder_type='U'
-				AND tally_name='P'
-		");
-		while (list($u_id, $prev_tally_value) = mysql_fetch_row($result)) {
-			$prevDayCount[$u_id] = $prev_tally_value;
-		}
-
-		$result = mysql_query("
-			SELECT holder_id, tally_value
-			FROM current_tallies
-			WHERE holder_type='U' AND tally_name='P'
-		");
-		while(list($u_id, $current_P_tally) = mysql_fetch_row($result)) {
-			$rank = $rankArray[$u_id];
-			$todaysCount = $current_P_tally - $prevDayCount[$u_id];
-			$updateCount = maybe_query("
-				INSERT INTO past_tallies
-				SET
-					timestamp   = $midnight,
-					holder_type = 'U',
-					holder_id   = $u_id,
-					tally_name  = 'P',
-					tally_delta = $todaysCount,
-					tally_value = $current_P_tally,
-					tally_rank  = $rank
-			");
-		}
-		$tracetimea = time();
-		$tooktime = $tracetimea - $tracetime;
-		maybe_query("INSERT INTO job_logs (filename, tracetime, event, comments) VALUES ('mbrstats.php', $tracetimea, 'END', 'Started at $tracetime, took $tooktime seconds total')");
-	}
+$err = $users_P_page_tallyboard->take_snapshot( $midnight, $testing );
+if ( $err )
+{
+	echo "<center>This script has already been run today!</center>\n";
+	maybe_query("INSERT INTO job_logs (filename, tracetime, event, comments) VALUES ('mbrstats.php', ".time().", 'FAIL', 'Already been run today!')");
+}
+else
+{
+	$tracetimea = time();
+	$tooktime = $tracetimea - $tracetime;
+	maybe_query("INSERT INTO job_logs (filename, tracetime, event, comments) VALUES ('mbrstats.php', $tracetimea, 'END', 'Started at $tracetime, took $tooktime seconds total')");
+}
 ?>
--- dp-devel/stats/teamstats.php
+++ dp-devel/stats/teamstats.php
@@ -30,52 +30,10 @@ function maybe_query( $query )
 $today = getdate();
 $midnight = mktime(0,0,0,$today['mon'],$today['mday'],$today['year']);
 
-//Find out if the script has been run once already for today
-$result = mysql_query("
-	SELECT MAX(timestamp)
-	FROM past_tallies
-	WHERE holder_type='T' AND tally_name='P'
-");
-$max_update = mysql_result($result,0,0);
-	if ($max_update == $midnight && !$testing) {
-		echo "<center>This script has already been run today!</center>";
-	} else {
-		//Update past_tallies with previous days page count; 
-		$rankArray = teams_get_page_tally_ranks();
+$err = $teams_P_page_tallyboard->take_snapshot( $midnight, $testing );
+if ( $err )
+{
+	echo "<center>This script has already been run today!</center>";
+}
 
-		$result = mysql_query("
-			SELECT holder_id, tally_value
-			FROM past_tallies
-			WHERE
-				timestamp = $max_update
-				AND holder_type='T'
-				AND tally_name='P'
-		");
-		while (list($team_id, $prev_tally_value) = mysql_fetch_row($result)) {
-			$prevDayCount[$team_id] = $prev_tally_value;
-		}
-
-		$result = mysql_query("
-			SELECT holder_id, tally_value
-			FROM current_tallies
-			WHERE holder_type='T' AND tally_name='P'
-		");
-		while(list($team_id, $current_P_tally) = mysql_fetch_row($result)) {
-			if ($team_id != 1) {
-				$rank = $rankArray[$team_id];
-				$todaysCount = $current_P_tally - $prevDayCount[$team_id];
-				$updateCount = maybe_query("
-					INSERT INTO past_tallies
-					SET
-						timestamp   = $midnight,
-						holder_type = 'T',
-						holder_id   = $team_id,
-						tally_name  = 'P',
-						tally_delta = $todaysCount,
-						tally_value = $current_P_tally,
-						tally_rank  = $rank
-				");
-			}
-		}
-	}
 ?>
