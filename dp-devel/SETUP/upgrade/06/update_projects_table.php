56881089de7d154685a0c82e6b65d1ef711d3891
big_bill_boy <>
2005-02-26 13:42:05 +0000

    database changes to support tracking institutional image providers: new image_provider column on projects table (Task #185)

--- dp-devel/SETUP/upgrade/06/update_projects_table.php
+++ dp-devel/SETUP/upgrade/06/update_projects_table.php
@@ -7,7 +7,8 @@ $db_Connection=new dbConnect();
 $result = mysql_query("ALTER TABLE `projects` ADD `final_page_count` SMALLINT( 4 ) UNSIGNED DEFAULT '0' NOT NULL") or die(mysql_error());
 $result = mysql_query("ALTER TABLE `projects` ADD `ppverifier` VARCHAR( 25 )") or die(mysql_error());
 $result = mysql_query("ALTER TABLE `projects` ADD `special` VARCHAR( 20 ) AFTER `projectid`") or die(mysql_error());
+$result = mysql_query("ALTER TABLE `projects` ADD `image_provider` VARCHAR( 10 )") or die(mysql_error());
 $result = mysql_query("ALTER TABLE `projects` ADD INDEX ( `special` )") or die(mysql_error());
 
-echo "<center><p>Addition of `final_page_count`, `special` and `ppverifier` fields to `projects` table complete!";
+echo "<center><p>Addition of `final_page_count`, `special`, `ppverifier` and `image_provider` fields to `projects` table complete!";
 ?>
