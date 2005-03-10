40d3274a05b26656a7737725e3dcda195a1fe7ae
jmdyck <>
2005-03-10 07:41:39 +0000

    tdetail.php takes optional 'tally_name' parameter, passes it to showTeamInformation()

--- dp-devel/stats/teams/tdetail.php
+++ dp-devel/stats/teams/tdetail.php
@@ -1,11 +1,14 @@
 <?
 $relPath="./../../pinc/";
+include_once($relPath.'misc.inc');
 include_once($relPath.'v_site.inc');
 include_once($relPath.'http_headers.inc');
 include_once($relPath.'connect.inc');
 include_once($relPath.'theme.inc');
 include_once('../includes/team.php');
 $db_Connection=new dbConnect();
+
+$tally_name = array_get( $_GET, 'tally_name', null );
 
 $result = select_from_teams("id = {$_GET['tid']}");
 $curTeam = mysql_fetch_assoc($result);
@@ -15,7 +18,7 @@ $stats = _("Statistics");
 theme($curTeam['teamname']." ".$stats, "header");
 echo "<br><center>";
 
-showTeamInformation($curTeam);
+showTeamInformation($curTeam, $tally_name);
 
 echo "</center>";
 theme("", "footer");
