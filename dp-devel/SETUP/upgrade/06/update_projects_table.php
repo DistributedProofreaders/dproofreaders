f6e0b83f3933a2285cf0ab27c7b6dbb9c7ff6058
big_bill_boy <>
2005-03-13 15:35:21 +0000

    initial version of support for uberprojects (task #612):
    added up_projectid column to track which UP, if any,
    a project is associated with

--- dp-devel/SETUP/upgrade/06/update_projects_table.php
+++ dp-devel/SETUP/upgrade/06/update_projects_table.php
@@ -9,7 +9,8 @@ $result = mysql_query("ALTER TABLE `projects` ADD `ppverifier` VARCHAR( 25 )") o
 $result = mysql_query("ALTER TABLE `projects` ADD `special` VARCHAR( 20 ) AFTER `projectid`") or die(mysql_error());
 $result = mysql_query("ALTER TABLE `projects` ADD `image_provider` VARCHAR( 10 )") or die(mysql_error());
 $result = mysql_query("ALTER TABLE `projects` ADD `smoothread_deadline` INT( 20 ) DEFAULT '0' NOT NULL") or die(mysql_error());
+$result = mysql_query("ALTER TABLE `projects` ADD `up_projectid` INT( 10 ) DEFAULT '0'") or die(mysql_error());
 $result = mysql_query("ALTER TABLE `projects` ADD INDEX ( `special` )") or die(mysql_error());
 
-echo "<center><p>Addition of `final_page_count`, `special`, `ppverifier`, `image_provider` and `smoothread_deadline` fields to `projects` table complete!";
+echo "<center><p>Addition of `final_page_count`, `special`, `ppverifier`, `image_provider`, `smoothread_deadline` and `up_projectid` fields to `projects` table complete!";
 ?>
