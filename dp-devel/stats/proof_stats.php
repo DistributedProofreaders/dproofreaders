db135b2eca84eec27ae382969966bd8d4132eefd
jmdyck <>
2005-03-27 23:50:49 +0000

    Generate a 'Top 100' table for each different page tally.

--- dp-devel/stats/proof_stats.php
+++ dp-devel/stats/proof_stats.php
@@ -35,8 +35,6 @@ dpsql_dump_themed_query("
 echo "<br>\n";
 echo "<br>\n";
 
-echo "<h3>" . _("Most Prolific Proofreaders") . "</h3>\n";
-
 if (isset($GLOBALS['pguser'])) 
 // if user logged on
 {
@@ -50,19 +48,29 @@ else
 	// hide names of users who don't want unlogged on people to see their names
 	$proofreader_expr = "IF(u_privacy != ".PRIVACY_PUBLIC.",'Anonymous', username)";
 }
-dpsql_dump_themed_ranked_query("
-	SELECT
-		$proofreader_expr AS 'Proofreader',
-		$user_P_page_tally_column AS 'Pages Proofread'
-	FROM users $joined_with_user_P_page_tallies
-	WHERE $user_P_page_tally_column > 0
-	ORDER BY 2 DESC, 1 ASC
-	LIMIT 100
-");
 
-echo "<br>\n";
+foreach ( $page_tally_names as $tally_name => $gerund )
+{
+	echo "<h3>" . _("Most Prolific Proofreaders") . ': ' . $tally_name . "</h3>\n";
 
-echo "<br>\n";
+	$users_tallyboard = new TallyBoard( $tally_name, 'U' );
+
+	list($joined_with_user_page_tallies,$user_page_tally_column) =
+		$users_tallyboard->get_sql_joinery_for_current_tallies('users.u_id');
+
+	dpsql_dump_themed_ranked_query("
+		SELECT
+			$proofreader_expr AS 'Proofreader',
+			$user_page_tally_column AS '$tally_name Pages Completed'
+		FROM users $joined_with_user_page_tallies
+		WHERE $user_page_tally_column > 0
+		ORDER BY 2 DESC, 1 ASC
+		LIMIT 100
+	");
+
+	echo "<br>\n";
+	echo "<br>\n";
+}
 
 theme("","footer");
 ?>
