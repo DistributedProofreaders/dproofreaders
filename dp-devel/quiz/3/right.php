1b4377e8bb322826c444c48ef390f0a0036840b0
richyfourtytwo <>
2005-03-30 16:20:49 +0000

    replaced by generic structure

--- dp-devel/quiz/3/right.php
+++
@@ -1,8 +0,0 @@
-<?
-$relPath='../../../pinc/';
-include_once('../small_theme.inc');
-?> 
-<h2>Quiz, part 3</h2>
-Try to correct the text on the bottom left, so it matches the text in the image above following the Proofreading Guidelines. When done click 'check'.
-</body>
-</html>
