f66387fe722316d6c17de6a25d485f6baef9fb41
jmdyck <>
2005-04-01 00:18:47 +0000

    Change 'include' to 'include_once'.

--- dp-devel/tools/proofers/processtext.php
+++ dp-devel/tools/proofers/processtext.php
@@ -1,7 +1,7 @@
 <?
 $relPath="./../../pinc/";
 include_once($relPath.'v_site.inc');
-include($relPath.'dp_main.inc');
+include_once($relPath.'dp_main.inc');
 include_once($relPath.'c_pages.inc');
 include_once($relPath.'project_continuity.inc');
 
