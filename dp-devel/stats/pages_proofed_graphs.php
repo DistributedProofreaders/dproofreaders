2b1088c316830196ff8628662a3d8d5aaf88686d
jmdyck <>
2005-04-06 00:50:38 +0000

    Take a 'tally_name' parameter and pass it on to pages_daily.php

--- dp-devel/stats/pages_proofed_graphs.php
+++ dp-devel/stats/pages_proofed_graphs.php
@@ -6,17 +6,25 @@ include($relPath.'connect.inc');
 include($relPath.'theme.inc');
 new dbConnect();
 
-theme('Pages Proofed Graphs','header');
-echo "<center><h1><i>Pages Proofed Graphs</i></h1></center>";
+$tally_name = @$_GET['tally_name'];
+if (empty($tally_name))
+{
+    die("parameter 'tally_name' is undefined/empty");
+}
+
+$title = sprintf( _('Graphs for Pages Saved-as-Done in Round %s'), $tally_name );
+
+theme($title,'header');
+echo "<center><h1><i>$title</i></h1></center>";
 
 echo "<br><br>";
 
-echo "<center><img src=\"jpgraph_files/pages_daily.php?cori=increments&timeframe=curr_month\"></center><br>";
-echo "<center><img src=\"jpgraph_files/pages_daily.php?cori=cumulative&timeframe=curr_month\"></center><br>";
-echo "<center><img src=\"jpgraph_files/pages_daily.php?cori=increments&timeframe=prev_month\"></center><br>";
-echo "<center><img src=\"jpgraph_files/pages_daily.php?cori=increments&timeframe=all_time\"></center><br>";
-echo "<center><img src=\"jpgraph_files/pages_daily.php?cori=cumulative&timeframe=all_time\"></center><br>";
-echo "<center><img src=\"jpgraph_files/total_pages_by_month_graph.php\"></center><br>";
+echo "<center><img src=\"jpgraph_files/pages_daily.php?tally_name=$tally_name&cori=increments&timeframe=curr_month\"></center><br>";
+echo "<center><img src=\"jpgraph_files/pages_daily.php?tally_name=$tally_name&cori=cumulative&timeframe=curr_month\"></center><br>";
+echo "<center><img src=\"jpgraph_files/pages_daily.php?tally_name=$tally_name&cori=increments&timeframe=prev_month\"></center><br>";
+echo "<center><img src=\"jpgraph_files/pages_daily.php?tally_name=$tally_name&cori=increments&timeframe=all_time\"></center><br>";
+echo "<center><img src=\"jpgraph_files/pages_daily.php?tally_name=$tally_name&cori=cumulative&timeframe=all_time\"></center><br>";
+echo "<center><img src=\"jpgraph_files/total_pages_by_month_graph.php?tally_name=$tally_name\"></center><br>";
 
 theme('','footer');
 ?>
