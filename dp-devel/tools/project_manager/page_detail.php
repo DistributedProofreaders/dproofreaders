f4c6a698c1a270ba3ed61988779f8bf6138e86c1
jmdyck <>
2005-04-18 02:16:54 +0000

    Rename $project as $projectid.

--- dp-devel/tools/project_manager/page_detail.php
+++ dp-devel/tools/project_manager/page_detail.php
@@ -4,8 +4,10 @@ include_once($relPath.'v_site.inc');
 include_once($relPath.'dp_main.inc');
 include_once('page_table.inc');
 
+$projectid = @$_GET['project'];
+
 $myresult = mysql_query("
-                SELECT nameofwork, state FROM projects WHERE projectid = '$project'
+                SELECT nameofwork, state FROM projects WHERE projectid = '$projectid'
         ");
 
 $row = mysql_fetch_assoc($myresult);
@@ -15,14 +17,14 @@ $title = $row['nameofwork'];
 $label = _("Return to Project Page for");
 
 echo "<br>\n";
-echo "<a href='$code_url/project.php?id=$project&amp;expected_state=$state'>$label $title</a>";
+echo "<a href='$code_url/project.php?id=$projectid&amp;expected_state=$state'>$label $title</a>";
 echo "<br>\n";
 
 include_once('detail_legend.inc');
 
 echo "N.B. It is <b>strongly</b> recommended that you view page differentials by right-clicking on a diff link and opening the link in a new window or tab."."<br>\n";
 
-echo_page_table( $project, $show_image_size);
+echo_page_table( $projectid, $show_image_size);
 
 ?>
 
