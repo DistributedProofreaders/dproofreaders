703d6ab2339e9044a727cadcdd8d92bad57a9b44
jmdyck <>
2005-04-18 21:04:49 +0000

    Unfold the only include('master_frame.inc'), making master_frame.inc unnecessary.

--- dp-devel/tools/proofers/proof.php
+++ dp-devel/tools/proofers/proof.php
@@ -12,5 +12,29 @@ $project, $proofstate
 $frameGet="?" . $_SERVER['QUERY_STRING'];
 
 //load the master frameset
-    include('master_frame.inc');
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+<?php
+include("./../../pinc/slim_header.inc");
+$res = mysql_fetch_assoc(mysql_query("
+    SELECT nameofwork FROM projects WHERE projectid = '$project';
+"));
+$nameofwork = $res['nameofwork'];
+// Add name of round before nameofwork
+// Since this is included from proof.php, we can for now safely assume $proofstate is set.
+$round = get_Round_for_project_state($proofstate);
+$rn = $round->id;
+$nameofwork = "[" . $rn . "] " . $nameofwork;
+slim_header($nameofwork." - "._("Proofreading Interface"),FALSE,FALSE);
+?>
+<script language="JavaScript" type="text/javascript" src="dp_proof.js?1.33.1"></script>
+<script language="JavaScript" type="text/javascript" src="dp_scroll.js"></script>
+</head>
+<frameset rows="*,73">
+<frame name="proofframe" src="<?PHP echo "$code_url/tools/proofers/proof_frame.php{$frameGet}";?>" marginwidth="2" marginheight="2" frameborder="0">
+<frame name="menuframe" src="ctrl_frame.php" marginwidth="2" marginheight="2" frameborder="0">
+</frameset>
+<noframes>
+<? _("Your browser currently does not display frames!"); ?>
+</noframes>
+</html>
