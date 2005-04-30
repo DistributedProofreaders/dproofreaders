6f3ec94e86d0caf08834b7ceb440e78fd7acb0de
jmdyck <>
2005-04-30 06:30:52 +0000

    It seems that some versions of MySQL (at least as late as 4.0.12, but not as late as 4.0.18) don't like:     INSERT INTO t SELECT ... FROM t ("ERROR 1093: INSERT TABLE 'site_tally_goals' isn't allowed in FROM table list") So rephrase the query.

--- dp-devel/crontab/extend_site_tally_goals.php
+++ dp-devel/crontab/extend_site_tally_goals.php
@@ -17,27 +17,42 @@ if ( is_null($current_max_date) )
     exit;
 }
 
+$res2 = dpsql_query("
+    SELECT tally_name, goal
+    FROM site_tally_goals
+    WHERE date='$current_max_date'
+") or die("Aborting");
+
+$values_list = '';
+
 // Ensure that the table is defined for at least the next 35 days.
 $desired_max_date = strftime('%Y-%m-%d',strtotime('+35 days'));
 
 // What I'd *like* to be able to write:
 // for ( $d = $current_max_date+1; $d <= today() + 35; $d++ )
 
-while(1)
+for ( $i = 1; ; $i++ )
 {
-    $date = strftime( '%Y-%m-%d', strtotime( "$current_max_date + 1 day" ) );
+    $date = strftime( '%Y-%m-%d', strtotime( "$current_max_date + $i day" ) );
     if ( $date > $desired_max_date ) break;
 
-    // Replicate the latest set of goals for one more day.
-    dpsql_query("
-        INSERT INTO site_tally_goals
-            SELECT '$date', tally_name, goal
-            FROM site_tally_goals
-            WHERE date='$current_max_date'
-    ") or die("Aborting");
+    mysql_data_seek( $res2, 0 ) or die(mysql_error());
+    while ( list($tally_name,$goal) = mysql_fetch_row($res2) )
+    {
+        if (!empty($values_list)) $values_list .= ',';
+        $values_list .= "( '$date', '$tally_name', $goal )\n";
+    }
 
-    $current_max_date = $date;
+    $values_list .= "\n";
 }
+
+if ( empty($values_list) ) return;
+
+dpsql_query("
+    INSERT INTO site_tally_goals
+    VALUES
+    $values_list
+") or die("Aborting");
 
 // vim: sw=4 ts=4 expandtab
 ?>
