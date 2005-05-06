23ec46c8963f6df6a92fd6400a4bb7909a6c926f
martinag <>
2005-05-06 19:44:02 +0000

    Script to POST to to allow the PP-er to add a comment without changing the state of the project (task #495).

---
+++ dp-devel/tools/post_proofers/postcomments.php
@@ -0,0 +1,22 @@
+<?
+$relPath="../../pinc/";
+include_once($relPath.'v_site.inc');
+include_once($relPath.'metarefresh.inc');
+include_once($relPath.'dp_main.inc');
+include_once($relPath.'Project.inc');
+include_once($relPath.'project_trans.inc');
+
+// Verify that it's the pp-er trying to perform this action.
+
+$project = new Project($projectid);
+if (! $project->PPer_is_current_user) {
+  echo _("The project is not checked out to you.");
+  exit;
+}
+
+add_postcomments($projectid, $postcomments);
+
+$msg = _("Comments added.");
+metarefresh(1, "$code_url/project.php?id=$projectid", $msg, $msg);
+
+?>
