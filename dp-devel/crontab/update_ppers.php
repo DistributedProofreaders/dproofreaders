6a36df9d8441853d204c5833e8a1607e7f115a59
jmdyck <>
2005-05-12 23:42:28 +0000

    Change     $row = mysql_fetch_assoc($result)     ... $row['postproofer'] ... to     list($postproofer) = mysql_fetch_row($result)     ... $postproofer ... for readability.

--- dp-devel/crontab/update_ppers.php
+++ dp-devel/crontab/update_ppers.php
@@ -5,12 +5,12 @@ include($relPath.'connect.inc');
 $db_Connection=new dbConnect();
 
 $result = mysql_query("SELECT DISTINCT(postproofer) FROM projects WHERE postproofer is not null");
-while ($row = mysql_fetch_assoc($result)) {
-	$isPPQuery = mysql_query("SELECT postprocessor FROM users WHERE username = '".$row['postproofer']."'");
+while (list($postproofer) = mysql_fetch_row($result)) {
+	$isPPQuery = mysql_query("SELECT postprocessor FROM users WHERE username = '$postproofer'");
 	$isPP = mysql_result($isPPQuery, 0, "postprocessor");
 	if ($isPP != "yes") {
-		$updatePPQuery = mysql_query("UPDATE users SET postprocessor = 'yes' WHERE username = '".$row['postproofer']."'");
-		echo "Added: ".$row['postproofer']."<br>";
+		$updatePPQuery = mysql_query("UPDATE users SET postprocessor = 'yes' WHERE username = '$postproofer'");
+		echo "Added: $postproofer<br>";
 	}
 }
 
