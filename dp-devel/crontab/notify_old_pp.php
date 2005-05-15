f601474d6edc63e9fb86cabfb838161b2bcbefa6
martinag <>
2005-05-15 17:10:35 +0000

    There's a new way of reporting activity with PP-projects: visiting the project home page (task #495).

--- dp-devel/crontab/notify_old_pp.php
+++ dp-devel/crontab/notify_old_pp.php
@@ -22,6 +22,7 @@ $db_Connection=new dbConnect();
     $projectslist = "";
     $displayprojectslist = "";
     $numprojs = 0;
+    $urlbase = "$code_url/project.php?expected_state=proj_post_first_checked_out&id=";
 
    while ($rownum < $numrows) {
 
@@ -44,8 +45,8 @@ $db_Connection=new dbConnect();
 			$message = "Hello $PPinQuestion,\n\nThis is an automated message.\n\n
 Our database indicates that you have had a PP project checked out for more than 90 days:\n\n
 $projectslist\n\n 
-If you haven't yet finished and wish to continue working on this book and/or need help please forward a copy of this email (quoting the information on the book, above) with a brief description of the status to dphelp@pgdp.net.\n\n
-If you have completed your work on the book, please log in to www.pgdp.net and visit the Post Processing page at $code_url/tools/pool.php?pool_id=PP and, under 'Books I have checked out for Post-Processing' select the 'Upload for verification' option and follow the prompts. You will be able to leave a message for the verifier during this process, if you have any special information or comments to pass on.\n\n
+If you haven't yet finished and wish to continue working on this book, please log in to www.pgdp.net and visit $url . This will update the status of the project. If you need help please forward a copy of this email (quoting the information on the book, above) with a brief description of the status to dphelp@pgdp.net.\n\n
+If you have completed your work on the book, please log in to www.pgdp.net and visit $url. Select the 'Upload for verification' option and follow the prompts. You will be able to leave a message for the verifier during this process, if you have any special information or comments to pass on.\n\n
 If you are waiting on missing images or page scans, please add the details to the Missing Page Wiki at: $forums_url/viewtopic.php?t=7584\n\n
 If you no longer wish to have this text assigned to you please visit the Distributed Proofreaders website Post Processing section and select Return to Available for this book, or forward this email to dphelp@pgdp.net and state that you would no longer like to have the book in question assigned to you so that we may return it to the available pool for someone else to work on.\n\n 
 Thanks!\nThe Distributed Proofreaders Team\n(http://www.pgdp.net)";
@@ -53,8 +54,8 @@ Thanks!\nThe Distributed Proofreaders Team\n(http://www.pgdp.net)";
 			$message = "Hello $PPinQuestion,\n\nThis is an automated message.\n\n
 Our database indicates that you have had $numprojs PP projects checked out for more than 90 days:\n\n 
 $projectslist\n\n 
-If you wish to continue working on some or all of these books and/or need help please forward this email, quoting the list of books, with a brief description of the status for each of the various books listed above to dphelp@pgdp.net.\n\n
-If you have completed your work on any of these books, please log in to www.pgdp.net and visit the Post Processing page at $code_url/tools/pool.php?pool_id=PP and, under 'Books I have checked out for Post-Processing' select the 'Upload for verification' option and follow the prompts. You will be able to leave a message for the verifier during this process, if you have any special information or comments to pass on.\n\n
+If you wish to continue working on some or all of these books, please log in to www.pgdp.net and visit each such project's home-page (copy the URL listed with the project above and paste it into your browser's address-field). Doing this will update the status of the project and let us know that you are still working on it. If you need help please forward this email, quoting the list of books, with a brief description of the status for each of the various books listed above that you need help with to dphelp@pgdp.net.\n\n
+If you have completed your work on any of these books, please log in to www.pgdp.net and visit each such project's home-page (copy the URL listed with the project above and paste it into your browser's address-field). Select the 'Upload for verification' option and follow the prompts. You will be able to leave a message for the verifier during this process, if you have any special information or comments to pass on.\n\n
 If you are waiting on missing images or page scans, please add the details to the Missing Page Wiki at: $forums_url/viewtopic.php?t=7584\n\n
 If you no longer wish to have some or all of these books assigned to you please visit the Distributed Proofreaders website Post Processing section and select Return to Available for the books in question or forward this email to dphelp@pgdp.net and state that you would no longer like to have the books in question assigned to you so that we may return them to the available pool for someone else to work on.\n\n 
 Thanks!\nThe Distributed Proofreaders Team\n(http://www.pgdp.net)";
@@ -70,15 +71,17 @@ Thanks!\nThe Distributed Proofreaders Team\n(http://www.pgdp.net)";
 	}
 
 	$numprojs++;
+
+	$url = $urlbase . $projectid;
 	
-	$projectslist .= "$nameofwork by $authorsname ($projectid), out since $nicedate\n\n";
+	$projectslist .= "$nameofwork by $authorsname ($projectid), out since $nicedate\n$url\n\n";
         if ($numprojs == 1) {
 		$subject = "DP: Status update needed for 1 project checked out for PPing over 90 days";
 	} else {
 		$subject = "DP: Status updates needed for $numprojs projects checked out for PPing over 90 days";
 	}	
 
-	$displayprojectslist .= "$nameofwork by $authorsname ($projectid), out since $nicedate\n". "<br>";
+	$displayprojectslist .= "<a href='$url'>$nameofwork by $authorsname ($projectid)</a>, out since $nicedate\n". "<br>";
 
         $rownum++;
 }
