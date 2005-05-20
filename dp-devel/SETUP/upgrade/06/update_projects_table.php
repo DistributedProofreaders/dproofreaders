861be998fae0f721494359f69b8bbced77f314e7
jmdyck <>
2005-05-20 00:43:33 +0000

    Combine the various adds into a single query.

--- dp-devel/SETUP/upgrade/06/update_projects_table.php
+++ dp-devel/SETUP/upgrade/06/update_projects_table.php
@@ -3,14 +3,18 @@ $relPath='./pinc/';
 include($relPath.'connect.inc');
 $db_Connection=new dbConnect();
 
-//----------------------------------------------------------------------------------------------------------------------
-$result = mysql_query("ALTER TABLE `projects` ADD `final_page_count` SMALLINT( 4 ) UNSIGNED DEFAULT '0' NOT NULL") or die(mysql_error());
-$result = mysql_query("ALTER TABLE `projects` ADD `ppverifier` VARCHAR( 25 )") or die(mysql_error());
-$result = mysql_query("ALTER TABLE `projects` ADD `special` VARCHAR( 20 ) AFTER `projectid`") or die(mysql_error());
-$result = mysql_query("ALTER TABLE `projects` ADD `image_provider` VARCHAR( 10 )") or die(mysql_error());
-$result = mysql_query("ALTER TABLE `projects` ADD `smoothread_deadline` INT( 20 ) DEFAULT '0' NOT NULL") or die(mysql_error());
-$result = mysql_query("ALTER TABLE `projects` ADD `up_projectid` INT( 10 ) DEFAULT '0'") or die(mysql_error());
-$result = mysql_query("ALTER TABLE `projects` ADD INDEX ( `special` )") or die(mysql_error());
+mysql_query("
+    ALTER TABLE projects
+        ADD special             VARCHAR(20) AFTER projectid,
+        ADD final_page_count    SMALLINT(4) UNSIGNED DEFAULT '0' NOT NULL,
+        ADD ppverifier          VARCHAR(25),
+        ADD image_provider      VARCHAR(10),
+        ADD smoothread_deadline INT(20)     DEFAULT '0' NOT NULL,
+        ADD up_projectid        INT(10)     DEFAULT '0',
+        ADD INDEX (special)
+") or die(mysql_error());
 
-echo "<center><p>Addition of `final_page_count`, `special`, `ppverifier`, `image_provider`, `smoothread_deadline` and `up_projectid` fields to `projects` table complete!";
+echo "<p>Addition of various fields to `projects` table is complete!";
+
+// vim: sw=4 ts=4 expandtab
 ?>
