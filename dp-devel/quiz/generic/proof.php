3da8652b48bc20a4eff15069b9f791a15176ae43
mike_cie <>
2005-05-20 18:59:29 +0000

    If testing, allow the user to cheat by pressing Alt-`. (If you press Ctrl, Escape and three other keys, a dancing squirrel appears!!)

--- dp-devel/quiz/generic/proof.php
+++ dp-devel/quiz/generic/proof.php
@@ -8,15 +8,23 @@ include './data/qd_' . $_REQUEST['type'] . '.inc';
 <html>
 <head>
 <meta name="generator" content="HTML Tidy, see www.w3.org">
+<script type='text/javascript'>
+s = "<?
+if ($testing)
+echo str_replace("\n",'\n',addslashes($solutions[0]));?>";
+</script>
 <title></title>
 <META http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 </head>
 <body bgcolor='#ffffff'>
 <form action="./returnfeed.php?type=<?php echo $_REQUEST['type']; ?>" target="right" method="post">
-<textarea rows="12" cols="60" name="output" wrap="off">
+<textarea rows="12" cols="60" name="output" id='output' wrap="off">
 <?php echo $ocr_text; ?>
 </textarea> <p>
-<input type="submit" value="check">
-<input type="reset" value="restart"></form>
+<input type="submit" value="Check">
+<input type="reset" value="Restart"></form>
+
+<a href='#' onclick="document.forms[0].elements['output'].value=s" accesskey='`'></a>
+
 </body>
 </html>
