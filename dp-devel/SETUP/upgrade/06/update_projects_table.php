acb9634ad249f19e16f1f8c7b46f21e2a70ccfec
jmdyck <>
2005-05-20 05:11:17 +0000

    Drop 'final_page_count' column; use 'n_pages' instead.

--- dp-devel/SETUP/upgrade/06/update_projects_table.php
+++ dp-devel/SETUP/upgrade/06/update_projects_table.php
@@ -10,7 +10,6 @@ mysql_query("
         ADD special             VARCHAR(20) AFTER projectid,
         ADD n_pages             SMALLINT(4) UNSIGNED DEFAULT '0' NOT NULL,
         ADD n_available_pages   SMALLINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-        ADD final_page_count    SMALLINT(4) UNSIGNED DEFAULT '0' NOT NULL,
         ADD ppverifier          VARCHAR(25),
         ADD image_provider      VARCHAR(10),
         ADD smoothread_deadline INT(20)     DEFAULT '0' NOT NULL,
