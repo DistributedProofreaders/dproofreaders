7b92d83b1398eeedce7d859f583107b26a3b0196
mike_cie <>
2005-05-24 20:59:12 +0000

    Break some long lines, and add a link to quiz_status.php

--- dp-devel/quiz/start.php
+++ dp-devel/quiz/start.php
@@ -9,13 +9,19 @@ theme(_('Proofreading Tutorial and Interactive Quiz'),'header');
 ?>
 <body bgcolor='#ffffff'>
 <h1>Proofreading Tutorial and Interactive Quiz</h1>
-<p>Welcome to DP's proofreading tutorial/quiz! As the title indicates you can use this in two different ways. If you are not yet familiar with the Proofreading Guidelines you should use it as a tutorial. You can do so by using the 'next step in tutorial' links.</p>
+<p>Welcome to DP's proofreading tutorial/quiz! As the title indicates you can 
+use this in two different ways. If you are not yet familiar with the Proofreading 
+Guidelines you should use it as a tutorial. You can do so by using the 'next 
+step in tutorial' links.</p>
 <p>
-If you already know the guidelines you can use it as a quiz only, just for fun or to confirm your knowledge. Do this by using the 'next step in quiz' links.</p>
+If you already know the guidelines you can use it as a quiz only, just for fun 
+or to confirm your knowledge. Do this by using the 'next step in quiz' links.</p>
 <p>
-At the moment we have 5 parts, all directed to beginners. Each part might take about 10 minutes, less if you don't read (and need!) the tutorial parts.</p>
+At the moment we have 5 parts, all directed to beginners. Each part might take 
+about 10 minutes, less if you don't read (and need!) the tutorial parts.</p>
 <p>
-This is still a preliminary version, feedback is highly welcome. Please post it <a href="http://www.pgdp.net/phpBB2/viewtopic.php?t=8539">here</a>.</p>
+This is still a preliminary version, feedback is highly welcome. Please post it
+ <a href="http://www.pgdp.net/phpBB2/viewtopic.php?t=8539">here</a>.</p>
 <p>
 <a href="./tuts/tut1.php">Start with step 1 in tutorial</a><br>
 <a href="./generic/main.php?type=step1">Start with step 1 in quiz</a></p>
@@ -25,6 +31,7 @@ This is still a preliminary version, feedback is highly welcome. Please post it 
 <a href="./tuts/tut4.php">part 4</a> (illustrations, footnotes)<br>
 <a href="./tuts/tut5.php">part 5</a> (poetry, block quotations)<br></p>
 <br><br>
+<a href='quiz_status.php'>Check my quiz status</a><br /><br />
 <?
 theme("", "footer");
 ?>
