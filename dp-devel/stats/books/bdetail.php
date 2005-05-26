9f1d32d892ff8d9725831b4e7705daf447d48578
jmdyck <>
2005-05-26 21:17:59 +0000

    book.php has been renamed book.inc

--- dp-devel/stats/books/bdetail.php
+++ dp-devel/stats/books/bdetail.php
@@ -3,7 +3,7 @@ $relPath="./../../pinc/";
 include_once($relPath.'v_site.inc');
 include_once($relPath.'connect.inc');
 include_once($relPath.'theme.inc');
-include_once('../includes/book.php');
+include_once('../includes/book.inc');
 $db_Connection=new dbConnect();
 
 $result = mysql_query("SELECT * FROM projects WHERE projectid = '".$_GET['project']."'");
