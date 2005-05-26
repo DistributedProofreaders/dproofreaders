932f56d8e00a37892cfa7c71acb4611db1ad866b
jmdyck <>
2005-05-26 21:45:21 +0000

    textframe_njs.inc has been renamed textframe_std.inc textframe_js.inc  has been renamed textframe_enh.inc

--- dp-devel/tools/proofers/text_frame_std.php
+++ dp-devel/tools/proofers/text_frame_std.php
@@ -3,7 +3,7 @@ $relPath="./../../pinc/";
 include_once($relPath.'dp_main.inc');
 
 if ($userP['i_type'] != 1)
-  {include('textframe_njs.inc');}
+  {include('textframe_std.inc');}
 else
-  {include('textframe_js.inc');}
+  {include('textframe_enh.inc');}
 ?>
