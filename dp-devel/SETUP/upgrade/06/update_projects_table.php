6ee8a0b8be2890322f0104475e3a8f0a1f8ab4ce
jmdyck <>
2005-06-01 01:44:04 +0000

    Move "special" info from comments to 'special' column.

--- dp-devel/SETUP/upgrade/06/update_projects_table.php
+++ dp-devel/SETUP/upgrade/06/update_projects_table.php
@@ -8,6 +8,36 @@ header('Content-type: text/plain');
 
 echo "\n";
 echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n";
+echo "Adding 'special' column...\n";
+
+mysql_query("
+    ALTER TABLE projects
+        ADD special VARCHAR(20) AFTER projectid
+") or die(mysql_error());
+
+echo "Populating 'special' column by extracting info from 'comments' column...\n";
+
+$res = mysql_query("
+    SELECT projectid, comments
+    FROM projects
+    WHERE LEFT(comments,8) = 'special:'
+") or die(mysql_error());
+
+while ( list($projectid,$comments) = mysql_fetch_row($res) )
+{
+    move_special_info( $projectid, $comments );
+}
+
+$res = mysql_query("
+    SELECT COUNT(*)
+    FROM projects
+    WHERE LEFT(comments,8) = 'special:'
+") or die(mysql_error());
+$num_left = mysql_result($res,0);
+echo "There are $num_left project comments left that begin with 'SPECIAL:'.\n";
+
+echo "\n";
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n";
 echo "Deleting 'Refer to the Guidelines' from project comments...\n";
 
 $text_to_delete = "<p>Refer to the <a href=\"$code_url/faq/document.php\">Proofreading Guidelines</a>.</p>";
@@ -32,7 +62,6 @@ echo "Adding various fields to `projects` table...\n";
 
 mysql_query("
     ALTER TABLE projects
-        ADD special             VARCHAR(20) AFTER projectid,
         ADD n_pages             SMALLINT(4) UNSIGNED DEFAULT '0' NOT NULL,
         ADD n_available_pages   SMALLINT(4) UNSIGNED DEFAULT '0' NOT NULL,
         ADD ppverifier          VARCHAR(25),
@@ -91,6 +120,63 @@ while ( list($projectid) = mysql_fetch_row($res) )
 
 echo "\n";
 echo "Done!\n";
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+function move_special_info( $projectid, $comments )
+// $comments matches /^special:/i
+// Extract the special_code from $comments and move it to the 'special' column.
+{
+    $subpatterns = array(
+        '((birth|other)day \d\d\d\d)\b\s*',
+        '(April Fool|St\. Pat|St\. Val|Nov 11)\.?\s+',
+        '(\S+) *([\n\r]\s*|$)',
+        '(\S+) *(?=<)',
+    );
+
+    $found = FALSE;
+    foreach ( $subpatterns as $subpattern )
+    {
+        $pattern = "/^special: *$subpattern/i";
+        $r = preg_match( $pattern, $comments, $matches );
+        if ( $r === FALSE )
+        {
+            die("bad return from preg_match, pattern='$pattern'");
+        }
+        else if ( $r == 0 )
+        {
+            // didn't match
+        }
+        else if ( $r == 1 )
+        {
+            // matched!
+            $special_comment = $matches[0];
+            $special_code = $matches[1];
+            // echo "delete from comments: '", $special_comment, "'\n";
+            // echo "special code: ", $special_code, "\n";
+
+            $c = strlen($special_comment);
+            $res = mysql_query("
+                UPDATE projects
+                SET
+                    special  = '$special_code',
+                    comments = SUBSTRING(comments,$c+1)
+                WHERE projectid='$projectid'
+            ") or die(mysql_error());
+            return;
+        }
+        else
+        {
+            assert( FALSE );
+        }
+    }
+
+    // no subpattern matched
+    echo "vvvvvvvvvvvvvvvvv\n";
+    echo "Unable to extract comments for $projectid. Comments start:\n";
+    echo addcslashes(substr($comments,0,50),"\r\n"), "\n";
+    echo "^^^^^^^^^^^^^^^^^\n";
+}
 
 // vim: sw=4 ts=4 expandtab
 ?>
