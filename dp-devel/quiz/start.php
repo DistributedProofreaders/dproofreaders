f8f631809b33c3b45f3bfbdca1c97e7fba3a1bad
mike_cie <>
2005-06-05 03:52:55 +0000

    If ?show_only=quiz_id, only show that quiz.

--- dp-devel/quiz/start.php
+++ dp-devel/quiz/start.php
@@ -8,18 +8,23 @@ include_once($relPath.'quizzes.inc');
 $db_Connection=new dbConnect();
 $theme_args["css_data"] = "th.q {background:$theme[color_headerbar_bg];
 color:$theme[color_headerbar_font];}";
-theme(_('Interactive Quizzes'),'header',$theme_args);
-?>
-<body bgcolor='#ffffff'>
 
-<h1>Interactive Quizzes</h1>
-
-<p>Welcome to DP's interactive quizzes! The following quizzes are available:</p>
-
-<?
-foreach ($defined_quizzes as $quiz) 
+if (!empty($_GET['show_only']) && array_search($_GET['show_only'],$defined_quizzes))
 {
-  ${$quiz}->show_results_table($pguser);
+    $quiz = $_GET['show_only'];
+    theme(${$quiz}->name,'header',$theme_args);
+    echo "<h1>".${$quiz}->name."</h1>";
+    ${$quiz}->show_results_table($pguser);
+}
+else
+{
+  theme(_('Interactive Quizzes'),'header',$theme_args);
+  echo "<h1>Interactive Quizzes</h1>
+        <p>Welcome to DP's interactive quizzes! The following quizzes are available:</p>";
+  foreach ($defined_quizzes as $quiz)
+  {
+    ${$quiz}->show_results_table($pguser);
+  }
 }
 ?>
 
