b593da8ba642966b66a31fe45c28ceb33152e5f4
jmdyck <>
2005-06-06 23:34:47 +0000

    Ensure that pre-existing tasks have a related_postings value that will unpack to an empty array. (The empty string isn't a valid value for unserialize().)

--- dp-devel/SETUP/upgrade/06/update_tasks_relatedPostings.php
+++ dp-devel/SETUP/upgrade/06/update_tasks_relatedPostings.php
@@ -5,5 +5,13 @@ $db_Connection=new dbConnect();
 
 //----------------------------------------------------------------------------------------------------------------------
 $result = mysql_query("ALTER TABLE `tasks` ADD `related_postings` MEDIUMTEXT NOT NULL") or die(mysql_error());
+
+$empty_array_value = base64_encode(serialize(array()));
+mysql_query("
+    UPDATE tasks
+    SET related_postings='$empty_array_value'
+    WHERE related_postings=''
+") or die(mysql_error());
+
 echo "<center><p>Addition of `related_postings` field to `tasks` table complete!";
 ?>
