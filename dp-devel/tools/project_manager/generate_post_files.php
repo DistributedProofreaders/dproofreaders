06000d0ec0fca3e388d5491d3657ae388b1f80e5
jmdyck <>
2005-06-09 06:40:51 +0000

    Add 'projectid' parameter, to allow invoker to specify a specify project.

--- dp-devel/tools/project_manager/generate_post_files.php
+++ dp-devel/tools/project_manager/generate_post_files.php
@@ -3,6 +3,7 @@ $relPath="./../../pinc/";
 include_once($relPath.'v_site.inc');
 include_once($relPath.'dp_main.inc');
 include_once($relPath.'project_states.inc');
+include_once($relPath.'stages.inc');
 include_once('./post_files.inc');
 
 if (!user_is_a_sitemanager())
@@ -11,12 +12,27 @@ if (!user_is_a_sitemanager())
     exit;
 }
 
+$projectid = @$_REQUEST['projectid'];
+
+echo "projectid = '$projectid'<br>\n";
+
+if ( empty($projectid) )
+{
+    die( "parameter 'projectid' is empty or unset" );
+}
 set_time_limit(0); // no time limit
 
+if ( $projectid == 'many' )
+{
     $project_condition = "
            state = '".PROJ_POST_FIRST_AVAILABLE."'
         OR state = '".PROJ_POST_FIRST_CHECKED_OUT."'
     ";
+}
+else
+{
+    $project_condition = "projectid='$projectid'";
+}
 
 $myresult = mysql_query("
     SELECT projectid, nameofwork FROM projects WHERE $project_condition
