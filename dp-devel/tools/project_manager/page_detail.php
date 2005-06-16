e975e843a70bdac21a0331ca993b1d5ec3364ce7
jmdyck <>
2005-06-16 22:38:05 +0000

    Add theme (without sidebar), and title.

--- dp-devel/tools/project_manager/page_detail.php
+++ dp-devel/tools/project_manager/page_detail.php
@@ -2,6 +2,7 @@
 $relPath="./../../pinc/";
 include_once($relPath.'v_site.inc');
 include_once($relPath.'dp_main.inc');
+include_once($relPath.'theme.inc');
 include_once($relPath.'Project.inc');
 include_once('page_table.inc');
 
@@ -11,11 +12,18 @@ $project = new Project( $projectid );
 
 $state = $project->state;
 $title = $project->nameofwork;
+$page_details_str = _('Page Details');
 
-$label = _("Return to Project Page for");
+$no_stats = 1;
+theme( "$page_details_str: $title", 'header' );
 
-echo "<br>\n";
-echo "<a href='$code_url/project.php?id=$projectid&amp;expected_state=$state'>$label $title</a>";
+echo "<h1>$title</h1>\n";
+echo "<h2>$page_details_str</h2>\n";
+
+$url = "$code_url/project.php?id=$projectid&amp;expected_state=$state";
+$label = _("Return to Project Page");
+
+echo "<a href='$url'>$label</a>";
 echo "<br>\n";
 
 include_once('detail_legend.inc');
@@ -23,6 +31,9 @@ include_once('detail_legend.inc');
 echo "N.B. It is <b>strongly</b> recommended that you view page differentials by right-clicking on a diff link and opening the link in a new window or tab."."<br>\n";
 
 echo_page_table( $project, $show_image_size);
+
+echo "<br>";
+theme( '', 'footer' );
 
 ?>
 
