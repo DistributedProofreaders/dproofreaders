044a168767ef27cf54fc22638af8531c3790b9df
mike_cie <>
2005-06-16 07:04:45 +0000

    Create access_log table.

---
+++ dp-devel/SETUP/upgrade/06/create_access_log_table.php
@@ -0,0 +1,23 @@
+<?
+
+// One-time script to create 'access_log' table
+
+$relPath='../../../pinc/';
+include_once($relPath.'connect.inc');
+include_once($relPath.'f_dpsql.inc');
+new dbConnect();
+
+dpsql_query("
+CREATE TABLE `access_log` (
+  `timestamp` INT( 20 ) NOT NULL ,
+  `subject_username` VARCHAR( 25 ) NOT NULL ,
+  `modifier_username` VARCHAR( 25 ) NOT NULL ,
+  `action` VARCHAR( 10 ) NOT NULL ,
+  `stage` VARCHAR( 10 ) NOT NULL ,
+  INDEX ( `subject_username`,`timestamp` )
+) TYPE = innodb;
+") or die("Aborting.");
+
+echo "Done!";
+
+?>
