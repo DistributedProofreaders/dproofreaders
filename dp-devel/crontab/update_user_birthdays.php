3d559d99d25e8dee0665e819bb6ae2fdb934d296
mike_cie <>
2005-07-09 06:24:41 +0000

    Do the working in the SQL, not PHP.
    (Thanks, jmdyck!)

--- dp-devel/crontab/update_user_birthdays.php
+++ dp-devel/crontab/update_user_birthdays.php
@@ -5,6 +5,11 @@ include($relPath.'connect.inc');
 new dbConnect();
 header("Content-Type: text/plain");
 
+$mtime = microtime();
+$mtime = explode(" ",$mtime);
+$mtime = $mtime[1] + $mtime[0];
+$starttime = $mtime;
+
 // Clear yesterday's birthdays.
 dpsql_query("DELETE FROM usersettings WHERE setting = 'birthday_today'");
 echo "Cleared yesterday's ".mysql_affected_rows()." birthdays.\n\n";
@@ -13,25 +18,20 @@ $now = time();
 $today = getdate($now);
 
 $months = 6;
-$max = $months * 30 * 24 * 60 * 60;
+$max = $months * 2592000;
 $res = dpsql_query("
-    SELECT username,date_created FROM users WHERE ($now - t_last_activity) < $max
+    SELECT username,date_created 
+    FROM users
+    WHERE
+        FROM_UNIXTIME(date_created, '%m-%d') = FROM_UNIXTIME($now,'%m-%d')
+        AND ($now - t_last_activity) < $max
+        AND ($now - date_created) > 28512000
 ") or die("Aborting");
-
-echo "Evaluating ".mysql_num_rows($res)." users.\n\n";
 
 while ( list($username,$date_created) = mysql_fetch_row($res) )
 {
     $user_day = getdate($date_created);
-
-    // Evaluate the most unlikely condition on its own first.
-    if ($user_day['mday'] === $today['mday'])
-    {
-        if (($user_day['mon'] == $today['mon']) && ($user_day['year'] != $today['year']))
-        {
-          $birthdays[$username] = ($today['year'] - $user_day['year']);
-        }
-    }
+    $birthdays[$username] = ($today['year'] - $user_day['year']);
 }
 
 if (count($birthdays) == 0)
@@ -53,5 +53,9 @@ foreach ($birthdays as $user => $years)
 
 echo "\nBirthdays updated. Mazel tov!";
 
-
+$mtime = microtime();
+$mtime = explode(" ",$mtime);
+$mtime = $mtime[1] + $mtime[0];
+$time = ($mtime - $starttime);
+echo "\n\nTook ".round($time,4)." seconds to run.";
 ?>
