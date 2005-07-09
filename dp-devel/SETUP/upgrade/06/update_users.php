252a37ab1d9d6c1145f8f19020ff49743c3bf0c8
jmdyck <>
2005-07-09 01:32:27 +0000

    Add 't_last_activity' column

---
+++ dp-devel/SETUP/upgrade/06/update_users.php
@@ -0,0 +1,26 @@
+<?php
+$relPath='../../../pinc/';
+include_once($relPath.'v_site.inc');
+include($relPath.'connect.inc');
+$db_Connection=new dbConnect();
+
+header('Content-type: text/plain');
+
+echo "\n";
+echo "Adding 't_last_activity' column...\n";
+
+mysql_query("
+    ALTER TABLE users
+        ADD COLUMN t_last_activity INT UNSIGNED NOT NULL AFTER last_login
+") or die(mysql_error());
+
+echo "Initializing it to the time of last login...\n";
+mysql_query("
+    UPDATE users
+    SET t_last_activity=last_login
+") or die(mysql_error());
+
+echo "Done!\n";
+
+// vim: sw=4 ts=4 expandtab
+?>
