ddcf029ea9c8d6eb7cff13700e04fb028246c730
jmdyck <>
2005-08-08 23:21:00 +0000

    change indents

--- dp-devel/crontab/inactivate_users.php
+++ dp-devel/crontab/inactivate_users.php
@@ -6,10 +6,12 @@ $db_Connection=new dbConnect();
 
 //this module sets users inactive who have not been active on the site in 6 months
 
-    $old_date = time() - 15768000; // 6 months ago.
+$old_date = time() - 15768000; // 6 months ago.
 
-    $result = mysql_query ("UPDATE `users` SET active = 'no' WHERE t_last_activity < $old_date AND active ='yes'");
-    $numrows = mysql_num_rows($result);
+$result = mysql_query ("UPDATE `users` SET active = 'no' WHERE t_last_activity < $old_date AND active ='yes'");
+$numrows = mysql_num_rows($result);
 
-    echo "inactivate_users.php set $numrows users who have not been active for 6 months as inactive";
+echo "inactivate_users.php set $numrows users who have not been active for 6 months as inactive";
+
+// vim: sw=4 ts=4 expandtab
 ?>
