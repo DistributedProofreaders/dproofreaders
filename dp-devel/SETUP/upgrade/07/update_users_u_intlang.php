123551723812daac2042a0efa5347a8466bbb547
smolenski <>
2005-10-04 09:47:35 +0000

    Script to update users.u_intlang column. This is neccesary for two reasons: - Some language tags might be longer than 5 characters, for example sr_YU.utf8@cyrillic - Default value of '' equates to browser default (it doesn't matter a lot anyway because default is set to user interface when creating an user).

---
+++ dp-devel/SETUP/upgrade/07/update_users_u_intlang.php
@@ -0,0 +1,20 @@
+<?php
+$relPath='../../../pinc/';
+include_once($relPath.'v_site.inc');
+include($relPath.'connect.inc');
+$db_Connection=new dbConnect();
+
+header('Content-type: text/plain');
+
+echo "\n";
+echo "Changing 'u_intlang' column...\n";
+
+mysql_query("
+    ALTER TABLE users
+        CHANGE COLUMN u_intlang u_intlang VARCHAR(5) DEFAULT '' 
+") or die(mysql_error());
+
+echo "Done!\n";
+
+// vim: sw=4 ts=4 expandtab
+?>
