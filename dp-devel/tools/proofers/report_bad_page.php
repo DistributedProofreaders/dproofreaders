e26a5ec6dfc283b5092969df0e0345305b3c0611
jmdyck <>
2005-12-28 03:58:59 +0000

    Drop unnecessary include() of DPage.inc

--- dp-devel/tools/proofers/report_bad_page.php
+++ dp-devel/tools/proofers/report_bad_page.php
@@ -8,7 +8,6 @@ include_once($relPath.'project_trans.inc');
 include_once($relPath.'stages.inc');
 include_once($relPath.'gettext_setup.inc');
 include_once($relPath.'theme.inc');
-include_once($relPath.'DPage.inc');
 include_once($relPath.'projectinfo.inc');
 include_once('PPage.inc');
 
