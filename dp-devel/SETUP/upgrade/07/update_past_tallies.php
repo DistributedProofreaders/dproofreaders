17bf9d0e8742a401456a53cf59e54dcc1a0f2a5b
jmdyck <>
2006-01-07 21:04:22 +0000

    Add index to speed up TallyBoard::get_time_of_latest_snapshot().

---
+++ dp-devel/SETUP/upgrade/07/update_past_tallies.php
@@ -0,0 +1,20 @@
+<?php
+$relPath='../../../pinc/';
+include_once($relPath.'connect.inc');
+new dbConnect();
+
+header('Content-type: text/plain');
+
+echo "\n";
+echo "Adding tallyboard_time index...\n";
+// This is to speed up TallyBoard::get_time_of_latest_snapshot()
+
+mysql_query("
+    ALTER TABLE past_tallies
+        ADD INDEX tallyboard_time (tally_name,holder_type,timestamp);
+") or die(mysql_error());
+
+echo "\nDone!\n";
+
+// vim: sw=4 ts=4 expandtab
+?>
