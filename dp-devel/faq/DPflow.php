38a085bc59537fa8d65f5673dc8369b329964c64
jmdyck <>
2006-07-10 20:53:33 +0000

    faq/project_workflow.png replaces faq/DPflow.gif

--- dp-devel/faq/DPflow.php
+++ dp-devel/faq/DPflow.php
@@ -10,13 +10,7 @@ theme('Workflow Diagram','header');
 
 <h1>Distributed Proofreaders General Workflow Diagram</h1>
 
-<p><b>1. </b>Project Manager acquires and pre-processes material.</p>
-<p><b>2. </b>Project Manager loads project on DP web site and 
-          proofreaders give it two rounds of proofreading. Formatters give it two rounds of formatting.</p>
-<p><b>3. </b>The Post-Processor assembles the book and uploads it to DP for verification.</p>
-<p><b>4. </b>A Post-Processing Verifier checks the uploaded book and posts it to PG.</p>
-<p>
-<img SRC="DPflow.gif" height=461 width=640></p><p>
+<img SRC="project_workflow.png"></p><p>
 <a href="ProoferFAQ.php">Back</a> to Proofreaders FAQ.</p>
 
 <?
