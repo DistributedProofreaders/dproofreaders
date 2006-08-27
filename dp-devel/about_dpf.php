f070e3e80cd1ab4de8c61fb4754b13e5adffeea3
donovang <>
2006-08-27 20:12:29 +0000

    Initial version of official "About DPF" page. Do not roll out yet pending change in theme.inc to modify link pointer.

---
+++ dp-devel/about_dpf.php
@@ -0,0 +1,176 @@
+<?
+$relPath="./pinc/";
+include_once($relPath.'dp_main.inc');
+include_once($relPath.'theme.inc');
+
+theme("The Distributed Proofreaders Foundation","header");
+?>
+
+<h1>The Distributed Proofreaders Foundation</h1>
+
+<p>
+This document provides some basic facts about the Distributed
+Proofreaders Foundation (DPF). DPF is the not-for-profit corporation
+that receives and processes donations to Distributed Proofeaders, and
+seeks fundraising opportunities.
+</p>
+
+<h2>Mission:</h2>
+
+<p>
+The Distributed Proofreaders Foundation has been formed to support the
+Distributed Proofreaders (DP) website (www.pgdp.net) which has existed,
+in one form or another, since about October, 2000. DP volunteers
+transcribe books and periodicals that are in the public domain into
+electronic texts using software that has been developed by DP
+volunteers. These electronic texts are freely distributed for the
+benefit of anyone who can use them. 
+</p>
+<p>
+While currently DP only transcribes text into ebooks, related activities
+that might be included on the existing website in the future, or
+supported by DPF in some fashion, include transcription of musical
+notation, cooperative translation, and public access to the scanned
+material produced by DP volunteers. In this sense, DPF's mission goes
+beyond simple support of the DP website to the wider goal of
+transcribing, into digital form, freely distributable material that is
+currently only available on paper.
+</p>
+
+<h2>Organization:</h2>
+
+<p>
+DPF is a not-for-profit corporation registered in the state of
+New Jersey. The registered office address of the corporation is:
+</p>
+<p style="margin: 0em 0em 0em 3em;">
+Distributed Proofreaders Foundation<br>
+319 Park St<br>
+Montclair, NJ 07043<br>
+USA
+</p>
+
+<h2>Tax Status:</h2>
+<p>
+DPF applied for tax exempt status by the IRS under section
+501(c)(3) of the US Internal Revenue Code on July 31, 2006. 
+</p>
+
+<h2>Donations:</h2>
+
+<p>
+Our understanding is that now that DPF has made the application for
+501(c)(3) status, donations are tax-deductible, with that status being
+removed retroactively if the application is turned down. We believe that
+DPF should have no trouble qualifying. All of this only matters to
+donors who file US tax returns.
+</p>
+<p>
+The PayPal donation button on the PGDP website directs donations to DPF.
+Donations made by check should be made payable to "Distributed
+Proofreaders Foundation" and mailed to the above address. DPF also
+gladly accepts donations of stock.
+
+</p>
+<p>
+DPF will report on its finances regularly (at least yearly, more often
+if it seems appropriate) to the volunteers. 
+</p>
+
+<h2>Use of Donations:</h2>
+
+<p>
+Money donated to DPF will be used to support this website, and
+possibly other DPs, as determined by the Board.
+</p>
+<p>
+In the past, expenses which PGLAF incurred on behalf of DP have included
+registration of domain names, buying high-speed scanner set-ups, buying
+our server, and paying for the hosting of our server. Hosting costs have
+been paid by PGLAF for this year (through Feb, 2007 I believe). DPF
+will, of course, be assuming responsibility for our future expenses. DPF
+has requested that PGLAF transfer ownership of the DP domain names to DPF.
+</p>
+<p>
+It is unlikely that DP will have any paid staff in the foreseeable
+future. Some software development may be contracted out, as necessary.
+Funds raised by DPF will be used to support the website, recruit new
+volunteers, improve the custom software, buy additional hardware as
+needed, and for such other items as the Board determines are
+appropriate.
+</p>
+
+<h2>Board Members:</h2>
+
+<p>
+There are currently three board members:
+</p>
+
+<ul>
+<li>  Charles Franks</li>
+<li>  Juliet Sutherland</li>
+<li>  Greg Newby</li>
+</ul>
+
+<h2>Employees:</h2>
+
+<p>
+DPF currently has no employees.
+</p>
+
+<h2>Bylaws and Articles of Incorporation:</h2>
+
+<ul>
+<li>DPF's Bylaws</li>
+<li>DPF's Articles of Incorporation</li>
+</ul>
+
+<p>
+These documents are currently in paper-only form, and will be made
+available electronically as soon as possible.
+</p>
+
+<h2>History:</h2>
+
+<p>
+When Distributed Proofreaders (DP) was founded by Charles Franks in 2000
+it was just a website hosted on his home computer. He accepted
+contributions towards the operating costs of the site, but they were not
+tax-deductible. Sometime around July 2002, he decided to officially
+associate the website with Project Gutenberg, so that PGLAF (Project
+Gutenberg Literary Archive Foundation), which already existed as a
+non-profit, tax-deductible donation-accepting organization, could deal
+with the financial matters. Charles turned over the money that had been
+donated to DP to PGLAF and from that time, PGDP operated under the
+financial umbrella and corporate protection of PGLAF. 
+</p>
+<p>
+Since then, DP has grown enough that it makes sense for us to become our
+own legal identity and have financial independence. The
+incorporation documents and by-laws have been written to be as generic
+as possible, allowing great flexibility on the part of the Board. This
+will allow the Foundation the maximum opportunity to grow and change as
+time and circumstances dictate. 
+</p>
+<p>
+As indicated by the presence of Greg Newby (CEO of PGLAF) as a Trustee, the
+establishment of DPF does not imply any change in DP's support of, or
+alignment with, PG's mission. DPF simply formalizes what has been true
+all along, that DP is a separate organization from PG. 
+</p>
+
+<h2>Contact Information:</h2>
+
+<p>
+dpfoundation@pgdp.org
+</p>
+
+<div style="height:4em;"><br /></div>
+
+<?
+theme("","footer");
+
+// ---------------------------------------------------------
+
+// vim: sw=4 ts=4 expandtab
+?>
