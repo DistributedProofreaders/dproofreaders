e3f799dba2eaa4669db5d7df2822fa4ab26f2c72
donovang <>
2006-10-27 01:46:47 +0000

    Grouped set of changes implementing fron page redesign as per Juliet. Declutters display, groups "like items" and makes site message more clear and concise for non-signed in (e.g. new) users.

--- dp-devel/default.php
+++ dp-devel/default.php
@@ -15,11 +15,51 @@ abort_if_in_maintenance_mode();
 theme(_("Welcome"), "header");
 $etext_limit = 10;
 
-
 default_page_heading();
 
+show_news_for_page("FRONT");
+
+?>
+
+<p><font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b><? echo _("Site Concept"); ?></b></font><br>
+<? echo _("Distributed Proofreaders provides a web-based method to ease the conversion of Public Domain books into e-books. By dividing the workload into individual pages, many volunteers can work on a book at the same time, which significantly speeds up the creation process."); ?></p>
+<p><? echo _("During proofreading, volunteers are presented with a scanned page image and the corresponding OCR text on a single web page. This allows the text to be easily compared to the image, proofread, and sent back to the site. A second volunteer is then presented with the first volunteer's work and the same page image, verifies and corrects the work as necessary, and submits it back to the site. The book then similarly progresses through two formatting rounds using the same web interface."); ?></p>
+<p><? echo _("Once all the pages have completed these steps, a post-processor carefully assembles them into an e-book and submits it to the Project Gutenberg archive."); ?></p>
+
+<p><font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b><? echo _("How You Can Help"); ?></b></font><br>
+<ul>
+<li>
+<? echo sprintf(_("<a href='accounts/addproofer.php'>Register</a> with the site as a volunteer.")); ?></li>
+<li>
+<? echo sprintf(_("Read the introductory email you receive and the <a href='%s/faq/ProoferFAQ.php'>Beginning Proofreader's FAQ</a>."),$code_url); ?></li>
+<li>
+<? echo sprintf(_("Confirm your registration, sign in, choose a project, and try proofreading a page or two!")); ?></li>
+</ul>
+
+<p><? echo _("Remember that there is no commitment expected on this site beyond the understanding that you do your best.");?></p>
+<p><? echo _("Proofread as often or as seldom as you like, and as many or as few pages as you like.  We encourage people to do 'a page a day', but it's entirely up to you! We hope you will join us in our mission of 'preserving the literary history of the world in a freely available form for everyone to use'."); ?></p>
+<p><? echo sprintf(_("There are many other ways to contribute to the site, including managing projects, providing content, or even helping develop improvements to the site! Join other members of our community in the <a href='%s'>Forums</a> to discuss these and many other topics."),$forums_url); ?></p>
+
+<font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b><? echo _("Current Progress"); ?></b></font><br>
+
+<?
+echo "<table><tr><td valign='top'>";
+
+//Gold E-texts
+showstartexts($etext_limit,'gold'); echo "</td><td valign='top'>";
+
+//Silver E-texts
+showstartexts($etext_limit,'silver'); echo "</td><td valign='top'>";
+
+//Bronze E-texts
+showstartexts($etext_limit,'bronze'); echo "</td></tr></table>";
+
+echo "<hr><center>\n";
+echo _("Our community of proofreaders, project managers, developers, etc. is composed entirely of volunteers.");
+echo "</center>\n";
+
 // Show the number of users that have been active over various recent timescales.
-foreach ( array(1,7,28) as $days_back )
+foreach ( array(1,7,30) as $days_back )
 {
     $res = mysql_query("
         SELECT COUNT(*)
@@ -37,39 +77,10 @@ foreach ( array(1,7,28) as $days_back )
     echo "<center><i><b>$msg</b></i></center>\n";
 }
 
-?>
+echo "<hr><center>\n";
+echo sprintf(_("Questions or comments? Please contact us at <a href='mailto:%s'>%s</a>."),$general_help_email_addr,$general_help_email_addr);
+echo "</center>&nbsp;<br>\n";
 
-<p><font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b><? echo _("About This Site") ?></b></font><br>
-<?
-echo sprintf( _("Distributed Proofreaders was founded in 2000 by Charles Franks to support the digitization of Public Domain books. Originally conceived to assist <a href='%s' target='_new'>Project Gutenberg</a> (PG), Distributed Proofreaders (DP) is now the main source of PG e-books. All our proofreaders, managers, developers and so on are volunteers."), $PG_home_url );
-echo "\n";
-echo sprintf( _("If you have any questions or comments regarding this site, please e-mail <a href='mailto:%s'>%s</a>."), $general_help_email_addr, $general_help_email_addr );
-echo "<br><br>";
-
-show_news_for_page("FRONT");
-?>
-</p>
-
-<p><font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b><? echo _("Site Concept"); ?></b></font><br>
-<? echo _("This site provides a web-based method of easing the proofreading work associated with the digitization of Public Domain books into Project Gutenberg e-books. By breaking the work into individual pages many proofreaders can be working on the same book at the same time. This significantly speeds up the proofreading/e-book creation process."); ?></p>
-<p><? echo _("When a proofreader elects to proofread a page of a particular book, the text and image file are displayed on a single web page. This allows the page text to be easily reviewed and compared to the image file, thus assisting the proofreading of the page text. The edited text is then submitted back to the site via the same web page that it was edited on. A second proofreader is then presented with the work of the first proofreader and the page image. Once they have verified the work of the first proofreader and corrected any additional errors the page text is again submitted back to the site. The book then progresses through two formatting rounds using the same web interface."); ?></p>
-<p><? echo _("Once all pages for a particular book have been processed, a post-processor joins the pieces, properly formats them into a Project Gutenberg e-book and submits it to the Project Gutenberg archive."); ?></p>
-
-<p><font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b><? echo _("How You Can Help"); ?></b></font><br>
-<? echo sprintf(_("The first step to take to help us out would be to <a href='accounts/addproofer.php'>register</a> to be a new proofreader. (A 'Register' link also appears at the top of the screen.)  After you register be sure to read over both the email you receive as well as  <a href='%s/faq/faq_central.php'>FAQ Central</a> which provides helpful resources on how to proofread.  (See also the 'Help' at the top of any screen.)  After you have registered &amp; read through some of the intro documents choose an interesting-looking book from our Current Projects and try proofreading a page or two."),$code_url); ?></p>
-
-<p><? echo _("You don't even have to register to have a look at the <a href='tools/post_proofers/smooth_reading.php'>Smooth Reading Pool Preview</a>, though you do to upload corrections. Follow the link for more information."); ?></p>
-
-<p><? echo _("Remember that there is no commitment expected on this site. Proofread as often or as seldom as you like, and as many or as few pages as you like.  We encourage people to do 'a page a day', but it's entirely up to you! We hope you will join us in our mission of 'preserving the literary history of the world in a freely available form for everyone to use'."); ?></p>
-
-<font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b><? echo _("Most Recent Projects"); ?></b></font><br>
-<?
-//Gold E-texts
-showstartexts($etext_limit,'gold');
-//Silver E-texts
-showstartexts($etext_limit,'silver');
-//Bronze E-texts
-showstartexts($etext_limit,'bronze');
 theme("", "footer");
 
 // vim: sw=4 ts=4 expandtab
