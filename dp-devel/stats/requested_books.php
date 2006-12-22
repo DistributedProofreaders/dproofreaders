f3085893cfef8063f85507b0e251163d650feb63
jmdyck <>
2006-12-22 06:58:46 +0000

    Factor out     create_temporary_project_event_subscription_summary_table() from     stats/requested_books.php to     pinc/user_project_info.inc

--- dp-devel/stats/requested_books.php
+++ dp-devel/stats/requested_books.php
@@ -5,6 +5,7 @@ include_once($relPath.'dpsql.inc');
 include_once($relPath.'project_states.inc');
 include_once($relPath.'stages.inc');
 include_once($relPath.'theme.inc');
+include_once($relPath.'user_project_info.inc');
 
 $title = _("Most Requested Books");
 theme($title,'header');
@@ -13,6 +14,8 @@ echo "<br><h2 style='color: $theme[color_headerbar_bg];'>$title</h2><br>\n
 <p>You can sign up for notifications by following the &quot;Click here to register for automatic email notification of when this has been posted to Project Gutenberg&quot; link on the Project Comments page when proofreading.</p>";
 
 echo "<br><br><h3 style='color: $theme[color_headerbar_bg];'>" . _("Most Requested Books Being Proofread") . "</h2><br>\n";
+
+create_temporary_project_event_subscription_summary_table();
 
 $comments_url1 = mysql_escape_string("<a href='$code_url/project.php?id=");
 $comments_url2 = mysql_escape_string("'>");
@@ -34,16 +37,14 @@ for ( $rn = 1; $rn <= MAX_NUM_PAGE_EDITING_ROUNDS; $rn++ )
 }
 dpsql_dump_themed_ranked_query("
     SELECT
-        CONCAT('$comments_url1',projectID,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
+        CONCAT('$comments_url1',projects.projectid,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
         authorsname AS 'Author', 
         genre AS 'Genre',
         language AS 'Language',
-        COUNT(*) AS 'Notification Requests' 
-    FROM usersettings, projects 
-    WHERE value = projectid 
-        AND setting = 'posted_notice' 
+        pesgbp.nouste_posted AS 'Notification Requests'
+    FROM project_event_subscriptions_grouped_by_project AS pesgbp, projects
+    WHERE pesgbp.projectid = projects.projectid
         AND ($state_condition)
-    GROUP BY value 
     ORDER BY 'Notification Requests' DESC 
     LIMIT 50
 ");
@@ -55,16 +56,14 @@ echo "<br><br><h3 style='color: $theme[color_headerbar_bg];'>" . _("Most Request
 
 dpsql_dump_themed_ranked_query("
     SELECT
-        CONCAT('$comments_url1',projectID,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
+        CONCAT('$comments_url1',projects.projectid,'$comments_url2', nameofwork, '$comments_url3') AS 'Title', 
         authorsname AS 'Author', 
         genre AS 'Genre',
         language AS 'Language',
-        COUNT(*) AS 'Notification Requests' 
-    FROM usersettings, projects 
-    WHERE value = projectid 
-        AND setting = 'posted_notice' 
+        pesgbp.nouste_posted AS 'Notification Requests'
+    FROM project_event_subscriptions_grouped_by_project AS pesgbp, projects
+    WHERE pesgbp.projectid = projects.projectid
         AND ".SQL_CONDITION_SILVER."
-    GROUP BY value 
     ORDER BY 'Notification Requests' DESC 
     LIMIT 50
 ");
