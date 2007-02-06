882b676c039ac1394ac6c9f06311caf49e5de9dc
jmdyck <>
2007-02-06 01:35:14 +0000

    Introduce and initialize 't_last_change_comments' column.

--- dp-devel/SETUP/upgrade/08/alter_projects_table.php
+++ dp-devel/SETUP/upgrade/08/alter_projects_table.php
@@ -6,6 +6,24 @@ new dbConnect();
 
 header('Content-type: text/plain');
 
+echo "Adding t_last_change_comments...\n";
+$sql = "
+    ALTER TABLE projects
+        ADD COLUMN t_last_change_comments INT NOT NULL
+            AFTER t_last_edit
+";
+echo "$sql\n";
+mysql_query($sql) or die( mysql_error() );
+
+echo "... and initializing it...\n";
+$sql = "
+    UPDATE projects
+    SET t_last_change_comments = t_last_edit
+";
+echo "$sql\n";
+mysql_query($sql) or die( mysql_error() );
+
+// ---------------------------------------------------
 
 echo "Adding an index for projectid,archived,state...\n";
 $sql = "
