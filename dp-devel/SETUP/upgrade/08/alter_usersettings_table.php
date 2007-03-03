bb686fc5d2170447ecd4382e44c44686237753d1
cpeel <>
2007-03-03 17:23:21 +0000

    Optimized indexes for usersettings table to eliminate tablescans for frequent queries.

---
+++ dp-devel/SETUP/upgrade/08/alter_usersettings_table.php
@@ -0,0 +1,52 @@
+<?php
+
+$relPath='../../../pinc/';
+include($relPath.'connect.inc');
+new dbConnect();
+
+header('Content-type: text/plain');
+
+// ------------------------------------------------------------
+
+echo "Adding an index for username, setting, value...\n";
+$sql = "
+    ALTER TABLE usersettings
+        ADD INDEX username_setting_val
+            ( username, setting, value );
+";
+
+echo "$sql\n";
+
+mysql_query($sql) or die( mysql_error() );
+
+// ------------------------------------------------------------
+
+echo "Updating the setting index...\n";
+echo "  dropping the old one...\n";
+$sql = "
+    ALTER TABLE usersettings
+        DROP INDEX setting;
+";
+
+echo "$sql\n";
+
+mysql_query($sql) or die( mysql_error() );
+
+echo "  creating the new one...\n";
+$sql = "
+    ALTER TABLE usersettings
+        ADD INDEX setting
+            ( setting, value desc );
+";
+
+echo "$sql\n";
+
+mysql_query($sql) or die( mysql_error() );
+
+// ------------------------------------------------------------
+
+echo "\nDone!\n";
+
+
+// vim: sw=4 ts=4 expandtab
+?>
