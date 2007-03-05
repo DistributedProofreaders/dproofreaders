bec85dc19910ad736a061b7da4e512149497359d
cpeel <>
2007-03-05 03:41:51 +0000

    Enhanced file to be more useful with a more robust form, ability to select rounds from a drop-down list, and a quick-pick list of pages that had been updated in the review round with links to a diff. Page retains backwards compatability with previous verison.

--- dp-devel/tools/proofers/review_work.php
+++ dp-devel/tools/proofers/review_work.php
@@ -4,43 +4,94 @@ include_once($relPath.'dp_main.inc');
 include_once($relPath.'dpsql.inc');
 include_once($relPath.'theme.inc');
 
+define("MESSAGE_INFO",0);
+define("MESSAGE_WARNING",1);
+define("MESSAGE_ERROR",2);
+
 error_reporting(E_ALL);
 
+// get an array of round IDs
+$rounds=array_keys($Round_for_round_id_);
+
+// load any data passed into the page
+$username = @$_REQUEST["username"];
+$work_round_id = @$_REQUEST["work_round_id"];
+$review_round_id = @$_REQUEST["review_round_id"];
+$sampleLimit = 6;
+
+// if the user isn't a site manager or an access request reviewer
+// they can only access their own pages
 if (!(user_is_a_sitemanager() || user_is_an_access_request_reviewer()))
 {
     $username = $pguser;
 }
-else
-{
-    $username = @$_REQUEST['username'];
 
-    if (empty($username))
-    {
-        echo "<form>";
-        echo "Username: <input name='username' type='text' size='26'><br />";
-        echo "Work Round ID: <input name='work_round_id' type='text' size='5'><br />";
-        echo "Review Round ID: <input name='review_round_id' type='text' size='5'><br />";
-        echo "<input type='submit'>";
-        echo "</form>";
-        exit;
-    }
-}
 
-$work_round_id   = @$_REQUEST['work_round_id'];
-$review_round_id = @$_REQUEST['review_round_id'];
-
-$work_round   = get_Round_for_round_id($work_round_id);
-$review_round = get_Round_for_round_id($review_round_id);
-
-if ( is_null($work_round  ) ) die( "bad work_round_id='$work_round_id'" );
-if ( is_null($review_round) ) die( "bad review_round_id='$review_round_id'" );
-
-$title = sprintf( _('Reviewing %s work of %s'), $work_round->id, $username );
+// start the page
+$title = _('Reviewing work');
 
 $no_stats = 1;
 theme( $title, 'header' );
 
-echo "<h2>$title</h2>\n";
+echo "<h1>$title</h1>\n";
+
+// show form
+    echo "<form action='review_work.php' method='GET'>";
+    echo "<table>";
+if (user_is_a_sitemanager() || user_is_an_access_request_reviewer()) {
+    // only let site admins or reviewers to access non-self records
+    echo  "<tr>";
+    echo   "<td>" . _("Username") . "</td>";
+    echo   "<td><input name='username' type='text' size='26' value='$username'></td>";
+    echo  "</tr>";
+}
+    echo  "<tr>";
+    echo   "<td>" . _("Work Round") . "</td>";
+    echo   "<td><select name='work_round_id'>";
+    _echo_round_select($rounds,$work_round_id);
+    echo    "</select>";
+    echo  "</tr>";
+    echo  "<tr>";
+    echo   "<td>" . _("Review Round") . "</td>";
+    echo   "<td><select name='review_round_id'>";
+    _echo_round_select(array_slice($rounds,1),$review_round_id);
+    echo     "</select>";
+    echo  "</tr>";
+    echo "</table>";
+    echo "<input type='submit' value='Search'>";
+    echo "</form>";
+
+function _echo_round_select($rounds,$selected) {
+    foreach($rounds as $round) {
+        echo "<option value='$round'";
+        if($round == $selected) echo " selected";
+        echo ">$round</option>";
+    }
+}
+
+// if not all values are set (ie: not passed into the script)
+// stop the page here
+if(empty($username) ||
+   empty($work_round_id) ||
+   empty($review_round_id)) {
+    theme('', 'footer');
+    exit;
+}
+
+// confirm the review_round_id is later than work_round_id
+if(array_search($review_round_id,$rounds)<=array_search($work_round_id,$rounds)) {
+    echo "<p class='error'>" . _("Work Round should be a round later than Review Round.") . "</p>";
+    theme('', 'footer');
+    exit;
+}
+
+
+echo "<hr>";
+
+// we should have valid information at this point
+$work_round   = get_Round_for_round_id($work_round_id);
+$review_round = get_Round_for_round_id($review_round_id);
+
 
 $res1 = dpsql_query("
     SELECT COUNT(*), COUNT(DISTINCT projectid)
@@ -48,13 +99,13 @@ $res1 = dpsql_query("
     WHERE round_id='$work_round->id' AND username='$username' AND event_type='saveAsDone'
 ") or die("Aborting");
 list($n_page_saves,$n_distinct_projects) = mysql_fetch_row($res1);
+mysql_free_result($res1);
 
 // Note that some of these saves might have been later undone by
 // the user reopening the page or the PM clearing the page.
 // So far, we don't care too much about that.
 
-echo "$n_page_saves page-saves in $n_distinct_projects projects<br>\n";
-echo "<br>\n";
+echo "<p>" . sprintf(_("Compared %d page-saves between rounds %s and %s within %d projects."),$n_page_saves,$work_round->id,$review_round->id,$n_distinct_projects) . "</p>";
 
 $res2 = dpsql_query("
     SELECT
@@ -90,42 +141,51 @@ $there_is_a_diff = "
 echo "<table border='1'>";
 
 echo "<tr>";
-echo "<th>title</th>";
-echo "<th>curr state</th>";
-echo "<th>time of latest save</th>";
-echo "<th># saved by user in $work_round->id</th>";
-echo "<th># saved by other in $review_round->id</th>";
-echo "<th># of those with a diff</th>";
+echo "<th>" . ("Title") . "</th>";
+echo "<th>" . ("Current State") . "</th>";
+echo "<th>" . ("Last Saved") . "</th>";
+echo "<th>" . sprintf(_("Pages saved by user in %s"),$work_round->id) . "</th>";
+echo "<th>" . sprintf(_("Pages saved by others in %s"),$review_round->id) . "</th>";
+echo "<th>" . _("Pages with differences") . "</th>";
+echo "<th>" . _("Recent Diff Samples") . "</th>";
 echo "</tr>";
 
 $total_n_saved   = 0;
 $total_n_latered = 0;
 $total_n_w_diff  = 0;
 
+$messages = array();
 while ( list($projectid, $state, $nameofwork, $time_of_latest_save) = mysql_fetch_row($res2) )
 {
     // $url = "$code_url/project.php?id=$projectid&amp;detail_level=4";
     $url = "$code_url/tools/project_manager/page_detail.php?project=$projectid&amp;select_by_user=$username";
 
-    $res3 = mysql_query("
+    $query = "select count(*) from $projectid where {$work_round->user_column_name}='$username' and $has_been_saved_in_review_round and $there_is_a_diff";
+    $query = "
         SELECT
             COUNT(*),
             IFNULL( SUM($has_been_saved_in_review_round), 0 ),
             IFNULL( SUM($has_been_saved_in_review_round AND $there_is_a_diff), 0 )
         FROM $projectid
-        WHERE {$work_round->user_column_name}='$username'
-    ");
+        WHERE {$work_round->user_column_name}='$username'";
+    $res3 = mysql_query($query);
     if ( $res3 !== FALSE )
     {
         list( $n_saved, $n_latered, $n_with_diff ) = mysql_fetch_row($res3);
+        mysql_free_result($res3);
+        if($n_latered > 0)
+            $n_with_diff_percent = sprintf("%d",($n_with_diff/$n_saved)*100);
+        else $n_with_diff_percent = 0;
+        $table_found = 1;
     }
     else
     {
         // something went wrong
         if ( mysql_errno() == 1146 )
         {
-            echo "(pages table no longer exists for: $nameofwork)<br>\n";
-            $n_saved = $n_latered = $n_with_diff = 0;
+            $messages[sprintf(_("Pages table no longer exists for: %s"),$nameofwork)] = MESSAGE_ERROR;
+            $n_saved = $n_latered = $n_with_diff  = $n_with_diff_percent = 0;
+            $table_found = 0;
         }
         else
         {
@@ -133,23 +193,47 @@ while ( list($projectid, $state, $nameofwork, $time_of_latest_save) = mysql_fetc
         }
     }
 
-    $n_latered_bg = ( $n_latered   > 0 ? "bgcolor='#ccffcc'" : "" );
-    $n_w_diff_bg  = ( $n_with_diff > 0 ? "bgcolor='#ccffcc'" : "" );
+    // now get the $sampleLimit most recent pages that are different
+    $query="select distinct pe.image from page_events as pe, $projectid as proj where pe.image = proj.image and username='$username' and event_type='saveAsDone' and $has_been_saved_in_review_round and $there_is_a_diff and pe.username = proj.{$work_round->user_column_name} order by timestamp desc, image limit $sampleLimit";
+    $result = mysql_query($query);
+    if ( $result !== FALSE ) {
+        $diffLinkString="";
+        while( list($image) = mysql_fetch_row($result) ) {
+            $diffLinkString.="<a href='../project_manager/diff.php?project=$projectid&amp;image=$image&amp;L_round_num=$work_round->round_number&amp;R_round_num=$review_round->round_number'>$image</a> ";
+        }
+        mysql_free_result($result);
+    } else {
+        $diffLinkString="";
+    }
+
+// not sure why the pages that have been saved in the review round
+// are highlighted as that data doesn't really tell me much
+//    $n_latered_bg = ( $n_latered   > 0 ? "bgcolor='#ccffcc'" : "" );
+    $n_latered_bg = '';
+    $n_w_diff_bg  = ( $n_with_diff > 0 ? "bgcolor='yellow'" : "" );
 
     $total_n_saved   += $n_saved;
     $total_n_latered += $n_latered;
     $total_n_w_diff  += $n_with_diff;
 
     echo "<tr>";
-    echo "<td><a href='$url'>$nameofwork</a></td>";
+    if($table_found)
+        echo "<td><a href='$url'>$nameofwork</a></td>";
+    else
+        echo "<td>$nameofwork</td>";
     echo "<td>$state</td>";
     echo "<td>$time_of_latest_save</td>";
     echo "<td align='center'>$n_saved</td>";
     echo "<td align='center' $n_latered_bg>$n_latered</td>";
-    echo "<td align='center' $n_w_diff_bg>$n_with_diff</td>";
+    echo "<td align='center' $n_w_diff_bg>$n_with_diff ($n_with_diff_percent%)</td>";
+    echo "<td $n_w_diff_bg>$diffLinkString</td>";
     echo "</tr>";
     echo "\n";
 }
+
+if($total_n_latered > 0) 
+    $total_n_w_diff_percent = sprintf("%d",($total_n_w_diff/$total_n_saved)*100);
+else $total_n_w_diff_percent = 0;
 
 echo "<tr>";
 echo "<th></th>";
@@ -157,10 +241,16 @@ echo "<th></th>";
 echo "<th></th>";
 echo "<th align='center'>$total_n_saved</th>";
 echo "<th align='center'>$total_n_latered</th>";
-echo "<th align='center'>$total_n_w_diff</th>";
+echo "<th align='center'>$total_n_w_diff ($total_n_w_diff_percent%)</th>";
 echo "</tr>";
 
 echo "</table>";
+
+if(count($messages)) {
+    echo "<h2>" . _("Messages returned") . "</h2>";
+    foreach($messages as $message => $messageClass)
+        echo "<p class='messageClass'>$message</p>";
+}
 
 echo "<br>";
 
