4390ff5a86321ebd8f85d0837edc6aa8bbecd4e9
jmdyck <>
2007-03-09 01:57:06 +0000

    Introduce and (somewhat) initialize 't_last_page_done' column.

--- dp-devel/SETUP/upgrade/08/alter_projects_table.php
+++ dp-devel/SETUP/upgrade/08/alter_projects_table.php
@@ -6,6 +6,8 @@ new dbConnect();
 
 header('Content-type: text/plain');
 
+// ---------------------------------------------------
+
 echo "Adding t_last_change_comments...\n";
 $sql = "
     ALTER TABLE projects
@@ -22,6 +24,40 @@ $sql = "
 ";
 echo "$sql\n";
 mysql_query($sql) or die( mysql_error() );
+
+// ---------------------------------------------------
+
+echo "Adding t_last_page_done...\n";
+$sql = "
+    ALTER TABLE projects
+        ADD COLUMN t_last_page_done INT NOT NULL
+            AFTER t_last_change_comments
+";
+echo "$sql\n";
+mysql_query($sql) or die( mysql_error() );
+
+echo "... and initializing it...\n";
+// Note that this ignores projects whose last save-as-done predates page_events.
+// I'm working on that.
+$sql = "
+    SELECT projectid, MAX(timestamp)
+    FROM page_events
+    WHERE event_type='saveAsDone'
+    GROUP BY projectid
+";
+$res = mysql_query($sql) or die( mysql_error() );
+$n = mysql_num_rows($res);
+echo "for $n projects...\n";
+while ( list($projectid,$max_timestamp) = mysql_fetch_row($res) )
+{
+    // echo "$projectid\n";
+    $sql = "
+        UPDATE projects
+        SET t_last_page_done = $max_timestamp
+        WHERE projectid = '$projectid'
+    ";
+    mysql_query($sql) or die( mysql_error() );
+}
 
 // ---------------------------------------------------
 
