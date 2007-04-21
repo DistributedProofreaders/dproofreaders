722343c236e75302b71e4bf1a33e9fb2a777e40d
lmpryor <>
2007-04-21 12:46:15 +0000

    Navigation through diff pages. Task #1071

--- dp-devel/tools/project_manager/diff.php
+++ dp-devel/tools/project_manager/diff.php
@@ -25,6 +25,8 @@ theme("$title: $project_title", "header", $extra_args);
 echo "<h1>$project_title</h1>\n";
 echo "<h2>$title</h2>\n";
 
+do_navigation($projectid, $image, $L_round_num, $R_round_num);
+
 $url = "$code_url/project.php?id=$projectid&amp;expected_state=$state";
 $label = _("Go to Project Page");
 
@@ -58,6 +60,7 @@ $res = mysql_query("
 ");
 list($L_text, $R_text) = mysql_fetch_row($res);
 
+
 // ---------------------------------------------------------
 
 class OutputPage {
@@ -81,4 +84,58 @@ DifferenceEngine::showDiff(
 );
 
 theme("", "footer");
+
+function do_navigation($projectid, $image, $L_round_num, $R_round_num) 
+{
+    $jump_to_js = "this.form.image.value=this.form.jumpto[this.form.jumpto.selectedIndex].value; this.form.submit();";
+
+    echo "\n<form method='get' action='diff.php'>";
+    echo "\n<input type='hidden' name='project' value='$projectid'>";
+    echo "\n<input type='hidden' name='image' value='$image'>";
+    echo "\n<input type='hidden' name='L_round_num' value='$L_round_num'>";
+    echo "\n<input type='hidden' name='R_round_num' value='$R_round_num'>";
+    echo "\nJump to: <select name='jumpto' onChange='$jump_to_js'>\n";
+
+    $query = "SELECT image FROM $projectid ORDER BY image ASC";
+    $res = mysql_query( $query) or die(mysql_error());
+    $num_rows = mysql_num_rows($res);
+    $prev_image = "";
+    $next_image = "";
+    // construct the dropdown
+    for ($row=0; $row<$num_rows;$row++)
+    {
+        $this_val = mysql_result($res, $row, "image");
+        echo "\n<option value='$this_val'";
+        if ($this_val == $image)
+        {
+            echo " selected";
+            if ( $row != 0 ) 
+            {
+                $prev_image = mysql_result($res, $row-1, "image");
+            }
+            if ( $row != $num_rows-1 ) 
+            {
+                $next_image = mysql_result($res, $row+1, "image");
+            }
+        }
+        echo ">$this_val</option>";
+    }
+    echo "\n</select>";
+    $previous_js = "this.form.image.value='$prev_image'; this.form.submit();";
+    $next_js = "this.form.image.value='$next_image'; this.form.submit();";
+
+    echo "\n<input type='button' value='Previous' onClick=\"$previous_js\"";
+    if ( $prev_image == "" ) {
+        echo " disabled";
+    }
+    echo ">";
+    echo "\n<input type='button' value='Next' onClick=\"$next_js\"";
+    if ( $next_image == "" ) {
+        echo " disabled";
+    }
+    echo ">";
+
+    echo "\n</form>";
+}
+
 ?>
