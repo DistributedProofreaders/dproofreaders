d45c91b3b64819627ea9d8af521be90ab4e9af35
lmpryor <>
2007-05-05 11:58:21 +0000

    If only showing pages for specified user in specified round, say so.

--- dp-devel/tools/project_manager/page_detail.php
+++ dp-devel/tools/project_manager/page_detail.php
@@ -76,7 +76,15 @@ echo "N.B. It is <b>strongly</b> recommended that you view page differentials by
 
 if ( !is_null($username_for_page_selection) )
 {
-    echo sprintf( _("Showing only the pages of user '%s'."), $username_for_page_selection );
+    echo sprintf( _("Showing only the pages of user '%s'"), $username_for_page_selection );
+    if ( !is_null($round_for_page_selection) )
+    {
+        echo sprintf(_(" in round %s."), $round_for_page_selection);
+    }
+    else
+    {
+        echo ".";
+    }
     $blurb = _("Show all pages instead.");
     echo "&nbsp;&nbsp;";
     echo "<a href='?project=$projectid&show_image_size=$show_image_size'>$blurb</a>";
