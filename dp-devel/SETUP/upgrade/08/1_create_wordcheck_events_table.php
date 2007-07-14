fbf306efddf43b9ad22fa5d17c9a29934a0ecc82
cpeel <>
2007-07-14 21:51:48 +0000

    File to create WordCheck storage table for upgrades.

---
+++ dp-devel/SETUP/upgrade/08/1_create_wordcheck_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+$relPath='../../../pinc/';
+include($relPath.'connect.inc');
+new dbConnect();
+
+header('Content-type: text/plain');
+
+echo "Creating wordcheck_events table...\n";
+$sql = "
+    CREATE TABLE wordcheck_events
+    (
+        check_id    INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        projectid   VARCHAR(22) NOT NULL,
+        timestamp   INT UNSIGNED NOT NULL,
+        image       VARCHAR(12) NOT NULL,
+        round_id    CHAR(2) NOT NULL,
+        username    VARCHAR(25) NOT NULL,
+        suggestions TEXT,
+        corrections TEXT
+    )
+";
+echo "$sql\n";
+mysql_query($sql) or die( mysql_error() );
+
+$sql = "CREATE INDEX pc_compound ON wordcheck_events(projectid,timestamp,image)";
+echo "$sql\n";
+mysql_query($sql) or die( mysql_error() );
+
+echo "\nDone!\n";
+
+// vim: sw=4 ts=4 expandtab
+?>
