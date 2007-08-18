a53dfc739fc40f7869db789e15724b0512798461
jmdyck <>
2007-08-18 00:56:37 +0000

    Under "Site Concept", in the description of the site workflow, mention smooth reading.

--- dp-devel/default.php
+++ dp-devel/default.php
@@ -24,7 +24,7 @@ show_news_for_page("FRONT");
 <p><font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b><? echo _("Site Concept"); ?></b></font><br>
 <? echo _("Distributed Proofreaders provides a web-based method to ease the conversion of Public Domain books into e-books. By dividing the workload into individual pages, many volunteers can work on a book at the same time, which significantly speeds up the creation process."); ?></p>
 <p><? echo _("During proofreading, volunteers are presented with a scanned page image and the corresponding OCR text on a single web page. This allows the text to be easily compared to the image, proofread, and sent back to the site. A second volunteer is then presented with the first volunteer's work and the same page image, verifies and corrects the work as necessary, and submits it back to the site. The book then similarly progresses through two formatting rounds using the same web interface."); ?></p>
-<p><? echo _("Once all the pages have completed these steps, a post-processor carefully assembles them into an e-book and submits it to the Project Gutenberg archive."); ?></p>
+<p><? echo _("Once all the pages have completed these steps, a post-processor carefully assembles them into an e-book, optionally makes it available to interested parties for 'smooth reading', and submits it to the Project Gutenberg archive."); ?></p>
 
 <p><font face="<? echo $theme['font_mainbody']; ?>" color="<? echo $theme['color_headerbar_bg']; ?>" size="+1"><b><? echo _("How You Can Help"); ?></b></font><br>
 <ul>
