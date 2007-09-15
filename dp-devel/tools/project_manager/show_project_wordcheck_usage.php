a229a2a643c83aac02c2c3ee3b2d5ecd89348120
cpeel <>
2007-09-15 20:07:43 +0000

    Restructure page to use projectID* table instead of page_events table. Incorporated feedback from code review by jmdyck.

--- dp-devel/tools/project_manager/show_project_wordcheck_usage.php
+++ dp-devel/tools/project_manager/show_project_wordcheck_usage.php
@@ -13,8 +13,6 @@ $projectid = $_GET["projectid"];
 
 enforce_edit_authorization($projectid);
 
-$roundIDs=_getRoundIDs();
-
 $title=_("WordCheck Project Usage");
 $no_stats = 1;
 theme($title,"header");
@@ -25,56 +23,15 @@ echo_stylesheet();
 echo "<h1>$title</h1>";
 echo "<h2>" . get_project_name($projectid) . "</h2>";
 
-// get the list of all pages
-$res = mysql_query( "SELECT image FROM $projectid ORDER BY image ASC") or die(mysql_error());
-while($result = mysql_fetch_assoc($res)) {
-    $page_usage[$result["image"]]=array();
-}
-mysql_free_result($res);
-
 // load the events
-$earliestTimestamp = time();
 $events = load_wordcheck_events($projectid);
 if(is_array($events)) {
     // parse the events complex array
     foreach( $events as $event) {
-        list($time,$round,$page,$proofer,$words,$corrections)=$event;
-        $page_usage[$page][$round]++;
-        if($earliestTimestamp > $time) $earliestTimestamp = $time;
+        list($time,$roundID,$page,$proofer,$words,$corrections)=$event;
+        @$wordcheck_usage[$page][$roundID]++;
     }
 }
-
-if(0) {
-// Optimal "get it all at once" query that needs an index:
-//   select image, username, round_id from page_events where projectid='projectID45b672b501229' and event_type='saveAsDone' order by image, timestamp;
-// Index needed: (projectid, event_type)
-
-// if the page has been marked saveAsDone, see who did that action last
-$query = "SELECT image, username, round_id FROM page_events WHERE projectid='$projectid' AND event_type='saveAsDone' ORDER BY image, timestamp";
-
-$res = mysql_query( $query ) or die(mysql_error());
-while($result = mysql_fetch_assoc($res)) {
-    @$page_event[$result["image"]][$result["round_id"]]=$result["username"];
-}
-mysql_free_result($res);
-
-} // optimal "all at once" way
-
-if(1) {
-// Query using an existing index but requires us to query per-image:
-//   select username, round_id from page_events where projectid='projectID45b672b501229' and image='000.png' and event_type='saveAsDone' order by timestamp;
-// Index used: (projectid, image)
-
-foreach($page_usage as $page => $val ) {
-    $query = "SELECT username, round_id FROM page_events WHERE projectid='$projectid' AND image='$page' AND event_type='saveAsDone' ORDER BY timestamp";
-
-    $res = mysql_query( $query ) or die(mysql_error());
-    while($result = mysql_fetch_assoc($res))
-        @$page_event[$page][$result["round_id"]]=$result["username"];
-    mysql_free_result($res);
-}
-
-} // end sub-optimal "one at a time" way
 
 echo "<p>" . _("The following table lists the number of times WordCheck was run against a page in each round and the last user to work on the page. Click the proofer's username to compose a private message to them.") . "</p>";
 
@@ -90,36 +47,82 @@ echo "</ul>";
 <table class='wordlisttable'>
 <tr>
     <td class='label'><? echo _("Page"); ?></td>
+    <td class='label'><? echo _("State"); ?></td>
 <?
-    foreach($roundIDs as $roundID)
-        echo "<td class='label'>$roundID</td>";
+    foreach($Round_for_round_id_ as $round)
+        echo "<td class='label'>$round->id</td>";
 ?>
 </tr>
 <?
-foreach($page_usage as $page => $val) {
+
+// build a list of user column names for the select statement
+// this avoids "select *" which unnecessarily pulls in the large
+// text fields
+$roundColumns=array();
+foreach($Round_for_round_id_ as $round) {
+    $roundColumns[]=$round->user_column_name;
+}
+$roundColumns=implode(", ",$roundColumns);
+
+// cycle through all pages, creating rows as we go
+$res = mysql_query( "SELECT image, state, $roundColumns FROM $projectid ORDER BY image ASC" ) or die(mysql_error());
+while($result = mysql_fetch_assoc($res)) {
+    $page = $result["image"];
     echo "<tr>";
     echo "<td>" . recycle_window_link("displayimage.php?project=$projectid&amp;imagefile=$page",$page,"pageView") . "</td>";
-    foreach($roundIDs as $roundID) {
-        $timesChecked = $val[$roundID];
-        $lastProofer = $page_event[$page][$roundID];
+    echo "<td>" . $result["state"] . "</td>";
+
+    // get the current round for the page
+    $currentRound = get_Round_for_page_state($result["state"]);
+
+    // foreach round print out the available info
+    foreach($Round_for_round_id_ as $round) {
+        $roundID = $round->id;
+
+        // the ' + 0' forces timesChecked to be numeric
+        $timesChecked = $wordcheck_usage[$page][$roundID] + 0;
+        $lastProofer = $result[$round->user_column_name];
+
+        // determine if the page has been marked 'Done' or not
+        // If one of the following is true, its done:
+        // * if the page is in a previous round
+        // * if the page is in the current round in the done state
+        $pageIsDoneInRound =
+            $currentRound->round_number > $round->round_number ||
+            $currentRound->page_save_state == $result["state"];
+
         if(!empty($lastProofer))
             $lastProoferLink = private_message_link($lastProofer);
         
-        if(!empty($lastProofer) && $timesChecked==0) {
-            $class = "class='noWC'";
-            $data = $lastProoferLink;
-        } elseif(!empty($lastProofer) && $timesChecked>0) {
+        $class = $data = "";
+        // if there is no proofer's name, the round was likely skipped
+        // or the page has never been worked on
+        if(empty($lastProofer)) {
+            // placeholder
+        }
+        // if the page is finished and WordCheck'd
+        elseif($pageIsDoneInRound && $timesChecked>0) {
             $class = "class='WC'";
             $data = "$timesChecked: $lastProoferLink";
-        } else { 
+        }
+        // if the page is finished but was not ran through WordCheck
+        elseif($pageIsDoneInRound && $timesChecked==0) {
+            $class = "class='noWC'";
+            $data = "$timesChecked: $lastProoferLink";
+        }
+        // the page has been worked on, but not finished
+        else {
             $class = "";
-            $data = "";
+            $data = "$timesChecked: $lastProoferLink";
         }
 
         echo "<td $class>$data</td>";
     }
+
     echo "</tr>\n";
 }
+mysql_free_result($res);
+
 ?>
 </table>
 <?
@@ -152,18 +155,6 @@ function echo_stylesheet() {
     </style>
 <?
 }
-
-function _getRoundIDs() {
-    global $Round_for_round_number_;
-
-    $roundNames = array();
-    foreach($Round_for_round_number_ as $round) {
-        $roundNames[] = $round->id;
-    }
-
-    return $roundNames;
-}
-
 
 // vim: sw=4 ts=4 expandtab
 ?>
