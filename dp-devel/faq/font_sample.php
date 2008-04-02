b4031998adba50696dec3600bec4fe804b3c6404
cpeel <>
2008-04-02 00:46:46 +0000

    Complete rewrite of page without changing the original functionality.
    New page uses _() correctly, no longer uses <font/> tags, does not allow
    selecting of fonts with no corresponding sample images, and has correct
    link to the wiki page.

--- dp-devel/faq/font_sample.php
+++ dp-devel/faq/font_sample.php
@@ -5,156 +5,117 @@ include_once($relPath.'connect.inc');
 include_once($relPath.'theme.inc');
 include_once($relPath.'prefs_options.inc');
 new dbConnect();
+
+/* TODO: 
+    * not all proofreading fonts have images
+    * code doesn't handle compare_font having spaces in it
+      (which isn't a problem currently but would be if
+      images were added for fonts with spaces)
+*/
+
+$title = _("Proofreading Font Comparison");
 $no_stats=1;
-theme(_('Custom Font Comparison'),'header');
+theme($title,'header');
 
-// if they're not logged in use arial
+// load all fonts into an array except BROWSER_DEFAULT_STR,
+// Monospaced, and DPCustomMono2 which are not selectable
+// because they are either not a specific font or because
+// it's DPCM2
+$selectable_fonts = array();
+foreach($f_f as $font)
+{
+    if($font == BROWSER_DEFAULT_STR ||
+        $font == "Monospaced" ||
+        $font == "DPCustomMono2")
+        continue;
 
-$tfont = 'Arial';
+    $selectable_fonts[] = $font;
+}
+sort($selectable_fonts);
 
-// determine user's current proofreading font, if any
-
+// determine user's current proofreading font, if any and use that as the compare_font
+$proofreading_font="";
 if ($userP['i_layout']==1)
 {
-        $tfonti = $userP['v_fntf'];
-        $tfont = $f_f[$tfonti];
-}  else if ((count($userP) > 0) and ($userP['i_layout']==0)) {
-        $tfonti = $userP['h_fntf'];
-        $tfont = $f_f[$tfonti];
+    $proofreading_fonti = $userP['v_fntf'];
+    $proofreading_font = $f_f[$proofreading_fonti];
 }
-
-$cfont = $tfont;
-
-
-// get font variable from url, if any
-
-if (isset($_GET['compare']) )
+else if ((count($userP) > 0) and ($userP['i_layout']==0))
 {
-        $tfont = $_GET['compare'];
+    $proofreading_fonti = $userP['h_fntf'];
+    $proofreading_font = $f_f[$proofreading_fonti];
+}
+
+// set the default compare_font to the user's proofreading font
+// if it is selectable
+if(in_array($proofreading_font, $selectable_fonts))
+    $compare_font = $proofreading_font;
+else
+    $compare_font = $selectable_fonts[0];
+
+// get any 'compare' variable from URL, if any
+$compare_font = get_enumerated_param( $_GET, 'compare', $compare_font, $selectable_fonts );
+
+// print page header
+echo "<h1>$title</h1>";
+
+echo "<p>" . _("This page demonstrates the differences between <span style='font-family: DPCustomMono2'>DPCustomMono2</span> and other fonts.") . "</p>";
+
+echo "<p>" . sprintf(_("DPCustomMono2 is a font adapted by DP's own big_bill, based on the suggestions and ideas of many experienced proofreaders. Using DPCustomMono2 as your proofreading font can help you find mistakes. You can change the font that you use for proofreading in your <a href='%s'>preferences</a>."), "$code_url/userprefs.php") . "</p>";
+
+if(!empty($proofreading_font))
+    echo "<p>" . sprintf(_("Your current proofreading font is <b>%s</b>."), $proofreading_font) . "</p>";
+
+echo "<p style='font-family: DPCustomMono2'>" . _("If you already have DPCustomMono2 installed, you will see this paragraph in that typeface. If this paragraph's font doesn't look radically different to that of the paragraph above, you can download DPCustomMono2 from <a href='DPCustomMono2.ttf'>here</a> (right click the link, and choose Save Target As..) After you have installed the font please refresh this page to make sure DPCustomMono2 is installed correctly.") . "</p>";
+
+echo "<p>" . sprintf(_("For more information on installing and using the font, see the <a href='http://www.pgdp.net/wiki/Installing_DPCustomMono'>Installing DPCustomMono</a> wiki page at pgdp.net."),"$code_url/userprefs.php") . "</p>";
+
+echo "<hr width='70%'>";
+
+// build a list of fonts that we have comparison images for
+$sample_font_links=array();
+foreach ($selectable_fonts as $font)
+{
+    // don't print a link if we don't have an image for the font
+    if(is_file("$code_dir/faq/images/${font}_A.gif"))
+    {
+        if($compare_font == $font)
+            $sample_font_links[] = "<span style='font-family: $font'>$font</span>";
+        else
+            $sample_font_links[] = "<a style='font-family: $font' href='?compare=$font'>$font</a>";
+    }
 }
 
 
-// use this font, unless it's DPCM2
+echo "<p>" . _("Select a font from the list below to see a sample text in the desired font compared to the sample text in DPCustomMono2.") . "</p>";
 
-if ($tfont != 'DPCustomMono2') {
-	echo "<FONT FACE='$tfont'>";
-} else {
-	echo "<FONT FACE='Times New Roman, Times, serif'>";
-}
+echo "<p>" . implode(' | ', $sample_font_links) . "</p>";
 
-if ($cfont != 'DPCustomMono2') {
-    	$DPCM = 0;
-} else {
-	$DPCM = 1;
-}
+// print out the comparison images
+echo "<table class='fontcompare'>";
+echo "<tr>";
+echo "<th>" . sprintf(_("Selected Font (%s)"), $compare_font) . "</th>";
+echo "<th>" . _("DPCustomMono2") . "</th>";
+echo "</tr>";
 
+echo "<tr>";
+echo "<td><img src='images/${compare_font}_A.gif'></td>";
+echo "<td><img src='images/DPCustomMono2_A.gif'></td>";
+echo "</tr>";
 
+echo "<tr>";
+echo "<td><img src='images/${compare_font}_B.gif'></td>";
+echo "<td><img src='images/DPCustomMono2_B.gif'></td>";
+echo "</tr>";
 
-// echo text
+echo "<tr>";
+echo "<td><img src='images/${compare_font}_C.gif'></td>";
+echo "<td><img src='images/DPCustomMono2_C.gif'></td>";
+echo "</tr>";
 
-
-
-$exp_text = "<H1><font face='DPCustomMono2'>DPCustomMono2</font>";
-
-if (($tfont != BROWSER_DEFAULT_STR) AND 
-	($tfont != 'DPCustomMono2') AND
-	($tfont != _("Monospaced"))) {
-
-$exp_text .= " vs. $tfont";
-}
-
-
-$exp_text .=
-	"</H1>
-        <P>"
-        ._("DPCustomMono2 is a font adapted by DP's own big_bill, based on the suggestions and ideas of many experienced proofreaders, that helps proofreaders find mistakes. ")
-        ._("You can change the font that you use for proofreading in your")
-        ." <a href='$code_url/userprefs.php'>"._("preferences")."</a>. "
-        ._("Here are some samples that compare DPCustomMono2 to other fonts. ")
-        ._("For information on installing and using the font, read the"). 
-	" <a href='$forums_url/viewtopic.php?p=31521#31521'>".
-	_("DPWiki post.")
-        ."</a></P>";
-
-echo $exp_text;
-
-$exp_text ="
-        <P>
-        <FONT FACE='DPCustomMono2, $tfont'>";
-
-if ($DPCM) {
-
-$exp_text .= _("You currently have DPCustomMono2 selected as your default proofreading font.")." ";
-
-} 
-
-$exp_text .= 
-	 _("If you already have the font installed, you will see this paragraph in the DPCustomMono2 typeface. ")
-        ._("If this paragraph's font doesn't look radically different to that of the paragraph above, you can download DPCustomMono2 from")
-        ." <a href='DPCustomMono2.ttf'>"._("here")."</a> "
-	._("(right click the link, and choose Save Target As...). ")
-	._("After you have installed the font please refresh this page to make sure DPCustomMono2 is installed correctly. ");
-
-if ($DPCM) {
-
-$exp_text .= _("If DPCustomMono2 is displayed correctly in this paragraph, then please browse through the gallery of font comparisons below to remind yourself why it's so useful.")." ";
-
-} 
-
-
-if (($tfont == BROWSER_DEFAULT_STR) OR ($tfont == _("Monospaced"))) {
-$exp_text .= _("Since you have the non-specific font type of $tfont selected, we don't have any specific comparison images to show you; but we encourage you to browse through the gallery of comparisons to specific fonts from the links below, to see them juxtaposed with DPCustomMono2.");
-
-}
-
-$exp_text .= "</FONT></P>";
-
-echo $exp_text;
-
-
-$first = 1;
-
-foreach ($f_f as $otherfont) {
-
-        if (($otherfont != $tfont) AND 
-		($otherfont != BROWSER_DEFAULT_STR) AND 
-		($otherfont != 'DPCustomMono2') AND
-		($otherfont != _("Monospaced"))) {
-                if (! $first) {
-                        echo " | ";
-                } else {
-                        $first = 0;
-                }
-
-                echo "<font face='$otherfont'><a href='font_sample.php?compare=$otherfont'>$otherfont</a></font>";
-
-        }
-}
-
-
-echo " | <a href='images/Original.gif'>"._("View original image")."</a></P><br><br>\n";
-
-
-if (($tfont != BROWSER_DEFAULT_STR) AND 
-	($tfont != 'DPCustomMono2') AND
-	($tfont != _("Monospaced"))) {
-
-	echo "<hr style='width: 546; text-align: left;'>";
-	echo "<P>";
-	echo _("On this page, the top font is")." <b>$tfont</b>, "._("and the bottom example is").
-        	" <b>DPCustomMono2</b>.</P>";
-
-	echo "
-	        <p><img border='0' src='images/".$tfont."_A.gif'></p>
-        	<p><img border='0' src='images/DPCustomMono2_A.gif' width='588' height='264'></p>
-        	<hr style='width: 546; text-align: left;'>
-	        <p><img border='0' src='images/".$tfont."_B.gif'></p>
-	        <p><img border='0' src='images/DPCustomMono2_B.gif' width='632' height='212'></p>
-	        <hr style='width: 546; text-align: left;'>
-	        <p><img border='0' src='images/".$tfont."_C.gif'></p>
-	        <p><img border='0' src='images/DPCustomMono2_C.gif' width='624' height='190'></p>
-	";
-}
+echo "</table>";
 
 theme("", "footer");
+
+// vim: sw=4 ts=4 expandtab
 ?>
