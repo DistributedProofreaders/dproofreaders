f8635c5dc24549cec2b32275e9047026d485350f
jmdyck <>
2008-05-13 06:12:50 +0000

    indentation and misc whitespace tweaks

--- dp-devel/tools/project_manager/show_image_sources.php
+++ dp-devel/tools/project_manager/show_image_sources.php
@@ -10,7 +10,7 @@ include_once($relPath.'project_states.inc');
 include_once($relPath.'dpsql.inc');
 
 // the user_is functions don't work unless this has been executed previously!
-// it's in dp_main.inc, but we also want this page to be accessible to 
+// it's in dp_main.inc, but we also want this page to be accessible to
 // people who aren't logged in, so we can't include that file,
 // which requries a visitor to log in
 dpsession_resume();
@@ -55,37 +55,39 @@ if (!isset($_GET['name']))
     }
 
     if ($logged_in) {
-        $query = "SELECT
-                    display_name as '"._("Name in<br>Dropdown")."',
-                    case when length(url) > 5 then concat('<a href=\"',url,'\">',full_name,'</a>') 
-                        else full_name end as '"._("Full Name")."',
-                    public_comment as '"._("Description")."',
-                    case when count(distinct  projectid) = 0 then '<center>0</center>' 
-                        else concat('<center><a href=\"show_image_sources.php?name=',code_name,'&which=ALL\" >',count(distinct projectid),'</a></center>') end as '"._("Works In Progress")."', 
-                    case when isnull(sum(".SQL_CONDITION_GOLD.")) then '<center>0</center>' 
-                         when sum(".SQL_CONDITION_GOLD.") = 0 then '<center>0</center>' 
-                        else concat('<center><a href=\"show_image_sources.php?name=',code_name,'\" >',sum(".SQL_CONDITION_GOLD."),'</a></center>') end as '"._("Works Completed")."',
-                    internal_comment as '"._("Notes")."'
-                FROM image_sources i left join projects p on i.code_name = p.image_source
-                WHERE info_page_visibility >= $min_vis_level
-                GROUP BY code_name
-                ORDER BY display_name";
+        $query = "
+            SELECT
+                display_name as '"._("Name in<br>Dropdown")."',
+                case when length(url) > 5 then concat('<a href=\"',url,'\">',full_name,'</a>')
+                    else full_name end as '"._("Full Name")."',
+                public_comment as '"._("Description")."',
+                case when count(distinct  projectid) = 0 then '<center>0</center>'
+                    else concat('<center><a href=\"show_image_sources.php?name=',code_name,'&which=ALL\" >',count(distinct projectid),'</a></center>') end as '"._("Works In Progress")."',
+                case when isnull(sum(".SQL_CONDITION_GOLD.")) then '<center>0</center>'
+                     when sum(".SQL_CONDITION_GOLD.") = 0 then '<center>0</center>'
+                    else concat('<center><a href=\"show_image_sources.php?name=',code_name,'\" >',sum(".SQL_CONDITION_GOLD."),'</a></center>') end as '"._("Works Completed")."',
+                internal_comment as '"._("Notes")."'
+            FROM image_sources i left join projects p on i.code_name = p.image_source
+            WHERE info_page_visibility >= $min_vis_level
+            GROUP BY code_name
+            ORDER BY display_name";
     } else {
 
-        $query =  " SELECT
-                       case when length(url) > 5 then concat('<a href=\"',url,'\">',full_name,'</a>') 
-                          else full_name end as '"._("Image Source")."',
-                       public_comment as '"._("Description")."',
-                       case when count(distinct  projectid) = 0 then '<center>0</center>' 
-                          else concat('<center><a href=\"show_image_sources.php?name=',code_name,'&which=ALL\" >',count(distinct projectid),'</a></center>') end as '"._("Works In Progress")."', 
-                       case when isnull(sum(".SQL_CONDITION_GOLD.")) then '<center>0</center>' 
-                          when sum(".SQL_CONDITION_GOLD.") = 0 then '<center>0</center>' 
-                          else concat('<center><a href=\"show_image_sources.php?name=',code_name,'\" >',sum(".SQL_CONDITION_GOLD."),'</a></center>') end as '"._("Works Completed")."'
-                    FROM image_sources i left join projects p on i.code_name = p.image_source
-                    WHERE info_page_visibility >= $min_vis_level
-                    GROUP BY code_name
-                    ORDER BY full_name";
-   }
+        $query = "
+            SELECT
+                case when length(url) > 5 then concat('<a href=\"',url,'\">',full_name,'</a>')
+                    else full_name end as '"._("Image Source")."',
+                public_comment as '"._("Description")."',
+                case when count(distinct  projectid) = 0 then '<center>0</center>'
+                    else concat('<center><a href=\"show_image_sources.php?name=',code_name,'&which=ALL\" >',count(distinct projectid),'</a></center>') end as '"._("Works In Progress")."',
+                case when isnull(sum(".SQL_CONDITION_GOLD.")) then '<center>0</center>'
+                    when sum(".SQL_CONDITION_GOLD.") = 0 then '<center>0</center>'
+                    else concat('<center><a href=\"show_image_sources.php?name=',code_name,'\" >',sum(".SQL_CONDITION_GOLD."),'</a></center>') end as '"._("Works Completed")."'
+            FROM image_sources i left join projects p on i.code_name = p.image_source
+            WHERE info_page_visibility >= $min_vis_level
+            GROUP BY code_name
+            ORDER BY full_name";
+    }
 
     dpsql_dump_query( $query);
 
@@ -95,15 +97,15 @@ if (!isset($_GET['name']))
 
     $imso = mysql_fetch_assoc( mysql_query("
         SELECT
-                full_name,
-                display_name,
-                public_comment,
-                internal_comment,
-                info_page_visibility,
-                concat('<a href=\"',url,'\">',url,'</a>') as 'more_info'
+            full_name,
+            display_name,
+            public_comment,
+            internal_comment,
+            info_page_visibility,
+            concat('<a href=\"',url,'\">',url,'</a>') as 'more_info'
         FROM image_sources
         WHERE code_name = '$imso_code'
-       "));
+    "));
 
     $visibility = $imso['info_page_visibility'];
 
@@ -114,24 +116,25 @@ if (!isset($_GET['name']))
     //  2 = also any logged-in user
     //  3 = anyone
 
-	// layout below intended to make logic easier to follow:
-	// see it as a tree lying on it's side, the root node the "or"
-	// on the third line
+    // layout below intended to make logic easier to follow:
+    // see it as a tree lying on it's side, the root node the "or"
+    // on the third line
 
-    $can_see = 	(
-				($visibility == 3) 
-			or 
-				($logged_in 
-					and 	
-							(	$visibility == 2 
-							   or 
-								(user_is_PM() and $visibility == 1 ) 
-							   or 
-								user_is_image_sources_manager()
-							)
-				)
-			);
-                                         
+    $can_see = (
+                ($visibility == 3)
+            or
+                ($logged_in
+                    and
+                    (
+                        $visibility == 2
+                        or
+                        (user_is_PM() and $visibility == 1)
+                        or
+                        user_is_image_sources_manager()
+                    )
+                )
+            );
+
     if ($can_see) {
 
         if (isset($_GET['which']))
@@ -140,24 +143,24 @@ if (!isset($_GET['name']))
 
             switch ($which)
             {
-               case 'ALL':
-                   $adjective = _("All");
-                   $where_cls = " ";
-                   $other_label = _("Completed");
-                   $other_which = 'DONE';
-                   $tense = _("being produced");
-                   $other_tense = _("produced");
-                   break;
-               case 'DONE':
-               default:
-                   $adjective = _("Completed");
-                   $where_cls = " AND  ".SQL_CONDITION_GOLD." ";
-                   $other_label = _("All");
-                   $other_which = 'ALL';
-                   $tense = _("produced");
-                   $other_tense = _("being produced");
+                case 'ALL':
+                    $adjective = _("All");
+                    $where_cls = " ";
+                    $other_label = _("Completed");
+                    $other_which = 'DONE';
+                    $tense = _("being produced");
+                    $other_tense = _("produced");
+                    break;
+                case 'DONE':
+                default:
+                    $adjective = _("Completed");
+                    $where_cls = " AND  ".SQL_CONDITION_GOLD." ";
+                    $other_label = _("All");
+                    $other_which = 'ALL';
+                    $tense = _("produced");
+                    $other_tense = _("being produced");
             }
-                  
+
         } else {
             $adjective = _("Completed");
             $where_cls = " AND  ".SQL_CONDITION_GOLD." ";
@@ -178,25 +181,25 @@ if (!isset($_GET['name']))
         echo "<br><h3>$sub_title</h3>\n";
 
         if ($logged_in) {
-		echo "<br><h4>$details</h4>\n";
-	 }
-       echo "\n<h4>$more_info</h4>\n\n";
+            echo "<br><h4>$details</h4>\n";
+        }
+        echo "\n<h4>$more_info</h4>\n\n";
 
 
 
         echo  "<a href='show_image_sources.php?name=".$imso_code.
-              "&which=".$other_which."'>"._("See list of")." ".$other_label." ".
-              _("Ebooks")." ".$other_tense." "._("from scans from")." ".$imso['full_name']."</a><br><br>";
+            "&which=".$other_which."'>"._("See list of")." ".$other_label." ".
+            _("Ebooks")." ".$other_tense." "._("from scans from")." ".$imso['full_name']."</a><br><br>";
 
         echo  "<a href='show_image_sources.php'>"._("Back to the full listing of all Image Sources")."</a><br><br>";
 
         dpsql_dump_query("
             SELECT
-               concat('<a href=\"$code_url/project.php?id=',projectid,'\">',nameofwork,'</a>') as "._('Title').",
-               authorsname as "._('Author').",
-               genre as "._('Genre').",
-               language as "._('Language').",
-               IF(postednum, concat('<a href=\"http://www.gutenberg.net/etext/',postednum,'\">',postednum,'</a>'),'In Progress') as '"._('PG Number<br>and Link')."'
+                concat('<a href=\"$code_url/project.php?id=',projectid,'\">',nameofwork,'</a>') as "._('Title').",
+                authorsname as "._('Author').",
+                genre as "._('Genre').",
+                language as "._('Language').",
+                IF(postednum, concat('<a href=\"http://www.gutenberg.net/etext/',postednum,'\">',postednum,'</a>'),'In Progress') as '"._('PG Number<br>and Link')."'
             FROM projects
             WHERE image_source = '".$imso_code."' ".$where_cls."
             ORDER BY nameofwork
@@ -217,4 +220,6 @@ if (!isset($_GET['name']))
 echo "<br>\n";
 
 theme("","footer");
+
+// vim: sw=4 ts=4 expandtab
 ?>
