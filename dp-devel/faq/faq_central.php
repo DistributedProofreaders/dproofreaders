941c0b0cc7a4501816e4f74030def09676d5717c
donovang <>
2008-08-02 20:58:34 +0000

    Minor content updates, rearrangment of items/categories in FAQ Central from acunning40.

--- dp-devel/faq/faq_central.php
+++ dp-devel/faq/faq_central.php
@@ -19,11 +19,10 @@ theme('FAQ Central','header', $theme_args);
 ?>
 
 <h1>FAQ Central</h1>
-<h3 align="right">Version 1.9, released June 3rd, 2005</h3>
-
 
 <p>This page contains links to all the Documentation and FAQ (Frequently Asked Questions)
-   files about the Distributed Proofreaders website.</p>
+   files about the <? echo "$site_name"; ?> website.</p>
+<p><small>Updated August 2, 2008</small></p>
 
 <?
 show_news_for_page("FAQ");
@@ -34,29 +33,31 @@ show_news_for_page("FAQ");
 <td width='5'>&nbsp;</td>
   <td width='49%' valign='top'>
   <table width='100%' cellpadding='4'>
-    <tr>
-      <td class='faqheader'>
-        Proofreading and Formatting
-      </td>
-    </tr>
-    <tr>
-     <td>
-
+  <tr>
+   <td class='faqheader'>&nbsp;Introductory Information</td>
+  </tr>
+  <tr>
+    <td>
    <p><a href="ProoferFAQ.php">Beginning Proofreader's FAQ </a><br>
    <font size="-1">Introduction to the site, general overview, beginner's questions.</font></p>
 
-  <p><a href="longabsent.php">Transition to Four Rounds</a><br>
-  <font size="-1">An overview of the site's recent change from two to four rounds,
+  <p><a href="longabsent.php">Welcome Back, Alumni</a><br>
+  <font size="-1">An overview of site changes from the last few years,
   including the distinction between proofreading and formatting.</font></p>
 
-  <p><a href="../quiz/start.php">Proofreading &amp; Formatting Quizzes &amp; Tutorials </a><br>
-  <font size="-1">Try the proofreading and formatting quizzes and tutorials.
-  They are a great walk through the basic Guidelines for beginners and an excellent refresher for old hands.</font></p>
-
-  <p><a href="proofing_summary.pdf">Proofreading</a> and <a href="formatting_summary.pdf">Formatting</a> Summaries</a><br>
-  <font size="-1">Printable (.pdf) two-page summaries of the most commonly needed
-  proofreading and formatting standards from the Proofreading and Formatting Guidelines, done as big examples! </font></p>
-
+  <p><a href="privacy.php"><? echo "$site_abbreviation"; ?> Privacy Policy</a><br>
+  <font size="-1">The <? echo "$site_name"; ?> Privacy Policy
+   that you saw when you first registered at this site.</font></p>
+    </td>
+  </tr>
+ </table>
+ <br />
+  <table width='100%' cellpadding='4'>
+  <tr>
+   <td class='faqheader'>&nbsp;Proofreading</td>
+  </tr>
+  <tr>
+    <td>
   <p><a href="proofreading_guidelines.php">Proofreading Guidelines</a><br>
   <span style='font-size: smaller; color: #335;'>Also available in 
      <a href='proofreading_guidelines_francaises.php'>French</a>,
@@ -66,56 +67,68 @@ show_news_for_page("FAQ");
      <a href='proofreading_guidelines_german.php'>German</a>.</span><br>
   <font size="-1">The details of the guidelines we use for proofreading documents.</font></p>
 
+  <p><a href="proofing_summary.pdf">Proofreading Summary</a><br>
+  <font size="-1">Printable (.pdf) two-page summary of the most commonly needed
+  proofreading standards from the Proofreading Guidelines, done as a big example! </font></p>
+
+  <p><a href="../quiz/start.php?show_only=PQ">Proofreading Quizzes &amp; Tutorials</a><br>
+  <font size="-1">Try the proofreading quizzes and tutorials.
+  They are a great walk through the basic Guidelines for beginners and an excellent refresher for old hands.</font></p>
+
+    </td>
+  </tr>
+ </table>
+ <br />
+  <table width='100%' cellpadding='4'>
+  <tr>
+   <td class='faqheader'>&nbsp;Formatting</td>
+  </tr>
+    <tr>
+     <td>
   <p><a href="document.php">Formatting Guidelines</a><br>
   <span style='font-size: smaller; color: #335;'>Also available in 
      <a href='formatting_guidelines_francaises.php'>French</a>,
      <a href='formatting_guidelines_portuguese.php'>Portuguese</a>,
      <a href='formatting_guidelines_dutch.php'>Dutch</a>, and
      <a href='formatting_guidelines_german.php'>German</a>.</span><br>
-  <font size="-1">The full details of the guidelines we use for proofreading and formatting documents.</font></p>
+  <font size="-1">The full details of the guidelines we use for formatting documents.</font></p>
+
+  <p><a href="formatting_summary.pdf">Formatting Summary</a><br>
+  <font size="-1">Printable (.pdf) two-page summary of the most commonly needed
+  formatting standards from the Formatting Guidelines, done as a big example! </font></p>
+
+  <p><a href="../quiz/start.php?show_only=FQ">Formatting Quiz</a><br>
+  <font size="-1">Try the formatting quiz.
+  It is a great walk through the basic Guidelines for beginners and an excellent refresher for old hands.</font></p>
+
+  </td>
+  </tr>
+ </table>
+ <br />
+  <table width='100%' cellpadding='4'>
+  <tr>
+   <td class='faqheader'>&nbsp;<? echo "$site_abbreviation"; ?> Tools</td>
+  </tr>
+  
+  <!-- As well as the font, this section should also hold any PP 
+       tools, &c. that are stored on site -->
+
+  <tr>
+  <td>
+  <p><a href="font_sample.php">DP Custom Proofreading Font</a><br>
+  <font size="-1">Sample and download the custom DP font.</font></p>
+
+  <p><a href="wordcheck-faq.php">WordCheck FAQ</a><br>
+  <font size="-1">Information on the WordCheck engine and interface.</font></p>
 
   <p><a href="prooffacehelp.php?i_type=0">Standard Proofreading Interface Help</a><br>
   <font size="-1">Help for the Standard Proofreading Interface.</font></p>
 
   <p><a href="prooffacehelp.php?i_type=1">Enhanced Proofreading Interface Help</a><br>
   <font size="-1">Help for the Enhanced Proofreading Interface.</font></p>
-
-  <p><a href="<?=$code_url?>/tools/proofers/for_mentors.php">Mentors' Page</a><br>
-  <font size="-1">A page detailing currently available mentor projects.</font></p>
-
   </td>
   </tr>
- </table>
- <br />
- <table width='100%' cellpadding='4'>
-  <tr>
-   <td class='faqheader'>&nbsp;Suggestions, Bugs, and Development</td>
-  </tr>
-  <tr>
-    <td>
-
-  <p><a href="<?=$code_url?>/tasks.php">Task Center</a><br>
-  <font size="-1">Here you will find a list of feature requests and bugs. 
-  You may add tasks after searching to see that the issue isn't already there.</font></p>
-
-  <p><a href="dev_process.php">Development Process</a><br>
-  <font size="-1">Information and guidelines for developers.</font></p>
-  </tr>
- </table>
-
- <br />
-  <table width='100%' cellpadding='4'>
-  <tr>
-   <td class='faqheader'>&nbsp;Privacy</td>
-  </tr>
-  <tr>
-  <td>
-  <p><a href="privacy.php">DP Privacy Policy</a><br>
-  <font size="-1">The Distributed Proofreaders Privacy Policy
-   that you saw when you first registered at this site.</font></p>
-  </td>
-  </tr>
- </table>
+  </table>
 
 </td>
 <td width='5'>&nbsp;</td>
@@ -139,6 +152,20 @@ show_news_for_page("FAQ");
 
   <p><a href="scanning.php">Scanning FAQ</a><br>
   <font size="-1">Basic information on scanners and how to use them, based on our experiences.</font></p>
+
+  </td>
+  </tr>
+ </table>
+ <br />
+ <table width='100%' cellpadding='4'>
+  <tr>
+   <td class='faqheader'>&nbsp;Mentoring</td>
+  </tr>
+  <tr>
+    <td>
+  <p><a href="<?=$code_url?>/tools/proofers/for_mentors.php">Mentors' Page</a><br>
+  <font size="-1">A page detailing currently available mentor projects.</font></p>
+
   </tr>
  </table>
 <br />
@@ -150,10 +177,10 @@ show_news_for_page("FAQ");
     <td>
 
   <p><a href="post_proof.php">Post-Processing FAQ</a><br>
-  <font size="-1">Information for new and aspiring Post-Processors.  (Post-Processors are people who do
-      all the processing  of a particular project after it has been proofread
+  <font size="-1">Information for new and aspiring Post-Processors.  (Post-Processors
+      process a particular project after it has been proofread and formatted
       on this site (combining all the pages, making them consistent, fixing problems,
-      and submitting it to Project Gutenberg).</font></p>
+      and submitting it to Project Gutenberg).)</font></p>
 
   <p><a href="ppv.php">Post-Processing Verification Guidelines</a><br>
   <font size="-1">Information for Post-Processing Verifiers.</font></p>
@@ -161,24 +188,22 @@ show_news_for_page("FAQ");
  </table>
  
  <br />
-  <table width='100%' cellpadding='4'>
+ <table width='100%' cellpadding='4'>
   <tr>
-   <td class='faqheader'>&nbsp;DP Tools</td>
+   <td class='faqheader'>&nbsp;Suggestions, Bugs, and Development</td>
   </tr>
-  
-  <!-- As well as the font, this section should also hold any PP 
-       tools, &c. that are stored on site -->
-
   <tr>
-  <td>
-  <p><a href="font_sample.php">DP Custom Proofreading Font</a><br>
-  <font size="-1">Sample and download the custom DP font.</font></p>
+    <td>
 
-  <p><a href="wordcheck-faq.php">WordCheck FAQ</a><br>
-  <font size="-1">Information on the WordCheck engine and interface.</font></p>
-  </td>
+  <p><a href="<?=$code_url?>/tasks.php">Task Center</a><br>
+  <font size="-1">Here you will find a list of feature requests and bugs. 
+  You may add tasks after searching to see that the issue isn't already there.</font></p>
+
+  <p><a href="dev_process.php">Development Process</a><br>
+  <font size="-1">Information and guidelines for developers.</font></p>
   </tr>
-  </table>
+ </table>
+
 <br />
   <table width='100%' cellpadding='4'>
   <tr>
@@ -187,7 +212,7 @@ show_news_for_page("FAQ");
   <tr>
   <td>
   <p><a href="http://www.pgdp.net/mailman/listinfo">Mailing Lists for Users</a><br>
-  <font size="-1">Information about the email lists DP has available for users.</font></p>
+  <font size="-1">Information about the email lists <? echo "$site_abbreviation"; ?> has available for users.</font></p>
   </td>
   </tr>
  </table>
@@ -200,7 +225,7 @@ show_news_for_page("FAQ");
   <tr>
   <td>
   <p><a href="<?=$PG_faq_url?>">Project Gutenberg FAQ</a><br>
-  <font size="-1">The <i>massive</i> FAQ from our parent site,
+  <font size="-1">The <i>massive</i> FAQ from 
       <a href="<?=$PG_home_url?>">Project Gutenberg</a>.</font></p>
   </td>
   </tr>
@@ -215,9 +240,9 @@ show_news_for_page("FAQ");
 <tr><td width='10'>&nbsp;</td>
     <td width='100%' align="center"><font face='verdana, helvetica, sans-serif' size='1'>
         Return to:
-        <a href="..">Distributed Proofreaders home page</a>,
+        <a href=".."><? echo "$site_name"; ?> home page</a>,
         &nbsp;&nbsp;&nbsp;
-        <a href="faq_central.php">DP FAQ Central page</a>,
+        <a href="faq_central.php"><? echo "$site_abbreviation"; ?> FAQ Central page</a>,
         &nbsp;&nbsp;&nbsp;
         <a href="<? echo $PG_home_url; ?>">Project Gutenberg home page</a>.
         </font>
--- dp-devel/faq/longabsent.php
+++ dp-devel/faq/longabsent.php
@@ -12,15 +12,15 @@ theme('Welcome Back, Alumni','header');
 $Workflow_Change_Announcement_URL = make_forum_url('t',0,15170);
 ?>
 
-<h1 align="center">Welcome Back to Distributed Proofreaders, Alumni</h1>
+<h1>Welcome Back to Distributed Proofreaders, Alumni</h1>
 
-<h3 align="center">Version 1.2, updated March 10, 2007</h3>
+<p><small>Version 1.2.1, updated August 2, 2008</small></p>
 
 <table border="0" cellspacing="0" width="100%" summary="Proofreading Guidelines">
   <tbody>
   <tr>
     <td width="1" bgcolor="silver">&nbsp;</td>
-    <td bgcolor="silver" align="center"><font size="+2"><b>Table of Contents</b></font></td>
+    <td bgcolor="silver" align="center"><h2 style="margin-top:0; margin-bottom:0;">Table of Contents</h2></td>
   </tr>
  </tbody>
 </table>
@@ -69,13 +69,16 @@ $Workflow_Change_Announcement_URL = make_forum_url('t',0,15170);
 </p>
 
 <p>Most proofreaders on the new site have started in the P1 round. After volunteers 
-   have completed the requirements listed on the Activity Hub and/or in the
-   <a href="<?=$Workflow_Change_Announcement_URL?>">Workflow Change
-   Announcement Thread</a>, they can move into P1, P2, P3, and F2 rounds should they 
+   have completed the requirements listed on each round's page,
+   they can move into P2, P3, F1, and F2 rounds should they 
    wish to do so. Each round's page can be visited from the Activity Hub, and the 
    requirements for proofreading in each round (for example, the number of pages 
-   completed well in other rounds, time spent on site and passes on the quizzes) 
+   completed in other rounds, time spent on site, and passes on the quizzes) 
    are shown on that round's page.
+</p>
+
+<p>The <a href="<?=$Workflow_Change_Announcement_URL?>">Workflow Change
+   Announcement Thread</a> from 2005 contains more details of the changes.
 </p>
 
 <p>The duties for each round are as follows:</p>
@@ -84,7 +87,7 @@ $Workflow_Change_Announcement_URL = make_forum_url('t',0,15170);
 
 <p>The aim of the P rounds is character proofreading: to match letter for letter, 
    punctuation mark for punctuation mark, the original text as if the original 
-   text had no italics, superscript numbers (footnotes), etc.
+   text had no italics, font changes, etc.
 </p>
 
 <p>Thus, proofreaders are responsible, among other things, for the following:</p>
@@ -107,14 +110,13 @@ $Workflow_Change_Announcement_URL = make_forum_url('t',0,15170);
     <li>	formatting the text: for example, adding markup for text style (italics, small caps etc.);</li>
     <li>	adding markup for sidenotes and footnotes;</li>
     <li>	adding markup for poetry or block-quotes; or</li>
-    <li>	unwrapping lines in poetry or indexes, adding lines for chapter headings, etc.</li>
+    <li>	unwrapping lines in poetry or indexes, adding blank lines for chapter headings, etc.</li>
 </ul>
 
 <p>If some formatting has already been included, for example, indented lines in 
-   poetry, the formatting is <b>not</b> removed unless it surrounds OCR 
-   garbage such as <tt>&lt;i&gt;</tt> *#4@P,,./<tt>&lt;/i&gt;</tt> . Even if 
-   the formatting is wrong as in, <tt>&lt;i&gt;</tt>i.e<tt>&lt;/i&gt;</tt>., 
-   please leave it to the formatters to repair and concentrate on the text.
+   poetry, the formatting can be left in place or removed. Even if 
+   the formatting is wrong as in <tt>&lt;i&gt;</tt>i.e<tt>&lt;/i&gt;</tt>., 
+   please do not move it.  Leave it to the formatters to repair and concentrate on the text.
 </p>
 
 <p>Please note that the above is by no means a comprehensive list. The P rounds 
@@ -124,13 +126,13 @@ $Workflow_Change_Announcement_URL = make_forum_url('t',0,15170);
    <a href="<?=$code_url?>/quiz/start.php?show_only=PQ">Proofreading Quiz</a>.
 </p>
 
-<p>P1 proofreaders should run the WordCheck on their pages.</p>
+<p>P1 proofreaders should run WordCheck on their pages.</p>
 
 <h2><a name="p2">P2 Proofreading Duties</a></h2>
 
 <p>This is the close reading of pages already read in P1 for any small errors 
    that might have been missed in the P1 round. Otherwise, the responsibilities 
-   for this round are as per the P1 round. P2 proofreaders are required to run the
+   for this round are as per the P1 round. P2 proofreaders are required to run
    WordCheck on their pages.
 </p>
 
@@ -139,7 +141,7 @@ $Workflow_Change_Announcement_URL = make_forum_url('t',0,15170);
 <p>This is the very close examination of pages already read in P1 and/or P2 for any
    remaining difficult-to-find errors which may have been missed in the previous 
    proofreading round(s). Otherwise, the responsibilities for this round are identical
-   to the earlier proofreading rounds. P3 proofreaders are required to run the WordCheck
+   to the earlier proofreading rounds. P3 proofreaders are required to run WordCheck
    on their pages.
 </p>
 
@@ -148,28 +150,27 @@ $Workflow_Change_Announcement_URL = make_forum_url('t',0,15170);
 
 <p>The aim of the F rounds is to insert all markup and formatting: for example, 
    footnotes and sidenotes markup, text style markup (italics, small caps, etc.), 
-   returns before and after chapter headings, and so on. Formatters arrange the 
-   text on the page as required: indenting poetry, inserting the correct blank 
-   lines for chapter headings, and new paragraph pages, etc. Formatters are not 
+   blank lines before and after chapter headings, and so on. Formatters arrange the 
+   text on the page as required: indenting poetry, aligning tables, etc. Formatters are not 
    responsible for finding and fixing scannos or doing a letter-by-letter comparison 
    of the text, although they are encouraged to fix anything that catches their eye. 
-   The F rounds are currently continuing to use the previous 
-   <a href="<?=$code_url?>/faq/document.php">Guidelines</a> with a few
+   The F rounds use the <a href="<?=$code_url?>/faq/document.php">Formatting Guidelines</a>,
+   which are similar to the previous guidelines, with a few
    modifications such as the addition of small caps notation. There is a new 
    <a href="<?=$code_url?>/quiz/start.php?show_only=FQ">Formatting Quiz</a>.
 </p>
 
 <h2><a name="counts">Page Counts</a></h2>
 
-<p>When the site was re-launched in June 2006, all users' page counts were saved. Everybody has 
+<p>When the site was re-launched in June 2005, all users' page counts were saved
+   as an "R*" page count (R* representing the old R1 and R2 rounds). Everybody has 
    started with a zero page count on the new site. User page counts are now kept separately
    in each round.
 </p>
 
 <h2><a name="track">Keeping Track of Your Projects and Difference Watching</a></h2>
 
-<p>At the top of the Activity Hub page, under the DP header and to the left of 
-   &#8220;My Preferences&#8221; there is a tab called &#8220;My Projects&#8221;.
+<p>At the top of the Activity Hub page, under the DP header there is a link called &#8220;My Projects&#8221;.
    Clicking on &#8220;My Projects&#8221; will bring up a list of the books you 
    have worked on. Clicking on a book title will bring up the Project Page window. If 
    you are looking for your diffs pages, there is a new feature in the Page Details 
@@ -187,11 +188,11 @@ $Workflow_Change_Announcement_URL = make_forum_url('t',0,15170);
    {R} after the title. These are projects that have been returned to the proofreading 
    rounds from the PP pool or from PPer's personal queues. As many of these projects 
    mention on the Project Page, these projects have been proofread under older versions of the Guidelines. 
-   As per above, proofreaders should <b>not</b> remove or adjust any of the formatting. 
+   As per above, proofreaders should <b>not</b> adjust any of the formatting. 
    Formatting checks will be completed by the formatters. Proofreaders should proof 
    according to <a href="<?=$code_url?>/faq/proofreading_guidelines.php">current
    Guidelines</a>, for example, insert page numbers into tables of contents
-   and indexes.
+   and indexes if they were removed previously.
 </p>
 
 <h2><a name="wordcheck">Wordcheck Word Lists and Interface</a></h2>
@@ -223,12 +224,11 @@ review the <a href="<?=$code_url?>/faq/wordcheck-faq.php">Wordcheck FAQ</a> whic
   <td width="10">&nbsp;</td>
   <td width="100%" align="center"><font face="verdana, helvetica, sans-serif" size="1">
      Return to:
-     <a href="..">Distributed Proofreaders home page</a>,
+     <a href=".."><? echo "$site_name"; ?> home page</a>,
      &nbsp;&nbsp;&nbsp;
-
-     <a href="faq_central.php">DP FAQ Central page</a>,
+     <a href="faq_central.php"><? echo "$site_abbreviation"; ?> FAQ Central page</a>,
      &nbsp;&nbsp;&nbsp;
-     <a href="<?=$PG_home_url?>">Project Gutenberg home page</a>.
+     <a href="<? echo $PG_home_url; ?>">Project Gutenberg home page</a>.
      </font>
   </td>
 </tr>
