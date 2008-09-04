b232fe4059833e2f783cf9fdfecf4c03d34312cc
cpeel <>
2008-09-04 16:44:35 +0000

    Improve the user experience by retaining the values they already filled out if one or more of the fields don't validate.

--- dp-devel/accounts/addproofer.php
+++ dp-devel/accounts/addproofer.php
@@ -9,20 +9,60 @@ include($relPath.'connect.inc');
 $db_Connection=new dbConnect();
 include($relPath.'theme.inc');
 
-// This function takes any error message given to it, displays it & terminates the page
-
-function abort_registration( $error )
+function _validate_fields($real_name, $username, $userpass, $userpass2, $email, $email2, $email_updates)
+// Validate the user input fields
+// Returns an empty string upon success and an error message upon failure
 {
-    $header = _("Registration Incomplete");
-    theme($header, "header");
-    echo "$error<br>\n";
-    $please = _("Please hit 'Back' and try again.");
-    echo "$please<br>\n";
-    $back = _("Back");
-    echo "<a href=\"addproofer.php\" onClick=\"history.go(-1); return false;\">".$back."</a>";
-    theme("", "footer");
-    exit;
+    // Make sure that password and confirmed password are equal.
+    if ($userpass != $userpass2)
+    {
+        return _("The passwords you entered were not equal.");
+    }
+
+    // Make sure that email and confirmed email are equal.
+    if ($email != $email2)
+    {
+        return _("The e-mail addresses you entered were not equal.");
+    }
+
+    // Do some validity-checks on inputted username, password, e-mail and real name
+
+    $err = check_username( $username );
+    if ( $err != '' )
+    {
+        return $err;
+    }
+
+    $err = check_email_address( $email );
+    if ( $err != '' )
+    {
+        return $err;
+    }
+
+    if (empty($userpass) || empty($real_name))
+    {
+        return _("You did not completely fill out the form.");
+    }
+
+    // Make sure that the username is not taken by a non-registered user.
+    $query = sprintf("
+        SELECT username
+        FROM users
+        WHERE username='%s'
+        ", mysql_real_escape_string($username));
+    $result = mysql_query ($query);
+    if (mysql_num_rows($result) > 0)
+    {
+        return _("That user name already exists, please try another.");
+    }
+
+    return '';
 }
+
+// ---------------------------------------------------------------------------
+
+// assume there is no error
+$error = "";
 
 $password = isset($_POST['password'])? $_POST['password']: '';
 if ($password=="proofer") {
@@ -37,93 +77,57 @@ if ($password=="proofer") {
     $email2 = $_POST['email2'];
     $email_updates = $_POST['email_updates'];
 
-    // Make sure that password and confirmed password are equal.
-    if ($userpass != $userpass2)
+    $error = _validate_fields($real_name, $username, $userpass, $userpass2, $email, $email2, $email_updates);
+
+    // if all fields validated, create the registration
+    if(empty($error))
     {
-        abort_registration( _("The passwords you entered were not equal.") );
-    }
+        $todaysdate = time();
 
-    // Make sure that email and confirmed email are equal.
-    if ($email != $email2)
-    {
-        abort_registration( _("The e-mail addresses you entered were not equal.") );
-    }
+        $ID = uniqid("userID");
 
-    // Do some validity-checks on inputted username, password, e-mail and real name
+        $digested_password = md5($userpass);
 
-    $err = check_username( $username );
-    if ( $err != '' )
-    {
-        abort_registration( $err );
-    }
+        $query = sprintf("INSERT INTO non_activated_users (id, real_name, username, email, date_created, email_updates, u_intlang, user_password) VALUES ('%s', '%s', '%s', '%s', $todaysdate, '%s', '%s', '%s')", mysql_real_escape_string($ID), mysql_real_escape_string($real_name), mysql_real_escape_string($username), mysql_real_escape_string($email), mysql_real_escape_string($email_updates), mysql_real_escape_string($intlang), mysql_real_escape_string($digested_password));
 
-    $err = check_email_address( $email );
-    if ( $err != '' )
-    {
-        abort_registration( $err );
-    }
+        $result = mysql_query ($query);
 
-    if (empty($userpass) || empty($real_name))
-    {
-        $error = _("You did not completely fill out the form.");
-        abort_registration($error);
-    }
+        if (!$result) {
+            if ( mysql_errno() == 1062 ) // ER_DUP_ENTRY
+            {
+                // The attempted INSERT violated a uniqueness constraint.
+                // The non_activated_users table has only one such constraint,
+                // the PRIMARY KEY on the 'username' column.
+                // Thus, $username duplicates a username value in non_activated_users.
+                $error = _("That username has already been requested. Please try another.");
+            }
+            else
+            {
+                $error = _("Can not initiate user registration.");
+            }
+        } else {
+            // Send them an activation e-mail
+            maybe_activate_mail($email, $real_name, $ID, stripslashes($username), $intlang);
 
-    $todaysdate = time();
+            // Page shown when account is successfully created
 
-    $ID = uniqid("userID");
+            $header = sprintf(_("User %s Registered Successfully"), $username);
+	        theme($header, "header");
 
-    // Make sure that the username is not taken by a non-registered user.
-    $query = sprintf("
-        SELECT username
-        FROM users
-        WHERE username='%s'
-        ", mysql_real_escape_string($username));
-    $result = mysql_query ($query);
-    if (mysql_num_rows($result) > 0) {
-        $error = _("That user name already exists, please try another.");
-        abort_registration($error);
-    }
-
-    $digested_password = md5($userpass);
-
-    $query = sprintf("INSERT INTO non_activated_users (id, real_name, username, email, date_created, email_updates, u_intlang, user_password) VALUES ('%s', '%s', '%s', '%s', $todaysdate, '%s', '%s', '%s')", mysql_real_escape_string($ID), mysql_real_escape_string($real_name), mysql_real_escape_string($username), mysql_real_escape_string($email), mysql_real_escape_string($email_updates), mysql_real_escape_string($intlang), mysql_real_escape_string($digested_password));
-
-    $result = mysql_query ($query);
-
-    if (!$result) {
-        if ( mysql_errno() == 1062 ) // ER_DUP_ENTRY
-        {
-            // The attempted INSERT violated a uniqueness constraint.
-            // The non_activated_users table has only one such constraint,
-            // the PRIMARY KEY on the 'username' column.
-            // Thus, $username duplicates a username value in non_activated_users.
-            $error = _("That username has already been requested. Please try another.");
-        }
-        else
-        {
-            $error = _("Can not initiate user registration.");
-        }
-        abort_registration($error);
-
-    } else {
-        // Send them an activation e-mail
-        maybe_activate_mail($email, $real_name, $ID, stripslashes($username), $intlang);
-
-        // Page shown when account is successfully created
-
-        $header = sprintf(_("User %s Registered Successfully"), $username);
-	theme($header, "header");
-
-        echo sprintf(
-               _("User %s registered successfully. Please check the e-mail being sent to you for further information about activating your account.".
-                 "This extra step is taken so that no-one can register you to the site without your knowledge."),
+            echo sprintf(
+               _("User %s registered successfully. Please check the e-mail being sent to you for further information about activating your account. This extra step is taken so that no-one can register you to the site without your knowledge."),
                $username);
 
+            theme("", "footer");
+            exit();
+        }
     }
 } else {
+    $email_updates = 1;
+}
 
 // This is the portion that shows up when no parameters are given to the file
+// or an error occurs during registration.
 //
 // When users fill the form out below, it will submit the information back
 // to this file & run the above commands.
@@ -144,8 +148,15 @@ if ($password=="proofer") {
     if ( $testing )
     {
 	    echo "<p style='color: red'>";
-	    echo _("Because this is a test site, that message won't be sent to you. Instead, when you hit the 'Send E-mail' button, the message will be displayed on the next screen. At that point, you can copy and paste the confirmation link into your browser's location field. <b>Your account won't be created until you access the confirmation link.</b>");
+	    echo _("Because this is a test site, an email won't be sent to you. Instead, when you hit the 'Send E-mail' button, the email will be displayed on the next screen. At that point, you need to copy and paste the confirmation link into your browser's location field. <b>Your account won't be created until you access the confirmation link.</b>");
 	    echo "</p>";
+    }
+
+    // If the user filled out the form but there was an error during the
+    // data validation, print out the error here and let them resubmit.
+    if(!empty($error))
+    {
+        echo "<p style='color: red; font-weight: bold'>$error</p>";
     }
 
     echo "<center>";
@@ -154,10 +165,10 @@ if ($password=="proofer") {
     echo "<table class='register'>";
     echo "<tr>";
     echo "  <td class='label'>" . _("Real Name") . ":</td>";
-    echo "  <td class='field'><input type='text' maxlength='70' name='real_name' size='20'></td>";
+    echo "  <td class='field'><input type='text' maxlength='70' name='real_name' size='20' value='$real_name'></td>";
     echo "</tr>\n<tr>";
     echo "  <td class='label'>" . _("User Name") . ":</td>";
-    echo "  <td class='field'><input type='text' maxlength='70' name='userNM' size='20'></td>";
+    echo "  <td class='field'><input type='text' maxlength='70' name='userNM' size='20' value='$username'></td>";
     echo "</tr>\n<tr>";
     echo "  <td class='label'>" . _("Password") . ":</td>";
     echo "  <td class='field'><input type='password' maxlength='70' name='userPW' size='20'></td>";
@@ -166,19 +177,19 @@ if ($password=="proofer") {
     echo "  <td class='field'><input type='password' maxlength='70' name='userPW2' size='20'></td>";
     echo "</tr>\n<tr>";
     echo "  <td class='label'>" . _("E-mail Address") . ":</td>";
-    echo "  <td class='field'><input type='text' maxlength='70' name='email' size='20'></td>";
+    echo "  <td class='field'><input type='text' maxlength='70' name='email' size='20' value='$email'></td>";
     echo "</tr>\n<tr>";
     echo "  <td class='label'>" . _("Confirm E-mail Address") . ":</td>";
-    echo "  <td class='field'><input type='text' maxlength='70' name='email2' size='20'></td>";
+    echo "  <td class='field'><input type='text' maxlength='70' name='email2' size='20' value='$email2'></td>";
     echo "</tr>\n<tr>";
     echo "  <td class='label'><b>" . _("E-mail Updates") . ":</td>";
     echo "  <td class='field'>";
-    echo "    <input type='radio' name='email_updates' value='1' checked>" . _("Yes") . "&nbsp;&nbsp;";
-    echo "    <input type='radio' name='email_updates' value='0'>" . _("No");
+    echo "    <input type='radio' name='email_updates' value='1' "; if($email_updates) echo "checked"; echo ">" . _("Yes") . "&nbsp;&nbsp;";
+    echo "    <input type='radio' name='email_updates' value='0' "; if(!$email_updates) echo "checked"; echo ">" . _("No");
     echo "  </td>";
     echo "</tr>\n<tr>";
     echo "  <td bgcolor='#336633' colspan='2' align='center'><input type='submit' value='" . _("Send E-Mail required to activate account") . "'>&nbsp;&nbsp;<input type='reset'></td>";
-    echo "</td></tr></table></form>";
+    echo "</tr></table></form>";
     echo "</center>";
 
     if(file_exists($code_dir.'/faq/'.lang_dir().'privacy.php')) {
@@ -187,7 +198,6 @@ if ($password=="proofer") {
         include($code_dir.'/faq/privacy.php');
     }
 
-}
 theme("", "footer");
 
 // vim: sw=4 ts=4 expandtab
