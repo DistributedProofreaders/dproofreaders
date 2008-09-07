74ab43dee62febc9d856e71c14f6ac4b55a97ffc
jmdyck <>
2008-09-07 18:28:07 +0000

    TaskForm(): Remove pointless (and unbalanced) <font> tag.

--- dp-devel/tasks.php
+++ dp-devel/tasks.php
@@ -536,7 +536,7 @@ function TaskForm($tid) {
     echo "<tr><td><b><font face='Verdana' color='#000000' style='font-size: 11px'>Summary&nbsp;</b>&nbsp;&nbsp;<input type='text' name='task_summary' value='$task_summary' size='60' maxlength='80' style='font-family: Verdana; font-size: 10; border: 1px solid #000000; padding: 0; background-color: #EEF7FF'></td></tr>\n";
     echo "<tr><td width='50%' align='left' valign='top'><table border='0' cellspacing='2' cellpadding='0'>\n";
     echo "<tr><td width='40%' align='right' valign='top'><b><font face='Verdana' color='#000000' style='font-size: 11px'>Task Type</font></b>&nbsp;</td><td width='60%' align='left' valign='top'>\n";
-        dropdown_select('task_type', $task_type, $tasks_array, "");  echo "<font face='Verdana' color='#000000' style='font-size: 11px'></td></tr>\n";
+        dropdown_select('task_type', $task_type, $tasks_array, "");  echo "</td></tr>\n";
     echo "<tr><td width='40%' align='right' valign='top'><b><font face='Verdana' color='#000000' style='font-size: 11px'>Category</font></b>&nbsp;</td><td width='60%' align='left' valign='top'>\n";
         dropdown_select('task_category', $task_category, $categories_array, "");  echo "</td></tr>\n";
     echo "<tr><td width='40%' align='right' valign='top'><b><font face='Verdana' color='#000000' style='font-size: 11px'>Status</font></b>&nbsp;</td><td width='60%' align='left' valign='top'>\n";
