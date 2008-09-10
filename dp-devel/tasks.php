545cabaf0c2c51049585b863322836258c68b8ea
jmdyck <>
2008-09-10 08:31:21 +0000

    onsider the Task Summary, and the various escapings it undergoes as it meanders its way through browsers, HTTP, PHP, and MySQL...
    When a user fills in the Add Task form, the Summary data is "pure" (not
    escaped), and we can assume that any escaping it undergoes when packed
    into an HTTP request is undone at the server either by Apache or PHP.
    But because we have magic_quotes_gpc set, when we pull the data out of
    $_POST['task_summary'] (at line 294), it is backslash-escaped (as if by
    addslashes).  We then push the data through htmlspecialchars() (with the
    default $quote_style, ENT_COMPAT) before we INSERT it into the db. (We
    also push it through another call to addslashes, but that's immediately
    undone by the MySQL parser.) So the data stored in the database is
    effectively:
        htmlspecialchars(addslashes(pure_data), ENT_COMPAT)
    
    When we show the Summary data in the (multiple tasks) Listing page (line
    580 in function ShowTasks), or in the (single task) Details page (line
    683 in function TaskDetails), we just extract it from the database
    and apply stripslashes, which (kind of) undoes the addslashes, leaving
        htmlspecialchars(pure_data, ENT_COMPAT)
    which we then insert into the HTML output. The ampersand-escaping
    ensures that any HTML-special characters (ampersand, less-than,
    greater-than, or double-quote) that the user typed will appear to
    the user as typed.
    
    But when we show the Summary data in the Edit Task page (line 612 in
    function TaskForm), there's an extra call to htmlspecialchars, which
    causes HTML-special characters to appear to the user as their
    ampersand-escaped versions (&amp;, &lt;, &gt;, or &quot;, respectively).
    We fix this bug by removing the extra call. However, this uncovers
    further problems:
    
     -- In the Listing and Details cases, the Summary data was inserted into
        the HTML output as general element content, so the $quote_style
        passed to htmlspecialchars was unimportant. But in the Edit Task
        case, the Summary data is inserted as the value of an attribute
        (of an <input> element), so the $quote_style becomes important.
        As noted above, it was ENT_COMPAT, in which single-quotes are not
        ampersand-escaped (but this was being masked by the bogus call to
        htmlspecialchars(_, ENT_QUOTES) that we just removed). Thus, in
        TaskForm, we either have to "manually" ampersand-escape just those
        single-quotes, or else delimit the attribute value with
        double-quotes (at line 620). (For now, we pick the latter, as it's
        simpler.) Either way, what the user sees is the unescaped special
        characters, and that's what goes into the form submission.
    
     -- But now, when we pull that data out of $_POST (line 328), it will
        only have magic_quote_gpc's implicit slash-escaping, and no
        ampersand-escaping. So we have to push it through htmlspecialchars
        before putting it into the UPDATE command, so that the data in the
        database continues to be
    	htmlspecialchars(addslashes(pure_data), ENT_COMPAT)
        as expected.
    
    These three changes mostly fix this bug. However, there's still the
    nagging problem that if, in the past, someone went into Edit Task and
    entered HTML-special characters in the Summary field, those characters
    would have been saved to the database (and then subsequently inserted
    into Listing and Details pages) without ampersand-escaping. (It looks
    like this has actually happened on pgdp.net.) So we may need to clean
    up some of the task_summary data in the 'tasks' table.
    
    (Really, I think it would be a lot simpler if we stored the "pure data"
    in the database. But that would definitely require tweaking the 'tasks'
    table.)

--- dp-devel/tasks.php
+++ dp-devel/tasks.php
@@ -325,7 +325,7 @@ if (isset($_GET['f']) && $_GET['f'] == "newtask") {
             $result = mysql_query("
                 UPDATE tasks
                 SET
-                    task_summary = '".addslashes($_POST['task_summary'])."',
+                    task_summary = '".addslashes(htmlspecialchars($_POST['task_summary']))."',
                     task_type = ".$_POST['task_type'].",
                     task_category = ".$_POST['task_category'].",
                     task_status = ".$_POST['task_status'].",
@@ -609,7 +609,7 @@ function TaskForm($tid) {
     if (empty($tid)) { $task_os = 0; } else { $task_os = mysql_result($result, 0, "task_os"); }
     if (empty($tid)) { $task_browser = 0; } else { $task_browser = mysql_result($result, 0, "task_browser"); }
     if (empty($tid)) { $task_assignee = 0; } else { $task_assignee = mysql_result($result, 0, "task_assignee"); }
-    if (empty($tid)) { $task_summary = ""; } else { $task_summary = htmlspecialchars(stripslashes(mysql_result($result, 0, "task_summary")),ENT_QUOTES); }
+    if (empty($tid)) { $task_summary = ""; } else { $task_summary = stripslashes(mysql_result($result, 0, "task_summary")); }
     if (empty($tid)) { $task_details = ""; } else { $task_details = stripslashes(mysql_result($result, 0, "task_details")); }
     if (empty($tid)) { $percent_complete = 0; } else { $percent_complete = mysql_result($result, 0, "percent_complete"); }
     if (empty($tid)) { $opened_by = ""; } else { $opened_by = mysql_result($result, 0, "opened_by"); }
@@ -617,7 +617,7 @@ function TaskForm($tid) {
     echo "<form action='tasks.php' method='post'><input type='hidden' name='newtask'>\n";
     if (!empty($tid)) { echo "<input type='hidden' name='task_id' value='$tid'>"; }
     echo "<table cellpadding='2' cellspacing='0' width='100%' bgcolor='#e6eef6' style='border-collapse: collapse; border: 1px solid #CCCCCC; padding: 0'>\n";
-    echo "<tr><td><b><font face='Verdana' color='#000000' style='font-size: 11px'>Summary&nbsp;</b>&nbsp;&nbsp;<input type='text' name='task_summary' value='$task_summary' size='60' maxlength='80' style='font-family: Verdana; font-size: 10; border: 1px solid #000000; padding: 0; background-color: #EEF7FF'></td></tr>\n";
+    echo "<tr><td><b><font face='Verdana' color='#000000' style='font-size: 11px'>Summary&nbsp;</b>&nbsp;&nbsp;<input type='text' name='task_summary' value=\"$task_summary\" size='60' maxlength='80' style='font-family: Verdana; font-size: 10; border: 1px solid #000000; padding: 0; background-color: #EEF7FF'></td></tr>\n";
     echo "<tr><td width='50%' align='left' valign='top'><table border='0' cellspacing='2' cellpadding='0'>\n";
     echo "<tr><td width='40%' align='right' valign='top'><b><font face='Verdana' color='#000000' style='font-size: 11px'>Task Type</font></b>&nbsp;</td><td width='60%' align='left' valign='top'>\n";
         dropdown_select('task_type', $task_type, $tasks_array);  echo "</td></tr>\n";
