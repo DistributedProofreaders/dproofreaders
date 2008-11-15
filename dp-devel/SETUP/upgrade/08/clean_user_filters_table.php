8c784f2ff0255c05a5330bde60e38c3b8047e1f4
cpeel <>
2008-11-15 18:57:19 +0000

    Remove any empty filters from the user_filters table. New code in filter_project_list.inc removes reset filters instead of setting them to empty.

---
+++ dp-devel/SETUP/upgrade/08/clean_user_filters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+$relPath='../../../pinc/';
+include($relPath.'connect.inc');
+new dbConnect();
+
+header('Content-type: text/plain');
+
+echo "Removing empty user filters from user_filters...\n";
+
+$sql = "
+    DELETE FROM user_filters
+        WHERE filtertype like '%_internal' and value = ' AND 1'
+";
+
+echo "$sql\n";
+
+mysql_query($sql) or die( mysql_error() );
+
+$sql = "
+    DELETE FROM user_filters
+        WHERE filtertype like '%_display' and value = ''
+";
+
+echo "$sql\n";
+
+mysql_query($sql) or die( mysql_error() );
+
+echo "\nDone!\n";
+
+// vim: sw=4 ts=4 expandtab
+?>
