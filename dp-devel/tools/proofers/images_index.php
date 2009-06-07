1b752b6c4a42423d374a266e6fd3f4369afcfb29
jmdyck <>
2009-06-07 23:37:32 +0000

    Split the single listing into two, one for page images (including missing ones) and one for non-page images.

--- dp-devel/tools/proofers/images_index.php
+++ dp-devel/tools/proofers/images_index.php
@@ -18,24 +18,53 @@ echo "
     <p><a href='$code_url/project.php?id=$projectid'>", _('Return to project page'), "</a></p>
     <h2>$image_index_str</h2>
     <P>Here are the individual images for this project:</P>
-    <HR>
 ";
+
+$page_image_names = array();
+$res = mysql_query("
+    SELECT image
+    FROM $projectid
+    ORDER BY image
+") or die(mysql_error());
+while ( list($image) = mysql_fetch_row($res) )
+{
+    $page_image_names[] = $image;
+}
 
 chdir("$projects_dir/$projectid");
 $existing_image_names = glob("*.{png,jpg}", GLOB_BRACE);
 
-list_images( $existing_image_names );
+$nonpage_image_names = array_diff($existing_image_names, $page_image_names);
 
-echo "
-    <HR>
-";
+echo "<table border='1'>\n";
+echo "<tr>\n";
+echo "<td valign='top'>\n";
+list_images( $page_image_names, TRUE );
+echo "</td>\n";
+echo "<td valign='top'>\n";
+list_images( $nonpage_image_names, FALSE );
+echo "</td>\n";
+echo "</tr>\n";
+echo "</table>\n";
 
 theme("", 'footer');
 
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-function list_images( $image_names )
+function list_images( $image_names, $these_are_page_images )
 {
+    global $existing_image_names;
+
+    if ( $these_are_page_images )
+    {
+        $header = _('Page Images');
+    }
+    else
+    {
+        $header = _('Non-Page Images (Illustrations)');
+    }
+    echo "<h4 align='center'>$header</h4>";
+
     echo "<table>\n";
 
     {
@@ -49,6 +78,12 @@ function list_images( $image_names )
     {
         echo "<tr>\n";
 
+        if ( $these_are_page_images && !in_array( $image_name, $existing_image_names ) )
+        {
+            echo "<td>$image_name</td>\n";
+            echo "<td bgcolor='#ff9999'>missing</td>\n";
+        }
+        else
         {
             global $projects_url, $projectid;
             $encoded_url = "$projects_url/$projectid/" . rawurlencode($image_name);
