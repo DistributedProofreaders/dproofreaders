625f03a8ad122f4490721c137666e323a30c1882
jmdyck <>
2009-08-08 06:25:04 +0000

    Add 'global $writeBIGtable'.

--- dp-devel/sample/shift_range.php
+++ dp-devel/sample/shift_range.php
@@ -172,7 +172,7 @@ function shift_range( $projectid, $n_digits, $lo, $hi, $offset, $dryrun )
 				echo "    updating the page table\n";
 				if ( !$dryrun )
 				{
-
+					global $writeBIGtable;
 					if ($writeBIGtable) {
 						$q = "UPDATE project_pages SET fileid='$padded_j', image='$png_filename_j' WHERE projectid = '$projectid' AND  image='$png_filename_i'";
 						// echo "    $q\n";
