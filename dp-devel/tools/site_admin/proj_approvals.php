d867b2cd0f2efb3c27b520a37adca7c18dd7c4ef
jmdyck <>
2009-08-10 20:47:13 +0000

    Fix indenting.

--- dp-devel/tools/site_admin/proj_approvals.php
+++ dp-devel/tools/site_admin/proj_approvals.php
@@ -25,79 +25,80 @@ if (!user_is_a_sitemanager())
 //----------------------------------------------------------------------------------
 if (isset($_GET['update']))
 {
-//update project approval status
-     if ($_GET['metadata'] =='approved')
-     {
-     $statuschange = 'project_new_app';
-     }
-     else
-     {
-     $statuschange = 'project_new_unapp';     
-     }
+    //update project approval status
+    if ($_GET['metadata'] =='approved')
+    {
+        $statuschange = 'project_new_app';
+    }
+    else
+    {
+        $statuschange = 'project_new_unapp';     
+    }
 
-     $result = mysql_query("UPDATE projects SET state = '$statuschange' WHERE projectid = '$update'");
+    $result = mysql_query("UPDATE projects SET state = '$statuschange' WHERE projectid = '$update'");
 
 
 }
 
 echo "<table border=1>\n";
-      // Header row
-		echo "<tr>\n";
-		echo "    <td align='center' colspan='4'><b>Books Waiting for Copyright Approval</b></td><tr></tr>\n";
+    // Header row
+    echo "<tr>\n";
+    echo "    <td align='center' colspan='4'><b>Books Waiting for Copyright Approval</b></td><tr></tr>\n";
             echo "    <td align='center' colspan='4'>The following books need to be approved/disapproved for copyright 
 clearance.</td><tr></tr>\n";
-		echo "    <td align='center' colspan='1'><b>Title</b></td>\n";
-		echo "    <td align='center' colspan='1'><b>Author</b></td>\n";
-		echo "    <td align='center' colspan='1'><b>Clearance Line</b></td>\n";
-		echo "    <td align='center' colspan='1'><b>Approved/Disapproved</b></td>\n";
-		echo "</tr>\n";
+    echo "    <td align='center' colspan='1'><b>Title</b></td>\n";
+    echo "    <td align='center' colspan='1'><b>Author</b></td>\n";
+    echo "    <td align='center' colspan='1'><b>Clearance Line</b></td>\n";
+    echo "    <td align='center' colspan='1'><b>Approved/Disapproved</b></td>\n";
+    echo "</tr>\n";
 
-      $result = mysql_query("SELECT projectid, nameofwork, authorsname, clearance, state FROM projects WHERE state = 
+    $result = mysql_query("SELECT projectid, nameofwork, authorsname, clearance, state FROM projects WHERE state = 
 'project_new_waiting_app'");
-	$numrows = mysql_num_rows($result);
-	$rownum = 0;
+    $numrows = mysql_num_rows($result);
+    $rownum = 0;
 
-      while ($rownum < $numrows) {
-           $projectid = mysql_result($result, $rownum, "projectid");
-           $state = mysql_result($result, $rownum, "state");
-           $name = mysql_result($result, $rownum, "nameofwork");
-           $author = mysql_result($result, $rownum, "authorsname");
-           $clearance = mysql_result($result, $rownum, "clearance");
+    while ($rownum < $numrows) {
+        $projectid = mysql_result($result, $rownum, "projectid");
+        $state = mysql_result($result, $rownum, "state");
+        $name = mysql_result($result, $rownum, "nameofwork");
+        $author = mysql_result($result, $rownum, "authorsname");
+        $clearance = mysql_result($result, $rownum, "clearance");
 
-	if ($rownum % 2 ) {
-			$row_color = $theme['color_mainbody_bg'];
-		} else {
-			$row_color = $theme['color_navbar_bg'];
-		}
-		
-      echo "<tr bgcolor='$row_color'>";
-      echo "<td align='right'><a href = '$code_url/project.php?id=$projectid'>$name</a></td>\n";
-      echo "<td align='right'>$author</td>\n";
-      echo "<td><input type='text' size='67' name='clearance' value='$clearance'></td>";
-      echo "<td><form action = \"proj_approvals.php?project=$projectid\"><input type ='hidden' name ='update' value 
-='$projectid'>";
+        if ($rownum % 2 ) {
+            $row_color = $theme['color_mainbody_bg'];
+        } else {
+            $row_color = $theme['color_navbar_bg'];
+        }
 
-      echo "Approved<input type='radio' name='metadata' value='approved'>
+        echo "<tr bgcolor='$row_color'>";
+        echo "<td align='right'><a href = '$code_url/project.php?id=$projectid'>$name</a></td>\n";
+        echo "<td align='right'>$author</td>\n";
+        echo "<td><input type='text' size='67' name='clearance' value='$clearance'></td>";
+        echo "<td><form action = \"proj_approvals.php?project=$projectid\"><input type ='hidden' name ='update' value 
+        ='$projectid'>";
+
+        echo "Approved<input type='radio' name='metadata' value='approved'>
             Disapproved<input type='radio' name='metadata' value='disapproved'>
             <INPUT TYPE=SUBMIT VALUE=\"update\"></td></form>";
 
-      $rownum++;
-      echo "</tr>";
-	}
+        $rownum++;
+        echo "</tr>";
+    }
 
-//echo "</table>";
-echo "<br>";
-		echo "<tr></tr>\n";
-		echo "<tr></tr>\n";
-		echo "<tr></tr>\n";
-		echo "<tr></tr>\n";
-		echo "<tr></tr>\n";
-		echo "<tr></tr>\n";
+    //echo "</table>";
+    echo "<br>";
+    echo "<tr></tr>\n";
+    echo "<tr></tr>\n";
+    echo "<tr></tr>\n";
+    echo "<tr></tr>\n";
+    echo "<tr></tr>\n";
+    echo "<tr></tr>\n";
 
 
 echo "</table>";
 echo "</center>";
 echo "<br>";
 theme("","footer");
-?>
 
+// vim: sw=4 ts=4 expandtab
+?>
