5334176dfaed2ba28e914c9e4509db6bc08ef43d
bpfoley <>
2010-05-14 22:13:48 +0000

    Require a user to login to show stats/software.php as this page contains potentially sensitive information about the server setup.

--- dp-devel/stats/software.php
+++ dp-devel/stats/software.php
@@ -1,6 +1,6 @@
 <?php
 $relPath="./../pinc/";
-include_once($relPath.'site_vars.php');
+include_once($relPath.'dp_main.inc'); // Require login to view this potentially sensitive information.
 
 // This include setup is highly unusual but neccesary because of mixing of
 // phpBB2's and DP's database setup
