52c5e3251c53f4fff0e2009ceb18e5b87a7b04f6
cpeel <>
2011-12-31 07:33:25 +0000

    To properly provide translated xml files, we must first set up gettext() via including gettext_setup.inc. This sets $intlang which we can output in the <Language/> tag.
    This now requires us to have a language-specific cache, so each file
    (xml/rss) has a cache per language.
    
    Finally, allow the browser to cache the page for as long as the server-side
    cache is valid.

--- dp-devel/feeds/backend.php
+++ dp-devel/feeds/backend.php
@@ -3,35 +3,21 @@ $relPath="./../pinc/";
 include_once($relPath.'site_vars.php');
 include_once($relPath.'misc.inc');
 include_once($relPath.'xml.inc');
+include_once($relPath.'gettext_setup.inc');
 
-//Try our best to make sure no browser caches the page
-header("Content-Type: text/xml");
-header("Expires: Sat, 1 Jan 2000 05:00:00 GMT");
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . "GMT");
-header("Cache-Control: no-cache, must-revalidate");
-header("Pragma: no-cache");
+$content = get_enumerated_param($_GET, 'content', 'posted', array('posted', 'postprocessing', 'proofing', 'news')); // Which feed the user wants
+$refreshDelay = 30 * 60; // Time in seconds for how often the feeds get refreshed
+$refreshAge = time()-$refreshDelay; // How long ago $refreshDelay was in UNIX time
 
-$content = get_enumerated_param($_GET, 'content', 'posted', array('posted', 'postprocessing', 'proofing', 'news')); //Which feed the user wants
-$refreshdelay = 30; //Time in minutes for how often the feeds get refreshed
-$refreshdelay = time()-($refreshdelay*60); //Find out how long ago $refreshdelay was in UNIX time
-
-//Determine if we should display a 0.91 compliant RSS feed or our own feed
+// Determine if we should display a 0.91 compliant RSS feed or our own feed
 if (isset($_GET['type'])) {
-    $xmlfile = $xmlfeeds_dir."/".$content."_rss.xml";
+    $xmlfile = "$xmlfeeds_dir/${content}_rss.$intlang.xml";
 } else {
-    $xmlfile = $xmlfeeds_dir."/".$content.".xml";
+    $xmlfile = "$xmlfeeds_dir/${content}.$intlang.xml";
 }
 
-//If the file does not exist let's create it and then set the refresh delay to now so it updates
-if (!file_exists($xmlfile)) {
-    touch($xmlfile);
-    $refreshdelay = time()+100;
-}
-
-//Determine if the feed needs to be updated.  If not display feed to user
-if (filemtime($xmlfile) > $refreshdelay) {
-    readfile($xmlfile);
-} else {
+// If the file does not exist or is stale, let's (re)create it
+if(!file_exists($xmlfile) || filemtime($xmlfile) < $refreshAge) {
     $relPath="./../pinc/";
     include($relPath.'site_vars.php');
     include($relPath.'pg.inc');
@@ -90,7 +76,7 @@ if (filemtime($xmlfile) > $refreshdelay) {
                 <title>".xmlencode($site_name)." - " . _("Latest Releases") . "</title>
                 <link>".xmlencode($code_url)."</link>
                 <description>" . sprintf( _("The latest releases posted to Project Gutenberg from %1\$s."), xmlencode($site_name)) . "</description>
-                <language>" . _("en-us") . "</language>
+                <language>" . $intlang /* from gettext_setup.inc */ . "</language>
                 <webMaster>".xmlencode($site_manager_email_addr)."</webMaster>
                 <pubDate>".xmlencode($lastupdated)."</pubDate>
                 <lastBuildDate>".xmlencode($lastupdated)."</lastBuildDate>
@@ -139,6 +125,19 @@ if (filemtime($xmlfile) > $refreshdelay) {
     $file = fopen($xmlfile,"w");
     fwrite($file,$xmlpage);
     $file = fclose($file);
-    readfile($xmlfile);
 }
+
+// If we're here, the file exists and is fresh, output it
+
+$fileModifiedTime=filemtime($xmlfile);
+$secondsOfFreshnessRemaining=$fileModifiedTime + $refreshDelay - time();
+
+// Let the browser cache it until the local cache becomes stale
+header("Content-Type: text/xml");
+header("Expires: " . gmdate("D, d M Y H:i:s",$fileModifiedTime + $refreshDelay) . " GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s", $fileModifiedTime) . " GMT");
+header("Cache-Control: max-age=$secondsOfFreshnessRemaining, public, must-revalidate");
+
+readfile($xmlfile);
+
 // vim: sw=4 ts=4 expandtab
