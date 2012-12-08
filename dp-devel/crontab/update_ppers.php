5edcba6f81a36f8137f8c38f07b93c44d536ff24
cpeel <>
2012-12-08 05:36:05 +0000

    Change tabs to spaces and add vim line, no functional change.

--- dp-devel/crontab/update_ppers.php
+++ dp-devel/crontab/update_ppers.php
@@ -9,14 +9,13 @@ if(!requester_is_localhost())
 
 $result = mysql_query("SELECT DISTINCT(postproofer) FROM projects WHERE postproofer is not null");
 while (list($postproofer) = mysql_fetch_row($result)) {
-	mysql_query("UPDATE users SET postprocessor = 'yes' WHERE username = '$postproofer'");
-	if ( mysql_affected_rows() > 0 )
-	{
-		echo "Added: $postproofer<br>";
-	}
+    mysql_query("UPDATE users SET postprocessor = 'yes' WHERE username = '$postproofer'");
+    if ( mysql_affected_rows() > 0 )
+    {
+        echo "Added: $postproofer<br>";
+    }
 }
 
 echo "<br><center>Update of Post Processors Complete!</center>";
 
-?>
-
+// vim: sw=4 ts=4 expandtab
