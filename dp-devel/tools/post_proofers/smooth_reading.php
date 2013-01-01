f5847c31feedda546c514e29ea93bca207e21cc1
donovang <>
2013-01-01 14:26:52 +0000

    By request: Normalize variations on Smooth Reading. Minor phrasing tweaks. Site-specific forums link removed. (SR people  want it in a news item.)

--- dp-devel/tools/post_proofers/smooth_reading.php
+++ dp-devel/tools/post_proofers/smooth_reading.php
@@ -31,23 +31,18 @@ echo "<h3>" . _("Smooth Reading") . "</h3>";
 
 if (!$logged_in)
 {
-    
-    echo  "<p style='font-size: 120%;'>" . _("This Preview page shows which books are currently available for Smooth Reading. Click on a book's title to view more information about it or to download the text.") . "</p>";
-	
 
-    
-    echo "<p>" . _("Please note that while unregistered guests are welcome to download texts for smooth reading, only registered volunteers are able to upload annotated texts. A registration link is available at the top of this page.") . "</p>";
-    
+    echo  "<p style='font-size: 120%;'>" . _("This Preview page shows which books are currently available for Smooth Reading. Click on a book's title to view more information about it or to download the text.") . "</p>";
+
+    echo "<p>" . _("Please note that while unregistered guests are welcome to download texts for Smooth Reading, only registered volunteers are able to upload annotated texts. A registration link is available at the top of this page.") . "</p>";
+
 }
 
 {
-    
-    echo "<p style='font-size: 120%;'>" . _("The goal of 'Smooth Reading' is to read the text attentively, as for pleasure, with just a little more attention than usual to punctuation, etc. This is NOT full scale proofreading, and comparison with the scans is not needed. Just read it as your normal, sensitized-to-proofreading-errors self, and report any problem that disrupts the sense or the flow of the book. Note that some of these will be due to the author and/or publisher.") . "</p>";
-    
 
-    
-    echo "<p>" . _("The way to report errors is by adding a comment of the form <blockquote style='color: red; background-color: inherit;'> [**correction or query] <br> </blockquote> immediately after the problem spot. Do not correct or change the problem, just note it in the above format.") . "</p>";
-    
+    echo "<p style='font-size: 120%;'>" . _("The goal of Smooth Reading is to read the text attentively, as for pleasure, with just a little more attention than usual to punctuation, etc. This is NOT full scale proofreading, and comparison with the scans is not needed. Just read it as your normal, sensitized-to-proofreading-errors self, and report any problem that disrupts the sense or the flow of the book. Note that some of these will be due to the author and/or publisher.") . "</p>";
+
+    echo "<p>" . _("Errors are reported by adding a comment of the form <blockquote style='color: red; background-color: inherit;'> [**correction or query] <br> </blockquote> immediately after the problem spot. Do not correct or change the problem, just note it in the above format.") . "</p>";
 
     echo "<h3>" . _("Examples:") . "</h3>";
     echo "<p><ul>";
@@ -56,10 +51,6 @@ if (!$logged_in)
     echo "<li>" . _("that was the emd.[**end] However, the next day") . "</li>";
     echo "</ul></p>";
 
-    
-    //This lines needs a new variable added to site_vars to store the link, then convert to sprintf.  Noted in site_vars task.
-    echo "<p>" . _("For more information on the origin of smoothreading, see <a href='http://www.pgdp.net/phpBB2/viewtopic.php?t=3429'>this thread</a>") . "</p>";
-    
 }
 
 show_projects_for_smooth_reading();
