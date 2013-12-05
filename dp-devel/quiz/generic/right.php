1630b80b502dffaea296b0f7fae6fca9385effcf
jmdyck <>
2013-12-05 18:40:45 +0000

    Extract code from     quiz/generic/right.php to define     qp_echo_introduction_html in     quiz/generic/quiz_page.inc

--- dp-devel/quiz/generic/right.php
+++ dp-devel/quiz/generic/right.php
@@ -6,18 +6,13 @@ include_once('../small_theme.inc'); // output_small_header
 
 $quiz_page_id = get_quiz_page_id_param($_REQUEST, 'type');
 
-include "./data/qd_${quiz_page_id}.inc"; // $welcome $constant_message
+include "./quiz_page.inc"; // qp_echo_introduction_html
 
 $quiz = get_Quiz_containing_page($quiz_page_id);
 
 output_small_header($quiz);
 
-echo $welcome;
-//If the quiz has a message to show all the time, put that in here
-if (@$constant_message != "")
-{
-    echo $constant_message;
-}
+qp_echo_introduction_html();
 ?>
 </body>
 </html>
