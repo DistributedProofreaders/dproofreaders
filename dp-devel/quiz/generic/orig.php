c0ed4a864303943e61c6a22d757b6c7cfd175069
jmdyck <>
2013-12-05 08:34:27 +0000

    Extract code from     quiz/generic/orig.php to define     qp_page_image_path() in     quiz/generic/quiz_page.inc

--- dp-devel/quiz/generic/orig.php
+++ dp-devel/quiz/generic/orig.php
@@ -5,12 +5,14 @@ include_once($relPath.'quizzes.inc'); // get_quiz_page_id_param
 
 $quiz_page_id = get_quiz_page_id_param($_REQUEST, 'type');
 
+include './quiz_page.inc'; // qp_page_image_path
+
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html><head>
 <META http-equiv="Content-Type" content="text/html; charset=<?php echo "$charset";?>">
 <title></title></head>
 <body bgcolor='#ffffff'>
-<img src="./images/qi_<?php echo $quiz_page_id; ?>.png" alt="">
+<img src="<?php echo qp_page_image_path(); ?>" alt="">
 </body>
 </html>
