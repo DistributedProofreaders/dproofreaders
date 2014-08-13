2f0a21af0658a330829a6fe1713c9fad59e0c1b3
jmdyck <>
2014-08-13 21:37:50 +0000

    On 2014-04-15, the PG folks stopped updating    http://www.gutenberg.org/feeds/catalog.rdf.bz2 but they left the file there, so we didn't notice it was stale until 2014-06-30.
    I modified this script to use the new catalog:
        http://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.bz2
    and it's been running okay on pgdp.net since 2014-07-01.
    
    Now I'm finally getting around to committing the code.

--- dp-devel/crontab/import_pg_catalog.php
+++ dp-devel/crontab/import_pg_catalog.php
@@ -2,9 +2,10 @@
 $relPath='../pinc/';
 include_once($relPath.'base.inc');
 include_once($relPath.'misc.inc');
+include_once($relPath.'Stopwatch.inc');
 
 // check that caller is localhost or bail
-if(!requester_is_localhost())
+if(!requester_is_localhost() && php_sapi_name() != 'cli' )
     die("You are not authorized to perform this request.");
 
 // Download the XML version of the Project Gutenberg catalog,
@@ -17,7 +18,7 @@ if(!requester_is_localhost())
 
 header('Content-type: text/plain');
 
-set_time_limit(90);
+set_time_limit(300);
 
 $display_mapping = array(
     'application/epub+zip'     => 'EPUB',
@@ -57,109 +58,186 @@ $display_mapping = array(
     'video/x-msvideo'          => 'MS Video',
 );
 
-$trace = FALSE;
 $start_from_scratch = TRUE;
 
-$remote_catalog_url = "http://www.gutenberg.org/feeds/catalog.rdf.bz2";
-$local_temp_file    = "/tmp/catalog.rdf.bz2";
-$local_catalog_file = "$dyn_dir/pg/catalog.rdf";
+$remote_catalog_url    = "http://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.bz2";
+$local_compressed_file = "/tmp/rdf-files.tar.bz2";
+$local_tar_file        = "/tmp/rdf-files.tar";
+$local_catalog_dir     = "$dyn_dir/pg/catalog";
+
+// -------------------------------
+
+$trace = TRUE;
+
+if ($trace)
+{
+    $watch = new Stopwatch();
+    $watch->start();
+}
+
+function trace($msg)
+{
+    global $trace, $watch;
+    if (!$trace) return;
+    $t = $watch->read();
+    echo sprintf("%05.1f %s\n", $t, $msg);
+}
+
+// -------------------------------
 
 if ($start_from_scratch)
 {
-    if ($trace) echo "Downloading $remote_catalog_url to $local_temp_file...\n";
-    copy( $remote_catalog_url, $local_temp_file ) or
-        die( "Unable to download $remote_catalog_url to $local_temp_file" );
+    trace("Downloading $remote_catalog_url to $local_compressed_file...");
+    copy( $remote_catalog_url, $local_compressed_file ) or
+        die( "Unable to download $remote_catalog_url to $local_compressed_file" );
 
-    $local_catalog_dir = dirname($local_catalog_file);
-    mkdir_recursive( $local_catalog_dir, 0777 );
-
-    if ($trace) echo "Decompressing $local_temp_file to $local_catalog_file...\n";
-    system( "bzip2 --decompress --stdout $local_temp_file > $local_catalog_file", $ret );
+    trace("Decompressing $local_compressed_file to $local_tar_file...");
+    system( "bzip2 --decompress --stdout $local_compressed_file > $local_tar_file", $ret );
     if ($ret)
     {
-        die( "Unable to decompress $local_temp_file to $local_catalog_file" );
+        die( "Unable to decompress $local_compressed_file to $local_tar_file" );
+    }
+
+    mkdir_recursive( $local_catalog_dir, 0777 );
+
+    trace("Extracting files from $local_tar_file to $local_catalog_dir...");
+    system("tar --extract --file=$local_tar_file --strip-components=3 --directory=$local_catalog_dir --overwrite", $ret);
+    // Each file in the tar archive describes one ebook in the PG collection,
+    // and has a path of the form:
+    //     cache/epub/NNN/pgNNN.rdf
+    // where "NNN" is the etext-number of the file's ebook.
+    // We use --strip-components=3 to strip off the
+    //     cache/epub/NNN
+    // part, and --directory=$local_catalog_dir so that we end up with
+    //     $local_catalog_dir/pgNNN.rdf
+    if ($ret)
+    {
+        die( "Unable to extract files from $local_tar_file to $local_catalog_dir" );
     }
 }
 
 $etexts = array();
 $mime_types_not_in_display_mapping = array();
 
-if ($trace) echo "Reading $local_catalog_file...\n";
-$fp = fopen($local_catalog_file, "r");
-if ( !$fp )
+trace("Scanning files in $local_catalog_dir...");
+
+$n_rdf_files_processed = 0;
+$n_rdf_files_skipped = 0;
+foreach (scandir($local_catalog_dir) as $filename)
 {
-    die( "Unable to open $local_catalog_file" );
+    if ($filename == '.' || $filename == '..') continue;
+
+    assert( preg_match('/^pg(\d+)\.rdf$/', $filename, $matches) );
+    $etext_number = $matches[1];
+
+    $display_formats = array();
+
+    $path = "$local_catalog_dir/$filename";
+    $root = simplexml_load_file($path);
+    // $etext_num_xpath = "/rdf:RDF/pgterms:ebook/@rdf:about";
+    $format_xpath = "
+        /rdf:RDF
+            /pgterms:ebook
+                /dcterms:hasFormat
+                    /pgterms:file
+                        /dcterms:format
+                            /rdf:Description
+                                /rdf:value";
+    $nodes = $root->xpath($format_xpath);
+    if ($nodes === FALSE)
+    {
+        // The docs say that SimpleXML::path() returns FALSE "in case of an error",
+        // but it doesn't say what constitutes an error.
+        // And there doesn't appear to be a programmatic way
+        // to find out what the specific error is.
+        //
+        // Annoyingly, it looks like some versions of SimpleXML::path()
+        // return FALSE when there's no error per se,
+        // but the XPath expression simply doesn't match any nodes.
+        // (In other versions, xpath() more sensibly returns an empty array.)
+        //
+        // We'll just assume that that's what's happened here.
+        // That is, no nodes in this file match $format_xpath.
+        // This is almost certainly becase the file is a placeholder,
+        // i.e. $etext_number has not yet been assigned to an ebook
+        // (or perhaps once *was* assigned, but got de-assigned).
+        //
+        // In such a case, it shouldn't really matter whether or not
+        // we put an entry for $etext_number in the pg_books table.
+        // For simplicity of coding, we just skip the file here.
+
+        $n_rdf_files_skipped += 1;
+        continue;
+
+        // But note that, with a more sensible version of SimpleXML::path(),
+        // such a case instead iterates over the empty array,
+        // and *does* end up putting an entry in the pg_books table,
+        // albeit one with an empty 'formats' value
+    }
+    foreach ( $nodes as $format_value_element)
+    {
+        $format = (string)$format_value_element;
+
+        if ($format == 'image/jpeg')
+        {
+            // This pgterms:file is a cover image or some such
+            // (which isn't distinguished from files
+            // that actually convey the content of the book).
+            continue;
+        }
+
+        if ($format == 'application/zip')
+        {
+            // This dcterms:format just conveys that the file is compressed.
+            // A different dcterms:format in the same pgterms:file
+            // gives the format of the uncompressed file.
+            continue;
+        }
+
+        if ( preg_match( '#^([^;]+);\s*[a-z]+=([^"]+)$#', $format, $format_groups ) )
+        {
+            $mime_type = $format_groups[1];
+            $sub_type  = $format_groups[2];
+        }
+        else
+        {
+            $mime_type = $format;
+            $sub_type  = "";
+        }
+
+        if ( $mime_type == 'application/octet-stream' )
+        {
+            $display_format = $sub_type;
+        }
+        else
+        {
+            $display_format = @$display_mapping[$mime_type];
+            if (empty($display_format))
+            {
+                @$mime_types_not_in_display_mapping[$mime_type] += 1;
+                $display_format = $mime_type;
+            }
+
+            if ( $sub_type )
+            {
+                $display_format .= " ($sub_type)";
+            }
+        }
+        // echo "    $format -> $display_format\n";
+
+        $display_formats[$display_format] = 1;
+    }
+
+    assert( !isset($etexts[$etext_number]) );
+    $etexts[$etext_number] = $display_formats;
+    $n_rdf_files_processed += 1;
+    if ($trace && $n_rdf_files_processed % 1000 == 0) echo ".";
+    // if ($n_rdf_files_processed == 2) exit;
 }
 
-$display_format = null;
-while( $line = fgets( $fp ) )
-{
-    // Flag the PG auto-generated formats
-    if ( preg_match(
-        '/<pgterms:file rdf:about=(.*)cache/', $line ) )
-    {
-        $autogen = TRUE;
-    }
-    if ( preg_match(
-        '#<dc:format><dcterms:IMT><rdf:value>(.*)</rdf:value></dcterms:IMT></dc:format>#',
-        $line, $line_groups ) )
-    {
-        $format = $line_groups[1];
-        if ( $format != 'application/zip' )
-        {
-            assert( is_null($display_format) );
-
-            // echo $format, "\n";
-            if ( preg_match( '#^([^;]+);\s*[a-z]+="([^"]+)"$#', $format, $format_groups ) )
-            {
-                $mime_type = $format_groups[1];
-                $sub_type  = $format_groups[2];
-            }
-            else
-            {
-                $mime_type = $format;
-                $sub_type  = "";
-            }
-
-            if ( $mime_type == 'application/octet-stream' )
-            {
-                $display_format = $sub_type;
-            }
-            else
-            {
-                $display_format = @$display_mapping[$mime_type];
-                if (empty($display_format))
-                {
-                    @$mime_types_not_in_display_mapping[$mime_type] += 1;
-                    $display_format = $mime_type;
-                }
-
-                if ( $sub_type )
-                {
-                    $display_format .= " ($sub_type)";
-                }
-            }
-            // echo $display_format, "\n";
-        }
-    }
-    else if ( preg_match( '@<dcterms:isFormatOf rdf:resource="#etext(\d+)" />@', $line, $line_groups ) )
-    {
-        $etext_number = $line_groups[1];
-        assert( !is_null($display_format) );
-        // echo "$etext_number $display_format \n";
-        if (!isset($etexts[$etext_number]))
-        {
-            $etexts[$etext_number] = array();
-        }
-        if ( !$autogen )
-        {
-            $etexts[$etext_number][$display_format] = 1;
-        }
-        $autogen = FALSE;
-        $display_format = null;
-    }
-}
-fclose($fp);
+if ($trace) echo "\n";
+trace("Finished processing $n_rdf_files_processed RDF files.");
+trace("(Skipped $n_rdf_files_skipped RDF files, probably just placeholders.)");
 
 if (count($mime_types_not_in_display_mapping)>0)
 {
@@ -171,7 +249,7 @@ if (count($mime_types_not_in_display_mapping)>0)
     }
 }
 
-if ($trace) echo "Putting the data into the table...\n";
+trace("Putting the data into the table...");
 
 ksort($etexts); // sort numerically by $etext_number
 foreach ($etexts as $etext_number => $formats )
@@ -184,7 +262,7 @@ foreach ($etexts as $etext_number => $formats )
         or die( mysql_error() );
 }
 
-if ($trace) echo "Done\n";
+trace("Done");
 
 // vim: sw=4 ts=4 expandtab
 ?>
