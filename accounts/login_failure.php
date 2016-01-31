9b1ba1398accbadd2cd6bad2bb9a1f258c2346bf
Casey Peel <cpeel@users.sourceforge.net>
2016-01-31 00:26:02 +0000

    Split authentication from user UI in login.php
    Split out user authentication and session set up (which requires no
    UI) from the UI portion reporting a login failure. This is part two in
    reducing the possibility of code collisions between phpBB and the DP
    code.
    
    To pass the error from login.php, pass a URL-friendly key to
    login_failure.php which is converted to a user-friendly error message
    and displayed to the user. We also handle the case where no error code,
    or an unrecognized error code, was indicated.

---
+++ accounts/login_failure.php
@@ -0,0 +1,64 @@
+<?php
+/*
+A user is redirected to this page upon login failure from login.php.
+
+By design, this file does not access any phpBB functions, delegating that to
+login.php.
+
+The authentication and user-presentation functions were split out to further
+minimize possible code collisions between the DP code and the phpBB code.
+*/
+
+$relPath="./../pinc/";
+include_once($relPath.'base.inc');
+include_once($relPath.'theme.inc');
+include_once($relPath.'misc.inc'); // undo_all_magic_quotes()
+include_once($relPath.'metarefresh.inc');
+
+undo_all_magic_quotes();
+
+// If the user is already logged in, send them to the Activity Hub
+if($user_is_logged_in)
+{
+    metarefresh(0, "$code_url/activity_hub.php");
+}
+
+// login.php loads this page with an error_code parameter to indicate what
+// went wrong. Here we change this into a user-visible string.
+$login_failures = array(
+    'invalid_username' => _("The username supplied is invalid."),
+    'no_password'      => _("You did not supply a password."),
+    'auth_failure'     => _("Unable to authenticate. The username/password may be incorrect or your account may be locked."),
+    'reg_mismatch'     => sprintf(_("You are registered with the forum software, but not with %s."), $site_abbreviation),
+);
+
+$error = @$login_failures[@$_GET['error_code']];
+if(!$error)
+{
+    $error = _("An undefined error occurred while attempting to log you in.");
+}
+
+$title = _("Login Failed");
+output_header($title);
+
+echo "<br>\n";
+echo "<b>$error</b>\n";
+
+echo "<p>" . _("Please attempt again to log in above. If problems persist, review the following possible fixes:") . "</p>";
+echo "<ol>";
+if ($testing)
+{
+    echo "<li>" . _("Register! (Note that this is a test site, and has a separate database from the production site, so you need to register separately.)") . "</li>\n";
+}
+echo "<li>" . _("Type your username in the exact same way as when you registered.") . "</li>\n";
+echo "<li>" . sprintf( _("<a href='%s'>Reset</a> your password."), get_reset_password_url()) . "</li>\n";
+echo "<li>" . _("Enable Javascript.") . "</li>\n";
+echo "<li>" . sprintf(_("Accept cookies (at least from us at %s)."), $_SERVER["HTTP_HOST"]) . "</li>\n";
+echo "<li>" . sprintf(_("Allow popup windows (at least from us at %s)."), $_SERVER["HTTP_HOST"]) . "</li>\n";
+echo "<li>" . _("Caching set to off (or: refresh page every visit).") . "</li>\n";
+echo "<li>" . _("Ensure your PC clock is set to the correct date &amp; time.") . "</li>\n";
+echo "</ol>";
+echo "<p>" . sprintf( _("If all of this fails, contact the <a href='%s'>site manager</a>."), "mailto:$site_manager_email_addr") . "</p>";
+echo "<p>" . sprintf( _("Note: If you have just registered, you will need to wait for the welcome mail to arrive to your mailbox. Once it does, please click the activation link to complete the registration (this is to prevent others from signing you up to the site without your knowledge). If you have waited for an hour or so and have still not received any mail from us (please check any spam filters!), it is likely that you misentered your email-address. Please contact the <a href='%s'>site manager</a> to solve the problem."), "mailto:$site_manager_email_addr") . "</p>";
+
+// vim: sw=4 ts=4 expandtab
