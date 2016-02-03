f229dd47b2c65cb84d4119ccc3270e8711a313b0
Casey Peel <cpeel@users.sourceforge.net>
2016-02-03 04:11:11 +0000

    Don't load User.inc in login.php
    DP and phpBB both have a User class. If we include pinc/User.inc
    here, when we make the phpBB call to log the user in and the phpBB
    code is loaded, we get an error that the User class has already been
    defined.
    
    Ideally, all user input would be validated before being passed down
    the stack and used. In this case, however, that additional work isn't
    necessary. Our phpBB2 login implementation correctly escapes $username
    before querying against the table and phpBB3 does as well.

--- accounts/login.php
+++ accounts/login.php
@@ -18,10 +18,11 @@ includes needed to provide authentication.
 
 $relPath="./../pinc/";
 include_once($relPath.'base.inc');
-include_once($relPath.'User.inc');
 include_once($relPath.'metarefresh.inc');
 include_once($relPath.'forum_interface.inc');
 include_once($relPath.'misc.inc'); // undo_all_magic_quotes()
+// We must *not* include User.inc here, as phpBB also defines a User class
+// and including pinc/User.inc here would cause a conflict.
 
 undo_all_magic_quotes();
 
@@ -30,10 +31,9 @@ $destination = ( isset($_REQUEST['destination']) ? $_REQUEST['destination'] : ''
 $userNM = @$_POST['userNM'];
 $userPW = @$_POST['userPW'];
 
-$err = check_username($userNM);
-if ($err != '')
+if ($userNM == '')
 {
-    login_failure('invalid_username', $destination);
+    login_failure('no_username', $destination);
 }
 
 
