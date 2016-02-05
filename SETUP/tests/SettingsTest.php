b09498678d392e3fcaa8a9f8c1a33caf36adecff
Casey Peel <cpeel@users.sourceforge.net>
2016-02-05 15:12:55 +0000

    Add unit tests for SettingsClass

---
+++ SETUP/tests/SettingsTest.php
@@ -0,0 +1,97 @@
+<?php
+
+class SettingsTest extends PHPUnit_Framework_TestCase
+{
+    private $TEST_USERNAME;
+    private $PREFIX = 'STU_';
+    private $NONEXISTENT_USERNAME = 'SettingsTestUser';
+
+    private $cleanup = FALSE;
+
+    protected function setUp()
+    {
+        // Load a valid username from the database for use during the tests
+        $sql = "SELECT username FROM usersettings LIMIT 1";
+        $result = mysql_query($sql);
+        $row = mysql_fetch_assoc($result);
+        mysql_free_result($result);
+        $this->TEST_USERNAME = $row['username'];
+    }
+
+    protected function tearDown()
+    {
+        $sql = sprintf("
+            DELETE FROM usersettings
+            WHERE username='%s' AND setting like '%s%%'
+        ", $this->TEST_USERNAME, $this->PREFIX);
+        mysql_query($sql);
+    }
+
+    public function testExisting()
+    {
+        $settings = new Settings($this->TEST_USERNAME);
+        $this->assertGreaterThan(0, $settings->settings_count());
+    }
+
+    public function testNonexisting()
+    {
+        $settings = new Settings($this->NONEXISTENT_USERNAME);
+        $this->assertEquals(0, $settings->settings_count());
+    }
+
+    public function testSetTrue()
+    {
+        $settings = new Settings($this->TEST_USERNAME);
+        $settings->set_true($this->PREFIX . "boolean_true");
+
+        $settings = new Settings($this->TEST_USERNAME);
+        $this->assertTrue(
+            $settings->get_boolean($this->PREFIX . "boolean_true"));
+    }
+
+    public function testSetFalse()
+    {
+        $settings = new Settings($this->TEST_USERNAME);
+        $settings->set_false($this->PREFIX . "boolean_false");
+
+        $settings = new Settings($this->TEST_USERNAME);
+        $this->assertFalse(
+            $settings->get_boolean($this->PREFIX . "boolean_false"));
+    }
+
+    public function testSetSingleValue()
+    {
+        $settings = new Settings($this->TEST_USERNAME);
+        $settings->set_value($this->PREFIX . "single_value", "single");
+
+        $settings = new Settings($this->TEST_USERNAME);
+        $this->assertEquals(
+            $settings->get_value($this->PREFIX . "single_value"), "single");
+    }
+
+    public function testSetSingleValueDefault()
+    {
+        $settings = new Settings($this->TEST_USERNAME);
+        $this->assertEquals(
+            $settings->get_value($this->PREFIX . "nonexistent_value", "default"), "default");
+    }
+
+    public function testSetSingleValueAgain()
+    {
+        $settings = new Settings($this->TEST_USERNAME);
+        $settings->set_value($this->PREFIX . "single_value", "single1");
+        $settings->set_value($this->PREFIX . "single_value", "single2");
+
+        $settings = new Settings($this->TEST_USERNAME);
+        $this->assertEquals(
+            $settings->get_value($this->PREFIX . "single_value"), "single2");
+    }
+
+    public function testSetUnsettableValue()
+    {
+        $this->markTestIncomplete("SettingsClass dies() here instead of throwing an exception we can catch.");
+
+        $settings = new Settings($this->TEST_USERNAME);
+        $settings->set_true("sitemanager");
+    }
+}
