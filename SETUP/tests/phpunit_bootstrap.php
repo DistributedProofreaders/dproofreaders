34c6aa3e9d013e089fd2bd4fa94ec24da2c2a9f6
Casey Peel <cpeel@users.sourceforge.net>
2016-02-05 15:11:38 +0000

    Make autoload use $class.inc and $classClass.inc
    Some older classes are in files named like {$classname}Class.inc
    instead of just {$classname}.inc.

--- SETUP/tests/phpunit_bootstrap.php
+++ SETUP/tests/phpunit_bootstrap.php
@@ -6,7 +6,14 @@ include_once($relPath.'base.inc');
 function dp_class_autoloader($class)
 {
     global $relPath;
-    include_once($relPath.$class.".inc");
+    if(is_file("$relPath$class.inc"))
+    {
+        include_once($relPath.$class.".inc");
+    }
+    else
+    {
+        include_once($relPath.$class."Class.inc");
+    }
 }
 
 spl_autoload_register('dp_class_autoloader');
