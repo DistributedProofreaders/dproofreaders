697500e218c09db9cf82b46c79690fb26914fe72
Casey Peel <cpeel@users.sourceforge.net>
2016-03-08 23:33:16 +0000

    Remove dep on magic quotes from greek picker
    Remove the dependency on magic quotes for the greek character picker.

--- tools/proofers/greek2ascii.php
+++ tools/proofers/greek2ascii.php
@@ -7,11 +7,11 @@ include_once($relPath.'misc.inc');
 
 require_login();
 
+undo_all_magic_quotes();
+
 $title = _("Greek to Latin-1 Transliteration");
 slim_header($title,TRUE,FALSE);
 $greek_contents = @$_GET['textbox'];
-if (get_magic_quotes_gpc())
-    $greek_contents = stripslashes($greek_contents);
 ?>
 <!-- Graphics and html/javascript for Greek text conversion
      by D Garcia 12/24/02 for Distributed Proofreaders
