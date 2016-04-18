e1ead87563c02856777922fa4d8abad61447c6c5
Casey Peel <cpeel@users.sourceforge.net>
2016-04-18 00:04:23 +0000

    Handle unassigned tasks correctly
    Before the script was changed to use the User class, it supported
    assigning a task to id=0, or Unassigned. b801aa broke this.

--- tasks.php
+++ tasks.php
@@ -485,8 +485,12 @@ function create_task_from_form_submission($formsub)
     $newt_browser  = (int) get_enumerated_param($formsub, 'task_browser', null, array_keys($browser_array));
     $newt_version  = (int) get_enumerated_param($formsub, 'task_version', null, array_keys($versions_array));
 
-    $task_assignee_user = new User();
-    $task_assignee_user->load('u_id', $newt_assignee);
+    // Validate the assignee, skipping the case where it is 0 (Unassigned).
+    if($newt_assignee != 0)
+    {
+        $task_assignee_user = new User();
+        $task_assignee_user->load('u_id', $newt_assignee);
+    }
 
     $sql_query = "
         INSERT INTO tasks
@@ -522,14 +526,19 @@ function create_task_from_form_submission($formsub)
     // Nobody could have subscribed to this particular task yet,
     // so the msg will only go to those with taskctr_notice = 'all'.
 
-    global $tasks_url, $code_url;
-    maybe_mail(
-        $task_assignee_user->email,
-        "DP Task Center: Task #$task_id has been assigned to you",
-        $task_assignee_user->username . ", you have been assigned task #$task_id.  Please visit this task at $tasks_url?action=show&task_id=$task_id.\n\nIf you do not want to accept this task please edit the task and change the assignee to 'Unassigned'.\n\n--\nDistributed Proofreaders\n$code_url\n\nThis is an automated message that you had requested please do not respond directly to this e-mail.\r\n"
-    );
+    // If $newt_assignee is 0, there is no user assigned so no notification
+    // to send out.
+    if($newt_assignee != 0)
+    {
+        global $tasks_url, $code_url;
+        maybe_mail(
+            $task_assignee_user->email,
+            "DP Task Center: Task #$task_id has been assigned to you",
+            $task_assignee_user->username . ", you have been assigned task #$task_id.  Please visit this task at $tasks_url?action=show&task_id=$task_id.\n\nIf you do not want to accept this task please edit the task and change the assignee to 'Unassigned'.\n\n--\nDistributed Proofreaders\n$code_url\n\nThis is an automated message that you had requested please do not respond directly to this e-mail.\r\n"
+        );
+    }
 
-    global $pguser;
+    // Subscribe the current user to this task for notification
     $userSettings =& Settings::get_Settings($pguser);
     $userSettings->add_value('taskctr_notice', $task_id);
 }
