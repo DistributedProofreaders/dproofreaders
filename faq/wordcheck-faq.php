07b39cef2a878ad9a063369476d3920dab96d802
Casey Peel <cpeel@users.sourceforge.net>
2017-02-10 14:22:33 +0000

    Redirect WordCheck FAQ to pgdp.net wiki

--- faq/wordcheck-faq.php
+++ faq/wordcheck-faq.php
@@ -4,6 +4,15 @@ include_once($relPath.'base.inc');
 include_once($relPath.'faq.inc');
 include_once($relPath.'wordcheck_engine.inc');
 include_once($relPath.'theme.inc');
+include_once($relPath.'metarefresh.inc');
+
+# SITE-SPECIFIC
+# Redirect users to this document in the wiki
+if(strpos($code_url, '://www.pgdp.'))
+{
+    $url = 'http://www.pgdp.net/wiki/DP_Official_Documentation:Proofreading/WordCheck_FAQ';
+    metarefresh(0, $url);
+}
 
 output_header('WordCheck FAQ', NO_STATSBAR);
 ?>
