13be302562f43f3fbfa7bc286e5fe306f3987b46
Casey Peel <cpeel@users.sourceforge.net>
2017-04-13 00:51:09 +0000

    Pull URLs out of translated strings

--- stats/PP_unknown.php
+++ stats/PP_unknown.php
@@ -16,7 +16,7 @@ echo "<br><br><h2>$title</h2><br>\n";
 
 echo "<br>\n";
 
-echo sprintf( _("We don't know for sure who PPd these books; if you do know, or if you did, please send an email: <a ref='%1\$s'>%2\$s</a> quoting the other information in the row, including the project ID. Thanks!"), "mailto:$general_help_email_addr", "$general_help_email_addr");
+echo sprintf( _("We don't know for sure who PPd these books; if you do know, or if you did, please send an email: <a href='%1\$s'>%2\$s</a> quoting the other information in the row, including the project ID. Thanks!"), "mailto:$general_help_email_addr", "$general_help_email_addr");
 
 //get projects that have been PPd but we don't know by whom
 $psd = get_project_status_descriptor('PPd');
