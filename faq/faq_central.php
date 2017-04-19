0387de7ce40611c805c080841f2d20d4c87fa5ca
Casey Peel <cpeel@users.sourceforge.net>
2017-04-19 19:15:45 +0000

    Point to pgdp.net for applicable content

--- faq/faq_central.php
+++ faq/faq_central.php
@@ -5,6 +5,17 @@ include_once($relPath.'pg.inc');
 include_once($relPath.'theme.inc');
 include_once($relPath.'site_news.inc');
 
+# SITE-SPECIFIC
+# pgdp.net/org have some documents in the wiki.
+if(strpos($code_url, '://www.pgdp.'))
+{
+    $use_pgdp_urls = TRUE;
+}
+else
+{
+    $use_pgdp_urls = FALSE;
+}
+
 $title = _("FAQ Central");
 
 $theme_args = array("css_data" => "
@@ -40,25 +51,44 @@ show_news_for_page("FAQ");
 
 // Introduction section
 $intro = new FAQSection(_("Introductory Information"));
+if($use_pgdp_urls)
+{
+    $urls = array(
+        "en" => "https://www.pgdp.net/wiki/DP_Official_Documentation:General/New_Volunteer_Frequently_Asked_Questions",
+        "fr" => 'https://www.pgdp.net/wiki/DP_Official_Documentation:General/French/Aper%C3%A7u_de_DP',
+    );
+}
+else
+{
+    $urls = array(
+        "en" => "ProoferFAQ.php",
+    );
+}
 $intro->add_entry(new FAQEntry(
     _("Beginning Proofreader's FAQ"),
-   "ProoferFAQ.php",
+    $urls,
     _("Introduction to the site, general overview, beginner's questions.")
 ));
 $intro->add_entry(new FAQEntry(
     _("Welcome Back, Alumni"),
-    "longabsent.php",
+    array(
+        "en" => "longabsent.php",
+    ),
     _("An overview of site changes from the last few years, including the distinction between proofreading and formatting.")
 ));
 $intro->add_entry(new FAQEntry(
     _("Proofreading Interface Diagram"),
-    "InterfaceDiagram.png",
+    array(
+        "en" => "InterfaceDiagram.png",
+    ),
     _("An overview flowchart which outlines the operation of the proofreading interface.")
 ));
 $intro->add_entry(new FAQEntry(
     // TRANSLATORS: %s is the site abbreviation
     sprintf(_("%s Privacy Policy"), $site_abbreviation),
-    "privacy.php",
+    array(
+        "en" => "privacy.php",
+    ),
     // TRANSLATORS: %s is the site name
     sprintf(_("The current version of the %s Privacy Policy."), $site_name)
 ));
@@ -66,75 +96,142 @@ $intro->add_entry(new FAQEntry(
 
 // Proofreading
 $proofing = new FAQSection(_("Proofreading"));
-$proofing->add_entry(new FAQEntry(
-    _("Proofreading Guidelines"),
-    "proofreading_guidelines.php",
-    _("The details of the guidelines we use for proofreading documents."),
-    array(
+if($use_pgdp_urls)
+{
+    $urls = array(
+        "en" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Proofreading/Proofreading_Guidelines",
+        "fr" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Proofreading/French/Directives_de_Relecture_et_de_Correction",
+        "pt" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Proofreading/Portuguese/Regras_de_Revis%C3%A3o",
+        "es" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Proofreading/Spanish/Reglas_de_Revisi%C3%B3n",
+        "nl" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Proofreading/Dutch/Proeflees-Richtlijnen",
+        "de" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Proofreading/German/Korrekturlese-Richtlinien",
+        "it" => "proofreading_guidelines_italian.php",
+    );
+}
+else
+{
+    $urls = array(
+        "en" => "proofreading_guidelines.php",
         "fr" => "proofreading_guidelines_francaises.php",
         "pt" => "proofreading_guidelines_portuguese.php",
         "es" => "proofreading_guidelines_spanish.php",
         "nl" => "proofreading_guidelines_dutch.php",
         "de" => "proofreading_guidelines_german.php",
         "it" => "proofreading_guidelines_italian.php",
-    )
+    );
+}
+$proofing->add_entry(new FAQEntry(
+    _("Proofreading Guidelines"),
+    $urls,
+    _("The details of the guidelines we use for proofreading documents.")
 ));
 $proofing->add_entry(new FAQEntry(
     _("Proofreading Summary"),
-    "proofing_summary.pdf",
-    _("Printable (.pdf) two-page summary of the most commonly needed proofreading standards from the Proofreading Guidelines, done as a big example!"),
     array(
+        "en" => "proofing_summary.pdf",
         "fr" => "proofing_summary_french.pdf",
-    )
+    ),
+    _("Printable (.pdf) two-page summary of the most commonly needed proofreading standards from the Proofreading Guidelines, done as a big example!")
 ));
 $proofing->add_entry(new FAQEntry(
     _("Proofreading Quizzes & Tutorials"),
-    "../quiz/start.php?show_only=PQ",
+    array(
+        "en" => "../quiz/start.php?show_only=PQ",
+    ),
     _("Try the proofreading quizzes and tutorials. They are a great walk through the basic Guidelines for beginners and an excellent refresher for old hands.")
 ));
 
 
 // Creating and Managing Projects
 $cp_pm = new FAQSection("Creating and Managing Projects");
+if($use_pgdp_urls)
+{
+    $urls = array(
+        "en" => "https://www.pgdp.net/wiki/DP_Official_Documentation:CP_and_PM/Project_Managing_FAQ",
+        "fr" => "https://www.pgdp.net/wiki/DP_Official_Documentation:CP_and_PM/FAQ_gestion_de_projet",
+        "pt" => "https://www.pgdp.net/wiki/DP_Official_Documentation:CP_and_PM/Gest%C3%A3o_de_Projectos",
+    );
+}
+else
+{
+    $urls = array(
+        "en" => "pm-faq.php",
+    );
+}
 $cp_pm->add_entry(new FAQEntry(
     _("Project Managers' FAQ"),
-    "pm-faq.php",
+    $urls,
     _("Information for new or aspiring Project Managers. Project Managers are people who manage the progress of a particular project (\"book\") through this site.")
 ));
+if($use_pgdp_urls)
+{
+    $urls = array(
+        "en" => "https://www.pgdp.net/wiki/DP_Official_Documentation:CP_and_PM/Content_Providing_FAQ",
+        "fr" => "https://www.pgdp.net/wiki/DP_Official_Documentation:CP_and_PM/FAQ_fourniture_de_contenu",
+        "pt" => "https://www.pgdp.net/wiki/DP_Official_Documentation:CP_and_PM/Fornecimento_de_Conte%C3%BAdos",
+    );
+}
+else
+{
+    $urls = array(
+        "en" => "cp.php",
+    );
+}
 $cp_pm->add_entry(new FAQEntry(
     _("Content Providers' FAQ"),
-    "cp.php",
+    $urls,
     _("Overview for people who want to contribute material to be proofread on this site.")
 ));
 $cp_pm->add_entry(new FAQEntry(
     _("Scanning FAQ"),
-    "scanning.php",
+    array(
+        "en" => "scanning.php",
+    ),
     _("Basic information on scanners and how to use them, based on our experiences.")
 ));
 
 
 // Formatting
 $formatting = new FAQSection(_("Formatting"));
-$formatting->add_entry(new FAQEntry(
-    _("Formatting Guidelines"),
-    "document.php",
-    _("The full details of the guidelines we use for formatting documents."),
-    array(
+if($use_pgdp_urls)
+{
+    $urls = array(
+        "en" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Formatting/Formatting_Guidelines",
+        "fr" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Formatting/French/Directives_de_Formatage",
+        "pt" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Formatting/Portuguese/Regras_de_Formata%C3%A7%C3%A3o",
+        "nl" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Formatting/Dutch/Formatteer-Richtlijnen",
+        "de" => "https://www.pgdp.net/wiki/DP_Official_Documentation:Formatting/German/Formatierungsrichtlinien",
+        "it" => "formatting_guidelines_italian.php",
+    );
+}
+else
+{
+    $urls = array(
+        "en" => "document.php",
         "fr" => "formatting_guidelines_francaises.php",
         "pt" => "formatting_guidelines_portuguese.php",
         "nl" => "formatting_guidelines_dutch.php",
         "de" => "formatting_guidelines_german.php",
         "it" => "formatting_guidelines_italian.php",
-    )
+    );
+}
+$formatting->add_entry(new FAQEntry(
+    _("Formatting Guidelines"),
+    $urls,
+    _("The full details of the guidelines we use for formatting documents.")
 ));
 $formatting->add_entry(new FAQEntry(
     _("Formatting Summary"),
-    "formatting_summary.pdf",
+    array(
+        "en" => "formatting_summary.pdf",
+    ),
     _("Printable (.pdf) two-page summary of the most commonly needed formatting standards from the Formatting Guidelines, done as a big example!")
 ));
 $formatting->add_entry(new FAQEntry(
     _("Formatting Quiz"),
-    "../quiz/start.php?show_only=FQ",
+    array(
+        "en" => "../quiz/start.php?show_only=FQ",
+    ),
     _("Try the formatting quiz. It is a great walk through the basic Guidelines for beginners and an excellent refresher for old hands.")
 ));
 
@@ -144,22 +241,30 @@ $formatting->add_entry(new FAQEntry(
 $tools = new FAQSection(sprintf(_("%s Tools"), $site_abbreviation));
 $tools->add_entry(new FAQEntry(
     _("DP Custom Proofreading Font"),
-    "font_sample.php",
+    array(
+        "en" => "font_sample.php",
+    ),
     _("Sample and download the custom DP font.")
 ));
 $tools->add_entry(new FAQEntry(
     _("WordCheck FAQ"),
-    "wordcheck-faq.php",
+    array(
+        "en" => "wordcheck-faq.php",
+    ),
     _("Information on the WordCheck engine and interface.")
 ));
 $tools->add_entry(new FAQEntry(
     _("Standard Proofreading Interface Help"),
-    "prooffacehelp.php?i_type=0",
+    array(
+        "en" => "prooffacehelp.php?i_type=0",
+    ),
     _("Help for the Standard Proofreading Interface.")
 ));
 $tools->add_entry(new FAQEntry(
     _("Enhanced Proofreading Interface Help"),
-    "prooffacehelp.php?i_type=1",
+    array(
+        "en" => "prooffacehelp.php?i_type=1",
+    ),
     _("Help for the Enhanced Proofreading Interface.")
 ));
 
@@ -168,21 +273,38 @@ $tools->add_entry(new FAQEntry(
 $mentoring = new FAQSection(_("Mentoring"));
 $mentoring->add_entry(new FAQEntry(
     _("Mentors' Page"),
-    "$code_url/tools/proofers/for_mentors.php",
+    array(
+        "en" => "$code_url/tools/proofers/for_mentors.php",
+    ),
     _("A page detailing currently available mentor projects.")
 ));
 
 
 // PP & PPV
 $ppv = new FAQSection(_("Post-Processing and Verification"));
+if($use_pgdp_urls)
+{
+    $urls = array(
+        "en" => "https://www.pgdp.net/wiki/DP_Official_Documentation:PP_and_PPV/Post-Processing_FAQ",
+        "fr" => "https://www.pgdp.net/wiki/French/FAQ_post-processing",
+    );
+}
+else
+{
+    $urls = array(
+        "en" => "post_proof.php",
+    );
+}
 $ppv->add_entry(new FAQEntry(
     _("Post-Processing FAQ"),
-    "post_proof.php",
+    $urls,
     _("Information for new and aspiring Post-Processors. Post-Processors process a particular project after it has been proofread and formatted on this site (combining all the pages, making them consistent, fixing problems, and submitting it to Project Gutenberg).")
 ));
 $ppv->add_entry(new FAQEntry(
     _("Post-Processing Verification Guidelines"),
-    "ppv.php",
+    array(
+        "en" => "ppv.php",
+    ),
     _("Information for Post-Processing Verifiers.")
 ));
 
@@ -191,17 +313,23 @@ $ppv->add_entry(new FAQEntry(
 $dev = new FAQSection(_("Suggestions, Bugs, and Development"));
 $dev->add_entry(new FAQEntry(
     _("Translation FAQ"),
-    "translate.php",
+    array(
+        "en" => "translate.php",
+    ),
     _("Information on how site translations work, including how to install and edit them.")
 ));
 $dev->add_entry(new FAQEntry(
     _("Task Center"),
-    "$code_url/tasks.php",
+    array(
+        "en" => "$code_url/tasks.php",
+    ),
     _("Here you will find a list of feature requests and bugs. You may add tasks after searching to see that the issue isn't already there.")
 ));
 $dev->add_entry(new FAQEntry(
     _("Development Process"),
-    "dev_process.php",
+    array(
+        "en" => "dev_process.php",
+    ),
     _("Information and guidelines for developers.")
 ));
 
@@ -210,7 +338,9 @@ $dev->add_entry(new FAQEntry(
 $pg = new FAQSection(_("Project Gutenberg"));
 $pg->add_entry(new FAQEntry(
     _("Project Gutenberg FAQ"),
-    $PG_faq_url,
+    array(
+        "en" => $PG_faq_url,
+    ),
     sprintf(_("The <i>massive</i> FAQ from <a href='%s''>Project Gutenberg</a>."), $PG_home_url)
 ));
 
@@ -284,13 +414,17 @@ class FAQSection
     {
         foreach($this->entries as $entry)
         {
-            echo "<p><a href='$entry->url'>" . html_safe($entry->title) . "</a><br>";
+            $english_url = $entry->urls["en"];
+            echo "<p><a href='$english_url'>" . html_safe($entry->title) . "</a><br>";
             echo "<span style='font-size: 0.8em;'>";
-            if($entry->alt_languages)
+            if(count($entry->urls) > 1)
             {
                 $links = array();
-                foreach($entry->alt_languages as $iso => $url)
+                foreach($entry->urls as $iso => $url)
                 {
+                    if($iso == "en")
+                        continue;
+
                     $links[] = "<a href='$url'>" . lang_name($iso) . "</a>";
                 }
                 // TRANSLATORS: %s is a comma-separated list of language names that link to FAQs in that language
@@ -305,12 +439,11 @@ class FAQSection
 
 class FAQEntry
 {
-    function __construct($title, $url, $text, $alt_languages=NULL)
+    function __construct($title, $urls, $text)
     {
         $this->title = $title;
-        $this->url = $url;
+        $this->urls = $urls;
         $this->text = $text;
-        $this->alt_languages = $alt_languages;
     }
 }
 
