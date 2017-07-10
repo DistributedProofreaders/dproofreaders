a6a65829c779354fcc8fd515c38a74163d435213
Casey Peel <cpeel@users.sourceforge.net>
2017-07-10 23:58:43 +0000

    Don't wrap <ul>s in <p> tags
    This also remove the unnecessary block indent.

--- tools/post_proofers/smooth_reading.php
+++ tools/post_proofers/smooth_reading.php
@@ -38,20 +38,16 @@ if (!$logged_in)
 
 }
 
-{
+echo "<p style='font-size: 120%;'>" . _("The goal of Smooth Reading is to read the text attentively, as for pleasure, with just a little more attention than usual to punctuation, etc. This is NOT full scale proofreading, and comparison with the scans is not needed. Just read it as your normal, sensitized-to-proofreading-errors self, and report any problem that disrupts the sense or the flow of the book. Note that some of these will be due to the author and/or publisher.") . "</p>";
 
-    echo "<p style='font-size: 120%;'>" . _("The goal of Smooth Reading is to read the text attentively, as for pleasure, with just a little more attention than usual to punctuation, etc. This is NOT full scale proofreading, and comparison with the scans is not needed. Just read it as your normal, sensitized-to-proofreading-errors self, and report any problem that disrupts the sense or the flow of the book. Note that some of these will be due to the author and/or publisher.") . "</p>";
+echo "<p>" . _("Errors are reported by adding a comment of the form <blockquote style='color: red; background-color: inherit;'> [**correction or query] <br> </blockquote> immediately after the problem spot. Do not correct or change the problem, just note it in the above format.") . "</p>";
 
-    echo "<p>" . _("Errors are reported by adding a comment of the form <blockquote style='color: red; background-color: inherit;'> [**correction or query] <br> </blockquote> immediately after the problem spot. Do not correct or change the problem, just note it in the above format.") . "</p>";
-
-    echo "<h2>" . _("Examples:") . "</h2>";
-    echo "<p><ul>";
-    echo "<li>" . _("that was the end,[**.] However, the next day") . "</li>";
-    echo "<li>" . _("that was the end[**.] However, the next day") . "</li>";
-    echo "<li>" . _("that was the emd.[**end] However, the next day") . "</li>";
-    echo "</ul></p>";
-
-}
+echo "<h2>" . _("Examples:") . "</h2>";
+echo "<ul>";
+echo "<li>" . _("that was the end,[**.] However, the next day") . "</li>";
+echo "<li>" . _("that was the end[**.] However, the next day") . "</li>";
+echo "<li>" . _("that was the emd.[**end] However, the next day") . "</li>";
+echo "</ul>";
 
 show_projects_for_smooth_reading();
 
