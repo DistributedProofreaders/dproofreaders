308f5ad475f920c9010f30e4e7ef7ca01607f1fe
Ray Papworth <raypapworth@gmail.com>
2017-08-03 10:20:24 +0000

    &amp; added

--- stats/user_logon_stats.php
+++ stats/user_logon_stats.php
@@ -6,10 +6,10 @@ include_once($relPath.'theme.inc');
 output_header(_("User Logon Statistics"));
 echo "<h1>" . _("User Logon Statistics") . "</h1>";
 
-echo "<img src=\"jpgraph_files/users_logging_on.php?past=day&preceding=hour\"><br>";
-echo "<img src=\"jpgraph_files/users_logging_on.php?past=year&preceding=hour\"><br>";
-echo "<img src=\"jpgraph_files/users_logging_on.php?past=year&preceding=day\"><br>";
-echo "<img src=\"jpgraph_files/users_logging_on.php?past=year&preceding=week\"><br>";
-echo "<img src=\"jpgraph_files/users_logging_on.php?past=year&preceding=fourweek\"><br>";
+echo "<img src=\"jpgraph_files/users_logging_on.php?past=day&amp;preceding=hour\"><br>";
+echo "<img src=\"jpgraph_files/users_logging_on.php?past=year&amp;preceding=hour\"><br>";
+echo "<img src=\"jpgraph_files/users_logging_on.php?past=year&amp;preceding=day\"><br>";
+echo "<img src=\"jpgraph_files/users_logging_on.php?past=year&amp;preceding=week\"><br>";
+echo "<img src=\"jpgraph_files/users_logging_on.php?past=year&amp;preceding=fourweek\"><br>";
 
 // vim: sw=4 ts=4 expandtab
