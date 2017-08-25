680fe8f6d976026f144a45f35a5695855a0b0cff
Casey Peel <cpeel@users.sourceforge.net>
2017-08-25 16:16:39 +0000

    Remove search form code from the PM page

--- tools/project_manager/projectmgr.php
+++ tools/project_manager/projectmgr.php
@@ -4,106 +4,77 @@ include_once($relPath.'base.inc');
 include_once($relPath.'user_is.inc');
 include_once($relPath.'theme.inc');
 include_once($relPath.'metarefresh.inc');
+include_once($relPath.'misc.inc'); // array_get()
 include_once($relPath.'SettingsClass.inc');
 include_once($relPath.'special_colors.inc');
 include_once($relPath.'gradual.inc');
-include_once($relPath.'ProjectSearchForm.inc');
 include_once($relPath.'ProjectSearchResults.inc');
 include_once('projectmgr.inc');
 
 require_login();
 
-if (user_is_PM() && empty($_GET['show'])) {
-    if ($userP['i_pmdefault'] == 0) {
-        metarefresh(0,"projectmgr.php?show=user_all","","");
-        exit();
-    } elseif ($userP['i_pmdefault'] == 1) {
-        metarefresh(0,"projectmgr.php?show=user_active","", "");
-        exit();
-    }
+$default_view = ($userP['i_pmdefault'] == 0 ?  "user_all" : "user_active");
+$show_view = array_get($_GET, 'show', $default_view);
+
+// Redirect to the new search page for bookmarked URLs
+if($show_view == "search")
+{
+    // pull out everything after the ? and redirect
+    $url = "$code_url/tools/search.php" . substr($_SERVER['REQUEST_URI'], stripos($_SERVER['REQUEST_URI'], '?'));
+    metarefresh(0, $url);
 }
 
-output_header(_("Project Search"), NO_STATSBAR);
+if(!user_is_PM())
+{
+    die(_("Your user permissions do not allow access to this script."));
+}
 
-$search_form = new ProjectSearchForm();
+output_header(_("Project Management"), NO_STATSBAR);
 
 $PROJECT_IS_ACTIVE_sql = "(state NOT IN ('".PROJ_SUBMIT_PG_POSTED."','".PROJ_DELETE."'))";
 
-if (!isset($_GET['show']) || $_GET['show'] == 'search_form') {
+echo_manager_header();
 
-    echo_manager_header();
+// Construct and submit the search query.
 
-    // New proofreaders are having a hard time finding stuff because they
-    // end up on the Project Search page instead of the starting round page.
-    // See if we can't help them out by pointing them to the starting
-    // round page.
-    $pagesproofed = get_pages_proofed_maybe_simulated();
-    if($pagesproofed < 100)
-    {
-        echo "<div class='callout'>";
-        echo "<div class='calloutheader'>";
-        echo _("Looking for projects to proofread?");
-        echo "</div>";
-
-        echo "<p>" . sprintf(_("If you're looking for projects to proofread, consider using the list on the <a href='%1\$s'>%2\$s</a> round page instead of this search form."), "$code_url/{$ELR_round->relative_url}#{$ELR_round->id}", $ELR_round->id) . "</p>";
-        echo "</p>";
-
-        echo "<p><small>";
-        echo _("After a period of time, this message will no longer appear.");
-        echo "</small></p>";
-        echo "</div>";
-    }
-
-    echo "
-        <center>
-        <h1>", _("Search for Projects"), "</h1>
-        "._("Search for projects matching the following criteria:")."<br>";
-
-    $search_form->render('projectmgr.php');
-
-    echo "</center>";
+if($show_view == "site_active") {
+    $view_title = _("Active Projects");
+    $condition = $PROJECT_IS_ACTIVE_sql;
+} elseif($show_view == "user_all") {
+    $condition = "username = '$pguser'";
+    $view_title = _("Your Projects");
 } else {
-    echo_manager_header();
-
-    // Construct and submit the search query.
-
-    if ($_GET['show'] == 'search') {
-        $condition = $search_form->get_widget_contribution();
-    } elseif ($_GET['show'] == "site_active") {
-        $condition = $PROJECT_IS_ACTIVE_sql;
-    } elseif ($_GET['show'] == "user_all") {
-        $condition = "username = '$pguser'";
-    } else {
-        // ($_GET['show'] == "user_active")
-        // plus some corner cases
-        $condition = "$PROJECT_IS_ACTIVE_sql AND username = '$pguser'";
-    }
-
-    // Determine whether to use special colors or not
-    // (this does not affect the alternating between two
-    // background colors) in the project listing.
-    $userSettings =& Settings::get_Settings($pguser);
-    $show_special_colors = !$userSettings->get_boolean('hide_special_colors');
-
-    echo "<h1>", _("Search Results"), "</h1>\n";
-
-    $search_results = new ProjectSearchResults($search_form->get_page_size());
-    $results_offset = intval(@$_GET['results_offset']);
-    $search_results->render($condition, $results_offset, $show_special_colors);
-
-    // special colours legend
-    // Don't display if the user has selected the
-    // setting "Show Special Colors: No".
-    // The legend has been put at the bottom of the page
-    // because the use of colors is presumably mostly
-    // useful as a check that no typo was made. The
-    // exact color probably doesn't matter and,
-    // furthermore, the PM 'knows' the project and
-    // what's so special about it.
-    if (!$userSettings->get_boolean('hide_special_colors')) {
-        echo_special_legend(" 1 = 1");
-    }
+    // "user_active" plus some corner cases
+    $condition = "$PROJECT_IS_ACTIVE_sql AND username = '$pguser'";
+    $view_title = _("Your Active Projects");
 }
+
+// Determine whether to use special colors or not
+// (this does not affect the alternating between two
+// background colors) in the project listing.
+$userSettings =& Settings::get_Settings($pguser);
+$show_special_colors = !$userSettings->get_boolean('hide_special_colors');
+
+echo "<h1>", _("Project Management"), "</h1>\n";
+echo "<h2>$view_title</h2>";
+
+$search_results = new ProjectSearchResults(100);
+$results_offset = intval(@$_GET['results_offset']);
+$search_results->render($condition, $results_offset, $show_special_colors);
+
+// special colours legend
+// Don't display if the user has selected the
+// setting "Show Special Colors: No".
+// The legend has been put at the bottom of the page
+// because the use of colors is presumably mostly
+// useful as a check that no typo was made. The
+// exact color probably doesn't matter and,
+// furthermore, the PM 'knows' the project and
+// what's so special about it.
+if (!$userSettings->get_boolean('hide_special_colors')) {
+    echo_special_legend(" 1 = 1");
+}
+
 echo "<br>";
 
 // vim: sw=4 ts=4 expandtab
