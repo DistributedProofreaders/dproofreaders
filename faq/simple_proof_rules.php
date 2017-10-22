5a70ec8f298c2c7a8b1291d4d6fd84cdfe18b94b
Casey Peel <cpeel@users.sourceforge.net>
2017-10-22 20:30:13 +0000

    Update styles and layout for new proofing text

--- faq/simple_proof_rules.php
+++ faq/simple_proof_rules.php
@@ -4,7 +4,5 @@ include_once($relPath.'base.inc');
 include_once($relPath.'theme.inc');
 
 output_header("Beginners' Simple Proofreading Rules", NO_STATSBAR);
-echo "<br><br>";
 include_once($relPath.'simple_proof_text.inc');
-echo "<br><br>";
 // vim: sw=4 ts=4 expandtab
