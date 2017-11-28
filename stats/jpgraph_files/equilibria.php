c393232037c2d1077cc5caf2fa8b2ca66186c097
Casey Peel <cpeel@users.sourceforge.net>
2017-11-28 16:16:00 +0000

    Include theme.inc for output_header()

--- stats/jpgraph_files/equilibria.php
+++ stats/jpgraph_files/equilibria.php
@@ -1,6 +1,7 @@
 <?php
 $relPath="./../../pinc/";
 include_once($relPath.'base.inc');
+include_once($relPath.'theme.inc');
 include_once($relPath.'misc.inc');
 include_once($relPath.'stages.inc');
 include_once($relPath.'project_states.inc');
