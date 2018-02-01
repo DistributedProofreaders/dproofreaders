1bee0bc2da386f679cbc2c4001d2d405ce6c29bc
Ray Papworth <raypapworth@gmail.com>
2018-02-01 21:10:18 +0000

    include metarefresh.inc moved from search.php
    to ProjectSearchResultsConfig.inc

--- tools/search.php
+++ tools/search.php
@@ -2,7 +2,6 @@
 $relPath="./../pinc/";
 include_once($relPath.'base.inc');
 include_once($relPath.'theme.inc');
-include_once($relPath.'metarefresh.inc');
 include_once($relPath.'SettingsClass.inc');
 include_once($relPath.'special_colors.inc');
 include_once($relPath.'gradual.inc');
