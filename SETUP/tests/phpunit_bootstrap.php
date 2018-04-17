dbb83ed44742cfa4d8370d7c7a67af7079d9f9b2
Casey Peel <cpeel@users.sourceforge.net>
2018-04-17 13:24:30 +0000

    Add autoloader for DP classes
    If PHP encounters a class that isn't defined, it will run an
    autoloader to attempt to load it before failing with a runtime
    exception. We already use this for the unit tests, move it so
    the entire codebase can use it too.

--- SETUP/tests/phpunit_bootstrap.php
+++ SETUP/tests/phpunit_bootstrap.php
@@ -3,18 +3,3 @@ global $relPath;
 $relPath='../../pinc/';
 include_once($relPath.'base.inc');
 include_once($relPath.'misc.inc');
-
-function dp_class_autoloader($class)
-{
-    global $relPath;
-    if(is_file($relPath.$class.".inc"))
-    {
-        include_once($relPath.$class.".inc");
-    }
-    elseif(is_file($relPath.$class."Class.inc"))
-    {
-        include_once($relPath.$class."Class.inc");
-    }
-}
-
-spl_autoload_register('dp_class_autoloader');
