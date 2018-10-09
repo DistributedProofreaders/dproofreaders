DistributedProofreaders/dproofreaders

commit f102b2ef462c75a795ad7d15d784370cfcbbd49e

Consolidate role access to User class
Move the requesting/granting/revoking of user roles into the User
class so it's all centralized in one place.

tools/modify_access.php
@@ -4,8 +4,6 @@ include_once($relPath.'base.inc');
 include_once($relPath.'user_is.inc');
 include_once($relPath.'stages.inc');
 include_once($relPath.'maybe_mail.inc');
-include_once($relPath.'access_log.inc');
-include_once($relPath.'SettingsClass.inc');
 include_once($relPath.'User.inc');
 include_once($relPath.'slim_header.inc');
 
@@ -14,8 +12,7 @@ require_login();
 $subject_username = array_get($_POST, 'subject_username', null);
 $notify_user = array_get($_POST, 'notify_user', null);
 
-if (check_username($subject_username) != '')
-    die("Invalid parameter subject_username.");
+$user = new User($subject_username);
 
 list($can_grant,$can_revoke) = user_can_modify_access_of($subject_username);
 if ( !$can_grant && !$can_revoke )
@@ -145,12 +142,11 @@ foreach ( $actions as $activity_id => $action_type )
 {
     echo "<br>\n";
     echo "$action_type $activity_id ...<br>\n";
-    $yesno = ( $action_type == 'grant' ? 'yes' : 'no' );
 
-    $userSettings =& Settings::get_Settings($subject_username);
-    $userSettings->set_value("$activity_id.access", $yesno);
-
-    log_access_change( $subject_username, $pguser, $activity_id, $action_type );
+    if($action_type == 'grant')
+        $user->grant_access("$activity_id.access", $pguser);
+    else
+        $user->revoke_access("$activity_id.access", $pguser);
 }
 
 echo "<br>\n";
@@ -161,10 +157,9 @@ echo "Hit 'Back' to return to user's detail page. (And you may need to reload.)<
 
 // -----------------------------------------------------------------------------
 
-function notify_user($username,$actions)
+function notify_user($username, $actions)
 {
     global $site_name, $site_signoff;
-    $user = new User($username);
     if ((count($actions) == 1) && (array_search('grant',$actions) !== false))
     {
         // Special case: If the user has been granted access to

pinc/User.inc
@@ -1,5 +1,7 @@
 <?php
 include_once($relPath.'misc.inc');
+include_once($relPath.'access_log.inc');
+include_once($relPath.'SettingsClass.inc');
 
 class NonexistentUserException extends Exception { }
 
@@ -118,6 +120,31 @@ class User
     public function save()
     {
         throw new NotImplementedException();
+    }
+
+    public function grant_access($activity, $requester)
+    {
+        $this->_change_access("grant", $activity, $requester);
+    }
+
+    public function revoke_access($activity, $requester)
+    {
+        $this->_change_access("revoke", $activity, $requester);
+    }
+
+    public function request_access($activity)
+    {
+        $this->_change_access("request", $activity, 'n/a');
+    }
+
+    private function _change_access($action_type, $activity, $requester)
+    {
+        $userSettings =& Settings::get_Settings($this->username);
+        if($action_type == "request")
+            $userSettings->set_value($activity, "requested");
+        else
+            $userSettings->set_boolean($activity, $action_type == 'grant');
+        log_access_change($this->username, $requester, $activity, $action_type);
     }
 
     // Static function to determine if the specified username is associated

tools/request_access.php
@@ -4,8 +4,7 @@ include_once($relPath.'base.inc');
 include_once($relPath.'maybe_mail.inc');
 include_once($relPath.'stages.inc');
 include_once($relPath.'slim_header.inc');
-include_once($relPath.'access_log.inc');
-include_once($relPath.'SettingsClass.inc');
+include_once($relPath.'User.inc');
 
 require_login();
 
@@ -59,12 +58,10 @@ else
             break;
 
         case 'sat-available':
+            $user = new User($pguser);
             if ( $stage->after_satisfying_minima == 'REQ-AUTO' )
             {
-                $userSettings =& Settings::get_Settings($pguser);
-                $userSettings->set_true("$stage_id.access");
-
-                log_access_change( $pguser, 'AUTO-GRANTED', $stage_id, 'grant' );
+                $user->grant_access("$stage_id.access", 'AUTO_GRANTED');
                 echo _('Access has been granted!');
             }
             elseif ( $stage->after_satisfying_minima == 'REQ-HUMAN' )
@@ -77,11 +74,7 @@ else
 
                 maybe_mail( $email_addr, $title, $body );
 
-                $userSettings =& Settings::get_Settings($pguser);
-                $userSettings->set_value("$stage_id.access", "requested");
-
-                log_access_change( $pguser, 'n/a', $stage_id, 'request' );
-
+                $user->request_access("$stage_id.access");
                 echo _('Your request has been submitted and logged.');
             }
             else

commit 08e9f2c56385733e1be8b828e57e1ff8a605f107

Fix user notifications for access updates
Commit f102b2ef broke user notification emails for access updates.

tools/modify_access.php
@@ -154,12 +154,12 @@ foreach ( $actions as $activity_id => $action_type )
 echo "<br>\n";
 echo "Done<br>\n<br>\n";
 if ($notify_user)
-    echo "Notifying user... ".notify_user($subject_username,$actions)."<br>\n<br>\n";
+    echo "Notifying user... ".notify_user($user, $actions)."<br>\n<br>\n";
 echo "Hit 'Back' to return to user's detail page. (And you may need to reload.)<br>\n";
 
 // -----------------------------------------------------------------------------
 
-function notify_user($username, $actions)
+function notify_user($user, $actions)
 {
     global $site_name, $site_signoff;
     if ((count($actions) == 1) && (array_search('grant',$actions) !== false))
@@ -168,7 +168,7 @@ function notify_user($username, $actions)
         // a single round, send a congratulations! email.
         list($activity_id) = array_keys($actions);
         $subject = "DP: You have been granted access to $activity_id!";
-        $message = "Hello $username,\n\nThis is a message from the $site_name website.\n\n" .
+        $message = "Hello $user->username,\n\nThis is a message from the $site_name website.\n\n" .
                    "Congratulations, you have been granted access to $activity_id projects!\n" .
                    "You can access this stage by following the link to it at the Activity Hub.\n\n" .
                    "$site_signoff";
@@ -180,7 +180,7 @@ function notify_user($username, $actions)
     else
     {
         $subject =  "DP: Your access has been modified";
-        $message =  "Hello $username,\n\nThis is a message from the $site_name website.\n\n" .
+        $message =  "Hello $user->username,\n\nThis is a message from the $site_name website.\n\n" .
                     "The following modifications have been made to the stages in which you can work:\n";
         foreach ( $actions as $activity_id => $action_type )
         {