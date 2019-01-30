3ed5d053becf450beaf90b6915621b0c92e330c6
Ray Papworth <raypapworth@gmail.com>
2019-01-30 09:17:22 +0000

    Calculate time from local clock
    Update day and time when day changes. Gets correct locale for day
    and don't need to consider time zone.

--- scripts/time.js
+++ scripts/time.js
@@ -1,16 +1,42 @@
-/*global $ apiUrl*/
+/*global $ apiUrl setInterval*/
 $(function () {
     "use strict";
     var timeSpan = $("#server-time");
+    var day = "";
+    var msec0 = Date.now();
 
-    function showTime(data) {
-        timeSpan.text(data.daytime);
+    function pad2(n) {
+        return ("0" + n).slice(-2);
     }
 
-    function updateClock() {
-        $.getJSON(apiUrl, {'q': 'v1/serverdaytime'}, showTime);
+    function showTime(hour, minute) {
+        timeSpan.text(day + " " + pad2(hour) + ":" + pad2(minute));
     }
 
-    setInterval(updateClock, 60000);
-    updateClock();
+    function resetTime(data) {
+        var hour = data.hour;
+        var minute = data.minute;
+        day = data.day;
+        // milliseconds from midnight
+        var msecs = 60000 * (parseInt(minute) + 60 * parseInt(hour));
+        // milliseconds from epoch to previous midnight
+        msec0 = Date.now() - msecs;
+        showTime(hour, minute);
+    }
+
+    function getServerTime() {
+        $.getJSON(apiUrl, {'q': 'v1/serverdaytime'}, resetTime);
+    }
+
+    function calcTime() {
+        var minutes = Math.round((Date.now() - msec0) / 60000);
+        if (minutes >= 1440) {
+            getServerTime();
+        } else {
+            showTime(Math.floor(minutes / 60), minutes % 60);
+        }
+    }
+
+    setInterval(calcTime, 60000);
+    getServerTime();
 });
