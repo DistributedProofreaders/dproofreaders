64d781867d544e39860382a9f2deb0ee16dff6f0
Casey Peel <cpeel@users.noreply.github.com>
2019-02-18 16:20:00 +0000

    Allow users to zoom in more than 100%
    f43d4da63 enforced maximum image zoom levels to 100%, but users
    need to be able to zoom in more than 100%.

--- tools/mentors/view_page_text_image.php
+++ tools/mentors/view_page_text_image.php
@@ -206,7 +206,7 @@ elseif ($frame=="image") {
 <form method="get" action="view_page_text_image.php">
 <input type="hidden" name="projectid" value="<?php echo $projectid; ?>">
 <input type="hidden" name="page" value="<?php echo $page; ?>">
-<input type="number" name="percent" value="<?php echo $percent; ?>" min="1" max="100" required>%
+<input type="number" name="percent" value="<?php echo $percent; ?>" min="1" max="999" required>%
 <input type="hidden" name="round_id" value="<?php echo $round_id; ?>">
 <input type="hidden" name="frame" value="image">
 <input type="submit" value="<?php echo _("Resize"); ?>" size="3">
