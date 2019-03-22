575548886cd51ecdc0e70193dea15656a87edfbf
Ray Papworth <31585092+70ray@users.noreply.github.com>
2019-03-22 00:48:14 +0000

    Allow project holds to be managed from PM page
    Consolidate add/remove project hold into Project() and expose them
    on the PM page.

---
+++ tools/remove_project_hold.php
@@ -0,0 +1,27 @@
+<?php
+
+// Allow authorized users to remove a hold on a project.
+
+$relPath="./../pinc/";
+include_once($relPath.'base.inc');
+include_once($relPath.'Project.inc'); // validate_projectID() Project() remove_holds()
+include_once($relPath.'metarefresh.inc'); // metarefresh()
+
+require_login();
+
+$projectid = validate_projectID('projectid', @$_POST['projectid']);
+
+$project = new Project($projectid);
+if (!$project->can_be_managed_by_current_user)
+{
+    die(_('You are not authorized to manage this project.'));
+}
+
+$return_uri = urldecode($_POST['return_uri']);
+$states = array($_POST['curr_state']);
+
+$project->remove_holds($states);
+$title = _("Removing hold");
+metarefresh(0, $return_uri, $title, $title);
+
+// vim: sw=4 ts=4 expandtab
