91e40b9fcc10c4bc2e7c2be13ed79525b05fc664
Casey Peel <cpeel@users.noreply.github.com>
2019-07-28 16:30:36 +0000

    Add new PP alert email content
    The script now supports two separate monthly emails:
    * one sent at the beginning of the month
    * one sent mid-month
    
    Update the crontab to run the mid-month script.

--- crontab/notify_old_pp.php
+++ crontab/notify_old_pp.php
@@ -13,25 +13,32 @@ if(!requester_is_localhost())
 
 $projects_by_PPer = get_pp_projects_past_threshold();
 
+// if the script is run before the 14th of the month, send out the first email
+// otherwise send out the second one
+if(strftime("%d", time()) < 14)
+{
+    $which_message = "first";
+}
+else
+{
+    $which_message = "second";
+}
+
 // now send them notifications
 foreach($projects_by_PPer as $PPer => $projects)
 {
-    send_pp_reminders($PPer, $projects);
+    send_pp_reminders($PPer, $projects, $which_message);
 }
 
 //---------------------------------------------------------------------------
 
-function send_pp_reminders($PPer, $projects)
+function send_pp_reminders($PPer, $projects, $which_message)
 {
-    global $code_url, $general_help_email_addr, $forums_url,
-           $site_name, $site_signoff, $site_abbreviation;
+    global $code_url, $db_requests_email_addr,
+           $site_signoff, $site_abbreviation;
     global $pp_alert_threshold_days;
 
-    $urlbase = "$code_url/project.php?id=";
-
-    $numprojs = count($projects);
-
-    $projects_list = "";
+    $projects_list = [];
     foreach($projects as $project)
     {
         $nameofwork = $project["nameofwork"];
@@ -40,65 +47,61 @@ function send_pp_reminders($PPer, $projects)
         $modifieddate = strftime("%e %B %Y", $project["modifieddate"]);
         $lastvisitdate = strftime("%e %B %Y", $project["lastvisitdate"]);
 
-        $url = $urlbase . $projectid;
         $work_details  = "$nameofwork by $authorsname ($projectid)";
         $time_details = "[checked out since $modifieddate, project last visited $lastvisitdate]";
-        $projects_list .= "$work_details\n$time_details\n    $url\n\n";
+        $projects_list[] = "$work_details\n$time_details\n    $code_url/project.php?id=$projectid";
     }
 
-    echo "\nNotifying $PPer about $numprojs project(s):\n$projects_list";
+    $projects_list_string = implode("\n\n", $projects_list);
+
+    $numprojs = count($projects);
+    echo "\nNotifying $PPer about $numprojs project(s):\n$projects_list_string\n\n";
 
     $user = new User($PPer);
     $email = $user->email;
 
-    if($numprojs == 1)
+
+    $message = [];
+    if($which_message == "first")
     {
-        // only one project
-        $url = $urlbase . $projects[0]["projectid"];
+        // TRANSLATORS: %s is the site abbreviation (eg: 'DP')
+        $subject = sprintf(_("%s: Renew or Return Post-Processing Projects"), $site_abbreviation);
 
-        $subject = "$site_abbreviation: Status update needed for 1 project checked out for PPing over $pp_alert_threshold_days days";
+        $message[] = sprintf(_("Hello %s,"), $PPer);
+        $message[] = _("This is an automated message.");
+        $message[] = sprintf(_("Our database system indicates that you have had one or more projects checked out for Post-Processing for more than %d days:"), $pp_alert_threshold_days);
+        $message[] = $projects_list_string;
+        $message[] = _("It is important that books don't get stuck in the Post-Processing process but keep moving towards being posted at Project Gutenberg. As a community, we have a lot vested in each project -- each represents many hours of volunteer work. We are all eager to see each book posted and there is also a risk that that work would be lost should other organizations produce and post a book before we finish.");
 
-        $message = <<<EOF
-Hello $PPer,
+        $message[] = '==' . _("Do you wish to continue or to stop working on these books?") . "==";
+        $message[] = _("If you haven't yet finished and wish to continue working on these books, please visit the project pages listed above. This will update the status of the projects.");
+        $message[] = _("If you no longer wish to have these projects assigned to you, please visit the project pages and select 'Return to Available', or reply to this email and state that you no longer wish to have the projects in question assigned to you so that we may return them to the pool for someone else to work on.");
 
-Our database indicates that you have had a PP project checked out for more than $pp_alert_threshold_days days:
+        $message[] = "==" . _("Have you completed your work?") . "==";
+        $message[] = _("If you have completed your post-processing work, please visit the appropriate link listed above, select the 'Upload for verification' option on the Project Page, and follow the prompts. If you have any special information or comments to pass on, you will be able to leave a message for the verifier during this process. If you have Direct Upload capability and no longer use Post-Processing Verification, please upload each completed project directly to PG.");
 
-$projects_list
-If you haven't yet finished and wish to continue working on this book, please visit $url. This will update the status of the project. If you need help please forward a copy of this email (quoting the information on the book, above) with a brief description of the status to $general_help_email_addr.
+        $message[] = "==" . _("Do you need help with a particular technical aspect of the book?") . "==";
+        $message[] = _("If you have questions about how to handle a difficult table or need some help tidying up awkward illustrations, remember that the DP Post-Processing forum is full of volunteers with a wide range of skills and interests who are able and willing to help.");
 
-If you have completed your work on the book, please visit $url. Select the 'Upload for verification' option and follow the prompts. You will be able to leave a message for the verifier during this process, if you have any special information or comments to pass on.
-
-If you are waiting on missing images or page scans, please add the details to the Missing Page Wiki at: $forums_url/viewtopic.php?t=7584
-
-If you no longer wish to have this text assigned to you please visit the $site_name website Post Processing section and select Return to Available for this book, or forward this email to $general_help_email_addr and state that you would no longer like to have the book in question assigned to you so that we may return it to the available pool for someone else to work on.
-
-$site_signoff
-EOF;
+        $message[] = "==" . _("Are there any problems?") . "==";
+        $message[] = sprintf(_("If any project is missing page images or illustrations, please contact the Project Manager (PM) for assistance. If the PM does not respond within a reasonable amount of time, please email %s and request help concerning any problems."), $db_requests_email_addr);
+        $message[] = $site_signoff;
     }
     else
     {
-        // more than one project
-        $subject = "$site_abbreviation: Status updates needed for $numprojs projects checked out for PPing over $pp_alert_threshold_days days";
+        // TRANSLATORS: %s is the site abbreviation (eg: 'DP')
+        $subject = sprintf(_("%s Second Notice: Renew or Return Post-Processing Projects"), $site_abbreviation);
 
-        $message = <<<EOF
-Hello $PPer,
-
-Our database indicates that you have had $numprojs PP projects checked out for more than $pp_alert_threshold_days days:
-
-$projects_list
-If you wish to continue working on some or all of these books, please visit each such project's home-page (copy the URL listed with the project above and paste it into your browser's address-field). Doing this will update the status of the project and let us know that you are still working on it. If you need help please forward this email, quoting the list of books, with a brief description of the status for each of the various books listed above that you need help with to $general_help_email_addr.
-
-If you have completed your work on any of these books, please visit each such project's home-page (copy the URL listed with the project above and paste it into your browser's address-field). Select the 'Upload for verification' option and follow the prompts. You will be able to leave a message for the verifier during this process, if you have any special information or comments to pass on.
-
-If you are waiting on missing images or page scans, please add the details to the Missing Page Wiki at: $forums_url/viewtopic.php?t=7584
-
-If you no longer wish to have some or all of these books assigned to you please visit the $site_name website Post Processing section and select Return to Available for the books in question or forward this email to $general_help_email_addr and state that you would no longer like to have the books in question assigned to you so that we may return them to the available pool for someone else to work on.
-
-$site_signoff
-EOF;
+        $message[] = sprintf(_("Hello %s,"), $PPer);
+        $message[] = _("An automated reminder was sent to you on the first of the month to ask whether you wished to renew the projects you have checked out for Post-Processing. You are receiving this second automated notice because you have not renewed or returned the projects.");
+        $message[] = $projects_list_string;
+        $message[] = sprintf(_("If you have already notified %s of an expected absence, you may safely ignore this message. Otherwise, any projects in your queue for which a previous notice has been sent may be reclaimed if you do not respond or renew within the next 7 days."), $db_requests_email_addr);
+        $message[] = $site_signoff;
     }
 
-    $mail_accepted = maybe_mail($email, $subject, $message);
+    $message_string = implode("\n\n", $message);
+
+    $mail_accepted = maybe_mail($email, $subject, $message_string);
     if(!$mail_accepted)
     {
         echo "WARNING: Email failed to send for $PPer <$email>\n";
