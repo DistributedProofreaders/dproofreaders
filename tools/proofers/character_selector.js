12885fec47a327a5261f7e8027dd1f0ae9effc3e
Ray Papworth <raypapworth@gmail.com>
2019-08-02 09:47:28 +0000

    Proofreading toolbox revised with new character picker
    Latin-1 only version

---
+++ tools/proofers/character_selector.js
@@ -0,0 +1,34 @@
+/*global $ document top */
+
+$(function () {
+    "use strict";
+
+    var charSelector = $("#char-selector");
+    var largeChar = document.getElementById("large_char");
+
+    function enableBoard(newCode) {
+        // hide the visible key block
+        $(".show", charSelector).removeClass("show");
+        // show the new one
+        $("." + newCode, charSelector).addClass("show");
+        // mark the new selected tab
+        $(".selected-tab", charSelector).removeClass("selected-tab");
+        $("#" + newCode, charSelector).addClass("selected-tab");
+        largeChar.value = ""; // remove old character
+        top.focusText();
+    }
+
+    // find the code of the first selector button
+    var initialCode = $("#selector_row > button")[0].id;
+    enableBoard(initialCode);
+
+    $(".selector_button", charSelector).click(function () {
+        enableBoard(this.id);
+    });
+
+    $(".picker", charSelector).click(function () {
+        top.insertCharacter(this.innerText);
+    }).mouseover(function () {
+        largeChar.value = this.innerText;
+    });
+});
