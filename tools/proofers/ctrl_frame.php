b0503984ec2b03892fda806756973c500b869a06
Ray Papworth <31585092+70ray@users.noreply.github.com>
2019-09-13 05:27:14 +0100

    Improve UI of character piker (#127)
    Update the character picker UI in the proofreading/formatting interface
    to be easier to use. The picker now shows available characters in an
    easier-to-select interface, including a large version of the character
    to be inserted. This new picker primes the interface for more robust
    Unicode support in the future.

--- tools/proofers/ctrl_frame.php
+++ tools/proofers/ctrl_frame.php
@@ -9,9 +9,12 @@ include_once($relPath.'ProofreadingToolbox.inc');
 $round_id = get_enumerated_param($_GET, 'round_id', null, array_keys($Round_for_round_id_));
 $round = get_Round_for_round_id($round_id);
 
-$header_args = array(
-    "body_attributes" => 'onLoad="top.cRef = top.markRef = document.markform;" style="background-color: #CDC0B0; padding: 0; margin: 0;"',
-);
+$header_args = [
+    "js_files" => [
+        "$code_url/tools/proofers/character_selector.js",
+    ],
+    "body_attributes" => "class='control-frame'",
+];
 slim_header(_("Control Frame"), $header_args);
 
 $toolbox = new ProofreadingToolbox();
