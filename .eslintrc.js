1e53b2f62ff2a1fc98e3d67af0e114230e0c0603
Ray Papworth <31585092+70ray@users.noreply.github.com>
2019-10-29 13:15:28 +0000

    Add JS linter config (#152)

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: "eslint:recommended",
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {
+        semi: ["error", "always"],
+        "indent": ["warn", 4],
+        'brace-style': ['warn', '1tbs'],
+        "space-before-blocks": "warn",
+        "no-multi-spaces": ["warn", { ignoreEOLComments: true }],
+        "no-trailing-spaces": "warn",
+        "space-infix-ops": "warn",
+        camelcase: 'warn',
+    }
+};
