afc7df3eee353213d0014a24e6d7feda03aab81f
Casey Peel <cpeel@users.noreply.github.com>
2019-11-04 05:57:53 +0000

    Use per-project character picker
    Use a per-project character picker based on the Project's gylphsets.

--- tools/proofers/ctrl_frame.php
+++ tools/proofers/ctrl_frame.php
@@ -4,8 +4,10 @@ include_once($relPath.'base.inc');
 include_once($relPath.'slim_header.inc');
 include_once($relPath.'stages.inc');
 include_once($relPath.'misc.inc'); // get_enumerated_param()
+include_once($relPath.'Project.inc'); // validate_projectID()
 include_once($relPath.'ProofreadingToolbox.inc');
 
+$projectid = validate_projectID($_GET, $_GET["projectid"]);
 $round_id = get_enumerated_param($_GET, 'round_id', null, array_keys($Round_for_round_id_));
 $round = get_Round_for_round_id($round_id);
 
@@ -19,4 +21,4 @@ $header_args = [
 slim_header(_("Control Frame"), $header_args);
 
 $toolbox = new ProofreadingToolbox();
-$toolbox->output($round);
+$toolbox->output($projectid, $round);
