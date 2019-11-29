a5abd6fe93de6f57c92a4fe49a19a14c37694441
Ray Papworth <raypapworth@gmail.com>
2019-11-29 12:47:21 +0000

    Language options re-arranged, Placeholder for required selectors
    image source default changed to empty string,
    function for language options added

--- tools/project_manager/editproject.php
+++ tools/project_manager/editproject.php
@@ -172,7 +172,7 @@ class ProjectInfoHolder
         $this->genre            = '';
         $this->difficulty_level = ( $pguser == "BEGIN" ? "beginner" : "average" );
         $this->special_code     = '';
-        $this->image_source     = '_internal';
+        $this->image_source     = '';
         $this->image_preparer   = $pguser;
         $this->text_preparer    = $pguser;
         $this->extra_credits    = '';
@@ -215,7 +215,7 @@ class ProjectInfoHolder
         $this->postednum        = '';
         $this->difficulty_level = ( $pguser == "BEGIN" ? "beginner" : "average" );
         $this->special_code     = '';
-        $this->image_source     = '_internal';
+        $this->image_source     = '';
         $this->image_preparer   = $pguser;
         $this->text_preparer    = $pguser;
         $this->extra_credits    = '';
@@ -855,7 +855,7 @@ class ProjectInfoHolder
         echo     "<input type='submit' name='save' value='".attr_safe(_("Save"))."'>";
         echo     "<input type='submit' name='saveAndQuit' value='".attr_safe(_("Save and Go To PM Page"))."'>";
         echo     "<input type='submit' name='saveAndProject' value='".attr_safe(_("Save and Go To Project"))."'>";
-        echo     "<input type='submit' name='quit' value='".attr_safe(_("Quit Without Saving"))."'>";
+        echo     "<input type='submit' name='quit' formnovalidate value='".attr_safe(_("Quit Without Saving"))."'>";
         echo   "</th>";
         echo "</tr>\n";
 
