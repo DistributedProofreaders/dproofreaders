DistributedProofreaders/dproofreaders

commit f102b2ef462c75a795ad7d15d784370cfcbbd49e

Consolidate role access to User class
Move the requesting/granting/revoking of user roles into the User
class so it's all centralized in one place.

tools/request_access.php
@@ -4,8 +4,7 @@ include_once($relPath.'base.inc');
 include_once($relPath.'maybe_mail.inc');
 include_once($relPath.'stages.inc');
 include_once($relPath.'slim_header.inc');
-include_once($relPath.'access_log.inc');
-include_once($relPath.'SettingsClass.inc');
+include_once($relPath.'User.inc');
 
 require_login();
 
@@ -59,12 +58,10 @@ else
             break;
 
         case 'sat-available':
+            $user = new User($pguser);
             if ( $stage->after_satisfying_minima == 'REQ-AUTO' )
             {
-                $userSettings =& Settings::get_Settings($pguser);
-                $userSettings->set_true("$stage_id.access");
-
-                log_access_change( $pguser, 'AUTO-GRANTED', $stage_id, 'grant' );
+                $user->grant_access("$stage_id.access", 'AUTO_GRANTED');
                 echo _('Access has been granted!');
             }
             elseif ( $stage->after_satisfying_minima == 'REQ-HUMAN' )
@@ -77,11 +74,7 @@ else
 
                 maybe_mail( $email_addr, $title, $body );
 
-                $userSettings =& Settings::get_Settings($pguser);
-                $userSettings->set_value("$stage_id.access", "requested");
-
-                log_access_change( $pguser, 'n/a', $stage_id, 'request' );
-
+                $user->request_access("$stage_id.access");
                 echo _('Your request has been submitted and logged.');
             }
             else

pinc/User.inc
@@ -1,5 +1,7 @@
 <?php
 include_once($relPath.'misc.inc');
+include_once($relPath.'access_log.inc');
+include_once($relPath.'SettingsClass.inc');
 
 class NonexistentUserException extends Exception { }
 
@@ -118,6 +120,31 @@ class User
     public function save()
     {
         throw new NotImplementedException();
+    }
+
+    public function grant_access($activity, $requester)
+    {
+        $this->_change_access("grant", $activity, $requester);
+    }
+
+    public function revoke_access($activity, $requester)
+    {
+        $this->_change_access("revoke", $activity, $requester);
+    }
+
+    public function request_access($activity)
+    {
+        $this->_change_access("request", $activity, 'n/a');
+    }
+
+    private function _change_access($action_type, $activity, $requester)
+    {
+        $userSettings =& Settings::get_Settings($this->username);
+        if($action_type == "request")
+            $userSettings->set_value($activity, "requested");
+        else
+            $userSettings->set_boolean($activity, $action_type == 'grant');
+        log_access_change($this->username, $requester, $activity, $action_type);
     }
 
     // Static function to determine if the specified username is associated

tools/modify_access.php
@@ -4,8 +4,6 @@ include_once($relPath.'base.inc');
 include_once($relPath.'user_is.inc');
 include_once($relPath.'stages.inc');
 include_once($relPath.'maybe_mail.inc');
-include_once($relPath.'access_log.inc');
-include_once($relPath.'SettingsClass.inc');
 include_once($relPath.'User.inc');
 include_once($relPath.'slim_header.inc');
 
@@ -14,8 +12,7 @@ require_login();
 $subject_username = array_get($_POST, 'subject_username', null);
 $notify_user = array_get($_POST, 'notify_user', null);
 
-if (check_username($subject_username) != '')
-    die("Invalid parameter subject_username.");
+$user = new User($subject_username);
 
 list($can_grant,$can_revoke) = user_can_modify_access_of($subject_username);
 if ( !$can_grant && !$can_revoke )
@@ -145,12 +142,11 @@ foreach ( $actions as $activity_id => $action_type )
 {
     echo "<br>\n";
     echo "$action_type $activity_id ...<br>\n";
-    $yesno = ( $action_type == 'grant' ? 'yes' : 'no' );
 
-    $userSettings =& Settings::get_Settings($subject_username);
-    $userSettings->set_value("$activity_id.access", $yesno);
-
-    log_access_change( $subject_username, $pguser, $activity_id, $action_type );
+    if($action_type == 'grant')
+        $user->grant_access("$activity_id.access", $pguser);
+    else
+        $user->revoke_access("$activity_id.access", $pguser);
 }
 
 echo "<br>\n";
@@ -161,10 +157,9 @@ echo "Hit 'Back' to return to user's detail page. (And you may need to reload.)<
 
 // -----------------------------------------------------------------------------
 
-function notify_user($username,$actions)
+function notify_user($username, $actions)
 {
     global $site_name, $site_signoff;
-    $user = new User($username);
     if ((count($actions) == 1) && (array_search('grant',$actions) !== false))
     {
         // Special case: If the user has been granted access to

commit b45d20c9b521efc12b19c6b51d5e5f6d302a009f

Correct AUTO-GRANTED modifier_username
f102b2e accidentally renamed AUTO-GRANTED to AUTO_GRANTED. This
corrects new entries and includes an upgrade script to rectify
existing entries.

SETUP/upgrade/13/20200110_update_access_log.php
@@ -1,0 +1,24 @@
+<?php
+$relPath='../../../pinc/';
+include_once($relPath.'base.inc');
+
+header('Content-type: text/plain');
+
+// ------------------------------------------------------------
+
+echo "Updating access_log.modifier_username, setting AUTO_GRANTED to AUTO-GRANTED.\n";
+
+$sql = "
+    UPDATE access_log
+    SET modifier_username = 'AUTO-GRANTED'
+    WHERE modifier_username = 'AUTO_GRANTED'
+";
+
+$result = mysqli_query(DPDatabase::get_connection(), $sql)
+    or die( mysqli_error(DPDatabase::get_connection()) );
+
+// ------------------------------------------------------------
+
+echo "\nDone!\n";
+
+// vim: sw=4 ts=4 expandtab

tools/request_access.php
@@ -61,7 +61,7 @@ else
             $user = new User($pguser);
             if ( $stage->after_satisfying_minima == 'REQ-AUTO' )
             {
-                $user->grant_access($stage_id, 'AUTO_GRANTED');
+                $user->grant_access($stage_id, 'AUTO-GRANTED');
                 echo _('Access has been granted!');
             }
             elseif ( $stage->after_satisfying_minima == 'REQ-HUMAN' )