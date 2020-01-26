62556f3b462385b39c0a32ce75a1aa206142b640
Casey Peel <cpeel@users.noreply.github.com>
2020-01-26 01:16:18 +0000

    Show error message if project topic create fails
    If the forum software fails to create the project forum topic, return
    that information to the user instead of blindly continuing. This is
    not a perfect solution but it's better than what can before it.

--- tools/proofers/project_topic.php
+++ tools/proofers/project_topic.php
@@ -4,6 +4,7 @@ $relPath="./../../pinc/";
 include_once($relPath.'base.inc');
 include_once($relPath.'Project.inc');
 include_once($relPath.'forum_interface.inc');
+include_once($relPath.'slim_header.inc');
 
 require_login();
 
@@ -14,7 +15,26 @@ $project = new Project($project_id);
 
 $topic_id = $project->ensure_topic();
 
-$redirect_url = get_url_to_view_topic($topic_id);
-header("Location: $redirect_url");
+if($topic_id == NULL)
+{
+    // an error occurred trying to create the topic, let the user know
+    $title = _("Failure creating project topic");
+    slim_header($title);
+    echo "<h1>$title</h1>";
+    echo "<p class='error'>" . sprintf(_("An error occurred when trying to create a new forum topic for this project. Please contact <a href='%s'>a site manager</a> and let them know the following information."), "mailto:$site_manager_email_addr") . "</p>";
+
+    echo <<<EOF
+<pre>
+ProjectID: $project_id
+Title: $project->nameofwork
+Author: $project->authorsname
+</pre>
+EOF;
+}
+else
+{
+    $redirect_url = get_url_to_view_topic($topic_id);
+    header("Location: $redirect_url");
+}
 
 // vim: sw=4 ts=4 expandtab
