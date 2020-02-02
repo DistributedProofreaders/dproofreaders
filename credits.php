91fe71df52456ab2f9c1f21cf32d3704ae346871
Casey Peel <cpeel@users.noreply.github.com>
2020-02-02 02:02:48 +0000

    Show attributions for 3rd party software
    Some of our 3rd party software requires attribution. This adds a
    credits page and shows the bundled software and gives the necessary
    attribution.

---
+++ credits.php
@@ -0,0 +1,34 @@
+<?php
+$relPath="./pinc/";
+include_once($relPath.'base.inc');
+include_once($relPath.'theme.inc');
+
+$title = _("Credits");
+output_header($title);
+
+echo "<h1>$title</h1>";
+
+echo "<h2>" . _("Developers") . "</h2>";
+
+echo "<p>" . sprintf(_("This software brought to you by many volunteer developers across the world and years. Recent developer contribution after the move to git can be seen on the project's <a href='%s'>Github Contributors page</a>."), "https://github.com/DistributedProofreaders/dproofreaders/graphs/contributors"). "</p>";
+
+echo "<h2>" . _("Open Source"). "</h2>";
+
+echo "<p>" . _("The following open source code is used and bundled with the DP code.") . "</p>";
+
+echo "<ul>";
+$dir_iter = new RecursiveDirectoryIterator($code_dir);
+$files = new RecursiveIteratorIterator($dir_iter);
+foreach($files as $file_info)
+{
+    $file = $file_info->getPathname();
+    if(basename($file) != "details.json")
+        continue;
+    $details = json_decode(file_get_contents($file), TRUE);
+    echo "<li>";
+    echo "<b>" . $details["name"] . "</b><br>";
+    echo "<a href='" . $details["url"] . "'>" . $details["url"] . "</a><br>";
+    echo "<a href='" . $details["license_url"] . "'>" . $details["license"] . "</a>";
+    echo "</li>";
+}
+echo "</ul>";
---
+++ pinc/3rdparty/README.md
@@ -0,0 +1,14 @@
+# 3rd Party Code
+
+This directory contains 3rd party code -- that is other open source code that
+the DP code relies on for various functionality.
+
+When adding files to this directory, you must ensure that the software's
+license allows it to be included. Include a copy of the license in the
+directory as well as a `README.md` file describing what it is, where it came
+from, and how to update it.
+
+You must also include a `details.json` file that includes meta information
+about the software. This is used on the open source attributions page to
+ensure we are meeting our legal requirements for attribution-required
+licenses.
