600d0e7a884bbefc037b5f731c3f254755e30fab
ray <70ray@users.noreply.github.com>
2020-02-19 15:04:40 +0000

    Validate text for file replace
    codepoint_validator.inc moved to pinc since now used in proofers and pm
    javascript files reorganised and those used in proofers and pm
    moved to scripts
    extra wrapper div round validator so can have different styles in
    proofing and handle_bad_page

--- tools/proofers/proofing.js
+++ tools/proofers/proofing.js
@@ -1,69 +1,7 @@
-/*global $ goodChars charMatch standardInterface switchConfirm revertConfirm XRegExp */
-
-// goodChars (constructed in character_test.js) matches good unicode characters
-// charMatch (constructed in character_test.js) matches any unicode character
-// possibly with combining marks: non-mark codepoint + 0 or more mark codes
-
-// this function is copied from dp_proof.js
-// could put it in another file misc.js
-function htmlSafe(str) {
-    // Return a version of str that is safe to send as element-content
-    // in an HTML document.
-    // That is, make the following replacements:
-    //    &  ->  &amp;
-    //    <  ->  &lt;
-    //    >  ->  &gt;
-    // This should be equivalent to PHP's
-    //     htmlspecialchars($str,ENT_NOQUOTES)
-    return str
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-}
+/*global $ goodChars validateText standardInterface switchConfirm revertConfirm */
 
 $(function () {
-    // to remove combining chars at line start which are not markable
-    const linecheckRegex = XRegExp("^\\pM", "Amg");
-
     var textArea = document.getElementById("text_data");
-
-    // check each character, if bad mark or remove it
-    function processText(text, clean) {
-        var bad = false;
-
-        function charReplacer(match) {
-            if(goodChars.test(match)) {
-                return match;
-            }
-            if(clean) {
-                return "";
-            }
-            bad = true;
-            return "<span class='bad-char'>" + match + "</span>";
-        }
-
-        text = text.replace(charMatch, charReplacer);
-        return {processedText: text, valid: !bad};
-    }
-
-    function validateText() {
-        var text = textArea.value;
-        text = text.normalize("NFC");
-        text = text.replace(linecheckRegex, "");
-        // replace the text with normalised version
-        textArea.value = text;
-        // convert any markup so does not get interpreted in the checker div
-        text = htmlSafe(text);
-
-        let procResult = processText(text, false);
-        if(procResult.valid) {
-            return true;
-        }
-        $("#checker").css("display", "flex");
-        $("#proofdiv").hide();
-        $("#check-text").html(procResult.processedText);
-        return false;
-    }
 
     // special handling for certain buttons
     // switch layout - validate before confirm
@@ -101,18 +39,6 @@ $(function () {
         if(!validateText()) {
             event.preventDefault();
         }
-    });
-
-    $("#cc-quit").click(function () {
-        $("#checker").hide();
-        $("#proofdiv").show();
-    });
-
-    $("#cc-remove").click(function () {
-        // textArea has already been normalised
-        textArea.value = processText(textArea.value, true).processedText;
-        $("#checker").hide();
-        $("#proofdiv").show();
     });
 
     var above = {
