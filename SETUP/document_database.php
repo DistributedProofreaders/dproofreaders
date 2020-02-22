23aea4edb6ccee4ad38feee08d1935cf6c1f2f64
mlazaric <marko.lazaric@gmail.com>
2020-02-22 01:28:34 +0100

    Generate documentation files for all tables in database

--- SETUP/document_database.php
+++ SETUP/document_database.php
@@ -1,12 +1,102 @@
 <?php
-$relPath='../pinc/';
-include_once($relPath.'base.inc');
-include_once($relPath.'misc.inc');
+
+// If this is not a PHPUnit test, include the dependencies, otherwise the bootstrap file will handle them
+if (!defined('PHPUNIT')) {
+    $relPath='../pinc/';
+    include_once($relPath.'base.inc');
+    include_once($relPath.'misc.inc');
+
+    // Do not define the functions if we are in a PHPUnit test.
+    // The test will define a fake function suitable for testing.
+
+    // ---------- Database functions ----------
+
+    /**
+     * Runs a "DESCRIBE $table_name" query and returns the result.
+     */
+    function query_columns_for_table(string $table_name): array {
+        $result = mysqli_query(DPDatabase::get_connection(), "DESCRIBE $table_name");
+
+        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        mysqli_free_result($result);
+
+        return $rows;
+    }
+
+    /**
+     * Runs a "SHOW TABLES" query and returns the result.
+     */
+    function query_table_names_in_current_database(): array {
+        $result = mysqli_query(DPDatabase::get_connection(), "SHOW TABLES");
+
+        $rows = mysqli_fetch_all($result);
+        mysqli_free_result($result);
+
+        return $rows;
+    }
+
+    // ---------- Other helper functions ----------
+
+    /**
+     * Saves the contents to the specified file path.
+     *
+     * Wrapper around file_put_contents which allows for easier mocking and unit testing.
+     */
+    function write_to_file(string $file_path, string $contents) {
+        file_put_contents($file_path, $contents);
+    }
+}
 
 // ---------- All tables operation functions ----------
 
-// ---------- Operation functions ----------
+/**
+ * Generic function which runs the operation function for each table in the database.
+ *
+ * Tables with 'projectID' prefix are handled in a special way to prevent spam.
+ * Instead of running the operation for each of them, the operation is run for only
+ * the first one and the $file_name and $display_name are set to '$projectid'.
+ *
+ * @param string $directory_path the directory in which to place the generated files
+ * @param callable $operation the operation to perform for each table
+ */
+function run_operation_for_all_tables(string $directory_path, callable $operation) {
+    $table_names = query_table_names_in_current_database();
+    $was_projectid_table_generated = false;
 
+    foreach ($table_names as $row) {
+        $table_name = $row[0];
+
+        if (startswith($table_name, 'projectID')) {
+            if ($was_projectid_table_generated) continue;
+
+            $was_projectid_table_generated = true;
+
+            $operation($table_name, $directory_path . '/$projectid.md', '$projectid');
+        }
+        else {
+            $operation($table_name, $directory_path . '/' . $table_name . '.md', $table_name);
+        }
+    }
+}
+
+/**
+ * Generates a markdown file documenting the table for each table in the database.
+ *
+ * @param string $directory_path the directory in which to place the generated files
+ */
+function generate_files_for_all_tables(string $directory_path) {
+    run_operation_for_all_tables($directory_path, 'generate_file_for_table');
+}
+
+// ---------- Single table operation functions ----------
+
+/**
+ * Generates a markdown file documenting the table.
+ *
+ * @param string $table_name the name of the table in the database, used for looking up columns
+ * @param string $file_path where to generate the file
+ * @param string $display_name the title of the markdown file
+ */
 function generate_file_for_table(string $table_name, string $file_path, string $display_name) {
     $columns = query_columns_for_table($table_name);
 
@@ -14,8 +104,8 @@ function generate_file_for_table(string $table_name, string $file_path, string $
     foreach ($columns as $index => $column_information) {
         $field = $column_information['Field'];
 
-        $column_information['Name'] = $field; // Save field name before adding the hyper link for column definition
-        $column_information['Field'] = "[$field](#$field)";
+        $columns[$index]['Name'] = $field; // Save field name before adding the hyper link for column definition
+        $columns[$index]['Field'] = "[$field](#$field)";
     }
 
     $lines = ['# ' . $display_name, ''];
@@ -27,36 +117,10 @@ function generate_file_for_table(string $table_name, string $file_path, string $
         $lines[] = '## ' . $column_information['Name'];
     }
 
-    file_put_contents($file_path, implode("\n", $lines));
+    $lines[] = '';
+
+    write_to_file($file_path, implode("\n", $lines));
 }
-
-// ---------- Database functions ----------
-
-/**
- * Runs a "DESCRIBE $table_name" query and returns the result.
- */
-function query_columns_for_table(string $table_name): array {
-    $result = mysqli_query(DPDatabase::get_connection(), "DESCRIBE $table_name");
-
-    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    mysqli_free_result($result);
-
-    return $rows;
-}
-
-/**
- * Runs a "SHOW TABLES" query and returns the result.
- */
-function query_table_names_in_current_database(): array {
-    $result = mysqli_query(DPDatabase::get_connection(), "SHOW TABLES");
-
-    $rows = mysqli_fetch_all($result);
-    mysqli_free_result($result);
-
-    return $rows;
-}
-
-// ---------- Other helper functions ----------
 
 /**
  * Creates a markdown table of the data with the only the specified columns shown and in the specified order.
