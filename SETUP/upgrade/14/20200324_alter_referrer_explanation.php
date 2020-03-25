6cd9104c7c2815a8ae32bbe68922fce4d53aaced
Chris Miceli <chrismiceli@outlook.com>
2020-03-24 22:25:26 -0500

    1769: Add a text box for the "Other" choice on registration page
    Add column to users for other explanation
    add column to non_activated_users for other explanation
    add javascript to show new field when other is selected
    update sql to handle new columns

---
+++ SETUP/upgrade/14/20200324_alter_referrer_explanation.php
@@ -0,0 +1,37 @@
+<?php
+$relPath='../../../pinc/';
+include_once($relPath.'base.inc');
+
+header('Content-type: text/plain');
+
+// ------------------------------------------------------------
+
+echo "Adding referrer_explanation column to non_activated_users...\n";
+
+$sql = "
+    ALTER TABLE non_activated_users
+        ADD COLUMN referrer_explanation varchar(256) DEFAULT '' AFTER referrer;
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()));
+
+// ------------------------------------------------------------
+
+echo "Adding referrer_explanation column to users...\n";
+
+$sql = "
+    ALTER TABLE users
+        ADD COLUMN referrer_explanation varchar(256) DEFAULT '' AFTER referrer;
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()));
+
+// ------------------------------------------------------------
+
+echo "\nDone!\n";
+
+// vim: sw=4 ts=4 expandtab
---
+++ accounts/addproofer.js
@@ -0,0 +1,29 @@
+/*global $*/
+
+function AddProofer() {
+  "use strict";
+
+  var that = this;
+  this.initForm = function() {
+    "use strict";
+
+    that.referrerOption = $('select[name=referrer]');
+    that.referrerExplanation = $('#referrer_explanation');
+    that.setOtherExplanation();
+    that.referrerOption.change(that.setOtherExplanation);
+  };
+
+  this.setOtherExplanation = function() {
+    "use string";
+
+    if (that.referrerOption.val() !== 'other') {
+      that.referrerExplanation.hide();
+    } else {
+      that.referrerExplanation.show();
+    }
+  }
+};
+
+var addProofer = new AddProofer();
+
+$(addProofer.initForm);
