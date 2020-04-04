854db2407fb3268c324317a8a7084c237747e28d
Chris Miceli <chrismiceli@outlook.com>
2020-04-04 15:41:17 -0500

    2274: Normalize text input for nameofwork (Title) and authorsname (Author) fields
    remove stop file
    use sprintf
    html_entity_decode some fields

--- SETUP/upgrade/14/20200404_normalize_project_details.php
+++ SETUP/upgrade/14/20200404_normalize_project_details.php
@@ -1,7 +1,6 @@
 <?php
 $relPath='../../../pinc/';
 include_once($relPath.'base.inc');
-include_once($relPath.'misc.inc');
 include_once($relPath.'unicode.inc');
 
 header('Content-type: text/plain');
@@ -24,43 +23,28 @@ while($row = mysqli_fetch_assoc($result))
     $projects[] = $row;
 }
 
-$stop_file = '/tmp/utf8_normalization';
-
-echo <<<EOF
-This will now start normalizing all of the project fields.
-This will likely take a while.
-To stop the process gracefully create $stop_file
-(eg: touch $stop_file) and it will stop after it
-finishes the table it is working on.
-
-EOF;
-
 echo count($projects) . " possible projects to normalize.\n";
 
 foreach($projects as $project)
 {
-    if(file_exists($stop_file))
-    {
-        echo "\n\n";
-        echo "Detected stop file ($stop_file), so stopping before starting " . $project["projectid"] . ".\n";
-        echo "Delete this file and restart to continue.\n";
-        exit();
-    }
+    $sql = sprintf("
+            UPDATE projects SET
+            nameofwork    = '%s',
+            authorsname   = '%s',
+            comments      = '%s',
+            extra_credits = '%s'
+            WHERE projectid = '%s';
+        ",
+        mysqli_real_escape_string(DPDatabase::get_connection(), utf8_normalize(html_entity_decode($project["nameofwork"]))),
+        mysqli_real_escape_string(DPDatabase::get_connection(), utf8_normalize(html_entity_decode($project["authorsname"]))),
+        mysqli_real_escape_string(DPDatabase::get_connection(), utf8_normalize($project["comments"])),
+        mysqli_real_escape_string(DPDatabase::get_connection(), utf8_normalize(html_entity_decode($project["extra_credits"]))),
+        $project["projectid"]
+    );
 
-    
-    $sql = "
-        UPDATE projects SET
-        nameofwork    = '".mysqli_real_escape_string(DPDatabase::get_connection(), utf8_normalize($project["nameofwork"]))."',
-        authorsname   = '".mysqli_real_escape_string(DPDatabase::get_connection(), utf8_normalize($project["authorsname"]))."',
-        comments      = '".mysqli_real_escape_string(DPDatabase::get_connection(), utf8_normalize($project["comments"]))."',
-        extra_credits = '".mysqli_real_escape_string(DPDatabase::get_connection(), utf8_normalize($project["extra_credits"]))."'
-        WHERE projectid = '".$project["projectid"]."';
-    ";
-
+    echo "Processing " . $project["projectid"] . "\n";
     mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()) );
 }
-
-echo "\n";
 
 // ------------------------------------------------------------
 
