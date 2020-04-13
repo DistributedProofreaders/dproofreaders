9a684feef1babcce4fd5e388a56dca300103a964
Casey Peel <cpeel@users.noreply.github.com>
2020-04-13 17:51:08 +0000

    Validate pickersets only contain valid codepoints
    Ensure that pickersets only contain characters that are valid in the
    character suite.

--- SETUP/lint_charsuites.php
+++ SETUP/lint_charsuites.php
@@ -6,6 +6,8 @@ include_once($relPath."CharSuites.inc");
 foreach(CharSuites::get_all() as $charsuite)
 {
     echo "Validating charsuite $charsuite->name...\n";
+
+    // Validate that the character suite only contains normalized codepoints
     $nonnormalized_codepoints = $charsuite->get_nonnormalized_codepoints();
     if($nonnormalized_codepoints)
     {
@@ -15,5 +17,24 @@ foreach(CharSuites::get_all() as $charsuite)
             echo sprintf("    %s normalized is %s\n", $orig, $norm);
         }
         exit(1);
+    }
+
+    // Validate pickersets only contain characters within the suite
+    foreach($charsuite->pickerset->get_subsets() as $title => $picker_subset)
+    {
+        foreach($picker_subset as $codepoints)
+        {
+            $string = implode("", convert_codepoint_ranges_to_characters($codepoints));
+            $invalid_chars = get_invalid_characters($string, $charsuite->codepoints);
+            if($invalid_chars)
+            {
+                echo "ERROR: pickerset $title has codepoints not in charsuite:\n";
+                foreach($invalid_chars as $char => $count)
+                {
+                    echo sprintf("    %s: %s\n", $char, utf8_chr_to_hex($char));
+                }
+                exit(1);
+            }
+        }
     }
 }
