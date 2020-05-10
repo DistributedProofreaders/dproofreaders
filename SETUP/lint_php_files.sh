05d52461552aa13f1a85e370d0be68e81246bc5f
Casey Peel <cpeel@users.noreply.github.com>
2020-05-10 19:47:29 +0000

    Update PHP linting to remove magic quotes hack

--- SETUP/lint_php_files.sh
+++ SETUP/lint_php_files.sh
@@ -15,10 +15,7 @@ echo "Checking all .php and .inc files under $BASE_DIR for linting errors..."
 
 for file in `find $BASE_DIR -name "*.php" -o -name "*.inc"`; do
     echo $file
-    # We route stderr to /dev/null to avoid flooding the console with
-    # deprecation errors about magic quotes. After that is resolved
-    # we should remove the redirect of stderr.
-    OUTPUT=`php -l $file 2>/dev/null`
+    OUTPUT=`php -l $file`
     echo $OUTPUT | grep -q 'No syntax errors detected'
     if [ $? -ne 0 ]; then
         echo "Lint failure in $file"
