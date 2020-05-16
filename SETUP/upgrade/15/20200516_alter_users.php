8ec37f41fa01e81b5e4ad0937e2d18dd45900e19
Casey Peel <cpeel@users.noreply.github.com>
2020-05-16 23:17:30 +0000

    Create a new api_key field in the users table
    This will be used to provide per-user access to the API.

---
+++ SETUP/upgrade/15/20200516_alter_users.php
@@ -0,0 +1,35 @@
+<?php
+$relPath='../../../pinc/';
+include_once($relPath.'base.inc');
+
+header('Content-type: text/plain');
+
+// ------------------------------------------------------------
+
+echo "Adding api_key column to users..\n";
+$sql = "
+    ALTER TABLE users
+        ADD COLUMN api_key varchar(32) UNIQUE
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()) );
+
+// ------------------------------------------------------------
+
+echo "Adding index to users..\n";
+$sql = "
+    CREATE INDEX api_key_username
+         ON users (api_key, username)
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()) );
+
+// ------------------------------------------------------------
+
+echo "\nDone!\n";
+
+// vim: sw=4 ts=4 expandtab
