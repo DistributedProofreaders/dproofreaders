12d3654cbb7d656ef39a7cb0f2c603856a927660
Casey Peel <cpeel@users.noreply.github.com>
2020-06-16 16:57:38 +0000

    Support multiple details in a single JSON
    Some directories might contain multiple entities that we want to
    call out separately in the Credits page. While we're here sort
    the listing.

--- credits.php
+++ credits.php
@@ -16,24 +16,41 @@ echo "<h2>" . _("Open Source"). "</h2>";
 
 echo "<p>" . _("The following open source code is used and bundled with the DP code.") . "</p>";
 
+$credit_details = load_credit_details($code_dir);
+asort($credit_details);
 echo "<ul>";
-$dir_iter = new PermissiveRecursiveDirectoryIterator($code_dir);
-$files = new RecursiveIteratorIterator($dir_iter);
-foreach($files as $file_info)
+foreach($credit_details as $detail)
 {
-    $file = $file_info->getPathname();
-    if(basename($file) != "details.json")
-        continue;
-    $details = json_decode(file_get_contents($file), TRUE);
     echo "<li>";
-    echo "<b>" . $details["name"] . "</b><br>";
-    echo "<a href='" . $details["url"] . "'>" . $details["url"] . "</a><br>";
-    echo "<a href='" . $details["license_url"] . "'>" . $details["license"] . "</a>";
+    echo "<b>" . $detail["name"] . "</b><br>";
+    echo "<a href='" . $detail["url"] . "'>" . $detail["url"] . "</a><br>";
+    echo "<a href='" . $detail["license_url"] . "'>" . $detail["license"] . "</a>";
     echo "</li>";
 }
 echo "</ul>";
 
 #----------------------------------------------------------------------------
+
+function load_credit_details($code_dir)
+{
+    $credit_details = [];
+    $dir_iter = new PermissiveRecursiveDirectoryIterator($code_dir);
+    $files = new RecursiveIteratorIterator($dir_iter);
+    foreach($files as $file_info)
+    {
+        $file = $file_info->getPathname();
+        if(basename($file) != "details.json")
+            continue;
+        $details = json_decode(file_get_contents($file), TRUE);
+        if(isAssoc($details))
+            $details = [ $details ];
+        foreach($details as $detail)
+        {
+            $credit_details[$detail["name"]] = $detail;
+        }
+    }
+    return $credit_details;
+}
 
 # Ignore exceptions when iterating over the directory, such as permission
 # errors from SETUP/
@@ -46,4 +63,11 @@ class PermissiveRecursiveDirectoryIterator extends RecursiveDirectoryIterator {
             return new RecursiveArrayIterator(array());
         }
     }
+}
+
+# https://stackoverflow.com/questions/173400/how-to-check-if-php-array-is-associative-or-sequential
+function isAssoc(array $arr)
+{
+    if (array() === $arr) return false;
+    return array_keys($arr) !== range(0, count($arr) - 1);
 }
