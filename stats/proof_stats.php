a3e5991d878ce2a8196552a6d6a56f43b70a799a
Casey Peel <cpeel@users.noreply.github.com>
2020-07-25 03:06:18 +0000

    Remove public stats and make private the default
    Now that all statistics require a login to access, remove the public
    option and default everyone to private.

--- stats/proof_stats.php
+++ stats/proof_stats.php
@@ -19,41 +19,31 @@ output_header($title);
 
 echo "<h1>$title</h1>\n";
 
-$sql_anonymous = mysqli_real_escape_string(DPDatabase::get_connection(), _("Anonymous"));
-
-// if user logged on
-if($pguser)
-{
-    // hide names of users who don't want even logged on people to see their names
-    $proofreader_expr = "IF(u_privacy = ".PRIVACY_ANONYMOUS.",'$sql_anonymous', username)";
-} 
-else
-{
-    // hide names of users who don't want unlogged on people to see their names
-    $proofreader_expr = "IF(u_privacy != ".PRIVACY_PUBLIC.",'$sql_anonymous', username)";
-}
-
-$subtitle = sprintf( _('Users with the Highest Number of Pages Saved-as-Done in Round %s'), $tally_name );
-
-echo "<h2>$subtitle</h2>\n";
+echo "<p>" . sprintf( _('Users with the Highest Number of Pages Saved-as-Done in Round %s'), $tally_name ) . "</p>";
 
 $users_tallyboard = new TallyBoard( $tally_name, 'U' );
 
 list($joined_with_user_page_tallies,$user_page_tally_column) =
     $users_tallyboard->get_sql_joinery_for_current_tallies('users.u_id');
 
-$sql_upt_column_name = mysqli_real_escape_string(DPDatabase::get_connection(),
-    // TRANSLATORS: %s is a page tally name (i.e. 'P1' or 'F2' or 'R*')
-    sprintf(_("%s Pages Completed"), $tally_name));
-dpsql_dump_themed_query("
+// TRANSLATORS: %s is a page tally name (i.e. 'P1' or 'F2' or 'R*')
+$sql_upt_column_name = sprintf(_("%s Pages Completed"), $tally_name);
+
+$sql = sprintf("
     SELECT
-        $proofreader_expr AS '" . mysqli_real_escape_string(DPDatabase::get_connection(), _("Proofreader")) . "',
-        $user_page_tally_column AS '$sql_upt_column_name'
+        IF(u_privacy = %d, '%s', username) AS '%s',
+        $user_page_tally_column AS '%s'
     FROM users $joined_with_user_page_tallies
     WHERE $user_page_tally_column > 0
     ORDER BY 2 DESC, 1 ASC
     LIMIT 100
-", 1, DPSQL_SHOW_RANK);
+", PRIVACY_ANONYMOUS,
+    DPDatabase::escape(_("Anonymous")),
+    DPDatabase::escape(_("Proofreader")),
+    DPDatabase::escape($sql_upt_column_name)
+);
+
+dpsql_dump_themed_query($sql, 1, DPSQL_SHOW_RANK);
 
 echo "<br>\n";
 
