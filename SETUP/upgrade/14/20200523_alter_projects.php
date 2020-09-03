10d45b58e7f778e0175fdabac1bc3d775db3970c
Casey Peel <cpeel@users.noreply.github.com>
2020-09-03 04:51:40 +0000

    End upgrade scripts with 'Done'
    Convention is to output 'Done' at the end of all upgrade scripts so
    the user knows it completed successfully.

--- SETUP/upgrade/14/20200523_alter_projects.php
+++ SETUP/upgrade/14/20200523_alter_projects.php
@@ -16,4 +16,8 @@ echo "$sql\n";
 
 mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()));
 
+// ------------------------------------------------------------
+
+echo "\nDone!\n";
+
 // vim: sw=4 ts=4 expandtab
