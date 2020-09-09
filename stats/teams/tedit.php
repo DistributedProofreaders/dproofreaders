DistributedProofreaders/dproofreaders

commit 1587d6d06744fbdfc3c33a0a2b7f4a76eb06703b

Allow a dynamic number of team memberships
Replace the 3-team hard-coded columns in the users table with a new
one-to-many mapping table (user_teams_membership). This keeps the
team limit to 3, but it is now dynamically configurable in teams.inc.

stats/teams/tedit.php
@@ -44,7 +44,7 @@ elseif (isset($_POST['edQuit']))
 {
     $title = _("Quit Without Saving");
     $desc = _("Quitting without saving...");
-    metarefresh(4,"tdetail.php?tid=$tid",$title,$desc);
+    metarefresh(0,"tdetail.php?tid=$tid",$title,$desc);
     exit;
 }
 elseif (isset($_POST['edPreview']))
@@ -117,9 +117,9 @@ elseif (isset($_POST['edMake']))
         mysqli_query(DPDatabase::get_connection(), sprintf("
             UPDATE user_teams
             SET
-                teamname='%s',
-                team_info='%s',
-                webpage='%s'
+                teamname = LEFT('%s', 50),
+                team_info = '%s',
+                webpage = LEFT('%s', 255),
             WHERE id='%s'
         ", mysqli_real_escape_string(DPDatabase::get_connection(), stripAllString(trim($_POST['teamname']))),
             mysqli_real_escape_string(DPDatabase::get_connection(), stripAllString($_POST['text_data'])),

SETUP/db_schema.sql
@@ -759,14 +759,29 @@ CREATE TABLE `user_teams` (
   `owner` int(10) unsigned NOT NULL default '0',
   `created` int(20) NOT NULL default '0',
   `member_count` int(20) NOT NULL default '0',
-  `active_members` int(11) NOT NULL default '0',
-  `daily_average` int(11) NOT NULL default '0',
   `avatar` varchar(25) NOT NULL default 'avatar_default.png',
   `icon` varchar(25) NOT NULL default 'icon_default.png',
   `topic_id` int(10) default NULL,
   `latestUser` mediumint(9) NOT NULL default '0',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `teamname` (`teamname`)
+);
+# --------------------------------------------------------
+
+#
+# Table structure for table `user_teams_membership`
+#
+# Creation:
+# Last update:
+#
+
+CREATE TABLE `user_teams_membership` (
+  `u_id` int(11) unsigned NOT NULL,
+  `t_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`u_id`,`t_id`),
+  KEY (`t_id`),
+  FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`),
+  FOREIGN KEY (`t_id`) REFERENCES `user_teams` (`id`)
 );
 # --------------------------------------------------------
 
@@ -798,9 +813,6 @@ CREATE TABLE `users` (
   `u_profile` int(10) unsigned NOT NULL default '0',
   `u_intlang` varchar(25) default '',
   `u_privacy` tinyint(1) default '0',
-  `team_1` int(10) unsigned NOT NULL default '0',
-  `team_2` int(10) unsigned NOT NULL default '0',
-  `team_3` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`username`),
   UNIQUE KEY `username` (`username`),
   KEY `u_id` (`u_id`),

SETUP/tests/UserTest.php
@@ -146,8 +146,8 @@ class UserTest extends PHPUnit\Framework\TestCase
     public function testSetters()
     {
         $user = new User();
-        $user->team_1 = 1;
-        $this->assertEquals($user->team_1, 1);
+        $user->i_theme = 1;
+        $this->assertEquals($user->i_theme, 1);
     }
 
     public function testSetNewImmutable()

SETUP/upgrade/15/20200815_create_team_membership.php
@@ -1,0 +1,96 @@
+<?php
+$relPath='../../../pinc/';
+include_once($relPath.'base.inc');
+
+header('Content-type: text/plain');
+
+// ------------------------------------------------------------
+
+echo "Creating user_teams_membership table\n";
+
+$sql = "
+    CREATE TABLE user_teams_membership (
+        u_id int unsigned NOT NULL,
+        t_id int unsigned NOT NULL,
+        PRIMARY KEY (u_id, t_id),
+        KEY (t_id),
+        FOREIGN KEY (u_id) REFERENCES users(u_id),
+        FOREIGN KEY (t_id) REFERENCES user_teams(id)
+    )
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()));
+
+// ------------------------------------------------------------
+
+echo "Populating new table from users table\n";
+
+$sql = "
+    INSERT INTO user_teams_membership (u_id, t_id)
+        SELECT u_id, team_1
+        FROM users
+        WHERE team_1 <> 0
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()));
+
+$sql = "
+    INSERT INTO user_teams_membership (u_id, t_id)
+        SELECT u_id, team_2
+        FROM users
+        WHERE team_2 <> 0
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()));
+
+$sql = "
+    INSERT INTO user_teams_membership (u_id, t_id)
+        SELECT u_id, team_3
+        FROM users
+        WHERE team_3 <> 0
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()));
+
+// ------------------------------------------------------------
+
+echo "Dropping team_# columns from users table\n";
+
+$sql = "
+    ALTER TABLE users
+        DROP COLUMN team_1,
+        DROP COLUMN team_2,
+        DROP COLUMN team_3
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()));
+
+// ------------------------------------------------------------
+
+echo "Dropping active_members and daily_average column from user_teams table\n";
+
+$sql = "
+    ALTER TABLE user_teams
+        DROP COLUMN active_members,
+        DROP COLUMN daily_average
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()));
+
+// ------------------------------------------------------------
+
+echo "\nDone!\n";
+
+// vim: sw=4 ts=4 expandtab

crontab/del_teams.php
@@ -5,21 +5,24 @@ include_once($relPath.'misc.inc');
 
 require_localhost_request();
 
-$old_date = time() - 7776000;
-    
-$result = mysqli_query(DPDatabase::get_connection(), "SELECT * FROM user_teams WHERE active_members <= 1");
+// Delete teams created more than 90 days that have no members
 
-while ($row = mysqli_fetch_assoc($result)) {
-    if ($row['created'] <= $old_date && $row['active_members'] == 0)
-    {
-        $del = mysqli_query(DPDatabase::get_connection(), "DELETE FROM user_teams WHERE id = ".$row['id']."");
-    }
-    elseif ($row['created'] <= $old_date && $row['active_members'] == 1)
-    {
-        $mbrCheck = mysqli_query(DPDatabase::get_connection(), "SELECT * FROM users WHERE {$row['id']} IN (team_1, team_2, team_3)");
-        if (mysqli_num_rows($mbrCheck) == 0)
-        {
-            $del = mysqli_query(DPDatabase::get_connection(), "DELETE FROM user_teams WHERE id = ".$row['id']."");
-        }
-    }
+$old_date = time() - 7776000; // 90 days
+
+$sql = sprintf("
+    SELECT id, teamname
+    FROM user_teams
+    WHERE
+        created <= %d AND
+        (
+            SELECT count(*)
+            FROM user_teams_membership
+            WHERE t_id = user_teams.id
+        ) = 0
+", $old_date);
+$result = DPDatabase::query($sql);
+
+while (list($id, $teamname) = mysqli_fetch_row($result)) {
+    echo "Deleting team $teamname as it has no members after 90 days\n";
+    DPDatabase::query("DELETE FROM user_teams WHERE id = $id");
 }

pinc/Team.inc
@@ -1,0 +1,34 @@
+<?php
+include_once($relPath.'misc.inc');
+
+// This is an incomplete abstration around the Teams
+
+class Team
+{
+    // static functions
+
+    static public function log_recent_join($tid, $uid)
+    {
+        $sql = sprintf("
+            UPDATE user_teams
+            SET latestUser = %d, member_count = member_count+1
+            WHERE id = %d
+        ", $uid, $tid);
+
+        DPDatabase::query($sql);
+    }
+
+    static public function active_member_count($tid)
+    {
+        $sql = sprintf("
+            SELECT count(*)
+            FROM user_teams_membership
+            WHERE t_id = %d
+        ", $tid);
+
+        $result = DPDatabase::query($sql);
+        list($count) = mysqli_fetch_row($result);
+        mysqli_free_result($result);
+        return $count;
+    }
+}

pinc/User.inc
@@ -186,6 +186,48 @@ class User
         throw new NotImplementedException();
     }
 
+    public function load_teams()
+    {
+        $sql = sprintf("
+            SELECT t_id
+            FROM user_teams_membership
+            WHERE u_id = %d
+        ", $this->u_id);
+        $result = DPDatabase::query($sql);
+        $teams = [];
+        while($row = mysqli_fetch_assoc($result))
+        {
+            $teams[] = $row["t_id"];
+        }
+        mysqli_free_result($result);
+        return $teams;
+    }
+
+    public function add_team($team_id)
+    {
+        if(in_array($team_id, $this->load_teams()))
+        {
+            return;
+        }
+
+        $sql = sprintf("
+            INSERT INTO user_teams_membership
+            SET u_id = %d, t_id = %d
+        ", $this->u_id, $team_id);
+        DPDatabase::query($sql);
+
+        Team::log_recent_join($team_id, $this->u_id);
+    }
+
+    public function remove_team($team_id)
+    {
+        $sql = sprintf("
+            DELETE FROM user_teams_membership
+            WHERE u_id = %d AND t_id = %d
+        ", $this->u_id, $team_id);
+        DPDatabase::query($sql);
+    }
+
     //
     // Activity access functions
     //

pinc/page_tally.inc
@@ -73,14 +73,9 @@ function page_tallies_add( $tally_name, $username, $amount )
 
     // update page tally for each team
     $team_tallyboard = new TallyBoard( $tally_name, 'T' );
-    // (The 'array_unique' shouldn't be necessary, but just in case.)
-    $teams = array_unique(array($user->team_1, $user->team_2, $user->team_3));
-    foreach ($teams as $team_id)
+    foreach ($user->load_teams() as $team_id)
     {
-        if ( $team_id != 0 )
-        {
-            $team_tallyboard->add_to_tally( $team_id, $amount );
-        }
+        $team_tallyboard->add_to_tally( $team_id, $amount );
     }
 }
 

commit 0045a34b41c5a1fdda5c8b2451bd89ea466d1430

Fix invalid SQL
1587d6d06 regressed saving team edits.

stats/teams/tedit.php
@@ -119,7 +119,7 @@ elseif (isset($_POST['edMake']))
             SET
                 teamname = LEFT('%s', 50),
                 team_info = '%s',
-                webpage = LEFT('%s', 255),
+                webpage = LEFT('%s', 255)
             WHERE id='%s'
         ", mysqli_real_escape_string(DPDatabase::get_connection(), stripAllString(trim($_POST['teamname']))),
             mysqli_real_escape_string(DPDatabase::get_connection(), stripAllString($_POST['text_data'])),