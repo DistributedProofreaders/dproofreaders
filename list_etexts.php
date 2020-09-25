094365566e9324671a64c8221222520f645b7d51
Casey Peel <cpeel@users.noreply.github.com>
2020-09-25 03:41:03 +0000

    Use a unicode star instead of graphics

--- list_etexts.php
+++ list_etexts.php
@@ -14,19 +14,19 @@ $offset   = get_integer_param($_GET, 'offset',    0, 0, NULL);
 $boilerplate = _("These e-texts are the product of hundreds of hours of labor donated by all of our volunteers. The list is sorted with the most recently submitted e-texts at the top. You can sort them based upon your own preferences by clicking below. Enjoy!!");
 
 if($x == "g") {
-    $type = "Gold";
+    $type = "gold";
     $title = _("Completed Gold E-Texts");
     $state = SQL_CONDITION_GOLD;
     $info = _("Below is the list of Gold e-texts that have been produced on this site. Gold e-texts are books that have passed through all phases of proofreading, formatting, and post-processing. They have been submitted to Project Gutenberg and are now available for your enjoyment and download.");
     $rss_content = "posted";
 } elseif ($x == "s") {
-    $type = "Silver";
+    $type = "silver";
     $title = _("In Progress Silver E-Texts");
     $state = SQL_CONDITION_SILVER;
     $info = _("Below is the list of Silver e-texts that have almost completed processing on our site. Silver e-texts are books that have passed through all phases of proofreading and formatting and are now in the post-processing phase. Post-processing is the final assembly stage in which one volunteer performs a series of checks for consistency and correctness before the e-book is submitted to Project Gutenberg for your enjoyment and download.");
     $rss_content = "postprocessing";
 } elseif ($x == "b") {
-    $type = "Bronze";
+    $type = "bronze";
     $title = _("Now Proofreading Bronze E-Texts");
     $state = SQL_CONDITION_BRONZE;
     $info = _("Below is the list of Bronze e-texts that are currently available for proofreading on this site. Bronze e-texts are what our newest volunteers see and what you can work on now by logging in. These e-texts are in the initial stages of proofreading where everyone has a chance to correct any OCR errors which may be found. After going through a number of other phases, the e-text then goes to an experienced volunteer for final assembly (post-processing), after which the e-text is submitted to Project Gutenberg for your enjoyment and download.");
@@ -41,20 +41,21 @@ $extra_args = array(
     "head_data" => "<link rel='alternate' type='application/rss+xml' href='$code_url/feeds/backend.php?content=$rss_content&type=rss' title='$attr_safe_title' />",
 );
 
-output_header($title, SHOW_STATSBAR, $extra_args);
+output_header($title, NO_STATSBAR, $extra_args);
 
-echo "<h1 style='color: $type;'>$title</h1>";
+echo "<h1>$title</h1>";
 
 $listsuffix = "&amp;sort=$sort&amp;per_page=$per_page";
 $menu = array();
 foreach(array("g" => _("Gold"), "s" => _("Silver"), "b"=> _("Bronze")) as $key => $type_option)
 {
-    if($type == $type_option)
-        $menu[] = $type;
+    if($x == $key)
+        $menu[] = $type_option;
     else
         $menu[] = "<a href='list_etexts.php?x=$key$listsuffix'>$type_option</a>";
 }
 echo "<p>" .  implode($menu, " | ") . "</p>";
+echo "<div class='star-$type' style='float: left; width: 1.5em;'>★</div>";
 echo "<p>$info</p>";
 echo "<p>$boilerplate</p>";
 
