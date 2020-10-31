DistributedProofreaders/dproofreaders

commit 5a21c6a8aebddbc87f33c6f4594df79ee88b5810

Update queries in some tools files (#393)
Update SQL queries in an array of files in tools/.

tools/project_manager/automodify.php
@@ -102,12 +102,16 @@ echo "<pre>\n";
 
 if ($one_project) {
     $verbose = $GLOBALS['testing'];
-    $condition = "projectid = '$one_project'";
+    $condition = sprintf("projectid = '%s'", DPDatabase::escape($one_project));
 
     // log tracetimes
     $tracetime = time();
-    mysqli_query(DPDatabase::get_connection(), "INSERT INTO job_logs (filename, tracetime, event, comments)
-               VALUES ('automodify.php', $tracetime, 'BEGIN', 'running for single proj $one_project')");
+    $sql = sprintf("
+        INSERT INTO job_logs (filename, tracetime, event, comments)
+        VALUES ('automodify.php', %d, 'BEGIN', 'running for single proj %s')",
+        $tracetime,
+        DPDatabase::escape($one_project));
+    DPDatabase::query($sql);
 
 
 
@@ -118,26 +122,31 @@ if ($one_project) {
     for ( $rn = 1; $rn <= MAX_NUM_PAGE_EDITING_ROUNDS; $rn++ )
     {
         $round = get_Round_for_round_number($rn);
-        $condition .= "
-            OR state = '{$round->project_available_state}'
-            OR state = '{$round->project_complete_state}'
-            OR state = '{$round->project_bad_state}'
-        ";
+        $condition .= sprintf("
+            OR state = '%s'
+            OR state = '%s'
+            OR state = '%s'",
+        DPDatabase::escape($round->project_available_state),
+        DPDatabase::escape($round->project_complete_state),
+        DPDatabase::escape($round->project_bad_state));
     }
 
     // log tracetimes
     $tracetime = time();
-    mysqli_query(DPDatabase::get_connection(), "INSERT INTO job_logs (filename, tracetime, event, comments)
-               VALUES ('automodify.php', $tracetime, 'BEGIN', 'running for all eligible projects')");
+    $sql = sprintf("
+        INSERT INTO job_logs (filename, tracetime, event, comments)
+        VALUES ('automodify.php', %d, 'BEGIN', 'running for all eligible projects')",
+        $tracetime);
+    DPDatabase::query($sql);
 
 
 }
-$allprojects = mysqli_query(DPDatabase::get_connection(), "
+$sql = "
     SELECT projectid, state, username, nameofwork
     FROM projects
     WHERE $condition
-    ORDER BY projectid
-");
+    ORDER BY projectid";
+$allprojects = DPDatabase::query($sql);
 // The "ORDER BY" clause isn't essential,
 // it's just there to ensure consistency of order when testing.
 
@@ -217,16 +226,19 @@ while ( $project = mysqli_fetch_assoc($allprojects) ) {
         $n_hours_to_wait = 4;
         $max_reclaimable_time = time() - $n_hours_to_wait * 60 * 60;
 
-        $res = mysqli_query(DPDatabase::get_connection(), "
+        $sql = sprintf("
             SELECT image
             FROM $projectid
-            WHERE state IN ('$round->page_out_state','$round->page_temp_state')
-                AND $round->time_column_name <= $max_reclaimable_time
-            ORDER BY image ASC
-        ");
-        if ( !$res )
-        {
-            echo DPDatabase::log_error(), "\n";
+            WHERE state IN ('%s','%s')
+                AND $round->time_column_name <= %d
+            ORDER BY image ASC",
+            $round->page_out_state,
+            $round->page_temp_state,
+            $max_reclaimable_time);
+        try {
+            $res = DPDatabase::query($sql);
+        } catch(DPQueryError $error) {
+            echo "$error->message\n";
             echo "Skipping further processing of this project.\n";
             continue;
         }
@@ -333,16 +345,25 @@ if (!$one_project)
     // log tracetimes
     $tracetimea = time();
     $tooktime = $tracetimea - $tracetime;
-    mysqli_query(DPDatabase::get_connection(), "INSERT INTO job_logs (filename, tracetime, event, comments)
-               VALUES ('automodify.php', $tracetimea, 'MIDDLE', 'pre autorelease, $tooktime seconds so far')");
+    $sql = sprintf("
+        INSERT INTO job_logs (filename, tracetime, event, comments)
+        VALUES ('automodify.php', %d, 'MIDDLE', 'pre autorelease, %d seconds so far')",
+        $tracetimea,
+        $tooktime);
+    DPDatabase::query($sql);
 
     autorelease();
 
     // log tracetimes
     $tracetimea = time();
     $tooktime = $tracetimea - $tracetime;
-    mysqli_query(DPDatabase::get_connection(), "INSERT INTO job_logs (filename, tracetime, event, comments)
-               VALUES ('automodify.php', $tracetimea, 'END', 'post autorelease, started at $tracetime, took $tooktime seconds')");
+    $sql = sprintf("
+        INSERT INTO job_logs (filename, tracetime, event, comments)
+        VALUES ('automodify.php', %d, 'END', 'post autorelease, started at %d, took %d seconds')",
+        $tracetimea,
+        $tracetime,
+        $tooktime);
+    DPDatabase::query($sql);
 
 }
 else
@@ -351,8 +372,13 @@ else
     // log tracetimes
     $tracetimea = time();
     $tooktime = $tracetimea - $tracetime;
-    mysqli_query(DPDatabase::get_connection(), "INSERT INTO job_logs (filename, tracetime, event, comments)
-               VALUES ('automodify.php', $tracetimea, 'END', 'end single, started at $tracetime, took $tooktime seconds')");
+    $sql = sprintf("
+        INSERT INTO job_logs (filename, tracetime, event, comments)
+        VALUES ('automodify.php', %d, 'END', 'end single, started at %d, took %d seconds')",
+        tracetimea,
+        tracetime,
+        tooktime);
+    DPDatabase::query($sql);
 
 
     echo "<META HTTP-EQUIV=\"refresh\" CONTENT=\"0 ;URL=$refresh_url\">";

tools/project_manager/add_files.php
@@ -157,7 +157,10 @@ if ( $loading_tpnv )
     system("cp *.jpg " . escapeshellarg($dest_project_dir));
     echo "</pre>\n";
 
-    $result = mysqli_query(DPDatabase::get_connection(), "UPDATE projects SET state = 'project_new_waiting_app' WHERE projectid = '$projectid'");
+    $sql = sprintf(
+        "UPDATE projects SET state = 'project_new_waiting_app' WHERE projectid = '%s'",
+        DPDatabase::escape($projectid));
+    $result = DPDatabase::query($sql);
 }
 else
 {
@@ -246,7 +249,9 @@ class Loader
 
         // Get the set of all 'image' fields in the page-table.
         $db_entries = array();
-        $res = mysqli_query(DPDatabase::get_connection(), "SELECT image,master_text FROM $this->projectid");
+        validate_projectID($this->projectid);
+        $sql = "SELECT image,master_text FROM $this->projectid";
+        $res = DPDatabase::query($sql);
         while( list($image,$text) = mysqli_fetch_row($res) )
         {
             $db_entries[$image] = $text;
@@ -378,11 +383,12 @@ class Loader
         // --------------------------------
 
         // Find out how long the 'image' field is.
-        $res = mysqli_query(DPDatabase::get_connection(), "
+        validate_projectID($this->projectid);
+        $sql = "
             SELECT image
             FROM $this->projectid
-            LIMIT 0
-        ") or die(DPDatabase::log_error());
+            LIMIT 0";
+        $res = DPDatabase::query($sql);
         $field_data = mysqli_fetch_field_direct($res, 0);
         $this->image_field_len = $field_data->length;
 

tools/project_manager/autorelease.inc
@@ -68,12 +68,13 @@ function autorelease_for_round( $round )
     echo "\n";
     echo "Starting autorelease for round {$round->id}...\n";
 
-    $q_res = mysqli_query(DPDatabase::get_connection(), "
+    $sql = sprintf("
         SELECT *
         FROM queue_defns
-        WHERE round_id='{$round->id}'
-        ORDER BY ordering
-    ") or die(DPDatabase::log_error());
+        WHERE round_id='%s'
+        ORDER BY ordering",
+        DPDatabase::escape($round->id));
+    $q_res = DPDatabase::query($sql);
 
     if ( mysqli_num_rows($q_res) == 0 )
     {
@@ -109,12 +110,13 @@ function autorelease_for_round( $round )
 
     // Release of Different types of Projects from various logical queues
 
-    $q_res = mysqli_query(DPDatabase::get_connection(), "
+    $sql = sprintf("
         SELECT *
         FROM queue_defns
-        WHERE enabled AND round_id='{$round->id}'
-        ORDER BY ordering
-    ") or die(DPDatabase::log_error());
+        WHERE enabled AND round_id='%s'
+        ORDER BY ordering",
+        DPDatabase::escape($round->id));
+    $q_res = DPDatabase::query($sql);
 
     while ( $qd = mysqli_fetch_object($q_res) )
     {
@@ -226,14 +228,15 @@ function maybe_release_projects(
     // Projects that are in this round's waiting state,
     // don't have a hold in that state,
     // and, if $qd is set, satisfy its project selector.
-    $waiting_res = mysqli_query(DPDatabase::get_connection(), "
+    $sql = sprintf("
         SELECT *
         FROM projects
             LEFT OUTER JOIN project_holds USING (projectid, state)
-        WHERE state = '{$round->project_waiting_state}' $and_extra_condition
+        WHERE state = '%s' $and_extra_condition
             AND project_holds.state IS NULL
-        ORDER BY modifieddate ASC, nameofwork ASC
-    ") or die(DPDatabase::log_error());
+        ORDER BY modifieddate ASC, nameofwork ASC",
+        DPDatabase::escape($round->project_waiting_state));
+    $waiting_res = DPDatabase::query($sql);
 
     $n_projects->waiting = mysqli_num_rows($waiting_res);
 
@@ -363,13 +366,13 @@ class ReleaseRestrictor
         // ----------------------------------------------------------
         // First, get the set of all authors with works in this round
         $this->this_round_authors = array();
-        $author_res =
-            mysqli_query(DPDatabase::get_connection(), "
+        $sql = sprintf("
                 SELECT authorsname
                 FROM projects
-                WHERE state = '{$round->project_available_state}'
-                ORDER BY authorsname
-            ") or die(DPDatabase::log_error());
+                WHERE state = '%s'
+                ORDER BY authorsname",
+                DPDatabase::escape($round->project_available_state));
+        $author_res = DPDatabase::query($sql);
         while ( $author_row = mysqli_fetch_assoc($author_res) )
         {
             $author = $author_row['authorsname'];
@@ -385,13 +388,13 @@ class ReleaseRestrictor
         // ----------------------------------------------------------
         // Next, get the set of all PMs with works in this round
         $this->this_round_pms = array();
-        $pm_res =
-            mysqli_query(DPDatabase::get_connection(), "
+        $sql = sprintf("
                 SELECT username
                 FROM projects
-                WHERE state = '{$round->project_available_state}'
-                ORDER BY username
-            ") or die(DPDatabase::log_error());
+                WHERE state = '%s'
+                ORDER BY username",
+                DPDatabase::escape($round->project_available_state));
+        $pm_res = DPDatabase::query($sql);
         while ( $pm_row = mysqli_fetch_assoc($pm_res) )
         {
             $pm = $pm_row['username'];
@@ -538,21 +541,21 @@ function AP_setup( $round )
     // Instead, we have to loop over the active projects.
 
     // Create the table.
-    mysqli_query(DPDatabase::get_connection(), "
+    $sql = "
         CREATE TEMPORARY TABLE active_page_counts
         (
             projectid  varchar(22),
             pages      INT(4)
-        )
-    ") or die(DPDatabase::log_error());
+        )";
+    DPDatabase::query($sql);
 
     // Get a list of active projects.
-    $projects_res =
-        mysqli_query(DPDatabase::get_connection(), "
+    $sql = sprintf("
         SELECT projectid
         FROM projects
-        WHERE state = '{$round->project_available_state}'
-    ") or die(DPDatabase::log_error());
+        WHERE state = '%s'",
+        DPDatabase::escape($round->project_available_state));
+    $projects_res = DPDatabase::query($sql);
 
     // Run through them and fill up the table.
     while( $project = mysqli_fetch_assoc($projects_res) )
@@ -563,33 +566,35 @@ function AP_setup( $round )
 
 function AP_teardown( $round )
 {
-    mysqli_query(DPDatabase::get_connection(), "
-        DROP TABLE active_page_counts
-    ") or die(DPDatabase::log_error());
+    $sql = "DROP TABLE active_page_counts";
+    DPDatabase::query($sql);
 }
 
 function AP_add_project( $round, $projectid )
 {
-    mysqli_query(DPDatabase::get_connection(), "
+    validate_projectID($projectid);
+    $sql = sprintf("
         INSERT INTO active_page_counts
         SELECT
             '$projectid',
-            SUM( state != '{$round->page_save_state}' ) as pages
-        FROM $projectid
-    ") or die(DPDatabase::log_error());
+            SUM( state != '%s' ) as pages
+        FROM $projectid",
+        DPDatabase::escape($round->page_save_state));
+    DPDatabase::query($sql);
 }
 
 function AP_evaluate_criteria( $round, $cooked_project_selector, $release_criterion )
 {
     // Get the criterion-evaluation environment
     // (values for 'projects', 'pages')
-    $res = mysqli_query(DPDatabase::get_connection(), "
+    $sql = sprintf("
         SELECT
-            SUM(projects.state='{$round->project_available_state}') as projects,
+            SUM(projects.state='%s') as projects,
             SUM(active_page_counts.pages) as pages
         FROM projects NATURAL JOIN active_page_counts
-        WHERE $cooked_project_selector
-    ") or die(DPDatabase::log_error());
+        WHERE $cooked_project_selector",
+        DPDatabase::escape($round->project_available_state));
+    $res = DPDatabase::query($sql);
     $env = mysqli_fetch_assoc($res);
     // print_r($env);
 

tools/project_manager/diff.php
@@ -74,13 +74,15 @@ if(!$format) // no parameter passed
     $format = ($L_format xor $R_format) ? "remove" : "keep";
 }
 
-$query = "
+validate_projectID($projectid);
+$query = sprintf("
     SELECT $L_text_column_name, $R_text_column_name,
-           $L_user_column_name, $R_user_column_name
+        $L_user_column_name, $R_user_column_name
     FROM $projectid
-    WHERE image='$image'";
+    WHERE image='%s'",
+    DPDatabase::escape($image));
 
-$res = mysqli_query(DPDatabase::get_connection(), $query) or die(DPDatabase::log_error());
+$res = DPDatabase::query($query);
 list($L_text, $R_text, $L_user, $R_user) = mysqli_fetch_row($res);
 $can_see_names_for_this_page = can_see_names_for_page($projectid, $image);
 if ( $can_see_names_for_this_page) {
@@ -176,8 +178,14 @@ function do_navigation($projectid, $image, $L_round_num, $R_round_num, $L_user_c
     $navigation_text .= "\n<input type='hidden' name='format' value='$format'>";
     $navigation_text .= "\n" . _("Jump to") . ": <select name='jumpto' onChange='$jump_to_js'>\n";
 
-    $query = "SELECT image, $L_user_column_name, ($L_text_column_name = $R_text_column_name) AS is_empty_diff FROM $projectid ORDER BY image ASC";
-    $res = mysqli_query(DPDatabase::get_connection(),  $query) or die(DPDatabase::log_error());
+    validate_projectID($projectid);
+    $query = "
+        SELECT image,
+            $L_user_column_name,
+            ($L_text_column_name = $R_text_column_name) AS is_empty_diff
+        FROM $projectid
+        ORDER BY image ASC";
+    $res = DPDatabase::query($query);
     $prev_image = "";
     $next_image = "";
     $prev_from_proofer = "";
@@ -280,8 +288,12 @@ function can_see_names_for_page($projectid, $image)
             }
             $fields .= $round->user_column_name;
         }
-        $query = "SELECT $fields from $projectid WHERE image = '$image'";
-        $res = mysqli_query(DPDatabase::get_connection(), $query) or die(DPDatabase::log_error());
+
+        validate_projectID($projectid);
+        $query = sprintf("
+            SELECT $fields from $projectid WHERE image = '%s'",
+            DPDatabase::escape($image));
+        $res = DPDatabase::query($query);
         $page_res = mysqli_fetch_array($res);
         foreach ($page_res as $page_user) {
             if ($page_user == $pguser) {

tools/project_manager/downloadproofed.php
@@ -18,7 +18,11 @@ if ($round_num == 0) {
     $text_column_name = $round->text_column_name;
 }
 
-$result = mysqli_query(DPDatabase::get_connection(), "SELECT $text_column_name FROM $project WHERE image = '$image'"); 
+validate_projectID($project);
+$sql = sprintf("
+    SELECT $text_column_name FROM $project WHERE image = '%s'",
+    DPDatabase::escape($image));
+$result = DPDatabase::query($sql);
 if ($result === FALSE)
 {
     // Likely the project's page-table does not exist (in this database).

tools/project_manager/edit_common.inc
@@ -158,7 +158,7 @@ function difficulty_list($difficulty_level)
 function special_list($special)
 {
     // get info on special days
-    $specs_result = mysqli_query(DPDatabase::get_connection(), "
+    $sql = "
         SELECT
             spec_code,
             display_name,
@@ -166,7 +166,8 @@ function special_list($special)
         FROM special_days
         WHERE enable = 1
         ORDER BY open_month, open_day
-    ");
+    ";
+    $specs_result = DPDatabase::query($sql);
 
     // it'd be nice to make this static, or something, so it only was loaded once
     $specials_array = array();
@@ -261,12 +262,13 @@ function image_source_list($image_source)
 {
     global $site_abbreviation;
     // get info on image_sources
-    $imso_result = mysqli_query(DPDatabase::get_connection(), "
+    $sql = "
         SELECT code_name, display_name
         FROM image_sources
         WHERE is_active = 1
         ORDER BY display_name
-    ") or die(DPDatabase::log_error());
+    ";
+    $imso_result = DPDatabase::query($sql);
 
     $imso_array = array(
         // TRANSLATORS: %s is the site abbreviation

tools/project_manager/edit_project_word_lists.php
@@ -500,9 +500,10 @@ class ProjectWordListHolder
         } else {
             $round_column="master_text";
         }
+        validate_projectID($this->projectid);
         $sql = "select count(*) from $this->projectid where $round_column <> ''";
 
-        $res = mysqli_query(DPDatabase::get_connection(), $sql);
+        $res = DPDatabase::query($sql);
         if ($res === FALSE)
         {
             return 0;

tools/project_manager/handle_bad_page.php
@@ -41,7 +41,11 @@ if($cancel)
 
 if (!$resolution) {
     //Find out information about the bad page report
-    $result = mysqli_query(DPDatabase::get_connection(), "SELECT * FROM $projectid WHERE image='$image'");
+    validate_projectID($projectid);
+    $sql = sprintf(
+        "SELECT * FROM $projectid WHERE image='%s'",
+        DPDatabase::escape($image));
+    $result = DPDatabase::query($sql);
     $page = mysqli_fetch_assoc($result);
     $state  = $page['state'];
     $b_User = $page['b_user'];

commit 80342c5c1c6511bea60360cbafb6eb7af2cc1d10

Correct access to exception message
681d486c9 failed to fully fix the regression from 5a21c6a8a

tools/project_manager/automodify.php
@@ -238,7 +238,7 @@ while ( $project = mysqli_fetch_assoc($allprojects) ) {
         try {
             $res = DPDatabase::query($sql);
         } catch(DBQueryError $error) {
-            echo "$error->message\n";
+            echo $error->getMessage() . "\n";
             echo "Skipping further processing of this project.\n";
             continue;
         }