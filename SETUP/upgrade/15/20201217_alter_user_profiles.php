98a5e9859be1576e3035c1027b5fb1c371b07f28
Casey Peel <cpeel@users.noreply.github.com>
2020-12-17 17:25:00 +0000

    Change new window default for new users
    For new users, do not open proofreading projects in new windows
    by default. This helps avoid popup blockers.

---
+++ SETUP/upgrade/15/20201217_alter_user_profiles.php
@@ -0,0 +1,23 @@
+<?php
+$relPath='../../../pinc/';
+include_once($relPath.'base.inc');
+
+header('Content-type: text/plain');
+
+// ------------------------------------------------------------
+
+echo "Changing default on user_profiles.i_newwin..\n";
+$sql = "
+    ALTER TABLE user_profiles
+         ALTER i_newwin SET DEFAULT 0
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()) );
+
+// ------------------------------------------------------------
+
+echo "\nDone!\n";
+
+// vim: sw=4 ts=4 expandtab
