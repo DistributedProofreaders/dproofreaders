641ed60564dc78421685a4f0b9fbf34ab7b99630
Casey Peel <cpeel@users.noreply.github.com>
2021-01-07 00:34:49 +0000

    Include troubleshooting tips on permission error
    It's possible that the archive directory is only writable by the web
    server. Provide some details on how to run it upon permission errors.

--- SETUP/upgrade/15/20210104_generate_archive_metadata.php
+++ SETUP/upgrade/15/20210104_generate_archive_metadata.php
@@ -9,6 +9,14 @@ header('Content-type: text/plain');
 
 echo "Generating missing archive project metadata..\n";
 
+echo "\n";
+echo "If you get permission errors, you may need to execute this script as\n";
+echo "the user the web server is running under, eg:\n";
+echo "  sudo su -s /bin/bash \ \n";
+echo "      -c '/usr/bin/php 20210104_generate_archive_metadata.php' \ \n";
+echo "      www-data\n";
+echo "\n";
+
 if(!is_dir($archive_projects_dir))
 {
 	echo "$archive_projects_dir (\$archive_projects_dir) does not exist so there's nothing to do.\n";
@@ -27,7 +35,7 @@ foreach(glob("$archive_projects_dir/projectID*") as $project_dir)
 	preg_match("/(projectID.*)/", $project_dir, $matches);
 	$projectid = $matches[1];
 
-	echo "Genearting metadata file for $projectid\n";
+	echo "Generating metadata file for $projectid\n";
 
 	$project = new Project($projectid);
 	generate_project_metadata_json($project, $metadata_filename, FALSE);
