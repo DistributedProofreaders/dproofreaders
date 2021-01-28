b1661c59965b3812d8457a8925d45d5a8a294478
Ray Papworth <raypapworth@gmail.com>
2021-01-28 16:32:17 +0000

    Use API for Page Browser
    Use the new DP API to load project info, images, and page texts
    in the Page Browser.

--- tools/page_browser.php
+++ tools/page_browser.php
@@ -1,30 +1,14 @@
 <?php
 $relPath='../pinc/';
 include_once($relPath.'base.inc');
-include_once($relPath.'misc.inc'); // get_enumerated_param()
 include_once($relPath.'slim_header.inc');
+include_once($relPath.'user_is.inc'); // user_can_mentor_in_any_round()
 include_once($relPath.'page_controls.inc'); // get_page_data_js()
 
 require_login();
 
-$error_message = "";
-$projectid = "";
-$imagefile = "";
-try
-{
-    $projectid = get_projectID_param($_GET, 'project', true);
-    $imagefile = get_page_image_param($_GET, 'imagefile', true);
-}
-catch(Exception $exception)
-{
-    $error_message = $exception->getMessage();
-}
-
-$round_id = get_enumerated_param($_GET, 'round_id', 'OCR', expanded_rounds());
-$mode = get_enumerated_param($_GET, 'mode', 'image', ['image', 'text', 'imageText']);
-$simpleHeader = get_enumerated_param($_GET, 'simpleHeader', 'false', ['true', 'false']);
-
-$title = _("Display Image for page");
+$title = _("Browse pages");
+$mentorMode = user_can_mentor_in_any_round() ? "true" : "false";
 
 $js_files = [
     "$code_url/scripts/splitControl.js",
@@ -34,9 +18,8 @@ $js_files = [
 
 $header_args = [
     "js_files" => $js_files,
-    "js_data" => get_page_data_js($projectid, $imagefile, $round_id, $error_message) . get_proofreading_interface_data_js() . "
-            var mode = '$mode';
-            var simpleHeader = $simpleHeader;
+    "js_data" => get_proofreading_interface_data_js() . "
+            var mentorMode = $mentorMode;
         ",
     "body_attributes" => 'class="no-margin overflow-hidden"',
 ];
--- tools/project_manager/show_good_word_suggestions_detail.js
+++ tools/project_manager/show_good_word_suggestions_detail.js
@@ -1,19 +1,10 @@
-/*global $ pageBrowserData imageControl pageChanger pageTitle splitControl showGoodWordSuggestionsDetail */
+/*global $ splitControl pageBrowse showGoodWordSuggestionsDetail */
 
 $(function () {
     let STORAGE_KEY = 'show_good_word_suggestions_detail';
-    var hiddenLayout = function() {
-        return $("<input>", {type: 'hidden', name: 'layout', value: showGoodWordSuggestionsDetail.layout});
-    };
-    var hiddenCutoff = function() {
-        return $("<input>", {type: 'hidden', name: 'timeCutoff', value: showGoodWordSuggestionsDetail.timeCutoff});
-    };
-    var hiddenWord = function() {
-        return $("<input>", {type: 'hidden', name: 'word', value: showGoodWordSuggestionsDetail.word});
-    };
-    var hiddenProjectId = function() {
-        return $("<input>", {type: 'hidden', name: 'projectid', value: showGoodWordSuggestionsDetail.projectid});
-    };
+    // this is a function to get a function to show a file
+    let getShowCurrentImageFile = null;
+
     let splitPercent = parseFloat(localStorage.getItem(STORAGE_KEY));
     if(!splitPercent) {
         // if not defined or 0, set the value to 30%.
@@ -27,33 +18,23 @@ $(function () {
     mainSplit.dragEnd.add(function (percent) {
         localStorage.setItem(STORAGE_KEY, percent);
     });
-    let topDiv = $("#page-browser");
-    // the non-scrolling area which will contain any error message and the form with controls
-    let fixHead = $("<div>", {class: 'fixed-box control-pane'});
-    topDiv.append(fixHead);
 
-    if(pageBrowserData.errorMessage) {
-        fixHead.append($("<p>", {class: 'error'}).append(pageBrowserData.errorMessage));
-    }
+    let params = new URLSearchParams();
+    params.set("mode", "image");
+    params.set("project", showGoodWordSuggestionsDetail.projectid);
+    params.set("simpleHeader", "true");
 
-    let pageControlForm = $("<form>", {method: "get"});
-    pageControlForm.append(hiddenProjectId());
-    pageControlForm.append(hiddenLayout());
-    pageControlForm.append(hiddenCutoff());
-    pageControlForm.append(hiddenWord());
-    pageControlForm.prop("class", "inline");
-    pageControlForm.append(pageChanger(pageControlForm));
-    fixHead.append(pageControlForm);
-
-    // if a page is given show its name in title and display it in
-    // scrollable area with controls and a button to change project.
-    pageTitle();
-
-    let stretchDiv = $("<div>", {class: 'stretch-box'});
-    topDiv.append(stretchDiv);
-
-    let theImageControl = imageControl();
-    fixHead.prepend(theImageControl.controls);
-    stretchDiv.addClass("overflow-auto image-pane").append(theImageControl.image);
-    theImageControl.setZoom();
+    $(".page-select").click( function () {
+        let imageFile = this.dataset.value;
+        let ShowCurrentImageFile;
+        // getShowCurrentImageFile will be null the first time
+        if(getShowCurrentImageFile && (ShowCurrentImageFile = getShowCurrentImageFile())) {
+            // ShowCurrentImageFile could be null if ajax failed or slow
+            ShowCurrentImageFile(imageFile);
+        } else {
+            params.set("imagefile", imageFile);
+            // give a no-action function for replace params
+            getShowCurrentImageFile = pageBrowse(params, function () {});
+        }
+    });
 });
