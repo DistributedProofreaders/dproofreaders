3c1bc11adf47219f6055c513e1724c970e47fde8
Casey Peel <cpeel@users.noreply.github.com>
2021-02-01 18:33:30 +0000

    Skip vendor/ for JSON validation

--- SETUP/lint_json_files.sh
+++ SETUP/lint_json_files.sh
@@ -11,9 +11,9 @@ else
     exit 1
 fi
 
-echo "Checking all .json files under $BASE_DIR excluding node_modules for linting errors..."
+echo "Checking all .json files under $BASE_DIR excluding node_modules/ and vendor/ for linting errors..."
 
-for file in `find $BASE_DIR -name "*.json" -not -path "*/node_modules/*"`; do
+for file in $(find $BASE_DIR -name "*.json" -not -path "*/node_modules/*" -not -path "*/vendor/*"); do
     echo $file
     OUTPUT=$(php -r "exit(json_decode(file_get_contents('$file')) === NULL);" 2>&1)
     if [ $? -ne 0 -o "$OUTPUT" = '1' ]; then
