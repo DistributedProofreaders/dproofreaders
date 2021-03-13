882f66fd81be8ecf5241aaa2735fbb513dde9705
Casey Peel <cpeel@users.noreply.github.com>
2021-03-13 21:49:02 +0000

    Increase the size of the user_password column
    phpBB increased the size of the maximum user_password so we need
    to increase ours in non_activated_users to match.

---
+++ SETUP/upgrade/16/20210313_alter_non_activated_users_table.php
@@ -0,0 +1,23 @@
+<?php
+$relPath='../../../pinc/';
+include_once($relPath.'base.inc');
+
+header('Content-type: text/plain');
+
+// ------------------------------------------------------------
+
+echo "Increasing size of non_activated_users.password column..\n";
+$sql = "
+    ALTER TABLE non_activated_users
+        CHANGE COLUMN user_password user_password varchar(255) NOT NULL default ''
+";
+
+echo "$sql\n";
+
+mysqli_query(DPDatabase::get_connection(), $sql) or die( mysqli_error(DPDatabase::get_connection()) );
+
+// ------------------------------------------------------------
+
+echo "\nDone!\n";
+
+// vim: sw=4 ts=4 expandtab
