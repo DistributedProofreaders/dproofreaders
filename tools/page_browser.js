e7aaabd0851143c73e2c993192cdf2d945b04dfc
Ray Papworth <raypapworth@gmail.com>
2021-04-26 17:22:33 +0000

    simplify url functions

--- tools/page_browser.js
+++ tools/page_browser.js
@@ -2,12 +2,10 @@
 
 $(function () {
     let url = new URL(window.location.href);
-    let params = new URLSearchParams(url.search);
 
     function replaceUrl() {
-        url.search = params.toString();
         window.history.replaceState(null, '', url.href);
     }
 
-    pageBrowse(params, replaceUrl, mentorMode);
+    pageBrowse(url.searchParams, replaceUrl, mentorMode);
 });
