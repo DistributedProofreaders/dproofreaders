cdae176e527b7577cac4257ce2cb0ae7a24d9ac8
Chris Miceli <chrismiceli@outlook.com>
2021-06-03 20:00:48 -0700

    add bar chart

--- stats/user_logon_stats.php
+++ stats/user_logon_stats.php
@@ -7,21 +7,29 @@ include_once($relPath.'graph_data.inc');
 require_login();
 
 $title = _("User Logon Statistics");
+$charts = [
+    ["id" => "past_day_preceding_hour", "type" => "barChart", "past" => "day", "preceding" => "hour"],
+    ["id" => "past_year_preceding_hour", "type" => "stackedAreaChart", "past" => "year", "preceding" => "hour"],
+    ["id" => "past_year_preceding_day", "type" => "stackedAreaChart", "past" => "year", "preceding" => "day"],
+    ["id" => "past_year_preceding_week", "type" => "stackedAreaChart", "past" => "year", "preceding" => "week"],
+    ["id" => "past_year_preceding_fourweek", "type" => "stackedAreaChart", "past" => "year", "preceding" => "fourweek"],
+];
+
+$js_data = '$(function(){';
+$js_data .= '';
+foreach ($charts as $chart) {
+    $js_data .= $chart["type"] . '("' . $chart["id"] . '", ' . json_encode(user_logging_on($chart["past"], $chart["preceding"])) . ');';
+}
+$js_data .= '});';
+
 output_header($title, SHOW_STATSBAR, [
     "js_files" => get_graph_js_files(),
-    "js_data" => '$(function(){
-  stackedAreaChart("past_year_preceding_hour", ' . json_encode(user_logging_on("year", "hour")) . ');
-  stackedAreaChart("past_year_preceding_day", ' . json_encode(user_logging_on("year", "day")) . ');
-  stackedAreaChart("past_year_preceding_week", ' . json_encode(user_logging_on("year", "week")) . ');
-  stackedAreaChart("past_year_preceding_fourweek", ' . json_encode(user_logging_on("year", "fourweek")) . ');
-});',
+    "js_data" => $js_data,
 ]);
 echo "<h1>$title</h1>";
 
 echo "<div style='max-width: 640px'>";
-// echo "<div id='past_day_preceding_hour' style='max-height: 400px'></div><hr>";
-echo "<div id='past_year_preceding_hour' style='max-height: 400px'></div><hr>";
-echo "<div id='past_year_preceding_day' style='max-height: 400px'></div><hr>";
-echo "<div id='past_year_preceding_week' style='max-height: 400px'></div><hr>";
-echo "<div id='past_year_preceding_fourweek' style='max-height: 400px'></div>";
+foreach ($charts as $chart) {
+    echo "<div id='" . $chart["id"] . "' style='max-height: 400px'></div><hr>";
+}
 echo "</div>";
