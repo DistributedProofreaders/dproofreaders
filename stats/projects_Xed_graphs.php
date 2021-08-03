5f7c1c9ef5c1d77a8d89b24b212112109687733f
Chris Miceli <chrismiceli@outlook.com>
2021-08-02 20:31:47 -0700

    convert projects_Xed_graphs

--- stats/projects_Xed_graphs.php
+++ stats/projects_Xed_graphs.php
@@ -4,6 +4,7 @@ include_once($relPath.'base.inc');
 include_once($relPath.'project_states.inc');
 include_once($relPath.'theme.inc');
 include_once($relPath.'misc.inc'); // get_enumerated_param()
+include_once($relPath.'graph_data.inc');
 
 require_login();
 
@@ -11,17 +12,28 @@ $which = get_enumerated_param($_GET, 'which', null, $project_status_descriptors)
 
 $psd = get_project_status_descriptor($which);
 
-output_header($psd->graphs_title);
+$charts = [
+    ["id" => "curr_month_proj", "config" => curr_month_proj($which)],
+    ["id" => "cumulative_month_proj", "config" => cumulative_month_proj($which)],
+    ["id" => "total_proj_graph", "config" => total_proj_graph($which)],
+    ["id" => "cumulative_total_proj_graph", "config" => cumulative_total_proj_graph($which)],
+];
+
+$js_data = '$(function(){';
+foreach ($charts as $chart) {
+    $js_data .= 'barChart("' . $chart["id"] . '", ' . json_encode($chart["config"]) . ');';
+}
+$js_data .= '});';
+
+output_header($psd->graphs_title, SHOW_STATSBAR, [
+    "js_files" => get_graph_js_files(),
+    "js_data" => $js_data,
+]);
 
 echo "<h1>$psd->graphs_title</h1>";
 
-$images = [
-    "jpgraph_files/curr_month_proj.php?which=$which",
-    "jpgraph_files/cumulative_month_proj.php?which=$which",
-    "jpgraph_files/total_proj_graph.php?which=$which",
-    "jpgraph_files/cumulative_total_proj_graph.php?which=$which",
-];
-
-foreach ($images as $image) {
-    echo "<img style='max-width: 100%' src='$image'><br>\n";
+echo "<div style='max-width: 640px'>";
+foreach ($charts as $chart) {
+    echo "<div id='" . $chart["id"] . "' style='max-height: 400px'></div><hr>";
 }
+echo "</div>";
