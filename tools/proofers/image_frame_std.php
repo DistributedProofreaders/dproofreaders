95613fdd3b2d5a4cd8f651ef2aa020fa97073bc7
Ray Papworth <raypapworth@gmail.com>
2021-08-17 07:27:18 +0000

    hide overflow from toolbar

--- tools/proofers/image_frame_std.php
+++ tools/proofers/image_frame_std.php
@@ -34,6 +34,6 @@ $header_args = [
 
 slim_header("Image Frame", $header_args);
 
-echo "<div style='height: 100vh;'>
+echo "<div style='height: 100vh; overflow: hidden;'>
 <div id='image-view'>
 </div></div>";
