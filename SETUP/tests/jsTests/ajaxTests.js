32647c232cde26c4262f3fb0151dde54616b8f9b
Ray Papworth <raypapworth@gmail.com>
2022-02-04 19:26:11 +0000

    test for sent data

--- SETUP/tests/jsTests/ajaxTests.js
+++ SETUP/tests/jsTests/ajaxTests.js
@@ -7,14 +7,14 @@ QUnit.module("Ajax test", function() {
 
     QUnit.test("Return correct data", function (assert) {
         function fetchPromise() {
-            const blob = new Blob([JSON.stringify("mydata")], {type : 'application/json'});
+            const blob = new Blob([JSON.stringify({key: "value"})], {type : 'application/json'});
             const init = {status: 200, headers: {'content-type': 'application/json'}};
             return Promise.resolve(new Response(blob, init));
         }
 
         return ajax("GET", "myUrl", {}, {}, fetchPromise)
             .then(function(data) {
-                assert.strictEqual(data, "mydata");
+                assert.deepEqual(data, {key: "value"});
             });
     });
 
@@ -57,4 +57,20 @@ QUnit.module("Ajax test", function() {
             });
     });
 
+    QUnit.test("Check sent data", function (assert) {
+        function fetchPromise(url, options) {
+            assert.strictEqual(url.href, "https://www.dummy.org/api/index.php?url=myUrl&querykey=queryvalue");
+            assert.strictEqual(options.method, "POST");
+            assert.strictEqual(options.headers['Content-Type'], 'application/json');
+            assert.strictEqual(options.headers["X-API-KEY"], "SESSION");
+            assert.strictEqual(options.headers.Accept, 'application/json');
+            assert.strictEqual(options.body, "{\"datakey\":\"datavalue\"}");
+            const blob = new Blob([JSON.stringify("mydata")], {type : 'application/json'});
+            const init = {status: 200, headers: {'content-type': 'application/json'}};
+            return Promise.resolve(new Response(blob, init));
+        }
+
+        return ajax("POST", "myUrl", {querykey: "queryvalue"}, {datakey: "datavalue"}, fetchPromise)
+            .then();
+    });
 });
