1eef71bc33429f9d9677cc5665db0f564e1597e8
srjfoo <srjfoo@users.noreply.github.com>
2022-03-10 22:42:46 +0000

    Added updated verify para to pgdp-production
    The GM requested that the paragraph concerning volunteer service
    verification be expanded and updated. The original para has been
    removed from `master`. This PR adds the expanded para into the
    `pgdp-production` branch.

--- default.php
+++ default.php
@@ -141,6 +141,13 @@ echo "<p>"
     . "</p>"
     . "\n";
 
+echo "<p>"
+    . sprintf(
+        _("Distributed Proofreaders regrets that we are unable to verify court-ordered or any official government-related service that requires confirmation of identity. Also, because our system cannot adequately record time spent participating, we are unable to verify hours worked for other community services that require tracking of time spent. For more information, please see <a href='%s'>this page</a>."),
+        "$wiki_url/DP_Official_Documentation:General/Certification_of_Volunteer_Work")
+    . "</p>"
+    . "\n";
+
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 echo "\n"
