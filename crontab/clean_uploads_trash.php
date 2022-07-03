50c7e2becb872a2328f8395d16856421aaf368ff
Casey Peel <cpeel@users.noreply.github.com>
2022-07-03 19:26:45 +0000

    Robustly escape all non-literals in shell commands

--- crontab/clean_uploads_trash.php
+++ crontab/clean_uploads_trash.php
@@ -12,7 +12,14 @@ if (!$trash_dir) {
 
 // remove files from TRASH subdirectory older than 30 days
 unset($output);
-exec("/usr/bin/find '$trash_dir' -type f -mtime +30 -delete", $output, $return);
+$cmd = join(" ", [
+    "/usr/bin/find",
+    escapeshellarg($trash_dir),
+    "-type f",
+    "-mtime +30",
+    "-delete",
+]);
+exec($cmd, $output, $return);
 if ($return != 0) {
     echo "An error occurred while cleaning up files.\n";
     echo "Return value: $return\n";
@@ -24,7 +31,14 @@ if ($return != 0) {
 
 // remove empty directories
 unset($output);
-exec("/usr/bin/find '$trash_dir' -type d -empty -delete", $output, $return);
+$cmd = join(" ", [
+    "/usr/bin/find",
+    escapeshellarg($trash_dir),
+    "-type d",
+    "-empty",
+    "-delete",
+]);
+exec($cmd, $output, $return);
 if ($return != 0) {
     echo "An error occurred while cleaning up files.\n";
     echo "Return value: $return\n";
