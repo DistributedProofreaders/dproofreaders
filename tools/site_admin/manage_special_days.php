1e5c32d3e0deb901f4e1557d629d1885cefb87ec
Ray Papworth <31585092+70ray@users.noreply.github.com>
2023-03-31 19:29:39 +0100

    Validate Special Day ID (#868)
    Check Special day ID for allowed characters or starts with Birthday or Otherday.
    
    Task 2050

--- tools/site_admin/manage_special_days.php
+++ tools/site_admin/manage_special_days.php
@@ -35,12 +35,17 @@ if ($action == 'update_oneshot') {
 
         $source = new SpecialDay($spec_code);
 
-        if (!isset($_POST['editing']) && !$source->new_source) {
-            $errmsgs .= _('A Special Day with this ID already exists. Please choose a different ID for this event.') . "<br>";
-        }
-
-        if (strlen($spec_code) < 1) {
-            $errmsgs .= _("A value for Special Day ID is required. Please enter one.") . "<br>";
+        if (!isset($_POST['editing'])) {
+            // we are creating a new special day
+            if (!$source->new_source) {
+                $errmsgs .= _('A Special Day with this ID already exists. Please choose a different ID for this event.') . "<br>";
+            } else {
+                if ((strlen($spec_code) < 1) || (preg_match("/[^A-Za-z0-9. -]/", $spec_code) === 1)) {
+                    $errmsgs .= _("Please enter a Special Day Code. Allowable characters are alphanumeric characters and the period, hyphen or space.") . "<br>";
+                } elseif ((startswithnocase($spec_code, "Otherday")) || (startswithnocase($spec_code, "Birthday"))) {
+                    $errmsgs .= _('The ID cannot start with "Otherday" or "Birthday".') . "<br>";
+                }
+            }
         }
 
         // validate the numeric fields
