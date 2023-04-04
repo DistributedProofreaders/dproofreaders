19ae87443aeb1e0b07c06647397485250ef69c9c
Ray Papworth <raypapworth@gmail.com>
2023-04-04 14:00:34 +0000

    renamed file, added comment, renumbered exceptions

--- SETUP/tests/phpunit_bootstrap.php
+++ SETUP/tests/phpunit_bootstrap.php
@@ -5,8 +5,6 @@ include_once($relPath.'base.inc');
 include_once($relPath.'misc.inc');
 include_once($relPath.'wordcheck_engine.inc');
 include_once($relPath.'TableDocumentation.inc');
-
-$apiPath = '../../api/';
-include_once($apiPath.'v1.inc');
+include_once($relPath.'../api/v1.inc');
 
 include_once("phpunit_test_helpers.inc");
