98ca6dcf391cfc7adce9e8e55111b065f7278176
Casey Peel <cpeel@users.noreply.github.com>
2023-12-28 18:53:40 +0000

    Set default u_neigh for new users to 0
    This ensures new users have good P1 page performance.

--- accounts/activate.php
+++ accounts/activate.php
@@ -105,7 +105,7 @@ $query = sprintf(
     INSERT INTO users (reg_token, real_name, username, email, date_created,
                        email_updates, referrer, referrer_details, http_referrer, u_neigh, u_intlang)
     VALUES ('%s', '%s', '%s', '%s', $user->date_created,
-            $user->email_updates, '%s', '%s', '%s', 10, '%s')
+            $user->email_updates, '%s', '%s', '%s', 0, '%s')
     ",
     DPDatabase::escape($reg_token),
     DPDatabase::escape($user->real_name),
