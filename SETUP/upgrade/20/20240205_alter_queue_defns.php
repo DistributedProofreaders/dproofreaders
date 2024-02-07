54f8e459a66d519fb5855e5de15857e1fe4a8703
Michael Dyck <jmdyck@ibiblio.org>
2024-02-06 20:55:38 -0500

    fixup: First check that the whole table can be converted before doing anything

--- SETUP/upgrade/20/20240205_alter_queue_defns.php
+++ SETUP/upgrade/20/20240205_alter_queue_defns.php
@@ -7,6 +7,37 @@ header('Content-type: text/plain');
 
 // ------------------------------------------------------------
 
+// Get data from the existing table
+$sql = "
+    SELECT id, round_id, name, release_criterion
+    FROM queue_defns
+    ORDER BY id
+";
+$result = mysqli_query(DPDatabase::get_connection(), $sql) or die(mysqli_error(DPDatabase::get_connection()));
+$n_rows = mysqli_num_rows($result);
+
+// Make a first pass over the table to check that it can be converted without errors.
+echo "Checking $n_rows rows for convertability...\n";
+$n_conversion_errors = 0;
+while ([$id, $round_id, $name, $release_criterion] = mysqli_fetch_row($result)) {
+    $queue_ident = "queue $id (round_id='$round_id', name='$name')";
+    // echo "$queue_ident:\n";
+    $targets_array = convert($release_criterion, $queue_ident);
+}
+
+if ($n_conversion_errors) {
+    echo "\n";
+    echo "Aborting (leaving the queue_defns table unchanged) due to $n_conversion_errors conversion errors noted above.\n";
+    echo "You might be able to modify this script to handle the problematic release_criterion values.\n";
+    echo "If not, you'll need to change those values in the table to something this script can handle.\n";
+    exit;
+}
+
+// ---------
+
+echo "\n";
+echo "All release_criterion values can be converted, so proceeding to upgrade the table.\n";
+echo "\n";
 echo "Adding projects_target and pages_target to queue_defns...\n";
 $sql = "
     ALTER TABLE queue_defns
@@ -15,14 +46,9 @@ $sql = "
 ";
 mysqli_query(DPDatabase::get_connection(), $sql) or die(mysqli_error(DPDatabase::get_connection()));
 
-$sql = "
-    SELECT id, round_id, name, release_criterion
-    FROM queue_defns
-    ORDER BY id
-";
-$result = mysqli_query(DPDatabase::get_connection(), $sql) or die(mysqli_error(DPDatabase::get_connection()));
-$n_rows = mysqli_num_rows($result);
 echo "Converting the release_criterion in {$n_rows} rows ...\n";
+
+mysqli_data_seek($result, 0) or die(DPDatabase::log_error());
 
 $n_conversion_errors = 0;
 while ([$id, $round_id, $name, $release_criterion] = mysqli_fetch_row($result)) {
@@ -39,6 +65,7 @@ while ([$id, $round_id, $name, $release_criterion] = mysqli_fetch_row($result)) 
 }
 
 if ($n_conversion_errors) {
+    // This can't happen?
     echo "Aborting (leaving 'release_criterion' column in place) due to $n_conversion_errors conversion errors noted above.\n";
     exit;
 }
