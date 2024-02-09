062ca5bd9bcf9a770a7201667eba75a4c8f21b11
Brian Foley <bpfoley@gmail.com>
2024-02-09 17:25:46 +0000

    SETUP: Make PHP/JSON linting run in parallel
    Our github action runners have 2 CPUs so this will speed up
    the linting step a bit.

--- SETUP/lint_php_files.sh
+++ SETUP/lint_php_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -euo pipefail
 
-if [ "_$1" != "_" ]; then
+if [ -n "${1-}" ]; then
     BASE_DIR=$1
 elif [ -d SETUP ]; then
     BASE_DIR=.
@@ -11,26 +12,32 @@ else
     exit 1
 fi
 
-echo "Checking all .php and .inc files under $BASE_DIR for linting errors..."
-
-for file in $(find $BASE_DIR -name "*.php" -o -name "*.inc" | grep -v vendor); do
+function lint_file()
+{
+    local file=$1
     echo $file
-    OUTPUT=`php -l $file`
-    echo $OUTPUT | grep -q 'No syntax errors detected'
-    if [ $? -ne 0 ]; then
+    OUTPUT=$(php -l $file)
+    if ! echo $OUTPUT | grep -q 'No syntax errors detected'; then
         echo "ERROR: PHP lint failure in $file"
         echo "$OUTPUT"
         exit 1
     fi
 
     OUTPUT=$(file $file)
-    echo $OUTPUT | grep -q 'ASCII'
-    ASCII=$?
-    echo $OUTPUT | grep -q 'UTF-8'
-    UTF8=$?
-    if [ $ASCII -eq 1 -a $UTF8 -eq 1 ]; then
+    if ! echo $OUTPUT | grep -Eq 'ASCII|UTF-8'; then
         echo "ERROR: Unexpected encoding in $file"
         echo "$OUTPUT"
         exit 1
     fi
+}
+
+echo "Checking all .php and .inc files under $BASE_DIR for linting errors..."
+N=$(nproc)
+for file in $(find $BASE_DIR -name "*.php" -o -name "*.inc" | grep -v /vendor/); do
+   lint_file "$file" &
+   # Run at most N at a time.
+   [[ $(jobs -r -p | wc -l) -ge $N ]] && wait -n
 done
+
+# Wait for all jobs to finish
+wait
