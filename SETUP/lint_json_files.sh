d1820152560327d3379866ed82b3eba46f3dafd4
Brian Foley <bpfoley@gmail.com>
2024-02-09 17:25:46 +0000

    SETUP: Make PHP/JSON linting run in parallel
    Our github action runners have 2 CPUs so this will speed up
    the linting step a bit.

--- SETUP/lint_json_files.sh
+++ SETUP/lint_json_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -euo pipefail
 
-if [ "_$1" != "_" ]; then
+if [ -n "${1-}" ]; then
     BASE_DIR=$1
 elif [ -d SETUP ]; then
     BASE_DIR=.
@@ -13,11 +14,23 @@ fi
 
 echo "Checking all .json files under $BASE_DIR excluding node_modules/ and vendor/ for linting errors..."
 
-for file in $(find $BASE_DIR -name "*.json" -not -path "*/node_modules/*" -not -path "*/vendor/*"); do
+function lint_json()
+{
+    local file=$1
     echo $file
     OUTPUT=$(php -r "exit(json_decode(file_get_contents('$file')) === NULL);" 2>&1)
     if [ $? -ne 0 -o "$OUTPUT" = '1' ]; then
         echo "ERROR: JSON lint failure in $file"
         exit 1
     fi
+}
+
+N=$(nproc)
+for file in $(find $BASE_DIR -name "*.json" -not -path "*/node_modules/*" -not -path "*/vendor/*"); do
+    lint_json "$file" &
+    # Run at most N at a time.
+    [[ $(jobs -r -p | wc -l) -ge $N ]] && wait -n
 done
+
+# Wait for all jobs to finish
+wait
