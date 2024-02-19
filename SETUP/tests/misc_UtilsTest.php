f4eaa9bc524ae01f38cdebde7a399ffd6f782527
Brian Foley <bpfoley@gmail.com>
2024-02-19 16:07:19 +0000

    misc.inc: Add `any()` and `all()` functions
    In the style of Python's builtin functions with the same names.
    
    Use it in a few places as examples.

--- SETUP/tests/misc_UtilsTest.php
+++ SETUP/tests/misc_UtilsTest.php
@@ -31,4 +31,20 @@ class MiscUtils extends PHPUnit\Framework\TestCase
         $changed = get_changed_fields_for_objects($obj1, $obj2);
         $this->assertEquals(["param2"], $changed);
     }
+
+    public function test_any_all()
+    {
+        $this->assertFalse(any([]));
+        $this->assertFalse(any([], 'is_numeric'));
+        $this->assertFalse(any([], fn ($r) => $r != 42));
+
+        $this->assertTrue(all([]));
+        $this->assertTrue(all([], 'is_string'));
+        $this->assertTrue(all([], fn ($i) => $i % 2 == 1));
+
+        $this->assertTrue(any(explode(",", "6502,6809,8086"), fn ($cpu) => $cpu == "8086"));
+
+        $this->assertTrue(any(explode(",", "6502,Z80"), fn ($cpu) => is_numeric($cpu)));
+        $this->assertFalse(all(explode(",", "6502,Z80"), 'is_numeric'));
+    }
 }
