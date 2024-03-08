7ba679bf6fb694dd8be3e28be194851317e709ac
Chris Miceli <chrismiceli@outlook.com>
2024-03-07 20:25:37 -0800

    lint fixes

--- tools/proofers/text_data_processor.js
+++ tools/proofers/text_data_processor.js
@@ -1,21 +1,21 @@
 window.addEventListener('DOMContentLoaded', function() {
-  const conversionMap = {
-    '‘': '\'',
-    '“': '"',
-    '’': '\'',
-    '”': '"',
-    '—': '--'
-  };
+    const conversionMap = {
+        '‘': '\'',
+        '“': '"',
+        '’': '\'',
+        '”': '"',
+        '—': '--'
+    };
 
-  document.getElementById('text_data').addEventListener('input', (event) => {
-    const textDataElement = event.target;
-    let selectionEnd = textDataElement.selectionEnd;
-    textDataElement.value = [...textDataElement.value].map((character) => {
-        const conversion = conversionMap[character] || character;
-        selectionEnd += (conversion.length - 1);
-        return conversion;
-    }).join('');
-    textDataElement.selectionStart = selectionEnd;
-    textDataElement.selectionEnd = selectionEnd;
-  });
+    document.getElementById('text_data').addEventListener('input', (event) => {
+        const textDataElement = event.target;
+        let selectionEnd = textDataElement.selectionEnd;
+        textDataElement.value = [...textDataElement.value].map((character) => {
+            const conversion = conversionMap[character] || character;
+            selectionEnd += (conversion.length - 1);
+            return conversion;
+        }).join('');
+        textDataElement.selectionStart = selectionEnd;
+        textDataElement.selectionEnd = selectionEnd;
+    });
 });
