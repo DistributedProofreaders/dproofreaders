47494d1750de0cb754652388ce83529289f11b29
Chris Miceli <chrismiceli@outlook.com>
2024-03-07 20:16:27 -0800

    Handle smart quotes in quizes also

--- quiz/generic/proof.php
+++ quiz/generic/proof.php
@@ -29,6 +29,7 @@ $valid_character_pattern = build_character_regex_filter($quiz->get_valid_codepoi
 $header_args = [
     "js_files" => [
         "$code_url/tools/proofers/process_diacritcal_markup.js",
+        "$code_url/tools/proofers/text_data_processor.js",
     ],
     "js_data" => "
         var validCharacterPattern = '$valid_character_pattern';
