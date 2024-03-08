8c06b9a0128f9e786319463e64573904d45c5997
Chris Miceli <chrismiceli@users.noreply.github.com>
2024-03-08 15:12:13 -0600

    Handle smart quotes in quizes also (#1135)

--- tools/proofers/proof_validate.js
+++ tools/proofers/proof_validate.js
@@ -44,24 +44,4 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
-
-    const conversionMap = {
-        '‘': '\'',
-        '“': '"',
-        '’': '\'',
-        '”': '"',
-        '—': '--'
-    };
-
-    document.getElementById('text_data').addEventListener('input', (event) => {
-        const textDataElement = event.target;
-        let selectionEnd = textDataElement.selectionEnd;
-        textDataElement.value = [...textDataElement.value].map((character) => {
-            const conversion = conversionMap[character] || character;
-            selectionEnd += (conversion.length - 1);
-            return conversion;
-        }).join('');
-        textDataElement.selectionStart = selectionEnd;
-        textDataElement.selectionEnd = selectionEnd;
-    });
 });
