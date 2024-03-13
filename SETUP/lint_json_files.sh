5e306cc133ad945495fc88df02925bb37cc51b80
Brian Foley <bpfoley@gmail.com>
2024-03-13 04:04:22 +0000

    lint_json_files.sh: Rework to be shellcheck clean

--- SETUP/lint_json_files.sh
+++ SETUP/lint_json_files.sh
@@ -17,15 +17,15 @@ echo "Checking all .json files under $BASE_DIR excluding node_modules/ and vendo
 function lint_json()
 {
     local file=$1
-    echo $file
-    OUTPUT=$(php -r "exit(json_decode(file_get_contents('$file')) === NULL);" 2>&1)
-    if [ $? -ne 0 -o "$OUTPUT" = '1' ]; then
+    echo "$file"
+    if ! OUT=$(php -r "exit(json_decode(file_get_contents('$file')) === NULL);" 2>&1) || [ "$OUT" = 1 ]; then
         echo "ERROR: JSON lint failure in $file"
         exit 1
     fi
 }
 
 N=$(nproc)
+# shellcheck disable=SC2044
 for file in $(find $BASE_DIR -name "*.json" -not -path "*/node_modules/*" -not -path "*/vendor/*"); do
     lint_json "$file" &
     # Run at most N at a time.
