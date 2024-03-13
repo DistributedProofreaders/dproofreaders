a3746724e2af47aeba2072d59ec5eda16f64d56e
Brian Foley <bpfoley@gmail.com>
2024-03-13 04:38:46 +0000

    locale/compile.sh: Make shellcheck clean

--- SETUP/locale/compile.sh
+++ SETUP/locale/compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-for po_file in `find . -name '*.po'`; do
-    mo_file=`echo $po_file | sed 's/.po/.mo/'`
-    echo Compiling $po_file to $mo_file
-    msgfmt $po_file -o $mo_file
+set -euo pipefail
+# shellcheck disable=SC2044
+for po_file in $(find . -name '*.po'); do
+    mo_file="${po_file%.po}.mo"
+    echo "Compiling $po_file to $mo_file"
+    msgfmt "$po_file" -o "$mo_file"
 done
