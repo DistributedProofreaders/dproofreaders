c3600a9d5c8da1a730ca9c10bfa71c4b50c9c5f2
Brian Foley <bpfoley@gmail.com>
2024-03-13 03:46:05 +0000

    generate_css_from_less.sh: Simplify and make shellcheck clean

--- SETUP/generate_css_from_less.sh
+++ SETUP/generate_css_from_less.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+set -euo pipefail
 
-if [ "$1" = "--check" ]; then
+CHECK=0
+if [ "${1-}" = "--check" ]; then
     CHECK=1
     shift
-else
-    CHECK=0
-
-    # exit on errors
-    set -e
 fi
 
-if [ "_$1" != "_" ]; then
+if [ -n "${1-}" ]; then
     STYLE_DIR=$1
 elif [ -d styles ]; then
     STYLE_DIR=styles
@@ -21,42 +18,37 @@ else
     exit 1
 fi
 
-LESSC_PATH=$STYLE_DIR/../node_modules/.bin
+LESSC=$STYLE_DIR/../node_modules/.bin/lessc
 
 # Array of .less files that should not turn into a .css file
-NON_CSS_LESS_FILES=("layout.less" "global.less" "page_interfaces.less")
+NON_CSS_LESS_FILES=(layout.less global.less page_interfaces.less)
 
 function run_less {
     less_file=$1
 
     # skip files in NON_CSS_LESS_FILES that shouldn't generate a .css
     less_basename=$(basename -- "$less_file")
-    if [[ " ${NON_CSS_LESS_FILES[@]} " =~ " ${less_basename} " ]]; then
-        echo "Skipping $less_file"
-        return;
-    fi
+    for skip_file in "${NON_CSS_LESS_FILES[@]}"; do
+        [[ "$less_basename" = "$skip_file" ]] && return;
+    done
 
     css_file="${less_file%.*}.css"
     if [ $CHECK -eq 0 ]; then
         echo "Generating $css_file"
-        $LESSC_PATH/lessc $less_file $css_file
+        $LESSC "$less_file" "$css_file"
     else
         echo "Validating $css_file"
-        new_css_file="${css_file}.new"
-        $LESSC_PATH/lessc $less_file $new_css_file
-        diff -q $css_file $new_css_file > /dev/null 2>&1
-        if [ $? -ne 0 ]; then
+        if ! diff -q "$css_file" <($LESSC "$less_file") > /dev/null 2>&1; then
             echo "ERROR: $css_file doesn't match generated less file"
-            rm $new_css_file
             exit 1
         fi
-        rm $new_css_file
     fi
 }
 
 # run over every .less file in our style directory
+# shellcheck disable=SC2044
 for file in $(find $STYLE_DIR -name "*.less"); do
-    run_less $file
+    run_less "$file"
 done
 
 # now the themes
