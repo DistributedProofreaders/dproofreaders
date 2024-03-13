5b216bd75d0e48ddf4a1d7cb24a2b8d11fd2e916
Brian Foley <bpfoley@gmail.com>
2024-03-13 03:24:48 +0000

    compile_locales.sh: Remove unreferenced script
    Rather than making it shellcheck clean.
    
    besides SETUP/locale/compile.sh now exists

--- SETUP/compile_locales.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#A script to compile all the .po locale files
-#Run it with directory where they are located
-#($dyn_locales_dir from pinc/site_vars.php) as the argument
-
-find $1 -name messages.po |
-sed "s/^\(.*\).po$/msgfmt \1.po -o \1.mo/" |
-sh
