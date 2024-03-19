3450eb4ae4a301f1e53de4db2ec4ffc220db7ad7
Casey Peel <cpeel@users.noreply.github.com>
2024-03-19 04:23:52 +0000

    Remove FAQs that exist in the wiki
    Remove FAQs that already exist in the wiki as well as any images
    that they, and only they, use.

--- faq/dev_process.php
+++
@@ -1,7 +0,0 @@
-<?php
-$relPath = '../pinc/';
-include_once($relPath.'base.inc');
-include_once($relPath.'metarefresh.inc');
-
-// The contents of this file have been moved to the pgdp.net wiki.
-metarefresh(0, "http://www.pgdp.net/wiki/DP_Code_Development");
