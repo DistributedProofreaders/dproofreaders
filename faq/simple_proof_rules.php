29ceab0bea11e57f75bd662a6023d0be54ab00c4
Casey Peel <cpeel@users.noreply.github.com>
2024-03-19 03:43:24 +0000

    Move simple proofreading rules into round.php

--- faq/simple_proof_rules.php
+++
@@ -1,7 +0,0 @@
-<?php
-$relPath = '../pinc/';
-include_once($relPath.'base.inc');
-include_once($relPath.'theme.inc');
-
-output_header("Beginners' Simple Proofreading Rules", NO_STATSBAR);
-include_once($relPath.'simple_proof_text.inc');
