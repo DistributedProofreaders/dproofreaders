DistributedProofreaders/dproofreaders

commit 61c6b1438421fabbd2a65576eaf1b4c573d3a6ba

stats: Fix some arithmetic errors for empty stats tables

stats/round_backlog.php
@@ -34,7 +34,7 @@ $stats = query_graph_cache("_get_round_backlog_data", [], 60 * 60 * 24);
 $stats_total = array_sum($stats);
 
 // calculate the goal percent as 100 / number_of_phases
-$goal_percent = ceil(100 / count($stats));
+$goal_percent = ceil(100 / min(1, count($stats)));
 
 // colors
 $barColors = [];

stats/round_backlog_days.php
@@ -55,7 +55,7 @@ $stats = query_graph_cache("_get_round_backlog_days_data", [], 60 * 60 * 24);
 $stats_total = array_sum($stats);
 
 // calculate the goal percent as 100 / number_of_phases
-$goal_percent = ceil(100 / count($stats));
+$goal_percent = ceil(100 / min(1, count($stats)));
 
 // colors
 $barColors = [];

stats/pages_in_states.php
@@ -31,7 +31,7 @@ $progordone_n_pages = [];
 // ---------------
 
 $stage_labels[] = 'New';
-$unavail_n_pages[] = $n_pages_[PROJ_NEW];
+$unavail_n_pages[] = $n_pages_[PROJ_NEW] ?? 0;
 $waiting_n_pages[] = 0;
 $available_n_pages[] = 0;
 $progordone_n_pages[] = 0;

commit 9ea9a25471a7b268fadae8caf419876aa179a016

Fix round backlog graphs
61c6b1438 inadvertently used min() when we really want max()

stats/round_backlog.php
@@ -34,7 +34,7 @@ $stats = query_graph_cache("_get_round_backlog_data", [], 60 * 60 * 24);
 $stats_total = array_sum($stats);
 
 // calculate the goal percent as 100 / number_of_phases
-$goal_percent = ceil(100 / min(1, count($stats)));
+$goal_percent = ceil(100 / max(1, count($stats)));
 
 // colors
 $barColors = [];

stats/round_backlog_days.php
@@ -55,7 +55,7 @@ $stats = query_graph_cache("_get_round_backlog_days_data", [], 60 * 60 * 24);
 $stats_total = array_sum($stats);
 
 // calculate the goal percent as 100 / number_of_phases
-$goal_percent = ceil(100 / min(1, count($stats)));
+$goal_percent = ceil(100 / max(1, count($stats)));
 
 // colors
 $barColors = [];