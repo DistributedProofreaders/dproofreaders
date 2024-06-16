b8540d6ac384176f2524c2c11e4152a1da622d9f
Ray Papworth <31585092+70ray@users.noreply.github.com>
2024-06-16 04:38:21 +0100

    Simplify credit selection in userprefs (#1234)
    Use javascript file and remove general function which is used in
    only one place.

---
+++ scripts/userprefs.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    let creditNameSelector = document.getElementById('credit_name');
+    let creditOtherInput = document.getElementById('credit_other');
+
+    function maybeShowOther() {
+        creditOtherInput.hidden = (creditNameSelector.value !== 'other');
+    }
+
+    if(creditNameSelector) {
+        creditNameSelector.addEventListener("change", maybeShowOther);
+        maybeShowOther();
+    }
+});
