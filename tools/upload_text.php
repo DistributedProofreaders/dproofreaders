2e67b263cd13a22aaaff75014ab236061b10904b
Casey Peel <cpeel@users.noreply.github.com>
2024-08-15 05:27:37 +0000

    Don't assume we're running under mod_php

--- tools/upload_text.php
+++ tools/upload_text.php
@@ -205,10 +205,13 @@ if (!isset($action)) {
     // make reasonably sure script does not timeout on large file uploads
     set_time_limit(14400);
 
-    // Disable gzip compression so we can flush the buffer after each step
+    // Disable compression so we can flush the buffer after each step
     // in the process to give the user some progress details. Note that this
     // doesn't necessarily work for all browsers.
-    apache_setenv('no-gzip', '1');
+    if (function_exists("apache_setenv")) {
+        apache_setenv('no-gzip', '1');
+        apache_setenv('no-brotli', '1');
+    }
 
     slim_header($title);
     echo "<h1>$title</h1>";
