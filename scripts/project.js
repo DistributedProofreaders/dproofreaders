27a243cb0bd7b7207242b875ff628cf2a5fd7288
Ray Papworth <raypapworth@gmail.com>
2024-10-16 10:24:16 +0000

    rearrange javascript file

--- scripts/project.js
+++ scripts/project.js
@@ -1,5 +1,4 @@
 window.addEventListener("DOMContentLoaded", function () {
-    const changeStateForms = document.getElementsByClassName("frm_chg_stt");
 
     function confirmSubmit(event) {
         if (this.dataset.confirmationQuestion && !confirm(this.dataset.confirmationQuestion)) {
@@ -7,16 +6,16 @@ window.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    const changeStateForms = document.getElementsByClassName("frm_chg_stt");
     for (const changeStateForm of changeStateForms) {
         changeStateForm.addEventListener("submit", confirmSubmit);
     }
-
-    const newTopicLink = document.getElementById("no_topic_yet");
 
     function disableLink() {
         this.outerHTML = this.firstChild.nodeValue;
     }
 
+    const newTopicLink = document.getElementById("no_topic_yet");
     if (newTopicLink) {
         // prevent clicking again if there is a delay in creating the topic
         newTopicLink.addEventListener("click", disableLink);
