8475f002139fd01a2c8e00d3b206c18b80b5bb56
Chris Miceli <chrismiceli@outlook.com>
2025-05-22 21:22:55 -0500

    Attempt to parse out failure text

--- SETUP/tests/jsTests/jsUnit.js
+++ SETUP/tests/jsTests/jsUnit.js
@@ -10,10 +10,18 @@ const firefox = require("selenium-webdriver/firefox");
     try {
         await driver.get(`file:///${process.cwd()}/SETUP/tests/jsTests/qunit.html`);
         await driver.wait(until.elementLocated(By.className("passed")), 10000);
-        var passed = parseInt(await (await driver.findElement(By.className("passed"))).getText(), 10);
-        var total = parseInt(await (await driver.findElement(By.className("total"))).getText(), 10);
-        var failed = parseInt(await (await driver.findElement(By.className("failed"))).getText(), 10);
+        const passed = parseInt(await (await driver.findElement(By.className("passed"))).getText(), 10);
+        const total = parseInt(await (await driver.findElement(By.className("total"))).getText(), 10);
+        const failed = parseInt(await (await driver.findElement(By.className("failed"))).getText(), 10);
         console.log(`${passed} assertions of ${total} passed, ${failed} failed.`);
+        if (failed !== 0) {
+            console.error("Failures:");
+            const failures = await driver.findElements(By.css(".fail .fail"));
+            for (const failure of failures) {
+                console.error("------");
+                console.error(await failure.getText());
+            }
+        }
         succeeded = passed === total && failed === 0;
     } catch (e) {
         console.error(e);
