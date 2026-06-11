f5f962ecff3b72746ec7171f3d13bf5350754540
Brian Foley <bpfoley@gmail.com>
2026-06-11 14:20:22 +0100

    smoketests: Add eng wordcheck files and proj charsuites

--- SETUP/tests/smoketests/smoketest_setup.sh
+++ SETUP/tests/smoketests/smoketest_setup.sh
@@ -3,9 +3,15 @@
 # Populate some DB tables used by the smoketests
 mysql -uroot -proot < SETUP/tests/smoketests/test_tables.sql
 
+# Create wordcheck files
+mkdir words
+echo arid > words/bad_words.eng.txt
+echo Sidenote > words/good_words.eng.txt
+
 # Create a project directory accessed by several pages
 mkdir -p "${HOME}/projects/projectID5e23a810ef693"
 for i in 001 002 003 004 005 illo; do
     cp SETUP/tests/smoketests/dot.png "${HOME}/projects/projectID5e23a810ef693/${i}.png"
 done
 mkdir -p "${HOME}/projects/projectID3141592653589"
+
