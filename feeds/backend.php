c722bc8b3a04f5220a246ef35aeb9e274c9b4bd1
Casey Peel <cpeel@users.noreply.github.com>
2026-09-01 16:28:37 -0700

    Handle undefined email addresses better

--- feeds/backend.php
+++ feeds/backend.php
@@ -34,7 +34,7 @@ echo $rssfeed;
  * @return string
  *   The XML document for the RSS feed
  */
-function generate_rss_feed(string $content, string $site_name, string $code_url, string $site_manager_email_addr)
+function generate_rss_feed(string $content, string $site_name, string $code_url, ?string $site_manager_email_addr)
 {
     $limit = 20; // Number of rows we query from the table, number of items in RSS feed
 
@@ -104,6 +104,9 @@ function generate_rss_feed(string $content, string $site_name, string $code_url,
         }
 
         $lastupdated = date("r");
+        $webmaster = $site_manager_email_addr ?
+            xmlencode($site_manager_email_addr)." (" . xmlencode(_("Site Manager")) .")" :
+            xmlencode(_("Site Manager"));
         $rssfeed = "<"."?"."xml version=\"1.0\" encoding=\"UTF-8\" ?".">
             <rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">
             <channel>
@@ -111,7 +114,7 @@ function generate_rss_feed(string $content, string $site_name, string $code_url,
             <title>".xmlencode($site_name)." - " . _("Latest Releases") . "</title>
             <link>".xmlencode($link)."</link>
             <description>".xmlencode($desc)."</description>
-            <webMaster>".xmlencode($site_manager_email_addr)." (" . xmlencode(_("Site Manager")) . ")</webMaster>
+            <webMaster>".xmlencode($webmaster)."</webMaster>
             <pubDate>".xmlencode($lastupdated)."</pubDate>
             <lastBuildDate>".xmlencode($lastupdated)."</lastBuildDate>
             $data
