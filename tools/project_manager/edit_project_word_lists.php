fea35fee3326e61372fd155364dcd76053076c0e
Brian Foley <bpfoley@gmail.com>
2026-09-06 14:00:22 +0100

    editproject.php: Refactor row formatting not to use callables & echo
    This allows us to put proper arguments and type hints on all the
    formatting functions rather than forcing them all through a common
    interface, and the direct calls allow PHPStan to do typechecking.
    
    It also allows us to identify and remove a dead function.

--- tools/project_manager/edit_project_word_lists.php
+++ tools/project_manager/edit_project_word_lists.php
@@ -3,7 +3,6 @@ $relPath = "./../../pinc/";
 include_once($relPath.'base.inc');
 include_once($relPath.'theme.inc');
 include_once($relPath.'links.inc');
-include_once('edit_common.inc');
 include_once($relPath.'wordcheck_engine.inc');
 include_once($relPath.'metarefresh.inc');
 include_once($relPath.'Project.inc');
